5fd340722b694e76c2e46d1997586e2a7e0c73cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:29 +0000

    Add package pyttb/1.8.1/pyttb-1.8.1-py3-none-any.whl

---
+++ packages/pyttb/pyttb-1.8.1-py3-none-any.whl/pyttb/symktensor.py
@@ -0,0 +1,15 @@
+"""Symmetric Kruskal Tensor Class Placeholder"""
+
+# Copyright 2024 National Technology & Engineering Solutions of Sandia,
+# LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the
+# U.S. Government retains certain rights in this software.
+
+
+class symktensor:
+    """
+    SYMKTENSOR Class for symmetric Kruskal tensors (decomposed).
+
+    """
+
+    def __init__(self):
+        assert False, "SYMKTENSOR class not yet implemented"
