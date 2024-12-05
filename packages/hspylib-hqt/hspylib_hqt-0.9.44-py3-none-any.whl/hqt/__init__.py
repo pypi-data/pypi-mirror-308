2492913fb28b3de4440d8eaadab2fd5d314a594d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:23 +0000

    Add package hspylib-hqt/0.9.44/hspylib_hqt-0.9.44-py3-none-any.whl

---
+++ packages/hspylib-hqt/hspylib_hqt-0.9.44-py3-none-any.whl/hqt/__init__.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-hqt v0.9.44
+#
+# Package: main.hqt
+"""Package initialization."""
+
+__all__ = [
+    'promotions', 
+    'qt_application', 
+    'stream_capturer', 
+    'views'
+]
+__version__ = '0.9.44'
---
+++ packages/hspylib-hqt/hspylib_hqt-0.9.44-py3-none-any.whl/hqt/promotions/hframe.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HsPyLib-Hqt
+   @package: hqt.promotions
+      @file: hframe.py
+   @created: Tue, 4 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright·(c)·2024,·HSPyLib
+"""
+
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtGui import QKeyEvent
+from PyQt5.QtWidgets import QFrame
+
+
+class HFrame(QFrame):
+    """TODO"""
+
+    keyPressed = pyqtSignal(int)
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        """TODO"""
+        self.keyPressed.emit(event.key())
