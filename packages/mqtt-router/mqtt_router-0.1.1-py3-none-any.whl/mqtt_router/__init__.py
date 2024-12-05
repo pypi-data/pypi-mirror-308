f8c96bd21646efe2a6615546206a65f72acc0ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:54 +0000

    Add package mqtt-router/0.1.1/mqtt_router-0.1.1-py3-none-any.whl

---
+++ packages/mqtt-router/mqtt_router-0.1.1-py3-none-any.whl/mqtt_router/__init__.py
@@ -0,0 +1,3 @@
+from .router import MQTTRouter
+
+__all__ = ["MQTTRouter"]
---
+++ packages/mqtt-router/mqtt_router-0.1.1-py3-none-any.whl/mqtt_router/converters.py
@@ -0,0 +1,48 @@
+import uuid
+
+
+class IntConverter:
+    regex = "[0-9]+"
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return str(value)
+
+
+class StringConverter:
+    regex = "[^/]+"
+
+    def to_python(self, value):
+        return value
+
+    def to_url(self, value):
+        return value
+
+
+class UUIDConverter:
+    regex = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
+
+    def to_python(self, value):
+        return uuid.UUID(value)
+
+    def to_url(self, value):
+        return str(value)
+
+
+class SlugConverter(StringConverter):
+    regex = "[-a-zA-Z0-9_]+"
+
+
+class PathConverter(StringConverter):
+    regex = ".+"
+
+
+DEFAULT_CONVERTERS = {
+    "int": IntConverter(),
+    "path": PathConverter(),
+    "slug": SlugConverter(),
+    "str": StringConverter(),
+    "uuid": UUIDConverter(),
+}
