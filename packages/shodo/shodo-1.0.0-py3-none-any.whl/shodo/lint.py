96cae81787a58eb1b98884d4f9e102f637e48b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:25 +0000

    Add package shodo/1.0.0/shodo-1.0.0-py3-none-any.whl

---
+++ packages/shodo/shodo-1.0.0-py3-none-any.whl/shodo/lint.py
@@ -0,0 +1,89 @@
+import time
+from dataclasses import asdict, dataclass
+from typing import Dict, Optional
+
+from shodo.api import lint_create, lint_result
+
+
+@dataclass
+class Pos:
+    line: int
+    ch: int
+
+    def __str__(self):
+        return f"{self.line+1}:{self.ch+1}"
+
+    def asdict(self):
+        return {
+            "line": self.line,
+            "ch": self.ch,
+        }
+
+
+@dataclass
+class Message:
+    ERROR = "error"
+    WARNING = "warning"
+
+    code: str
+    message: str
+    severity: str
+    from_: Pos
+    to: Pos
+    index: int
+    index_to: int
+    score: float
+    before: Optional[str] = None
+    after: Optional[str] = None
+    operation: Optional[str] = None
+    meta: Optional[Dict] = None
+
+    @classmethod
+    def load(cls, data):
+        data["from_"] = Pos(**data.pop("from"))
+        data["to"] = Pos(**data.pop("to"))
+        return cls(**data)
+
+    def asdict(self):
+        data = asdict(self)
+        del data["from_"]
+        data["from"] = self.from_.asdict()
+        data["to"] = self.to.asdict()
+        return data
+
+
+class LintFailed(Exception):
+    pass
+
+
+class Lint:
+    STATUS_PROCESSING = "processing"
+    STATUS_FAILED = "failed"
+
+    def __init__(self, body, lint_id, profile):
+        self.body = body
+        self.lint_id = lint_id
+        self.body = None
+        self.status = self.STATUS_PROCESSING
+        self.messages = []
+        self.profile = profile
+
+    def results(self):
+        while self.status == self.STATUS_PROCESSING:
+            time.sleep(0.5)
+            self.status, messages = lint_result(self.lint_id, self.profile)
+            msgs = [Message.load(m) for m in messages]
+            self.messages = sorted(msgs, key=lambda m: (m.from_.line, m.from_.ch))
+
+        if self.status == self.STATUS_FAILED:
+            raise LintFailed
+
+        return self.messages
+
+    def __repr__(self):
+        return f"Lint({self.lint_id})"
+
+    @classmethod
+    def start(cls, body: str, is_html: bool = False, profile: Optional[str] = None):
+        lint_id = lint_create(body, is_html, profile)
+        return cls(body, lint_id, profile)
