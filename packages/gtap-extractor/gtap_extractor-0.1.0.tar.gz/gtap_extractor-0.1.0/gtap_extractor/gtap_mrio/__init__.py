ea3355840a5c10abe25cdb0cf9bf9f63a98fbde7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:17:01 +0000

    Add package gtap-extractor/0.1.0/gtap_extractor-0.1.0.tar.gz

---
+++ packages/gtap-extractor/gtap_extractor-0.1.0.tar.gz/gtap_extractor-0.1.0/gtap_extractor/gtap_mrio/__init__.py
@@ -0,0 +1,3 @@
+from .mrio_builder import build_io
+
+__all__ = ["build_io"]
---
+++ packages/gtap-extractor/gtap_extractor-0.1.0.tar.gz/gtap_extractor-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gtap-extractor',
+    version='0.1.0',
+    author='Timothe Beaufils',
+    author_email='timothe.beaufils@pik-potsdam.de',
+    description='Extract GTAP data from .har files and save it as .nc file.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitlab.pik-potsdam.de/mrio_toolbox/gtap_extractor',
+    packages=find_packages(),
+    install_requires=[
+        "netcdf4",
+        "xarray",
+        "mrio_toolbox-1.0.0"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+    ],
+    keywords=[
+        'GTAP', 
+        'MRIO', 
+        "Input-Output", 
+        "Economics",
+        "Industrial Ecology", 
+        "netcdf",
+        "International Trade"],
+)
