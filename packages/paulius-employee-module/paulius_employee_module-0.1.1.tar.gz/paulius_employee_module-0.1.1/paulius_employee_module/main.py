5ea9e6af02ab5c43e9a2f478622c9c78bdf124e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:16:59 +0000

    Add package paulius-employee-module/0.1.1/paulius_employee_module-0.1.1.tar.gz

---
+++ packages/paulius-employee-module/paulius_employee_module-0.1.1.tar.gz/paulius_employee_module-0.1.1/paulius_employee_module/main.py
@@ -0,0 +1,9 @@
+from employee import Employee
+
+darb1 = Employee("Paulius", "Lapienis", 2000)
+
+# this is a comment
+if input().strip() == "with hello":
+    print(f"Hello, {darb1}!")
+else:
+    print(darb1)
---
+++ packages/paulius-employee-module/paulius_employee_module-0.1.1.tar.gz/paulius_employee_module-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paulius_employee_module",
+    version="0.1.1",
+    author="PowFox",
+    email="lapienis.paulius@yahoo.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6"
+)
