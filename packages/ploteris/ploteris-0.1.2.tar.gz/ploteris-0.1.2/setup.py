bc1de91624a1b9fda39ac07839715b199adcc3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:56 +0000

    Add package ploteris/0.1.2/ploteris-0.1.2.tar.gz

---
+++ packages/ploteris/ploteris-0.1.2.tar.gz/ploteris-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+setup(
+    name="ploteris",
+    version="0.1.2",
+    author="lmacionis",
+    author_email="lmacionis@yahoo.com",
+    packages=find_packages(),
+    install_requires=[
+                      "numpy",
+                      "contourpy==1.3.0",
+                      "cycler==0.12.1",
+                      "fonttools==4.54.1",
+                      "kiwisolver==1.4.7",
+                      "matplotlib==3.9.2",
+                      "packaging==24.1",
+                      "pillow==11.0.0",
+                      "pyparsing==3.2.0",
+                      "python-dateutil==2.9.0.post0",
+                      "six==1.16.0"
+                      ],
+    python_requires=">=3.6"
+)
