8e39ef5a8b439536cf01f931f5939d27d79a1973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:23 +0000

    Add package CodeChat-Server/0.2.25/CodeChat_Server-0.2.25-py3-none-any.whl

---
+++ packages/CodeChat-Server/CodeChat_Server-0.2.25-py3-none-any.whl/CodeChat_Server/__init__.py
@@ -0,0 +1,36 @@
+# .. Copyright (C) 2012-2022 Bryan A. Jones.
+#
+#   This file is part of the CodeChat System.
+#
+#   The CodeChat System is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   The CodeChat System is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with the CodeChat System.  If not, see
+#   <http://www.gnu.org/licenses/>.
+#
+# *****************************
+# |docname| - Module definition
+# *****************************
+# Give the version number, which is read by `../setup.py` during packaging.
+# This is chosen following the `version convention
+# <http://packaging.python.org/en/latest/tutorial.html#version>`_.
+__version__ = "0.2.25"
+
+# Contents:
+#
+# .. toctree::
+#   :maxdepth: 2
+#
+#   server.py
+#   render_manager.py
+#   renderer.py
+#   __main__.py
+#   constants.py
