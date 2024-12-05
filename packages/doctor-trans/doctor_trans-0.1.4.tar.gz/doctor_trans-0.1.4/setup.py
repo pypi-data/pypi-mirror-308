7905e714f137d758c00c7c90e535076b6a8622f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:28 +0000

    Add package doctor-trans/0.1.4/doctor_trans-0.1.4.tar.gz

---
+++ packages/doctor-trans/doctor_trans-0.1.4.tar.gz/doctor_trans-0.1.4/setup.py
@@ -0,0 +1,35 @@
+# doctor_trans/setup.py
+
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import pathlib
+
+# Create the extension for Cython file
+ext_modules = [
+    Extension(
+        name="doctor_trans.trans",
+        sources=["doctor_trans/trans.pyx"],  # Path to your .pyx file
+    ),
+]
+
+# Read the contents of README.md
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+
+setup(
+    name='doctor_trans',
+    version='0.1.4',
+    packages=['doctor_trans'],
+    ext_modules=cythonize(ext_modules),
+    install_requires=[
+        'pandas',
+        'requests'
+    ],
+    author='Nirmal Patel',
+    author_email='nirmalpatel1705@gmail.com',
+    description='This package translate whole dataframe in any language without any limits.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=''
+)
