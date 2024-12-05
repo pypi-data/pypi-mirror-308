4804dce870da1f0dfe5d7cd0cdd518173a29eb60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:36 +0000

    Add package eur-to-usd/0.1.1/eur_to_usd-0.1.1.tar.gz

---
+++ packages/eur-to-usd/eur_to_usd-0.1.1.tar.gz/eur_to_usd-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eur-to-usd",
+    version="0.1.1",
+    author="Asasai001",
+    email="ambrasas.arnoldas@gmail.com",
+    description="simple converter eur to usd",
+    packages=find_packages(),
+    install_requires=["currencyconverter"],
+    python_requires=">=3.6",
+)
