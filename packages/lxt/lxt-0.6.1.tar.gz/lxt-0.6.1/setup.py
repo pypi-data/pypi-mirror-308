ed36bf1dea84cbf1ac0fa6405ec3027f701b2d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:20 +0000

    Add package lxt/0.6.1/lxt-0.6.1.tar.gz

---
+++ packages/lxt/lxt-0.6.1.tar.gz/lxt-0.6.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='lxt',  
+    version='0.6.1',
+    install_requires=[
+        'torch',
+        'transformers>=4.46.2',
+        'accelerate',
+        'tabulate',
+        'matplotlib',
+        'bitsandbytes',
+        'open_clip_torch',
+    ],
+    author="Reduan Achtibat",
+    license='BSD 3-clause',
+    description="LRP explains Transformers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rachtibat/LRP-for-Transformers",
+    packages=setuptools.find_packages(),
+)
