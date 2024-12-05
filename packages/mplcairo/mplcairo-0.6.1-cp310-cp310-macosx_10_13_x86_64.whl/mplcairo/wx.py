6376eb9dd590a49ff31735539fcea3f82f7fa607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:18 +0000

    Add package mplcairo/0.6.1/mplcairo-0.6.1-cp310-cp310-macosx_10_13_x86_64.whl

---
+++ packages/mplcairo/mplcairo-0.6.1-cp310-cp310-macosx_10_13_x86_64.whl/mplcairo/wx.py
@@ -0,0 +1,41 @@
+from matplotlib.backends.backend_wx import (
+    _BackendWx, _FigureCanvasWxBase, FigureFrameWx)
+import wx
+
+from . import _mplcairo, _util
+from .base import FigureCanvasCairo
+
+
+class FigureFrameWxCairo(FigureFrameWx):
+    def get_canvas(self, fig):
+        return FigureCanvasWxCairo(self, -1, fig)
+
+
+class FigureCanvasWxCairo(FigureCanvasCairo, _FigureCanvasWxBase):
+    def __init__(self, parent, id, figure):
+        # Inline the call to FigureCanvasCairo.__init__ as _FigureCanvasWxBase
+        # has a different signature and thus we cannot use cooperative
+        # inheritance.
+        _util.fix_ipython_backend2gui()
+        _FigureCanvasWxBase.__init__(self, parent, id, figure)
+
+    def draw(self, drawDC=None):
+        super().draw()
+        # The source of wx.lib.wxcairo.BitmapFromImageSurface seems to suggest
+        # that one can directly pass premultiplied RGBA to wx.Bitmap, but this
+        # is incorrect (likely a bug?).
+        buf = _mplcairo.cairo_to_straight_rgba8888(
+            self.get_renderer()._get_buffer())
+        height, width, _ = buf.shape
+        self.bitmap = wx.Bitmap.FromBufferRGBA(width, height, buf)
+        self._isDrawn = True
+        self.gui_repaint(drawDC=drawDC)
+
+    def blit(self, bbox=None):
+        self.draw()
+
+
+@_BackendWx.export
+class _BackendWxCairo(_BackendWx):
+    FigureCanvas = FigureCanvasWxCairo
+    _frame_class = FigureFrameWxCairo
