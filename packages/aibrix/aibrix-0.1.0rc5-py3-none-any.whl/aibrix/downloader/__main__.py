4de70ae79ff9ba5ef1f1fa7d74be6264273e90d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:23 +0000

    Add package aibrix/0.1.0rc5/aibrix-0.1.0rc5-py3-none-any.whl

---
+++ packages/aibrix/aibrix-0.1.0rc5-py3-none-any.whl/aibrix/downloader/__main__.py
@@ -0,0 +1,32 @@
+import argparse
+
+from aibrix.downloader import download_model
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Download model from HuggingFace")
+    parser.add_argument(
+        "--model-uri",
+        type=str,
+        default="deepseek-ai/deepseek-coder-6.7b-instruct",
+        required=True,
+        help="model uri from different source, support HuggingFace, AWS S3, TOS",
+    )
+    parser.add_argument(
+        "--local-dir",
+        type=str,
+        default=None,
+        help="base dir of the model file. If not set, it will used with env `DOWNLOADER_LOCAL_DIR`",
+    )
+    parser.add_argument(
+        "--model-name",
+        type=str,
+        default=None,
+        help="subdir of the base dir to save model files",
+    )
+    args = parser.parse_args()
+    download_model(args.model_uri, args.local_dir, args.model_name)
+
+
+if __name__ == "__main__":
+    main()
