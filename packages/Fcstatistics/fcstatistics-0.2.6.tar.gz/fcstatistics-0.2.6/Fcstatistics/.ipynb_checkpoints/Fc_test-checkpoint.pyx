2d2e504b39537998e1d546373f234d73659359a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:56 +0000

    Add package Fcstatistics/0.2.6/fcstatistics-0.2.6.tar.gz

---
+++ packages/Fcstatistics/fcstatistics-0.2.6.tar.gz/fcstatistics-0.2.6/Fcstatistics/.ipynb_checkpoints/Fc_test-checkpoint.pyx
@@ -0,0 +1,4 @@
+import Fcstatistics.Fc as fc
+
+data = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10]
+print("Mean:", fc.Mean(data))
---
+++ packages/Fcstatistics/fcstatistics-0.2.6.tar.gz/fcstatistics-0.2.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+# Cython으로 빌드할 확장 모듈 정의
+extensions = [
+    Extension("Fcstatistics.Fc", ["Fcstatistics/Fc_test.pyx"]),
+]
+
+# setup 함수 정의
+setup(
+    name="Fcstatistics",
+    version="0.2.6",
+    long_description=open("README.md", encoding="utf-8").read(),
+    ext_modules=cythonize(extensions),    # Cython 컴파일 활성화
+    include_package_data=True,
+)
