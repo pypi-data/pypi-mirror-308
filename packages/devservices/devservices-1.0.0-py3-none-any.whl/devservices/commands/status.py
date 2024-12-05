d66e8a60291e6aef1823b0bbb74c96453ad95e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:15 +0000

    Add package devservices/1.0.0/devservices-1.0.0-py3-none-any.whl

---
+++ packages/devservices/devservices-1.0.0-py3-none-any.whl/devservices/commands/status.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+import json
+from argparse import _SubParsersAction
+from argparse import ArgumentParser
+from argparse import Namespace
+
+from devservices.exceptions import DependencyError
+from devservices.exceptions import DockerComposeError
+from devservices.utils.console import Console
+from devservices.utils.dependencies import install_and_verify_dependencies
+from devservices.utils.docker_compose import run_docker_compose_command
+from devservices.utils.services import find_matching_service
+
+LINE_LENGTH = 40
+
+
+def add_parser(subparsers: _SubParsersAction[ArgumentParser]) -> None:
+    parser = subparsers.add_parser("status", help="View status of a service")
+    parser.add_argument(
+        "service_name",
+        help="Name of the service to view status for",
+        nargs="?",
+        default=None,
+    )
+    parser.set_defaults(func=status)
+
+
+def format_status_output(status_json: str) -> str:
+    # Docker compose ps is line delimited json, so this constructs this into an array we can use
+    service_statuses = status_json.split("\n")[:-1]
+    output = []
+    output.append("-" * LINE_LENGTH)
+    for service_status in service_statuses:
+        service = json.loads(service_status)
+        name = service["Service"]
+        state = service["State"]
+        container_name = service["Name"]
+        health = service.get("Health", "N/A")
+        ports = service.get("Publishers", [])
+        running_for = service.get("RunningFor", "N/A")
+
+        output.append(f"{name}")
+        output.append(f"Container: {container_name}")
+        output.append(f"Status: {state}")
+        output.append(f"Health: {health}")
+        output.append(f"Uptime: {running_for}")
+
+        if ports:
+            output.append("Ports:")
+            for port in ports:
+                output.append(
+                    f"  {port['URL']}:{port['PublishedPort']} -> {port['TargetPort']}/{port['Protocol']}"
+                )
+        else:
+            output.append("No ports exposed")
+
+        output.append("")  # Empty line for readability
+
+    return "\n".join(output)
+
+
+def status(args: Namespace) -> None:
+    """Start a service and its dependencies."""
+    console = Console()
+    service_name = args.service_name
+    try:
+        service = find_matching_service(service_name)
+    except Exception as e:
+        console.failure(str(e))
+        exit(1)
+
+    modes = service.config.modes
+    # TODO: allow custom modes to be used
+    mode_to_view = "default"
+    mode_dependencies = modes[mode_to_view]
+
+    try:
+        remote_dependencies = install_and_verify_dependencies(service)
+    except DependencyError as de:
+        console.failure(str(de))
+        exit(1)
+    try:
+        status_json_results = run_docker_compose_command(
+            service,
+            "ps",
+            mode_dependencies,
+            remote_dependencies,
+            options=["--format", "json"],
+        )
+    except DockerComposeError as dce:
+        console.failure(f"Failed to get status for {service.name}: {dce.stderr}")
+        exit(1)
+
+    # Filter out empty stdout to help us determine if the service is running
+    status_json_results = [
+        status_json for status_json in status_json_results if status_json.stdout
+    ]
+    if len(status_json_results) == 0:
+        console.warning(f"{service.name} is not running")
+        return
+    output = f"Service: {service.name}\n\n"
+    for status_json in status_json_results:
+        output += format_status_output(status_json.stdout)
+    output += "=" * LINE_LENGTH
+    console.info(output + "\n")
---
+++ packages/devservices/devservices-1.0.0-py3-none-any.whl/devservices/commands/stop.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+from argparse import _SubParsersAction
+from argparse import ArgumentParser
+from argparse import Namespace
+
+from devservices.exceptions import DependencyError
+from devservices.exceptions import DockerComposeError
+from devservices.utils.console import Console
+from devservices.utils.console import Status
+from devservices.utils.dependencies import get_non_shared_remote_dependencies
+from devservices.utils.dependencies import install_and_verify_dependencies
+from devservices.utils.docker_compose import run_docker_compose_command
+from devservices.utils.services import find_matching_service
+from devservices.utils.state import State
+
+
+def add_parser(subparsers: _SubParsersAction[ArgumentParser]) -> None:
+    parser = subparsers.add_parser("stop", help="Stop a service and its dependencies")
+    parser.add_argument(
+        "service_name", help="Name of the service to stop", nargs="?", default=None
+    )
+    parser.add_argument(
+        "--debug",
+        help="Enable debug mode",
+        action="store_true",
+        default=False,
+    )
+    parser.set_defaults(func=stop)
+
+
+def stop(args: Namespace) -> None:
+    """Stop a service and its dependencies."""
+    console = Console()
+    service_name = args.service_name
+    try:
+        service = find_matching_service(service_name)
+    except Exception as e:
+        console.failure(str(e))
+        exit(1)
+
+    modes = service.config.modes
+    # TODO: allow custom modes to be used
+    mode_to_stop = "default"
+    mode_dependencies = modes[mode_to_stop]
+
+    state = State()
+    started_services = state.get_started_services()
+    if service.name not in started_services:
+        console.warning(f"{service.name} is not running")
+        exit(0)
+
+    with Status(
+        lambda: console.warning(f"Stopping {service.name}"),
+        lambda: console.success(f"{service.name} stopped"),
+    ) as status:
+        try:
+            remote_dependencies = install_and_verify_dependencies(service)
+        except DependencyError as de:
+            status.failure(str(de))
+            exit(1)
+        remote_dependencies = get_non_shared_remote_dependencies(
+            service, remote_dependencies
+        )
+        try:
+            run_docker_compose_command(
+                service,
+                "down",
+                mode_dependencies,
+                remote_dependencies,
+            )
+        except DockerComposeError as dce:
+            status.failure(f"Failed to stop {service.name}: {dce.stderr}")
+            exit(1)
+
+    # TODO: We should factor in healthchecks here before marking service as stopped
+    state = State()
+    state.remove_started_service(service.name)
