c613d46f7438db9be9cf71ca585b78e4cfaf78e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:24:32 +0000

    Add package code-embedder/0.5.2.dev4/code_embedder-0.5.2.dev4.tar.gz

---
+++ packages/code-embedder/code_embedder-0.5.2.dev4.tar.gz/code_embedder-0.5.2.dev4/src/main.py
@@ -0,0 +1,42 @@
+import glob
+import sys
+
+import typer
+from loguru import logger
+
+from src.code_embedding import CodeEmbedder
+from src.script_content_reader import ScriptContentReader
+from src.script_metadata_extractor import ScriptMetadataExtractor
+
+app = typer.Typer()
+
+
+@app.command(help="Embed code from scripts to markdown files.")
+def run(
+    changed_files: list[str] = typer.Argument(None, help="List of changed files to process."),
+):
+    logger.remove()
+    logger.add(sys.stderr, level="INFO")
+
+    readme_paths = glob.glob("**/*.md", recursive=True)
+
+    if not readme_paths:
+        logger.info("No markdown files found in the current repository.")
+        exit(0)
+
+    logger.info(f"Found {len(readme_paths)} markdown files in the current repository.")
+
+    script_metadata_extractor = ScriptMetadataExtractor()
+    script_content_reader = ScriptContentReader()
+    code_embedder = CodeEmbedder(
+        readme_paths=readme_paths,
+        script_metadata_extractor=script_metadata_extractor,
+        script_content_reader=script_content_reader,
+    )
+    code_embedder()
+
+    logger.info("Code Embedder finished successfully.")
+
+
+if __name__ == "__main__":
+    typer.run(run)
