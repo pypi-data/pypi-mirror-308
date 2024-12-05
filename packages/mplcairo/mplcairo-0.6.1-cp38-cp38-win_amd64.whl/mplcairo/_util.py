b5f32fcff60471c539e1c499d70d7a582bc9cfe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:22 +0000

    Add package mplcairo/0.6.1/mplcairo-0.6.1-cp38-cp38-win_amd64.whl

---
+++ packages/mplcairo/mplcairo-0.6.1-cp38-cp38-win_amd64.whl/mplcairo/_util.py
@@ -0,0 +1,71 @@
+import functools
+import importlib
+import os
+import sys
+
+import matplotlib as mpl
+
+from . import _backports
+
+
+@functools.lru_cache(1)
+def get_tex_font_map():
+    return mpl.dviread.PsfontsMap(mpl.dviread.find_tex_file("pdftex.map"))
+
+
+def get_glyph_name(dvitext):
+    ps_font = get_tex_font_map()[dvitext.font.texname]
+    return (_backports._parse_enc(ps_font.encoding)[dvitext.glyph]
+            if ps_font.encoding is not None else None)
+
+
+def get_matplotlib_gtk_backend():
+    import gi
+    required = gi.get_required_version("Gtk")
+    if required == "4.0":
+        versions = [4]
+    elif required == "3.0":
+        versions = [3]
+    elif os.environ.get("_GTK_API"):  # Private undocumented API.
+        versions = [int(os.environ["_GTK_API"])]
+    else:
+        versions = [4, 3]
+    for version in versions:
+        # Matplotlib converts require_version ValueErrors into ImportErrors.
+        try:
+            mod = importlib.import_module(
+                f"matplotlib.backends.backend_gtk{version}")
+            return mod, getattr(mod, f"_BackendGTK{version}")
+        except ImportError:
+            pass
+    raise ImportError("Failed to import any Matplotlib GTK backend")
+
+
+@functools.lru_cache(1)
+def fix_ipython_backend2gui():  # matplotlib#12637 (<3.1).
+    # Fix hard-coded module -> toolkit mapping in IPython (used for `ipython
+    # --auto`).  This cannot be done at import time due to ordering issues (so
+    # we do it when creating a canvas) and should only be done once (hence the
+    # `lru_cache(1)`).
+    if sys.modules.get("IPython") is None:  # Can be explicitly set to None.
+        return
+    import IPython
+    ip = IPython.get_ipython()
+    if not ip:
+        return
+    from IPython.core import pylabtools as pt
+    pt.backend2gui.update({
+        "module://mplcairo.gtk": "gtk3",
+        "module://mplcairo.qt": "qt",
+        "module://mplcairo.tk": "tk",
+        "module://mplcairo.wx": "wx",
+        "module://mplcairo.macosx": "osx",
+    })
+    # Work around pylabtools.find_gui_and_backend always reading from
+    # rcParamsOrig.
+    orig_origbackend = mpl.rcParamsOrig["backend"]
+    try:
+        mpl.rcParamsOrig["backend"] = mpl.rcParams["backend"]
+        ip.enable_matplotlib()
+    finally:
+        mpl.rcParamsOrig["backend"] = orig_origbackend
---
+++ packages/mplcairo/mplcairo-0.6.1-cp38-cp38-win_amd64.whl/mplcairo/qt.py
@@ -0,0 +1,66 @@
+import ctypes
+
+# This does support QT_API=pyqt6 on Matplotlib versions that can handle it.
+try:
+    from matplotlib.backends.backend_qt import _BackendQT, FigureCanvasQT
+except ImportError:
+    from matplotlib.backends.backend_qt5 import (
+        _BackendQT5 as _BackendQT, FigureCanvasQT)
+from matplotlib.backends.qt_compat import QtCore, QtGui
+
+from . import _mplcairo
+from .base import FigureCanvasCairo
+
+
+class FigureCanvasQTCairo(FigureCanvasCairo, FigureCanvasQT):
+    def paintEvent(self, event):
+        if hasattr(self, "_update_dpi") and self._update_dpi():
+            return  # matplotlib#19123 (<3.4).
+        # We always repaint the full canvas (doing otherwise would require an
+        # additional copy of the buffer into a contiguous block, so it's not
+        # clear it would be faster).
+        buf = _mplcairo.cairo_to_premultiplied_argb32(
+            self.get_renderer()._get_buffer())
+        height, width, _ = buf.shape
+        # The image buffer is not necessarily contiguous, but the padding
+        # in the ARGB32 case (each scanline is 32-bit aligned) happens to
+        # match what QImage requires; in the RGBA128F case the documented Qt
+        # requirement does not seem necessary?
+        if QtGui.__name__.startswith("PyQt6"):
+            from PyQt6 import sip
+            ptr = sip.voidptr(buf)
+        else:
+            ptr = buf
+        qimage = QtGui.QImage(
+            ptr, width, height, QtGui.QImage.Format(6))  # ARGB32_Premultiplied
+        getattr(qimage, "setDevicePixelRatio", lambda _: None)(
+            self.device_pixel_ratio)
+        # https://bugreports.qt.io/browse/PYSIDE-140
+        if (QtCore.__name__.startswith("PySide")
+                and QtCore.__version_info__ < (5, 12)):
+            ctypes.c_long.from_address(id(buf)).value -= 1
+        painter = QtGui.QPainter(self)
+        painter.eraseRect(self.rect())
+        painter.drawImage(0, 0, qimage)
+        self._draw_rect_callback(painter)
+        painter.end()
+
+    def blit(self, bbox=None):  # matplotlib#17478 (<3.3).
+        # See above: we always repaint the full canvas.
+        self.repaint(self.rect())
+
+    def print_figure(self, *args, **kwargs):
+        # Similar to matplotlib#26309: Qt may trigger a redraw after closing
+        # the file save dialog, in which case we don't want to redraw based on
+        # the savefig-generated renderer but the earlier, gui one.
+        lrc = self._last_renderer_call
+        self._last_renderer_call = None, None
+        try:
+            super().print_figure(*args, **kwargs)
+        finally:
+            self._last_renderer_call = lrc
+
+
+@_BackendQT.export
+class _BackendQTCairo(_BackendQT):
+    FigureCanvas = FigureCanvasQTCairo
