19f81350513b9eb0ccdc6bb0de457f03ae85be13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:14 +0000

    Add package Marius-valiutu-konvertavimu-modulis/0.1.1/marius_valiutu_konvertavimu_modulis-0.1.1.tar.gz

---
+++ packages/Marius-valiutu-konvertavimu-modulis/marius_valiutu_konvertavimu_modulis-0.1.1.tar.gz/marius_valiutu_konvertavimu_modulis-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Marius_valiutu_konvertavimu_modulis",
+    version="0.1.1",
+    author="Marelis272",
+    email="Mariusstosk@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6"
+)
