c1235eab7a656eba4bfdf5951bc406193f47a652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:34 +0000

    Add package ploteris/0.1.1/ploteris-0.1.1-py3-none-any.whl

---
+++ packages/ploteris/ploteris-0.1.1-py3-none-any.whl/ploteris/main.py
@@ -0,0 +1,4 @@
+from ploteris.ploteris import graph
+from duomenys import x, y
+
+graph(x, y)
---
+++ packages/ploteris/ploteris-0.1.1-py3-none-any.whl/ploteris/ploteris.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+
+def graph (x, y):
+    plt.plot(x, y, "o")
+    plt.show()
