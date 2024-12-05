e56f2b0da22a998aae0fa9a5d7e2c3317c79ab7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:24:57 +0000

    Add package matchms/0.28.2/matchms-0.28.2.tar.gz

---
+++ packages/matchms/matchms-0.28.2.tar.gz/matchms-0.28.2/matchms/filtering/peak_processing/normalize_intensities.py
@@ -0,0 +1,32 @@
+import logging
+import numpy as np
+from matchms.typing import SpectrumType
+from ...Fragments import Fragments
+
+
+logger = logging.getLogger("matchms")
+
+
+def normalize_intensities(spectrum_in: SpectrumType) -> SpectrumType:
+    """Normalize intensities of peaks to unit height."""
+
+    if spectrum_in is None:
+        return None
+
+    spectrum = spectrum_in.clone()
+
+    if len(spectrum.peaks) == 0:
+        return spectrum
+
+    max_intensity = np.max(spectrum.peaks.intensities)
+
+    if max_intensity <= 0:
+        logger.warning("Spectrum with all peak intensities <= 0 was set to None.")
+        return None
+
+    # Normalize peak intensities
+    mz, intensities = spectrum.peaks.mz, spectrum.peaks.intensities
+    normalized_intensities = intensities / max_intensity
+    spectrum.peaks = Fragments(mz=mz, intensities=normalized_intensities)
+
+    return spectrum
