5c81bf234c22621d13fd8f8494c95bfe05bb9bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:26 +0000

    Add package ididi/0.2.6/ididi-0.2.6.tar.gz

---
+++ packages/ididi/ididi-0.2.6.tar.gz/ididi-0.2.6/ididi/utils/param_utils.py
@@ -0,0 +1,34 @@
+import typing as ty
+
+# __all__ = ["NULL", "Nullable", "is_not_null"]
+
+
+class _Missed:
+    """
+    Sentinel object to represent a null value.
+    bool(NULL) is False.
+    """
+
+    __slots__ = ()
+
+    def __repr__(self) -> str:
+        return "NULL"
+
+    def __bool__(self) -> bool:
+        return False
+
+
+MISSING = _Missed()
+
+
+type Maybe[T] = T | _Missed
+"""
+Nullable[int] == int | NULL
+"""
+
+
+def is_provided[T](value: Maybe[T]) -> ty.TypeGuard[T]:
+    """
+    Check if the value is not MISSING.
+    """
+    return value is not MISSING
