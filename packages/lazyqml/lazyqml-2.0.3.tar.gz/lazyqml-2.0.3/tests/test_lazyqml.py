814159843740ea884734b86d65a24c08f7b4f09f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:58 +0000

    Add package lazyqml/2.0.3/lazyqml-2.0.3.tar.gz

---
+++ packages/lazyqml/lazyqml-2.0.3.tar.gz/lazyqml-2.0.3/tests/test_lazyqml.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""Tests for `lazyqml` package."""
+
+
+import unittest
+
+from lazyqml.supervised import QuantumClassifier
+
+
+class TestLazyqml(unittest.TestCase):
+    """Tests for `lazyqml` package."""
+
+    def test_multiclass(self):
+        from sklearn.datasets import load_breast_cancer, load_iris
+        from sklearn.model_selection import train_test_split
+        data = load_iris()
+        X = data.data
+        y = data.target
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.3,random_state =123)  
+
+        q = QuantumClassifier(nqubits=4,classifiers=["all"])
+
+        scores = q.fit(X_train, y_train, X_test, y_test)
+
+        print(scores)
+
+    def test_binary(self):
+        from sklearn.datasets import load_breast_cancer, load_iris
+        from sklearn.model_selection import train_test_split
+        data = load_breast_cancer()
+        X = data.data
+        y = data.target
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.3,random_state =123)  
+
+        q = QuantumClassifier(nqubits=2,classifiers=["all"])
+
+        scores = q.fit(X_train, y_train, X_test, y_test)
+
+        print(scores)
