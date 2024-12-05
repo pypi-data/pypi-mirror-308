1e459ff1a5240a344eb916ebe46fa36b4c6ad26e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:50 +0000

    Add package RpiL/0.1.4/rpil-0.1.4.tar.gz

---
+++ packages/RpiL/rpil-0.1.4.tar.gz/rpil-0.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RpiL",  # Package name
+    version="0.1.4",  # Version
+    packages=find_packages(where="src"),  # Automatically find packages in 'src'
+    package_dir={"": "src"},  # Tell setuptools where your package is
+    install_requires=["RPi.GPIO", "Adafruit_DHT"],  # List of dependencies (only external packages)
+    description="Library for controlling Raspberry Pi hardware. CAN ONLY BE INSTALLED ON RASPBERRY PI (due to dependancies).",
+    author="Zevi Berlin",
+    author_email="zeviberlin@gmail.com",
+    license="MIT",  # Open source license
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
