c447b5b271234dfc43092a909a8d3542f9b6a346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:19 +0000

    Add package workflows-manager/0.1.1/workflows_manager-0.1.1.tar.gz

---
+++ packages/workflows-manager/workflows_manager-0.1.1.tar.gz/workflows_manager-0.1.1/src/workflows_manager/exceptions.py
@@ -0,0 +1,25 @@
+"""
+This module contains custom exceptions.
+"""
+
+class InvalidConfiguration(Exception):
+    """
+    Exception raised when configuration is invalid.
+    """
+
+
+class MissingParameter(InvalidConfiguration):
+    """
+    Exception raised when parameter is missing.
+    """
+
+
+class MissingStep(Exception):
+    """
+    Exception raised when step from configuration is not registered.
+    """
+
+class UnknownOption(Exception):
+    """
+    Exception raised when unknown option is passed.
+    """
