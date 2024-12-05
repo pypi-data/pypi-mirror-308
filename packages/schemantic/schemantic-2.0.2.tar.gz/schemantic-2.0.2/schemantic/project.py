4b1cff194fc4497c0243e295e9d2fc9aac688b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:43 +0000

    Add package schemantic/2.0.2/schemantic-2.0.2.tar.gz

---
+++ packages/schemantic/schemantic-2.0.2.tar.gz/schemantic-2.0.2/schemantic/project.py
@@ -0,0 +1,45 @@
+from typing import Any, ClassVar
+
+from pydantic import BaseModel
+
+
+class SchemanticProjectMixin:
+    include_private: ClassVar[bool] = False
+
+    @classmethod
+    def fields_to_exclude_from_single_schema(cls) -> set[str]:
+        """
+        Some required fields for a class are sometimes highly specific to its respective object. These fields should
+        be recorded in this class-property to be excluded by the config generator function
+
+        Usage:
+            @classmethod
+            @property
+            def fields_to_exclude_from_single_schema(cls) -> set[str]:
+                upstream = super().fields_to_exclude_from_single_schema()
+                upstream.update(("SOME", "FIELD"))
+                return upstream
+
+        :return:
+        """
+        return set()
+
+    @classmethod
+    def single_schema_kwargs(cls) -> dict[str, Any]:
+        return dict(
+            include_private=cls.include_private,
+            fields_to_exclude=cls.fields_to_exclude_from_single_schema(),
+        )
+
+
+SchemanticProjectType = type[SchemanticProjectMixin]
+
+
+class SchemanticProjectModelMixin(BaseModel, SchemanticProjectMixin):
+    @classmethod
+    def fields_to_exclude_from_single_schema(cls) -> set[str]:
+        return super().fields_to_exclude_from_single_schema()
+
+    @classmethod
+    def single_schema_kwargs(cls) -> dict[str, Any]:
+        return super().single_schema_kwargs()
