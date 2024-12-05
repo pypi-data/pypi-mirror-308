dbdf47087c0f175d8e94507cc0b55e43c2925b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:05 +0000

    Add package deeporigin-data-sdk/0.1.0a16/deeporigin_data_sdk-0.1.0a16.tar.gz

---
+++ packages/deeporigin-data-sdk/deeporigin_data_sdk-0.1.0a16.tar.gz/deeporigin_data_sdk-0.1.0a16/src/deeporigin_data/types/shared/__init__.py
@@ -0,0 +1,7 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .update_column import UpdateColumn as UpdateColumn
+from .add_column_base import AddColumnBase as AddColumnBase
+from .add_column_union import AddColumnUnion as AddColumnUnion
+from .update_column_base import UpdateColumnBase as UpdateColumnBase
+from .update_column_union import UpdateColumnUnion as UpdateColumnUnion
