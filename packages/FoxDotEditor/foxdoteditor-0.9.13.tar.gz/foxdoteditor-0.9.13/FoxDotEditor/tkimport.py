ec307c9cf8746c5f552915aab9cd6e65ad321d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:17:01 +0000

    Add package FoxDotEditor/0.9.13/foxdoteditor-0.9.13.tar.gz

---
+++ packages/FoxDotEditor/foxdoteditor-0.9.13.tar.gz/foxdoteditor-0.9.13/FoxDotEditor/tkimport.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import json
+import ttkbootstrap as tb
+
+if sys.version_info[0] >= 3:
+    from tkinter import *
+    from tkinter import ttk
+    from tkinter import font as tkFont
+    from tkinter import filedialog as tkFileDialog
+    from tkinter import messagebox as tkMessageBox
+else:
+    from Tkinter import *
+    import ttk
+    import tkFont
+    import tkFileDialog
+    import tkMessageBox
