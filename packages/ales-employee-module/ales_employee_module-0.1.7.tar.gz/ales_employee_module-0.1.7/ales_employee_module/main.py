49ed587893279e5f6e72bce38b9b3b333316fd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:15 +0000

    Add package ales-employee-module/0.1.7/ales_employee_module-0.1.7.tar.gz

---
+++ packages/ales-employee-module/ales_employee_module-0.1.7.tar.gz/ales_employee_module-0.1.7/ales_employee_module/main.py
@@ -0,0 +1,9 @@
+from ales_employee_module.employee import Employee
+
+darb1 = Employee("Alessandro", "Caliva", 10000)
+
+# this is a comment
+if input().strip() == "with hello":
+    print(f"Hello, {darb1}!")
+else:
+    print(darb1)
---
+++ packages/ales-employee-module/ales_employee_module-0.1.7.tar.gz/ales_employee_module-0.1.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ales_employee_module",
+    version="0.1.7",
+    author="ales-sandrito",
+    email="calivaalessandro@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6"
+)
