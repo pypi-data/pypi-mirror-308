bbdbd9a7840cf792fc47610e8f94544a37ea8b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:24:21 +0000

    Add package pymuco/1.1.2/pymuco-1.1.2.tar.gz

---
+++ packages/pymuco/pymuco-1.1.2.tar.gz/pymuco-1.1.2/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pymuco",
+    packages=["pymuco"],
+    version="1.1.2",
+    description="A Python Music Computation Library",
+    author="German Margon",
+    author_email="gmargon@pymuco.org",
+    url="https://pymuco.org/",
+    license="BSD-3-Clause",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Artistic Software",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
