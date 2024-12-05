d995830da284ec777f697796347e88e092fa2486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:34 +0000

    Add package survey-manager/1.0/survey_manager-1.0.tar.gz

---
+++ packages/survey-manager/survey_manager-1.0.tar.gz/survey_manager-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+token = 'pypi-AgEIcHlwaS5vcmcCJDgzYzg4ZGMzLTY3MmMtNDMxMy04MjUxLTExMTgxY2E3Y2I3NwACKlszLCJkOWYzYTIwZi01YjE4LTQ4NGUtYWVjZS0yNjBmZWM5NzVhYjIiXQAABiB60YYu5UsUtuMn4mNmlDay4149VyugqP6XxhhZrVAHTg'
+setup(name='survey_manager',
+      version='1.0',
+      description='survey_manager can split data of soc.surveys and convert it to x,y coords',
+      packages=['survey_manager'],
+      author_email='andreyfakerare@gmail.com',
+      zip_safe=False)
