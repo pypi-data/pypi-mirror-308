fdc4d9775a251c4fd85a9e0045caf962f870dd57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:13 +0000

    Add package pydal/20241111.1/pydal-20241111.1.tar.gz

---
+++ packages/pydal/pydal-20241111.1.tar.gz/pydal-20241111.1/pydal/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "20241111.1"
+
+from .base import DAL
+from .helpers.classes import SQLCustomType
+from .helpers.methods import geoLine, geoPoint, geoPolygon
+from .objects import Field
