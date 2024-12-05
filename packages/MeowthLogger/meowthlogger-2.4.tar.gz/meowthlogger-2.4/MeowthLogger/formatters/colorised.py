06e7660a93afa3435109e3b2bd132c730981c0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:29 +0000

    Add package MeowthLogger/2.4/meowthlogger-2.4.tar.gz

---
+++ packages/MeowthLogger/meowthlogger-2.4.tar.gz/meowthlogger-2.4/MeowthLogger/formatters/colorised.py
@@ -0,0 +1,42 @@
+import logging
+
+from colorama import Back, Fore, Style
+
+from .formatter import AbstractFormatter
+
+class ColorisedFormatter(AbstractFormatter):
+    def prepare_log_string(
+            self,
+            datetime,
+            levelname,
+            filename,
+            line,
+            message,
+        ):
+
+        datetime = f"{Style.DIM}{datetime}{Style.NORMAL}"
+        filename = f"{Fore.LIGHTYELLOW_EX}{filename}{Fore.RESET}"
+        line = f"{Fore.LIGHTYELLOW_EX}{line}{Fore.RESET}"
+
+        return super().prepare_log_string(
+            datetime,
+            levelname,
+            filename,
+            line,
+            message,
+        )
+    
+    def prepare_levelname(self, levelname):
+        match levelname:
+            case logging.INFO:
+                return f"{Fore.GREEN}INFO{Fore.RESET}"
+            case logging.ERROR:
+                return f"{Fore.RED}ERROR{Fore.RESET}"
+            case logging.WARN:
+                return f"{Fore.YELLOW}WARNING{Fore.RESET}"
+            case logging.DEBUG:
+                return f"{Fore.LIGHTBLACK_EX}DEBUG{Fore.RESET}"
+            case logging.CRITICAL:
+                return f"{Back.RED} CRITICAL {Back.RESET}"
+            case _:
+                return f"LEVEL :{levelname}"
---
+++ packages/MeowthLogger/meowthlogger-2.4.tar.gz/meowthlogger-2.4/MeowthLogger/formatters/default.py
@@ -0,0 +1,4 @@
+from .formatter import AbstractFormatter
+
+class DefaultFormatter(AbstractFormatter):
+    pass
