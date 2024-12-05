4aa749f5aa70a48d49c42978ffd647774f525a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:26 +0000

    Add package sympla-tools/0.1.8/sympla_tools-0.1.8-py3-none-any.whl

---
+++ packages/sympla-tools/sympla_tools-0.1.8-py3-none-any.whl/sympla_tools/tickets/signature/__main__.py
@@ -0,0 +1,10 @@
+import sys
+from .token import validate
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Uso: python -m sympla_tools.tickets.signature.token.validate <signature>")
+        sys.exit(1)
+
+    signature = sys.argv[1]
+    validate(signature)
