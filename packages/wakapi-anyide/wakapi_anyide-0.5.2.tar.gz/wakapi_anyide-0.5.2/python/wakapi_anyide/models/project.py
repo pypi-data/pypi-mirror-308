4ce066b7bf09830aac6592e2c34ced6699f79a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:33 +0000

    Add package wakapi-anyide/0.5.2/wakapi_anyide-0.5.2.tar.gz

---
+++ packages/wakapi-anyide/wakapi_anyide-0.5.2.tar.gz/wakapi_anyide-0.5.2/python/wakapi_anyide/models/project.py
@@ -0,0 +1,34 @@
+from typing import List
+from typing import Literal
+
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from pydantic_settings import TomlConfigSettingsSource
+from pydantic_settings.main import SettingsConfigDict
+
+
+class ProjectMeta(BaseModel):
+    version: Literal[1]
+
+
+class ProjectFiles(BaseModel):
+    include: List[str]
+    exclude: List[str]
+    exclude_files: List[str]
+    exclude_binary_files: bool = True
+
+
+class ProjectDescription(BaseModel):
+    name: str
+
+
+class Project(BaseSettings):
+    model_config = SettingsConfigDict(toml_file="wak.toml")
+    
+    meta: ProjectMeta
+    files: ProjectFiles
+    project: ProjectDescription
+    
+    @classmethod
+    def settings_customise_sources(cls, settings_cls, *_args, **_kwargs):
+        return (TomlConfigSettingsSource(settings_cls), )
