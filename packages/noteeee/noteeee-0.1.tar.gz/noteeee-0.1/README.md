b9c7929876efea38fa16bce81b7a41a1a3eeb96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:24:30 +0000

    Add package noteeee/0.1/noteeee-0.1.tar.gz

---
+++ packages/noteeee/noteeee-0.1.tar.gz/noteeee-0.1/README.md
@@ -0,0 +1,10 @@
+# safuaaaa
+
+A Python package to fetch and save a PDF from a GitHub repository.
+
+## Installation
+
+You can install this package using `pip`:
+
+```bash
+pip install safuaaaa
---
+++ packages/noteeee/noteeee-0.1.tar.gz/noteeee-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="noteeee",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["requests"],
+    author="Safuan P Anvar",
+    description="A package to fetch and save a Jupyter Notebook from GitHub.",
+    url="https://github.com/saffuanaanvrr/exam",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
