971b107d0a3a61869023631d4c47eb6e57b7c5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:14 +0000

    Add package cfcp/0.1.3/cfcp-0.1.3-py3-none-any.whl

---
+++ packages/cfcp/cfcp-0.1.3-py3-none-any.whl/bot/config.py
@@ -0,0 +1,7 @@
+import os
+
+
+DATA_FILE_PATH = os.path.join(os.path.dirname(__file__), 'data.json')
+MAX_CONTEXT_LENGTH = 400
+THRESHOLD = 0.9
+MODEL = 'llama3.1'
---
+++ packages/cfcp/cfcp-0.1.3-py3-none-any.whl/bot/response_generator.py
@@ -0,0 +1,25 @@
+import asyncio
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from typing import List, Dict
+from bot.config import MODEL
+
+class ResponseGenerator:
+    def __init__(self, template: str, context_length: int):
+        self.model_llm = OllamaLLM(model=MODEL)
+        self.prompt = ChatPromptTemplate.from_template(template)
+        self.chain = self.prompt | self.model_llm
+        self.context_length = context_length
+
+    async def generate_response(self, context: str, qna_dict: Dict[str, str], question: str, name: str, description: str) -> str:
+        return await asyncio.to_thread(self.chain.invoke, {
+            "context": context,
+            "doc": list(qna_dict.values()),
+            "question": question,
+            "name": name,
+            "description": description
+        })
+    
+    def update_context(self, context: str, user_input: str, result: str) -> str:
+        context += f"\nUser: {user_input}\nAI: {result}"
+        return context[-self.context_length:]
