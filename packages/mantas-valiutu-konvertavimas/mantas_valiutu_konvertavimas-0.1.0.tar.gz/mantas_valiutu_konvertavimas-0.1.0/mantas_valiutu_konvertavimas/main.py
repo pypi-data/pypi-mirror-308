0c22eee00f8e6beb4197f66ed1abae399d7df643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:24:27 +0000

    Add package mantas-valiutu-konvertavimas/0.1.0/mantas_valiutu_konvertavimas-0.1.0.tar.gz

---
+++ packages/mantas-valiutu-konvertavimas/mantas_valiutu_konvertavimas-0.1.0.tar.gz/mantas_valiutu_konvertavimas-0.1.0/mantas_valiutu_konvertavimas/main.py
@@ -0,0 +1,3 @@
+from valiutos_konvertavimas import eur_to_usd
+
+print(eur_to_usd(100))
---
+++ packages/mantas-valiutu-konvertavimas/mantas_valiutu_konvertavimas-0.1.0.tar.gz/mantas_valiutu_konvertavimas-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mantas_valiutu_konvertavimas",
+    version="0.1.0",
+    author="Mantaszup123",
+    email="mantaszup@gmail.com",
+    packages=find_packages(),
+    install_requires=["currency_converter"],
+    python_requires=">=3.6"
+)
