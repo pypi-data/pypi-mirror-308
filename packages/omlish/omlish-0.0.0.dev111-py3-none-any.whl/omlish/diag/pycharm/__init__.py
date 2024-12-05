b84fb8c5a7062f453b995bd3df80519564ddcad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:43 +0000

    Add package omlish/0.0.0.dev111/omlish-0.0.0.dev111-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev111-py3-none-any.whl/omlish/diag/pycharm/__init__.py
@@ -0,0 +1,7 @@
+from .pycharm import (  # noqa
+    PycharmRemoteDebugger,
+    get_pycharm_version,
+    is_pycharm_hosted,
+    pycharm_remote_debugger_attach,
+    read_darwin_pycharm_info_plist,
+)
