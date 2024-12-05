4d4d89b37b1bc722249f737973639cfb6535f7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:38 +0000

    Add package colcon-poetry-ros/0.9.0/colcon_poetry_ros-0.9.0.tar.gz

---
+++ packages/colcon-poetry-ros/colcon_poetry_ros-0.9.0.tar.gz/colcon_poetry_ros-0.9.0/colcon_poetry_ros/dependencies/install.py
@@ -0,0 +1,156 @@
+import argparse
+from pathlib import Path
+import sys
+from typing import List
+import subprocess
+import logging
+
+from colcon_poetry_ros.package_identification.poetry import (
+    PoetryPackage,
+    NotAPoetryPackageError,
+)
+
+
+def main():
+    args = _parse_args()
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+
+    for project in _discover_packages(args.base_paths):
+        logging.info(f"Installing dependencies for {project.path.name}...")
+        _install_dependencies(
+            project, args.install_base, args.merge_install
+        )
+
+    logging.info("\nDependencies installed!")
+
+
+def _discover_packages(base_paths: List[Path]) -> List[PoetryPackage]:
+    projects: List[PoetryPackage] = []
+
+    potential_packages = []
+    for path in base_paths:
+        potential_packages += list(path.glob("*"))
+
+    for path in potential_packages:
+        if path.is_dir():
+            try:
+                project = PoetryPackage(path)
+            except NotAPoetryPackageError:
+                continue
+            else:
+                projects.append(project)
+
+    if len(projects) == 0:
+        base_paths_str = ", ".join([str(p) for p in base_paths])
+        logging.error(
+            f"No packages were found in the following paths: {base_paths_str}"
+        )
+        sys.exit(1)
+
+    return projects
+
+
+def _install_dependencies(
+    project: PoetryPackage, install_base: Path, merge_install: bool
+) -> None:
+    """Uses poetry-bundle-plugin to create a virtual environment with all the project's
+    dependencies in Colcon's install directory. We need to use the bundle plugin because
+    Poetry does not natively let us install projects to a custom location.
+    """
+    try:
+        subprocess.run(
+            ["poetry", "bundle", "venv", "--help"],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    except subprocess.CalledProcessError:
+        logging.error(
+            "The Poetry bundle plugin does not appear to be installed! See the "
+            "project page for installation instructions: "
+            "https://github.com/python-poetry/poetry-plugin-bundle"
+        )
+        sys.exit(1)
+
+    if not merge_install:
+        install_base /= project.name
+
+    subprocess.run(
+        [
+            "poetry",
+            "bundle",
+            "venv",
+            "--no-interaction",
+            str(install_base.absolute()),
+        ],
+        check=True,
+        cwd=project.path
+    )
+
+    _enable_system_site_packages(install_base)
+
+
+def _enable_system_site_packages(install_base: Path) -> None:
+    """Enables system site packages for a virtual environment. This allows system
+    packages to be imported from within the virtual environment, which is necessary
+    since ROS depends on some Python packages supplied by APT.
+
+    Based on this: https://stackoverflow.com/a/40972692/2159348
+
+    :param install_base: The location of the virtual environment
+    """
+    pyvenv_config_file = install_base / "pyvenv.cfg"
+
+    # Even though the pyvenv.cfg file looks like a ConfigParser-style ini file, it's not
+    # because it's missing sections, so we're stuck doing string manipulation instead
+    old_config = pyvenv_config_file.read_text()
+    new_config = ""
+    for line in old_config.splitlines():
+        if line.startswith("include-system-site-packages"):
+            new_config += "include-system-site-packages = true\n"
+        else:
+            new_config += f"{line}\n"
+
+    pyvenv_config_file.write_text(new_config)
+
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Searches for Poetry packages and installs their dependencies "
+        "to a configurable install base"
+    )
+
+    parser.add_argument(
+        "--base-paths",
+        nargs="+",
+        type=Path,
+        default=[Path.cwd()],
+        help="The paths to start looking for Poetry projects in. Defaults to the "
+        "current directory."
+    )
+
+    parser.add_argument(
+        "--install-base",
+        type=Path,
+        default=Path("install"),
+        help="The base path for all install prefixes (default: install)",
+    )
+
+    parser.add_argument(
+        "--merge-install",
+        action="store_true",
+        help="Merge all install prefixes into a single location",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="If provided, debug logs will be printed",
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/colcon-poetry-ros/colcon_poetry_ros-0.9.0.tar.gz/colcon_poetry_ros-0.9.0/colcon_poetry_ros/task/poetry/build.py
@@ -0,0 +1,227 @@
+import shutil
+from pathlib import Path
+
+import toml
+from colcon_core.environment import create_environment_hooks, \
+    create_environment_scripts
+from colcon_core.logging import colcon_logger
+from colcon_core.plugin_system import satisfies_version
+from colcon_core.shell import get_command_environment, create_environment_hook
+from colcon_core.task import TaskExtensionPoint
+from colcon_core.task import run
+
+from colcon_poetry_ros import config
+
+
+logger = colcon_logger.getChild(__name__)
+
+
+class PoetryBuildTask(TaskExtensionPoint):
+    """Builds Python packages using Poetry"""
+
+    def __init__(self):
+        super().__init__()
+        satisfies_version(TaskExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
+
+    async def build(self, *, additional_hooks=None):
+        pkg = self.context.pkg
+        args = self.context.args
+
+        if pkg.type != "poetry.python":
+            logger.error(
+                f"The Poetry build was invoked on the wrong package type! Expected "
+                f"'poetry.python' but got '{pkg.type}'."
+            )
+
+        logger.info(f"Building Poetry Python package in '{args.path}'")
+
+        try:
+            env = await get_command_environment(
+                "build", args.build_base, self.context.dependencies
+            )
+        except RuntimeError as e:
+            logger.error(str(e))
+            return 1
+
+        # In this step, we want to install the ROS package's source without touching
+        # its dependencies, because those are managed by
+        # colcon_poetry_ros.dependencies.install. Poetry (even with bundle) doesn't have
+        # a way to re-install only the source code to a target directory, so instead we
+        # export the project as a wheel and use Pip to install the source instead.
+        # Related: https://github.com/python-poetry/poetry-plugin-bundle/issues/87
+        completed = await run(
+            self.context,
+            ["poetry", "build", "--format", "wheel"],
+            cwd=args.path,
+            env=env,
+        )
+        if completed.returncode:
+            logger.error(f"Poetry failed to build the package for '{args.path}'")
+            return completed.returncode
+
+        poetry_dist = Path(args.build_base) / "poetry_dist"
+        if poetry_dist.exists():
+            shutil.rmtree(str(poetry_dist))
+
+        shutil.move(
+            str(Path(args.path) / "dist"),
+            str(poetry_dist)
+        )
+
+        # Find the wheel file that Poetry generated
+        wheels = list(poetry_dist.glob("*.whl"))
+        if len(wheels) == 0:
+            logger.error(f"Poetry failed to produce a wheel file in '{poetry_dist}'")
+        wheel_name = str(wheels[0])
+
+        # Include any extras that are needed at runtime. Extras are included by adding
+        # a bracket-surrounded comma-separated list to the end of the package name, like
+        # "colcon-poetry-ros[cool_stuff,other_stuff]"
+        extras = config.run_depends_extras.get()
+        if len(extras) > 0:
+            extras_str = ",".join(extras)
+            extras_str = f"[{extras_str}]"
+            wheel_name += extras_str
+
+        venv_python_executable = Path(args.install_base) / "bin/python"
+        if not venv_python_executable.exists():
+            logger.error(
+                f"No virtual environment exists in {args.install_base}. Have "
+                f"dependencies not been installed yet?"
+            )
+            return 1
+
+        # Install Poetry's generated wheel
+        completed = await run(
+            self.context,
+            [
+                str(venv_python_executable),
+                "-m",
+                "pip",
+                "install",
+                wheel_name,
+                # pip will skip installation if the package version is the same
+                # but we want the installed version to always reflect the source
+                # regardless of the package version
+                "--force-reinstall",
+                # Turns off Pip's check to ensure installed binaries are in the
+                # PATH. ROS workspaces take care of setting the PATH, but Pip
+                # doesn't know that.
+                "--no-warn-script-location",
+                "--no-deps",
+            ],
+            cwd=args.path,
+            env=env,
+        )
+        if completed.returncode:
+            logger.error(f"Failed to install Poetry's wheel for '{args.path}'")
+            return completed.returncode
+
+        # Poetry installs scripts to {prefix}/bin, but ROS wants them at
+        # {prefix}/lib/{package_name}
+        poetry_script_dir = Path(args.install_base) / "bin"
+        if not poetry_script_dir.exists():
+            logger.info(f"Attempting to use .../local/bin instead for poetry_script_dir")
+            poetry_script_dir = Path(args.install_base) / "local" / "bin"
+        ros_script_dir = Path(args.install_base) / "lib" / pkg.name
+        if poetry_script_dir.is_dir():
+            ros_script_dir.mkdir(parents=True, exist_ok=True)
+
+            script_files = poetry_script_dir.glob("*")
+            script_files = filter(Path.is_file, script_files)
+
+            for script in script_files:
+                shutil.copy2(str(script), str(ros_script_dir))
+        else:
+            logger.warning(
+                "Poetry did not install any scripts. Are you missing a "
+                "[tool.poetry.scripts] section?"
+            )
+
+        return_code = await self._add_data_files()
+        if return_code != 0:
+            return return_code
+
+        # This hook is normally defined by AmentPythonBuildTask, but since this class
+        # replaces that, we have to define it ourselves
+        additional_hooks = create_environment_hook(
+            "ament_prefix_path",
+            Path(args.install_base),
+            self.context.pkg.name,
+            "AMENT_PREFIX_PATH",
+            "",
+            mode="prepend",
+        )
+
+        hooks = create_environment_hooks(args.install_base, pkg.name)
+        create_environment_scripts(
+            pkg, args, default_hooks=list(hooks), additional_hooks=additional_hooks
+        )
+
+    async def _add_data_files(self) -> int:
+        """Installs data files based on the [tool.colcon-poetry-ros.data-files] table.
+        Poetry's support for data files is fairly incomplete at the time of writing, so
+        we need to do this ourselves.
+
+        See: https://github.com/python-poetry/poetry/issues/2015
+        """
+        pkg = self.context.pkg
+        args = self.context.args
+
+        pyproject_toml = pkg.path / "pyproject.toml"
+        pyproject = toml.loads(pyproject_toml.read_text())
+
+        try:
+            data_files = pyproject["tool"]["colcon-poetry-ros"]["data-files"]
+        except KeyError:
+            logger.warning(
+                f"File {pyproject_toml} does not define any data files, so none will "
+                f"be included in the installation"
+            )
+            return 0
+
+        if not isinstance(data_files, dict):
+            logger.error(f"{_DATA_FILES_TABLE} must be a table")
+            return 1
+
+        for destination, sources in data_files.items():
+            if not isinstance(sources, list):
+                logger.error(
+                    f"Field '{destination}' in {_DATA_FILES_TABLE} must be an array"
+                )
+
+            dest_path = Path(args.install_base) / destination
+            dest_path.mkdir(parents=True, exist_ok=True)
+
+            for source in sources:
+                source_path = pkg.path / Path(source)
+                _copy_path(source_path, dest_path)
+
+        return 0
+
+
+def _copy_path(src: Path, dest: Path) -> None:
+    """Copies a file or directory to a destination"""
+    if src.is_dir():
+        # The provided destination is interpreted as a parent directory when copying
+        # directories
+        dest_dir = dest / src.name
+        # shutil.copytree can not completely overwrite a directory, so we need to delete
+        # the existing one in advance
+        if dest_dir.exists():
+            _delete_path(dest_dir)
+
+        shutil.copytree(str(src), str(dest_dir))
+    else:
+        shutil.copy2(str(src), str(dest))
+
+
+def _delete_path(path: Path) -> None:
+    """Deletes a file or directory"""
+    if path.is_file():
+        path.unlink()
+    elif path.is_dir():
+        shutil.rmtree(str(path))
+
+
+_DATA_FILES_TABLE = "[tool.colcon-poetry-ros.data-files]"
