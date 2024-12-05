2daf655a3c5e8106260abab895bf592209268f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:16 +0000

    Add package repo2llm/0.2.1/repo2llm-0.2.1-py3-none-any.whl

---
+++ packages/repo2llm/repo2llm-0.2.1-py3-none-any.whl/repo2llm/constants.py
@@ -0,0 +1,22 @@
+DEFAULT_IGNORE_PATTERNS = {
+    # Directories to ignore
+    '.git',
+    '__pycache__',
+    '.pytest_cache',
+    'node_modules',
+    'venv',
+    '.venv',
+    '.env',
+    '.vscode',
+    '.idea',
+    'build',
+    'dist',
+    # File patterns to ignore
+    '*.pyc',
+    '*.log',
+    '*.sqlite3',
+    '*.db',
+    'poetry.lock',
+    '.gitignore',
+    '.pre-commit-config.yaml',
+}
