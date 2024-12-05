dc6e4c1de163f9c5a6440fb5c65139fa15609a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:24:41 +0000

    Add package bimbim/0.1.3/bimbim-0.1.3.tar.gz

---
+++ packages/bimbim/bimbim-0.1.3.tar.gz/bimbim-0.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bimbim",
+    version="0.1.3",
+    author="balvakokay",
+    email="balvakokay@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=0.1"
+)
