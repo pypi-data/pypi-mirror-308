2aa7d568a8f4d5854ed9382fcb079561bcb71ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:22 +0000

    Add package opengis/3.0.3/opengis-3.0.3.tar.gz

---
+++ packages/opengis/opengis-3.0.3.tar.gz/opengis-3.0.3/opengis/__init__.py
@@ -0,0 +1,1 @@
+from .batch_reprojection import batch_reproject
---
+++ packages/opengis/opengis-3.0.3.tar.gz/opengis-3.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+# Runtime Requirements
+inst_reqs = [
+        "gdal",
+        "pymodis",
+]
+
+setup(
+    name="opengis",
+    version="3.0.3",
+    description="Open source GIS tools.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Zijie Wang",
+    author_email="773598627@qq.com",
+    packages=find_packages(),
+    install_requires=inst_reqs,
+    python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    keywords="GIS Remote-sensing",
+    project_urls={
+        "Source":"https://github.com/caaszj/opengis",
+        "Tracker":"https://github.com/caaszj/opengis/issues"
+        }
+)
