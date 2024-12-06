cb906b942731cae184af57ced9ddbf266f67784b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:15 +0000

    Add package calkit-python/0.3.3/calkit_python-0.3.3-py3-none-any.whl

---
+++ packages/calkit-python/calkit_python-0.3.3-py3-none-any.whl/calkit/core.py
@@ -0,0 +1,56 @@
+"""Core functionality."""
+
+from __future__ import annotations
+
+import glob
+import logging
+import os
+
+import ruamel.yaml
+from git import Repo
+from git.exc import InvalidGitRepositoryError
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__package__)
+
+ryaml = ruamel.yaml.YAML()
+ryaml.indent(mapping=2, sequence=4, offset=2)
+ryaml.preserve_quotes = True
+ryaml.width = 70
+
+
+def find_project_dirs(relative=False, max_depth=3) -> list[str]:
+    """Find all Calkit project directories."""
+    if relative:
+        start = ""
+    else:
+        start = os.path.expanduser("~")
+    res = []
+    for i in range(max_depth):
+        pattern = os.path.join(start, *["*"] * (i + 1), "calkit.yaml")
+        res += glob.glob(pattern)
+        # Check GitHub documents for users who use GitHub Desktop
+        pattern = os.path.join(
+            start, "*", "GitHub", *["*"] * (i + 1), "calkit.yaml"
+        )
+        res += glob.glob(pattern)
+    final_res = []
+    for ck_fpath in res:
+        path = os.path.dirname(ck_fpath)
+        # Make sure this path is a Git repo
+        try:
+            Repo(path)
+        except InvalidGitRepositoryError:
+            continue
+        final_res.append(path)
+    return final_res
+
+
+def load_calkit_info(wdir=None) -> dict:
+    fpath = "calkit.yaml"
+    if wdir is not None:
+        fpath = os.path.join(wdir, fpath)
+    if os.path.isfile(fpath):
+        with open(fpath) as f:
+            return ryaml.load(f)
+    return {}
