4a48d7881961e2e7ec97f749bb0f30cbd18f5670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:34 +0000

    Add package deeporigin/2.5.2/deeporigin-2.5.2.tar.gz

---
+++ packages/deeporigin/deeporigin-2.5.2.tar.gz/deeporigin-2.5.2/src/data_hub/dataframe.py
@@ -0,0 +1,406 @@
+"""
+This module defines a class called DataFrame that is a drop-in
+replacement for a pandas DataFrame, but also allows for easy
+updating of Deep Origin databases.
+"""
+
+from datetime import datetime, timezone
+
+import humanize
+import pandas as pd
+from beartype import beartype
+from dateutil.parser import parse
+from deeporigin.data_hub import api
+from deeporigin.platform.api import get_last_edited_user_name
+from deeporigin.utils.config import construct_resource_url
+from deeporigin.utils.constants import DataType, IDFormat
+from deeporigin.utils.network import check_for_updates
+
+check_for_updates()
+
+
+__NO_NEW_ROWS_MSG__ = "Adding rows is not allowed, because this dataframe corresponds to a subset of the rows in the corresponding database."
+
+
+class DataFrame(pd.DataFrame):
+    """A subclass of pandas DataFrame that allows for easy updating of a Deep Origin database. This can be used as a drop-in replacement for a pandas DataFrame, and should support all methods a pandas DataFrame supports.
+
+    The primary method of creating an object of this type is to use the [from_deeporigin][src.data_hub.dataframe.DataFrame.from_deeporigin] class method.
+    """
+
+    auto_sync: bool = False
+    """When `True`, changes made to the dataframe will be automatically synced to the Deep Origin database this dataframe represents."""
+
+    _modified_columns: dict = dict()
+    """if data is modified in a dataframe, and auto_sync is False, this list will contain the columns that have been modified so that the Deep Origin database can be updated. If an empty list, the Deep Origin database will not be updated, and the dataframe matches the Deep Origin database at the time of creation."""
+
+    _allow_adding_rows: bool = True
+    """If `True`, new rows can be added to the dataframe. If `False`, new rows cannot be added to the dataframe."""
+
+    @property
+    def loc(self):
+        class _LocIndexer:
+            def __init__(self, df):
+                self.df = df
+
+            def __getitem__(self, key):
+                # first call the superclass method
+                df = super(DataFrame, self.df).loc[key]
+
+                # inherit attributes
+                df.attrs = self.df.attrs
+
+                # disallow adding rows
+                df._allow_adding_rows = False
+
+                df._modified_columns = self.df._modified_columns
+
+                return df
+
+            def __setitem__(self, key, value):
+                """callback for adding a new row, typically"""
+
+                if not self.df._allow_adding_rows:
+                    # adding rows is not allowed
+                    if isinstance(key, (list, pd.Index)):
+                        if not all(k in self.df.index for k in key):
+                            raise ValueError(__NO_NEW_ROWS_MSG__)
+                    elif key not in self.df.index:
+                        raise ValueError(__NO_NEW_ROWS_MSG__)
+                super(DataFrame, self.df).loc[key] = value
+
+                # TODO we need to mark that every column has been modified, but only this row
+
+        # Return the custom _LocIndexer instance
+        return _LocIndexer(self)
+
+    class AtIndexer:
+        """this class override is used to intercept calls to at indexer of a pandas dataframe"""
+
+        def __init__(self, obj):
+            self.obj = obj
+
+        def __getitem__(self, key):
+            """intercept for the set operation"""
+
+            return self.obj._get_value(*key)
+
+        def __setitem__(self, key, value) -> None:
+            """intercept for the set operation"""
+
+            if (
+                isinstance(value, pd.Series)
+                and len(value) > len(self)
+                and not self._allow_adding_rows
+            ):
+                raise ValueError(__NO_NEW_ROWS_MSG__)
+
+            old_value = self.obj._get_value(*key)
+
+            # the reason this is in a try block is because
+            # this can fail for any number of reasons.
+            # for example, types of the two things may be
+            # different, or the old or new value may be missing,
+            # in which case an equality is meaningless.
+            try:
+                if value == old_value:
+                    # noop
+                    return
+            except Exception:
+                pass
+
+            rows = [key[0]]
+            column = key[1]
+
+            # Perform the actual setting operation
+            self.obj._set_value(*key, value)
+
+            # now update the DB. note that self is an AtIndexer
+            # object, so we need to index into the pandas object
+            if self.obj.auto_sync:
+                # auto sync enabled, simply write ASAP
+                self.obj.to_deeporigin()
+            else:
+                # auto sync not enabled, so we need to
+                # keep track of changes in _modified_columns
+                if column not in self.obj._modified_columns.keys():
+                    # this is the first time we're modifying this column
+                    self.obj._modified_columns[column] = set(rows)
+                else:
+                    # we've already modified this column before, so update the rows we're touched
+                    self.obj._modified_columns[column].update(set(rows))
+
+    @property
+    def at(self):
+        """Override the `at` property to return an AtIndexer"""
+        return self.AtIndexer(self)
+
+    def __setitem__(self, key, value):
+        """Override the __setitem__ method to update the Deep Origin database when changes are made to the local
+        dataframe. This method is called when an entire column is being updated"""
+
+        # first, call the pandas method
+        super().__setitem__(key, value)
+
+        # now, update the Deep Origin database with the changes
+        # we just made
+        if self.auto_sync:
+            self.to_deeporigin()
+        else:
+            if isinstance(key, str):
+                # an empty set means "all "
+                self._modified_columns[key] = set()
+            elif isinstance(key, list):
+                for item in key:
+                    self._modified_columns[item] = set()
+
+    def head(self, n=5):
+        """Override the `head` method so that we don't display a spurious modified warning"""
+
+        df = super().head(n)
+        df._modified_columns = None
+        return df
+
+    def tail(self, n=5):
+        """Override the `tail` method so that we don't display a spurious modified warning"""
+
+        df = super().tail(n)
+        df._modified_columns = None
+        return df
+
+    def append(
+        self,
+        other,
+        ignore_index=False,
+        verify_integrity=False,
+        sort=False,
+    ):
+        """Override the `append` method"""
+        if self._allow_adding_rows:
+            return super().append(other, ignore_index, verify_integrity, sort)
+        else:
+            raise ValueError(__NO_NEW_ROWS_MSG__)
+
+    def _repr_html_(self):
+        """method override to customize printing in a Jupyter notebook"""
+
+        # the entirety of this code is in a try/catch
+        # block because pretty printing may fail,
+        # and failure to pretty print should never
+        # block other more mission critical code
+        try:
+            name = self.attrs["metadata"]["name"]
+            hid = self.attrs["metadata"]["hid"]
+
+            # placeholder URL, only used if something goes wrong
+            url = "https://os.deeporigin.com/"
+
+            # extract org name from url
+            try:
+                url = construct_resource_url(
+                    name=hid,
+                    row_type="database",
+                )
+                url_parts = url.split("/")
+                org_name = url_parts[url_parts.index("org") + 1]
+            except Exception:
+                org_name = ""
+
+            # Convert the string to a datetime object
+            date_str = self.attrs["metadata"]["dateCreated"]
+            date_obj = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S.%f").replace(
+                tzinfo=timezone.utc
+            )
+
+            now = datetime.now(timezone.utc)
+
+            # Convert the time difference into "x time ago" format
+            created_time_ago = humanize.naturaltime(now - date_obj)
+
+            date_str = self.attrs["last_updated_row"].date_updated
+            date_obj = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S.%f").replace(
+                tzinfo=timezone.utc
+            )
+            edited_time_ago = humanize.naturaltime(now - date_obj)
+
+            header = f'<h4 style="color: #808080;">Deep Origin / {org_name} / <a href = "{url}">{name} </a></h4>'
+            txt = f'<p style="font-size: 12px; color: #808080;">Created {created_time_ago}. Row {self.attrs["last_updated_row"].hid} was last edited {edited_time_ago}'
+            try:
+                last_edited_by = get_last_edited_user_name(
+                    self.attrs["last_updated_row"]
+                )
+                txt += "  by " + last_edited_by + ".</p>"
+            except Exception:
+                # give up. this should not cause the dataframe to
+                # not print.
+                txt += ".</p>"
+
+            if self._modified_columns:
+                txt += '<p style="color: #808080; font-size: 12px">⚠️ This dataframe contains changes that have not been written back to the Deep Origin database.</p>'
+            elif self.auto_sync:
+                txt += '<p style="color: #808080; font-size: 12px">🧬 This dataframe will automatically write changes made to it back to Deep Origin.</p>'
+            df_html = super()._repr_html_()
+            return header + txt + df_html
+        except Exception:
+            return super()._repr_html_()
+
+    def __repr__(self):
+        """method override to customize printing in an interactive session"""
+
+        df_representation = super().__repr__()
+        try:
+            header = f'{self.attrs["metadata"]["hid"]}\n'
+        except Exception:
+            header = ""
+
+        return header + df_representation
+
+    @classmethod
+    def from_deeporigin(
+        cls,
+        database_id: str,
+        *,
+        use_file_names: bool = True,
+        reference_format: IDFormat = "human-id",
+        client=None,
+    ):
+        """Create a local Deep Origin DataFrame from a Deep Origin database.
+
+        Args:
+            database_id (str): The ID of the Deep Origin database.
+            use_file_names (bool, optional): Whether to use the file names in the Deep Origin database. Defaults to True.
+            reference_format (IDFormat, optional): The format of the IDs in the Deep Origin database. Defaults to "human-id".
+
+        """
+
+        return api.get_dataframe(
+            database_id=database_id,
+            use_file_names=use_file_names,
+            reference_format=reference_format,
+            return_type="dataframe",  # we need this
+            client=client,
+        )
+
+    def to_deeporigin(self):
+        """Write data in dataframe to Deep Origin
+
+        !!! tip "Deep Origin DataFrames can automatically synchronize"
+            To automatically save changes to local DataFrames to Deep Origin databases, set the `auto_sync` attribute of the dataframe `True`.
+        """
+
+        columns = self._modified_columns.copy()
+
+        for column in columns.keys():
+            if column in ["Validation Status", "ID"]:
+                continue
+
+            rows = columns[column]
+
+            column_metadata = [
+                col for col in self.attrs["metadata"]["cols"] if col["name"] == column
+            ]
+
+            if len(column_metadata) == 0:
+                # new column to add
+                print("Creating new column...")
+                # infer column type
+                column_type = _infer_column_type(self[column])
+
+                # make a new column
+                response = api.add_database_column(
+                    database_id=self.attrs["metadata"]["hid"],
+                    type=column_type,
+                    name=column,
+                )
+
+                # add column metadata to column
+                self.attrs["metadata"]["cols"].append(response["data"]["column"])
+            else:
+                # column already exists
+                column_metadata = column_metadata[0]
+
+                if column_metadata["type"] == "file":
+                    continue
+
+            if rows == set():
+                # we're updating a whole column
+                rows = list(self.index)
+
+            api.set_data_in_cells(
+                values=self[column][list(rows)],
+                row_ids=rows,
+                column_id=column,
+                database_id=self.attrs["id"],
+                columns=self.attrs["metadata"]["cols"],
+            )
+
+            print(f"✔︎ Wrote {len(rows)} rows in {column} to Deep Origin database.")
+
+            # remove this from modified columns
+            self._modified_columns.pop(column, None)
+
+        # fetch info for the last modified row so we can update what we show
+        try:
+            self.attrs["last_updated_row"] = api.describe_row(row_id=rows[-1])
+        except Exception:
+            pass
+
+    @property
+    def _constructor(self):
+        """this method overrides the _constructor property to return a DataFrame and is required for compatibility with a pandas DataFrame"""
+
+        return DataFrame
+
+
+@beartype
+def _infer_column_type(column: pd.Series) -> DataType:
+    """utility function to infer type of data in a pandas column"""
+
+    non_null_values = column.dropna()
+
+    if non_null_values.empty:
+        return "string"
+
+    # Helper functions for more complex checks
+    def is_bool(val):
+        if isinstance(val, str):
+            val = val.lower()
+            return val in ["true", "false"]
+        return isinstance(val, bool)
+
+    def is_int(val):
+        try:
+            return float(val).is_integer()
+        except (ValueError, TypeError):
+            return False
+
+    def is_float(val):
+        try:
+            float(val)
+            return True
+        except (ValueError, TypeError):
+            return False
+
+    def is_date(val):
+        try:
+            parse(val, fuzzy=False)
+            return True
+        except (ValueError, TypeError):
+            return False
+
+    # Inference checks
+    types = {
+        "boolean": non_null_values.apply(is_bool).all(),
+        "integer": non_null_values.apply(lambda x: is_int(x)).all(),
+        "float": non_null_values.apply(lambda x: is_float(x)).all(),
+        "date": non_null_values.apply(lambda x: is_date(x)).all(),
+        "text": non_null_values.apply(lambda x: isinstance(x, str)).all(),
+    }
+
+    # Priority order of types (boolean -> integer -> float -> date -> string)
+    for dtype, result in types.items():
+        if result:
+            return dtype
+
+    # no clear type, fall back to string
+    return "text"
---
+++ packages/deeporigin/deeporigin-2.5.2.tar.gz/deeporigin-2.5.2/tests/test_dataframe.py
@@ -0,0 +1,208 @@
+"""this module contains tests for the DataFrame class"""
+
+import uuid
+
+import numpy as np
+import pandas as pd
+import pytest
+from deeporigin.data_hub import api
+from deeporigin.data_hub.dataframe import DataFrame
+
+from tests.utils import clean_up_test_objects
+
+TEST_PREFIX = "tc-"
+
+NUMERIC_COLUMNS = ["float", "integer"]
+
+SKIP_MSG = "Skipping this test because we can't test this well using a mocked client"
+
+
+@pytest.fixture(scope="function", autouse=True)
+def config(pytestconfig):
+    """this config block sets up a database that we can operate on in the tests"""
+
+    salt = str(uuid.uuid4())[:8]
+
+    TEST_DB_NAME = TEST_PREFIX + salt + "-db"
+
+    data = dict()
+    if pytestconfig.getoption("client") == "mock":
+        data["mock"] = True
+
+        # Create the DataFrame
+        data["df"] = DataFrame(
+            pd.DataFrame(
+                {
+                    "integer": np.random.randint(1, 100, size=10),
+                    "float": np.random.random(10),
+                },
+                index=[f"x-{i}" for i in range(1, 11)],
+            )
+        )
+
+    else:
+        data["mock"] = False
+
+        rows = api.list_rows(row_type="database")
+        if TEST_DB_NAME in [row.hid for row in rows]:
+            api.delete_database(database_id=TEST_DB_NAME)
+
+        api.create_database(name=TEST_DB_NAME)
+
+        # make columns for values and squared values
+        for column in NUMERIC_COLUMNS:
+            api.add_database_column(
+                database_id=TEST_DB_NAME,
+                name=column,
+                type=column,
+            )
+
+            api.add_database_column(
+                database_id=TEST_DB_NAME,
+                name="sq_" + column,
+                type=column,
+            )
+
+        # make rows. we'll only fill some rows
+        response = api.make_database_rows(
+            database_id=TEST_DB_NAME,
+            n_rows=10,
+        )
+        row_ids = [row.hid for row in response.rows]
+
+        api.set_data_in_cells(
+            values=np.random.random(5),
+            row_ids=row_ids[0:5],
+            column_id="float",
+            database_id=TEST_DB_NAME,
+        )
+
+        api.set_data_in_cells(
+            values=np.random.randint(100, size=5),
+            row_ids=row_ids[0:5],
+            column_id="integer",
+            database_id=TEST_DB_NAME,
+        )
+
+        data["db-name"] = TEST_DB_NAME
+
+    # tests run on yield
+    yield data
+
+    # teardown code
+    if pytestconfig.getoption("client") != "mock":
+        clean_up_test_objects(TEST_DB_NAME)
+
+
+@pytest.mark.parametrize("column", NUMERIC_COLUMNS)
+def test_dataframe_read_modify(config, column):  # noqa: F811
+    """this function tests our ability to fetch data, modify it, and write it back"""
+
+    if config["mock"]:
+        pytest.skip(SKIP_MSG)
+
+    df = DataFrame.from_deeporigin(config["db-name"])
+    df["sq_" + column] = df[column] ** 2
+    df.to_deeporigin()
+
+
+@pytest.mark.parametrize("column", NUMERIC_COLUMNS)
+def test_dataframe_write_new_columns(config, column):  # noqa: F811
+    """this function tests our ability to write new columns to a database"""
+
+    if config["mock"]:
+        pytest.skip(SKIP_MSG)
+
+    df = DataFrame.from_deeporigin(config["db-name"])
+    df["cube_" + column] = df[column] ** 3
+    df.to_deeporigin()
+
+
+def test_slicing_restrictions(config):
+    """check that we control what happens when a dataframe is sliced by rows"""
+
+    if config["mock"]:
+        df = DataFrame(config["df"])
+    else:
+        df = DataFrame.from_deeporigin(config["db-name"])
+
+    assert df._allow_adding_rows is True, "Expected _allow_adding_rows to be True"
+
+    # slice to 2 rows
+    df = df.loc[df.index[:2]]
+
+    assert df._allow_adding_rows is False, "Expected _allow_adding_rows to be False"
+
+
+def test_slice_and_modify(config):
+    """check that we control what happens when a dataframe is sliced by rows"""
+
+    if config["mock"]:
+        df = DataFrame(config["df"])
+    else:
+        df = DataFrame.from_deeporigin(config["db-name"])
+
+    assert df._allow_adding_rows is True, "Expected _allow_adding_rows to be True"
+
+    # slice to 2 rows
+    df = df.loc[df.index[:2]]
+
+    assert df._allow_adding_rows is False, "Expected _allow_adding_rows to be False"
+
+    # we should be allowed to modify a slice
+    row_id = df.index[0]
+    df.at[row_id, "integer"] = 100
+
+    if config["mock"]:
+        return
+
+    # should be allowed to write back to DB
+    df.to_deeporigin()
+
+
+def test_slice_and_extend_loc(config):
+    """test that we can add a row to a whole df, but not to a slice of it"""
+
+    if config["mock"]:
+        df = DataFrame(config["df"])
+        row_prefix = "x"
+    else:
+        df = DataFrame.from_deeporigin(config["db-name"])
+        row_prefix = df.attrs["metadata"]["hid_prefix"]
+
+    # should be possible to add a new row
+    df.loc[row_prefix + "-" + str(len(df) + 1)] = list(df.loc[df.index[0]])
+
+    if not config["mock"]:
+        df.to_deeporigin()
+
+    # slice to 2 rows
+    df = df.loc[df.index[:2]]
+
+    # should not be possible to add a new row
+    with pytest.raises(ValueError, match="Adding rows is not allowed"):
+        df.loc[row_prefix + "-" + str(len(df) + 1)] = list(df.loc[df.index[0]])
+
+
+@pytest.mark.parametrize("row", [0, -1])
+def test_modify_cell(config, row):
+    """test that we can modify a cell, write to it, and check that the changes stick on the remote DB
+
+    we are parameterizing over first and last row because
+    the first row has non-missing data, and the last row has missing data"""
+
+    if config["mock"]:
+        pytest.skip(SKIP_MSG)
+
+    df = DataFrame.from_deeporigin(config["db-name"])
+    row_id = df.index[row]
+
+    # modify
+    salt = np.random.random()
+    df.at[row_id, "float"] = salt
+    df.to_deeporigin()
+
+    df = DataFrame.from_deeporigin(config["db-name"])
+    assert (
+        df.at[row_id, "float"] == salt
+    ), "Failed to successfully modify a cell using the at syntax and write to DB"
