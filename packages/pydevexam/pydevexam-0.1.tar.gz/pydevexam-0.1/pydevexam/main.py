56f113ee27a447fbc6a4010c60c9fdd8d7965e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:07 +0000

    Add package pydevexam/0.1/pydevexam-0.1.tar.gz

---
+++ packages/pydevexam/pydevexam-0.1.tar.gz/pydevexam-0.1/pydevexam/main.py
@@ -0,0 +1,16 @@
+def dev():
+    num = int(input())
+    if(num==1):
+        print('''
+
+this is python package example for devops
+created:-
+Aayush Doshi A014
+Jash Mehta A047
+Rahul Chaudhari A007
+
+''')
+    else:
+        print(
+            "type 1 in input"
+        )
