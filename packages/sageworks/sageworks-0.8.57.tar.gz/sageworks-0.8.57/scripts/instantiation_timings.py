9902182ae44f2d7a9e7ca8ac6e68c57e6f8d86c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:46 +0000

    Add package sageworks/0.8.57/sageworks-0.8.57.tar.gz

---
+++ packages/sageworks/sageworks-0.8.57.tar.gz/sageworks-0.8.57/scripts/instantiation_timings.py
@@ -0,0 +1,14 @@
+import time
+from sageworks.cached.cached_meta import CachedMeta
+from sageworks.cached.cached_model import CachedModel
+
+# Instantiate all the Model objects
+models = CachedMeta().models()
+for model_name in models["Model Group"]:
+    start_time = time.time()
+    model = CachedModel(model_name)
+    print(f"{model_name} instantiation Time: {time.time() - start_time:.3f} seconds")
+
+# Sleep to let's Cache Threads spin down
+print("Cache spindown...")
+time.sleep(5)
