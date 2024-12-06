5532f0514c226ef1a1077432f8142fe43a95ad96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:53 +0000

    Add package SigProfilerAssignment/0.1.9/SigProfilerAssignment-0.1.9-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.1.9-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.1.9'
+version = '0.1.9'
+Update = 'v0.1.9: Replace PdfMerger with PdfReader and PdfWriter for pypdf 5.0.0'
+
+    
+    
