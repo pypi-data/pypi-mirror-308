453a850119ea0c1fbb42525c492383c4e54f2ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:40 +0000

    Add package boltz/0.0.0/boltz-0.0.0.tar.gz

---
+++ packages/boltz/boltz-0.0.0.tar.gz/boltz-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="boltz",
+    version="0.0.0",
+    author="Jeremy Wohlwend",
+    author_email="jeremy.wohlwend@gmail.com",
+    description="Boltz",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
