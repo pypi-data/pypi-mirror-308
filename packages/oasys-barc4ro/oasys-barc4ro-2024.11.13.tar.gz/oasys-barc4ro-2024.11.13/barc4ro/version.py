dd4c8804476e260e8ba8f5298e204b54f8422816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:05:46 +0000

    Add package oasys-barc4ro/2024.11.13/oasys-barc4ro-2024.11.13.tar.gz

---
+++ packages/oasys-barc4ro/oasys-barc4ro-2024.11.13.tar.gz/oasys-barc4ro-2024.11.13/barc4ro/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM barc4ro SETUP.PY
+short_version = '2024.11.13'
+version = '2024.11.13'
+full_version = '2024.11.13.dev0+6ef1e05'
+git_revision = '6ef1e053dce24c0980e668471e2bf13f8b649e04'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
