14e0fbb0513253ee149ce677a8cdb5588f491cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:17 +0000

    Add package cobweb-launcher/1.2.29/cobweb-launcher-1.2.29.tar.gz

---
+++ packages/cobweb-launcher/cobweb-launcher-1.2.29.tar.gz/cobweb-launcher-1.2.29/cobweb/utils/dotting.py
@@ -0,0 +1,28 @@
+import json
+
+from aliyun.log import LogClient, PutLogsRequest, LogItem
+
+from cobweb import setting
+
+
+class LoghubDot:
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client = LogClient(**setting.LOGHUB_CONFIG)
+
+    def build(self, topic, data):
+        log_item = LogItem()
+        for key, value in data.items():
+            if not isinstance(value, str):
+                data[key] = json.dumps(value, ensure_ascii=False)
+        contents = sorted(data.items())
+        log_item.set_contents(contents)
+        request = PutLogsRequest(
+            project="databee-download-log",
+            logstore="cobweb_log",
+            topic=topic,
+            logitems=contents,
+            compress=True
+        )
+        self.client.put_logs(request=request)
