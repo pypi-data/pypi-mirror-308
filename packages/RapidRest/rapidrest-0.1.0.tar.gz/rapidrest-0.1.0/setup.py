62be07aa7c0aa22935d8bb7e548ea98f5d0fc0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:30 +0000

    Add package RapidRest/0.1.0/rapidrest-0.1.0.tar.gz

---
+++ packages/RapidRest/rapidrest-0.1.0.tar.gz/rapidrest-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RapidRest",
+    version="0.1.0",
+    author="shubham chavan",
+    author_email="shubhamchavan7920@gmail.com",
+    description="A Django package to quickly generate serializers and views for Django models",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "Django>=3.2",
+    ],
+    python_requires=">=3.6",
+)
