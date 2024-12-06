46349b18af0d72a12b2ceb79cc517dc44b583ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:05 +0000

    Add package bpcells/0.3.0rc2/bpcells-0.3.0rc2-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/bpcells/bpcells-0.3.0rc2-cp311-cp311-macosx_11_0_arm64.whl/bpcells-cpp/simd/bp128/d1z_maxbits.cpp
@@ -0,0 +1,64 @@
+// Copyright 2023 BPCells contributors
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// Compilation speed is a concern, hence why each bp128 function is split up (better
+// parallelization). This is because 32-step unrolled loop * 32 bitwidths * 6 functions * 6-8
+// architectures = a lot of code When in debug mode AND optimize is turned off, disable dynamic
+// dispatch and compile for just baseline architecture
+#if !defined(NDEBUG) && !defined(__OPTIMIZE__)
+#define HWY_COMPILE_ONLY_STATIC 1
+#endif
+
+#undef HWY_TARGET_INCLUDE
+#define HWY_TARGET_INCLUDE "simd/bp128/d1z_maxbits.cpp"
+#include <hwy/foreach_target.h>
+
+#include "bp128-helper-inl.h"
+#include <hwy/highway.h>
+
+HWY_BEFORE_NAMESPACE();
+
+namespace BPCells::simd::bp128::HWY_NAMESPACE {
+
+// Need to do this to prevent the comma from counting as a macro argument separator
+#define D_SIGNED_DECL Rebind<int32_t, D> d_signed
+BP128_MAXBITS_DECL(
+    maxbits_d1z,
+    // Setup Code
+    D_SIGNED_DECL;
+    Vec prevOffset = Set(d, initvalue);
+    ,
+    // Transform Code (InReg is the input data)
+    {
+        // Delta encode: InReg=[a,b,c,d]; prevOffset=[?,?,?,h]; Output [a-h, b-a, c-b, d-c]
+        const auto tmp = Sub(InReg, CombineShiftRightLanes<3>(d, InReg, prevOffset));
+        // Update offset for next iteration
+        prevOffset = InReg;
+        // Zigzag encode: (i >> 31) ^ (i << 1)    (arithmetic shift right here)
+        // See https://lemire.me/blog/2022/11/25/making-all-your-integers-positive-with-zigzag-encoding/
+        InReg = Xor(BitCast(d, ShiftRight<31>(BitCast(d_signed, tmp))), ShiftLeft<1>(tmp));
+    },
+    // Arguments
+    uint32_t initvalue,
+    const uint32_t *HWY_RESTRICT in
+)
+
+} // namespace BPCells::simd::bp128::HWY_NAMESPACE
+
+HWY_AFTER_NAMESPACE();
+
+#if HWY_ONCE
+namespace BPCells::simd::bp128 {
+
+HWY_EXPORT(maxbits_d1z);
+uint32_t maxbits_d1z(uint32_t initvalue, const uint32_t *HWY_RESTRICT in) {
+    return HWY_DYNAMIC_DISPATCH(maxbits_d1z)(initvalue, in);
+}
+
+} // namespace BPCells::simd::bp128
+#endif
