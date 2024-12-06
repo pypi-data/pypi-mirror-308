6b57c6e0bc3b0b579882cfa1a71a4582acf2720d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:35 +0000

    Add package epregistry/1.1.3/epregistry-1.1.3.tar.gz

---
+++ packages/epregistry/epregistry-1.1.3.tar.gz/epregistry-1.1.3/src/epregistry/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "1.1.3"
+
+from importlib.metadata import EntryPoint
+from epregistry.epregistry import (
+    EntryPointRegistry,
+    ModuleEntryPointRegistry,
+    available_groups,
+    filter_entry_points,
+    search_entry_points,
+    list_distributions,
+)
+
+
+__all__ = [
+    "EntryPoint",
+    "EntryPointRegistry",
+    "ModuleEntryPointRegistry",
+    "available_groups",
+    "filter_entry_points",
+    "search_entry_points",
+    "list_distributions",
+]
