2edc5aaad447b9db6fcb970962f080ee7a128788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:20 +0000

    Add package sherbystats/0.0.26/sherbystats-0.0.26.tar.gz

---
+++ packages/sherbystats/sherbystats-0.0.26.tar.gz/sherbystats-0.0.26/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+
+setuptools.setup(
+    name="sherbystats",
+    version="0.0.26",
+    author="Ryan Gosselin",
+    author_email="ryan.gosselin@usherbrooke.ca",
+    url="https://www.usherbrooke.ca/gchimiquebiotech/departement/professeurs/ryan-gosselin/",
+    packages=["sherbystats"],
+    description="Ryan @ UdeS",
+    long_description="GCB140, GCH755, GCH757:\
+    \n\
+    \nanova\
+    \nbinaire\
+    \nbinaire3D\
+    \ncolorspectra\
+    \ndoe\
+    \ndtw\
+    \ndtw_multi\
+    \nlags\
+    \nlda\
+    \nmlr\
+    \nnormplot\
+    \npca\
+    \npca_trou\
+    \npcr\
+    \npls\
+    \ternaire\
+    \vif\
+    \vip\
+    \nxlsread",
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
