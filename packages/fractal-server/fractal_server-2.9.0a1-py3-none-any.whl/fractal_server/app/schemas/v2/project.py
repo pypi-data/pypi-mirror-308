c308a642f170643ff6108698eee02da4b1696254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:06:24 +0000

    Add package fractal-server/2.9.0a1/fractal_server-2.9.0a1-py3-none-any.whl

---
+++ packages/fractal-server/fractal_server-2.9.0a1-py3-none-any.whl/fractal_server/app/schemas/v2/project.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+from pydantic import Extra
+from pydantic import validator
+
+from .._validators import valstr
+from .._validators import valutc
+
+
+class ProjectCreateV2(BaseModel, extra=Extra.forbid):
+
+    name: str
+    # Validators
+    _name = validator("name", allow_reuse=True)(valstr("name"))
+
+
+class ProjectReadV2(BaseModel):
+
+    id: int
+    name: str
+    timestamp_created: datetime
+    # Validators
+    _timestamp_created = validator("timestamp_created", allow_reuse=True)(
+        valutc("timestamp_created")
+    )
+
+
+class ProjectUpdateV2(BaseModel, extra=Extra.forbid):
+
+    name: Optional[str]
+    # Validators
+    _name = validator("name", allow_reuse=True)(valstr("name"))
