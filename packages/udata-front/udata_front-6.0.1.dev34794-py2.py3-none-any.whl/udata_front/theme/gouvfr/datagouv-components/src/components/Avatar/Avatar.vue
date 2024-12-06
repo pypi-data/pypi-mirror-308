4d3e1c291ea9c3b3542306451c334c92c30ec4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:13 +0000

    Add package udata-front/6.0.1.dev34794/udata_front-6.0.1.dev34794-py2.py3-none-any.whl

---
+++ packages/udata-front/udata_front-6.0.1.dev34794-py2.py3-none-any.whl/udata_front/theme/gouvfr/datagouv-components/src/components/Avatar/Avatar.vue
@@ -0,0 +1,25 @@
+<template>
+  <img
+    :class="{'rounded-circle border border-default-grey': rounded}"
+    :src="avatarUrl"
+    :width="size"
+    :height="size"
+    loading="lazy"
+    alt=""
+  />
+</template>
+
+<script setup lang="ts">
+import useUserAvatar from "../../composables/useUserAvatar";
+import type { User } from "../../types/users";
+type Props = {
+  rounded?: boolean;
+  size?: number;
+  user: User;
+}
+const props = withDefaults(defineProps<Props>(), {
+  rounded: false,
+  size: 40,
+});
+const avatarUrl = useUserAvatar(props.user, props.size);
+</script>
