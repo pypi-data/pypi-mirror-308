848f861ae46fc0ccb32c08ac30b06bf9834f3ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:26 +0000

    Add package bigcodebench/0.2.1.post2/bigcodebench-0.2.1.post2.tar.gz

---
+++ packages/bigcodebench/bigcodebench-0.2.1.post2.tar.gz/bigcodebench-0.2.1.post2/release_docker.sh
@@ -0,0 +1,36 @@
+# argument version
+
+set -eux
+
+while getopts "v:" opt; do
+  case $opt in
+    v)
+      version=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+if [ -z "$version" ]; then
+  echo "version is required"
+  exit 1
+fi
+
+export PYTHONPATH=$PWD pytest tests
+
+docker build -f Docker/Evaluate.Dockerfile . -t bigcodebench/bigcodebench-evaluate:$version
+docker tag bigcodebench/bigcodebench-evaluate:$version bigcodebench/bigcodebench-evaluate:latest
+docker push bigcodebench/bigcodebench-evaluate:$version
+docker push bigcodebench/bigcodebench-evaluate:latest
+
+docker build -f Docker/Generate.Dockerfile . -t bigcodebench/bigcodebench-generate:$version
+docker tag bigcodebench/bigcodebench-generate:$version bigcodebench/bigcodebench-generate:latest
+docker push bigcodebench/bigcodebench-generate:$version
+docker push bigcodebench/bigcodebench-generate:latest
+
+docker build -f Docker/Gradio.Dockerfile . -t bigcodebench/bigcodebench-gradio:$version
+docker tag bigcodebench/bigcodebench-gradio:$version bigcodebench/bigcodebench-gradio:latest
+docker push bigcodebench/bigcodebench-gradio:$version
+docker push bigcodebench/bigcodebench-gradio:latest
