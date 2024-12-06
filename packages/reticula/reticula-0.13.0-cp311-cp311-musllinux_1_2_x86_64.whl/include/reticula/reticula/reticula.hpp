e4eaefeb635a990b04c8a64c2dda09b540842035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:06:26 +0000

    Add package reticula/0.13.0/reticula-0.13.0-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/reticula/reticula-0.13.0-cp311-cp311-musllinux_1_2_x86_64.whl/include/reticula/reticula/reticula.hpp
@@ -0,0 +1,31 @@
+#ifndef INCLUDE_RETICULA_RETICULA_HPP_
+#define INCLUDE_RETICULA_RETICULA_HPP_
+
+/**
+  Complex network analysis toolkit
+ */
+namespace reticula {}
+
+#include "utils.hpp"
+#include "stats.hpp"
+#include "type_traits.hpp"
+#include "intervals.hpp"
+#include "network_concepts.hpp"
+#include "temporal_adjacency.hpp"
+#include "static_edges.hpp"
+#include "static_hyperedges.hpp"
+#include "temporal_edges.hpp"
+#include "temporal_hyperedges.hpp"
+#include "networks.hpp"
+#include "io.hpp"
+#include "components.hpp"
+#include "distributions.hpp"
+#include "random_networks.hpp"
+#include "operations.hpp"
+#include "algorithms.hpp"
+#include "temporal_algorithms.hpp"
+#include "implicit_event_graphs.hpp"
+#include "generators.hpp"
+#include "microcanonical_reference_models.hpp"
+
+#endif  // INCLUDE_RETICULA_RETICULA_HPP_
