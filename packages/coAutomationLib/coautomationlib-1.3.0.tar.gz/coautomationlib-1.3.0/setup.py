bb40e61a3699d3a626a338f1b112d15e0e3bed67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:27:11 +0000

    Add package coAutomationLib/1.3.0/coautomationlib-1.3.0.tar.gz

---
+++ packages/coAutomationLib/coautomationlib-1.3.0.tar.gz/coautomationlib-1.3.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='coAutomationLib',
+    version='1.3.0',
+    description='A Python package to create servicenow change order',
+    url='',
+    author='',
+    author_email='',
+    license='BSD 2-clause',
+    packages=['coAutomationLib'],
+    install_requires=['mpi4py>=2.0',
+                      'numpy',
+                      ],
+
+    classifiers=[
+    ],
+)
