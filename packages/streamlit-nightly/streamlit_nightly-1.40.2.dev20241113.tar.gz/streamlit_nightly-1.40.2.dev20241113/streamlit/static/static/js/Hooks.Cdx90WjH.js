aec786298424a27a7ea105b7c700d4988cdea49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:37 +0000

    Add package streamlit-nightly/1.40.2.dev20241113/streamlit_nightly-1.40.2.dev20241113.tar.gz

---
+++ packages/streamlit-nightly/streamlit_nightly-1.40.2.dev20241113.tar.gz/streamlit_nightly-1.40.2.dev20241113/streamlit/static/static/js/Hooks.Cdx90WjH.js
@@ -0,0 +1,2 @@
+import{r as t}from"./index.BgmMNK46.js";const o=r=>{const e=t.useRef();return t.useEffect(()=>{e.current=r},[r]),e.current};export{o as u};
+//# sourceMappingURL=Hooks.Cdx90WjH.js.map
