d8fa9714237b5f6b8d051c888ff74c8a61c9325b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:06 +0000

    Add package quantio/0.5.0/quantio-0.5.0.tar.gz

---
+++ packages/quantio/quantio-0.5.0.tar.gz/quantio-0.5.0/quantio/__init__.py
@@ -0,0 +1,18 @@
+"""The main quantio package."""
+
+from .exceptions import CanNotAddTypesError, CanNotSubtractTypesError
+from .quantities import Acceleration, Angle, Area, Length, Mass, Time, Velocity
+from .vector import Vector
+
+__all__ = [
+    "Acceleration",
+    "Angle",
+    "Area",
+    "Length",
+    "Mass",
+    "Time",
+    "Velocity",
+    "Vector",
+    "CanNotAddTypesError",
+    "CanNotSubtractTypesError",
+]
