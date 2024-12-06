aaf2bcde0fe0ea376e08641c19864902b64a5e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:45 +0000

    Add package af-practical-astronomy/1.0.5/af_practical_astronomy-1.0.5.tar.gz

---
+++ packages/af-practical-astronomy/af_practical_astronomy-1.0.5.tar.gz/af_practical_astronomy-1.0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="af_practical_astronomy",
+    version="1.0.5",
+    packages=find_packages(include=["practical_astronomy", "practical_astronomy.*"]),
+    install_requires=[
+    'astronomy_types',
+    ],
+    author="Artur Foden",
+    description="A collection of astronomy functions for practical applications",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Arturius771/practical_astronomy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.12.4',  
+)
