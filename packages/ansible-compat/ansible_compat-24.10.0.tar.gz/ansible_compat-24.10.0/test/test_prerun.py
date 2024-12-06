30bd6b8f6a4f1267f306da42c1016c522e73996a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:05:49 +0000

    Add package ansible-compat/24.10.0/ansible_compat-24.10.0.tar.gz

---
+++ packages/ansible-compat/ansible_compat-24.10.0.tar.gz/ansible_compat-24.10.0/test/test_prerun.py
@@ -0,0 +1,12 @@
+"""Tests for ansible_compat.prerun module."""
+
+from pathlib import Path
+
+from ansible_compat.prerun import get_cache_dir
+
+
+def test_get_cache_dir_relative() -> None:
+    """Test behaviors of get_cache_dir."""
+    relative_path = Path()
+    abs_path = relative_path.resolve()
+    assert get_cache_dir(relative_path) == get_cache_dir(abs_path)
