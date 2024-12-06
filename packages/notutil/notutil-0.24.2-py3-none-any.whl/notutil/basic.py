9c4e78bba6c83326a6630228335b7c1eb42267ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:57 +0000

    Add package notutil/0.24.2/notutil-0.24.2-py3-none-any.whl

---
+++ packages/notutil/notutil-0.24.2-py3-none-any.whl/notutil/basic.py
@@ -0,0 +1,12 @@
+from typing import Callable
+
+class on_travel():
+    def __init__(self, *target):
+        self._list = target
+    
+    def __call__(self, func: Callable):
+        def wrapper(start:int = 0):
+            for i, (args) in enumerate(zip(*self._list)):
+                if i >= start:
+                    yield from func(i, *args)
+        return wrapper
