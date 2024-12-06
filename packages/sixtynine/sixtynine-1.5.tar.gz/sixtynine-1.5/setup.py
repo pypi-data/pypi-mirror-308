76fad235020d4f98ef04f18e9260c4c71191d017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:05:45 +0000

    Add package sixtynine/1.5/sixtynine-1.5.tar.gz

---
+++ packages/sixtynine/sixtynine-1.5.tar.gz/sixtynine-1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sixtynine',
+    version='1.5',
+    packages=find_packages(),
+    install_requires=[],
+    package_data={'ipcvlab': ['*.zip', '*.py']},
+)
