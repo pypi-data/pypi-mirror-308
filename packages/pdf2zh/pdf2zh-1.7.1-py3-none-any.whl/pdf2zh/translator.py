0ae017571589f666786af924ff14a33b44f84ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:04 +0000

    Add package pdf2zh/1.7.1/pdf2zh-1.7.1-py3-none-any.whl

---
+++ packages/pdf2zh/pdf2zh-1.7.1-py3-none-any.whl/pdf2zh/translator.py
@@ -0,0 +1,124 @@
+import html
+import re
+import os
+import ollama
+import requests
+import openai
+import deepl
+
+class BaseTranslator:
+    def __init__(self, service, lang_out, lang_in, model):
+        self.service = service
+        self.lang_out = lang_out
+        self.lang_in = lang_in
+        self.model = model
+
+    def translate(self, text):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __str__(self):
+        return f"{self.service} {self.lang_out} {self.lang_in}"
+
+
+class GoogleTranslator(BaseTranslator):
+    def __init__(self, service, lang_out, lang_in, model):
+        lang_out='zh-CN' if lang_out=='auto' else lang_out
+        lang_in='en' if lang_in=='auto' else lang_in
+        super().__init__(service, lang_out, lang_in, model)
+        self.session = requests.Session()
+        self.base_link = "http://translate.google.com/m"
+        self.headers = {
+            "User-Agent": "Mozilla/4.0 (compatible;MSIE 6.0;Windows NT 5.1;SV1;.NET CLR 1.1.4322;.NET CLR 2.0.50727;.NET CLR 3.0.04506.30)"
+        }
+
+    def translate(self, text):
+        text = text[:5000]  # google translate max length
+        response = self.session.get(
+            self.base_link,
+            params={"tl": self.lang_out, "sl": self.lang_in, "q": text},
+            headers=self.headers,
+        )
+        re_result = re.findall(
+            r'(?s)class="(?:t0|result-container)">(.*?)<', response.text
+        )
+        if len(re_result) == 0:
+            raise ValueError("Empty translation result")
+        else:
+            result = html.unescape(re_result[0])
+        return result
+
+
+class DeepLTranslator(BaseTranslator):
+    def __init__(self, service, lang_out, lang_in, model):
+        lang_out='ZH' if lang_out=='auto' else lang_out
+        lang_in='EN' if lang_in=='auto' else lang_in
+        super().__init__(service, lang_out, lang_in, model)
+        self.session = requests.Session()
+        auth_key = os.getenv('DEEPL_AUTH_KEY')
+        server_url = os.getenv('DEEPL_SERVER_URL')
+        self.client = deepl.Translator(auth_key, server_url=server_url)
+
+    def translate(self, text):
+        response = self.client.translate_text(
+            text,
+            target_lang=self.lang_out,
+            source_lang=self.lang_in
+        )
+        return response.text
+
+
+class OllamaTranslator(BaseTranslator):
+    def __init__(self, service, lang_out, lang_in, model):
+        lang_out='zh-CN' if lang_out=='auto' else lang_out
+        lang_in='en' if lang_in=='auto' else lang_in
+        super().__init__(service, lang_out, lang_in, model)
+        self.options = {"temperature": 0}  # 随机采样可能会打断公式标记
+        # OLLAMA_HOST
+        self.client = ollama.Client()
+
+    def translate(self, text):
+        response = self.client.chat(
+            model=self.model,
+            options=self.options,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a professional,authentic machine translation engine.",
+                },
+                {
+                    "role": "user",
+                    "content": f"Translate the following markdown source text to {self.lang_out}. Keep the formula notation $v*$ unchanged. Output translation directly without any additional text.\nSource Text: {text}\nTranslated Text:",
+                },
+            ],
+        )
+        return response["message"]["content"].strip()
+
+class OpenAITranslator(BaseTranslator):
+    def __init__(self, service, lang_out, lang_in, model):
+        lang_out='zh-CN' if lang_out=='auto' else lang_out
+        lang_in='en' if lang_in=='auto' else lang_in
+        super().__init__(service, lang_out, lang_in, model)
+        self.options = {"temperature": 0}  # 随机采样可能会打断公式标记
+        # OPENAI_BASE_URL
+        # OPENAI_API_KEY
+        self.client = openai.OpenAI()
+
+    def translate(self, text):
+        response = self.client.chat.completions.create(
+            model=self.model,
+            **self.options,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a professional,authentic machine translation engine.",
+                },
+                {
+                    "role": "user",
+                    "content": f"Translate the following markdown source text to {self.lang_out}. Keep the formula notation $v*$ unchanged. Output translation directly without any additional text.\nSource Text: {text}\nTranslated Text:",
+                },
+            ],
+        )
+        return response.choices[0].message.content.strip()
