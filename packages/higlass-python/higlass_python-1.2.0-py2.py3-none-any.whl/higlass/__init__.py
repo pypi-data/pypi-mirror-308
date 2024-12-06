04b80b5435114c695805947e18819daed287b061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:05:20 +0000

    Add package higlass-python/1.2.0/higlass_python-1.2.0-py2.py3-none-any.whl

---
+++ packages/higlass-python/higlass_python-1.2.0-py2.py3-none-any.whl/higlass/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("higlass-python")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+
+from higlass_schema import *
+
+import higlass.tilesets
+from higlass._display import renderers
+from higlass.api import *
+from higlass.fuse import fuse
+from higlass.server import HiGlassServer, _create_tileset_helper
+from higlass.tilesets import remote
+
+server = HiGlassServer()
+
+bigwig = _create_tileset_helper(server, higlass.tilesets.bigwig)
+multivec = _create_tileset_helper(server, higlass.tilesets.multivec)
+cooler = _create_tileset_helper(server, higlass.tilesets.cooler)
+hitile = _create_tileset_helper(server, higlass.tilesets.hitile)
+bed2ddb = _create_tileset_helper(server, higlass.tilesets.bed2ddb)
+beddb = _create_tileset_helper(server, higlass.tilesets.beddb)
