cfce8913dbf54834979f1f6666640706d56d7aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:47 +0000

    Add package kcweb/6.4.1/kcweb-6.4.1.tar.gz

---
+++ packages/kcweb/kcweb-6.4.1.tar.gz/kcweb-6.4.1/kcweb/kcweb.py
@@ -0,0 +1,5 @@
+from .common import *
+from kcw import kcw
+def cill_start(fdgrsgrsegsrsgrsbsdbftbrsbfdrtrtbdfsrsgr='kcweb'):
+    "脚本入口"
+    return kcw.cill_start(fdgrsgrsegsrsgrsbsdbftbrsbfdrtrtbdfsrsgr=fdgrsgrsegsrsgrsbsdbftbrsbfdrtrtbdfsrsgr)
