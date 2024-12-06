2b304ab9e0792727a940e62e07d571b1d1c9e37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:03 +0000

    Add package ATACS/0.1.1/atacs-0.1.1.tar.gz

---
+++ packages/ATACS/atacs-0.1.1.tar.gz/atacs-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ATACS',
+    version='0.1.1',
+    author='Chirag Bhatia',
+    author_email='chiragbhatia2002@yahoo.com',
+    description='A Python package for financial analysis including RSI, MACD, and statistical insights.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'scipy',
+        'numpy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
