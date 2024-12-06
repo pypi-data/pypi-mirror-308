1d025d98524cea09ea396c3bc10f1929eb7185ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:32 +0000

    Add package changelist-init/0.1.1/changelist_init-0.1.1-py3-none-any.whl

---
+++ packages/changelist-init/changelist_init-0.1.1-py3-none-any.whl/changelist_init/git/__init__.py
@@ -0,0 +1,15 @@
+""" Git Management Package.
+"""
+from changelist_init.git.git_status_lists import GitStatusLists
+from changelist_init.git import status_runner, status_reader
+
+
+def get_status_lists() -> GitStatusLists:
+    """ Executes the Complete Git Status into File Change Operation.
+
+    Returns:
+    list[FileChange] - The List of FileChange information from Git Status.
+    """
+    return status_reader.read_git_status_output(
+        status_runner.run_git_status()
+    )
