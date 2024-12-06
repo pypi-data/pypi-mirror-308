8430b17db29eb9c4563538cf2184f6dd487f2387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:14 +0000

    Add package botterino/0.5.8/botterino-0.5.8.tar.gz

---
+++ packages/botterino/botterino-0.5.8.tar.gz/botterino-0.5.8/botterino/Loader/loader.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from ..config import roundfile, archivefile, hintfile
+import ruamel.yaml
+
+yaml = ruamel.yaml.YAML()
+yaml.preserve_quotes = True
+yaml.allow_duplicate_keys = True
+
+
+def dump(data, file):
+    with open(file, "w", encoding="utf-8") as f:
+        yaml.dump(data, f)
+
+
+def append(data, file):
+    with open(file, "r", encoding="utf-8") as f:
+        x = yaml.load(f)
+    if not x:
+        dump(data, file)
+        return
+    x.update(data)
+    dump(x, file)
+
+
+def load(file):
+    with open(file, "r", encoding="utf-8") as f:
+        return yaml.load(f)
+
+
+def getRound():
+    x = load(roundfile)
+    if not x:
+        return None
+    k = next(iter(x))
+    top = x.pop(k)
+    if x:
+        dump(x, roundfile)
+    else:
+        open(roundfile, "w", encoding="utf-8").close()
+    y = load(archivefile) or {}
+    y[k] = top
+    dump(y, archivefile)
+    return k, top
+
+def loadHints(key):
+    with open(hintfile, "r", encoding="utf-8") as f:
+        hints_data = yaml.load(f)
+    return (hints_data or {}).get(key, {}).get("hints", [])
---
+++ packages/botterino/botterino-0.5.8.tar.gz/botterino-0.5.8/botterino/__init__.py
@@ -0,0 +1,43 @@
+import os
+import json
+from pathlib import Path
+from configparser import ConfigParser
+
+
+class files:
+    def __init__(self):
+        self.home = os.path.expanduser("~")
+        self.botconfig = os.path.join(self.home, "botterino-config")
+        self.configfile = os.path.join(self.botconfig, "config.ini")
+        self.roundsdir = os.path.join(self.botconfig, "rounds")
+        self.rounds = os.path.join(self.roundsdir, "rounds.yaml")
+        self.archive = os.path.join(self.roundsdir, "archive.yaml")
+        self.prawconfig = os.path.join(self.botconfig, "praw.ini")
+        self.hintfile = os.path.join(self.botconfig, "hints.yaml")
+
+
+botfiles = files()
+dirs = [botfiles.botconfig, botfiles.roundsdir]
+files = [botfiles.rounds, botfiles.archive, botfiles.prawconfig, botfiles.hintfile]
+for d in dirs:
+    Path(d).mkdir(parents=True, exist_ok=True)
+for f in files:
+    with open(f, "a+"):
+        pass
+
+if not os.path.exists(botfiles.configfile):
+    parser = ConfigParser()
+    parser.add_section("config")
+    parser.set("config", "correct_message", "+correct")
+    parser.set("config", "incorrect_message", "❌")
+    parser.set("config", "hints", "[25,45]")
+    with open(botfiles.configfile, "w", encoding="utf-8") as f:
+        parser.write(f)
+
+parser = ConfigParser()
+parser.read(botfiles.configfile, encoding="utf-8")
+correctMessage, incorrectMessage = (
+    parser["config"]["correct_message"],
+    parser["config"]["incorrect_message"],
+)
+hints = json.loads(parser["config"].get("hints", "[]"))
