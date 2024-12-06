bfd644e1e3b3ffd7b85cb0a395373726444fb370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:31 +0000

    Add package staticat/0.0.5/staticat-0.0.5-py3-none-any.whl

---
+++ packages/staticat/staticat-0.0.5-py3-none-any.whl/staticat/__init__.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import pydantic
+
+from .lib import Catalog
+
+
+class ConfigTOML(pydantic.BaseModel):
+    directory: Path
+    catalog_template: Path | None = None
+    dataset_template: Path | None = None
+    convert_excel: bool = True
+
+
+def staticat(config):
+    catalog = Catalog(config)
+    catalog.process()
