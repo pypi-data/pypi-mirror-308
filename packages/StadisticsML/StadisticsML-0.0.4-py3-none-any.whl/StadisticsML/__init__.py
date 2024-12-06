e3ed5a3a778906b14fc21d8b24f1ee9bf01d5651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:17 +0000

    Add package StadisticsML/0.0.4/StadisticsML-0.0.4-py3-none-any.whl

---
+++ packages/StadisticsML/StadisticsML-0.0.4-py3-none-any.whl/StadisticsML/__init__.py
@@ -0,0 +1,6 @@
+from .Funciones import detec_outliers 
+from .Funciones import cal_random_probability
+from .Funciones import durbin_watson
+from .Funciones import calculate_dL
+from .Funciones import calculate_dU
+from .Funciones import train_neural_network
