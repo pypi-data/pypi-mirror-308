9309c85d4a25b3f1451c2c501dd7d2f5fa1e3ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:29 +0000

    Add package gensec/0.0.4/gensec-0.0.4.tar.gz

---
+++ packages/gensec/gensec-0.0.4.tar.gz/gensec-0.0.4/README.md
@@ -0,0 +1,3 @@
+# gensec
+
+gensec is a command-line tool designed to simplify the process of generating scure configuration files such as Dockerfiles and Helm charts. The CLI prompts users for input as needed and includes interactive options to customize configurations effectively.
---
+++ packages/gensec/gensec-0.0.4.tar.gz/gensec-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gensec",
+    version="0.0.4",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A Python package for generating secure docker and k8s resources",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "click", 
+        "InquirerPy",
+    ],
+    entry_points={
+        "console_scripts": [
+            "gensec=gensec.main:main",
+        ],
+    },
+)
