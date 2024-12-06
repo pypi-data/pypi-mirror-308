ab319147e25fcf4fec9aec22dd786df8dfaad84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:06:34 +0000

    Add package odoo12-addon-account-move-payroll-import/12.0.1.1.2/odoo12_addon_account_move_payroll_import-12.0.1.1.2-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-account-move-payroll-import/odoo12_addon_account_move_payroll_import-12.0.1.1.2-py2.py3-none-any.whl/odoo/addons/account_move_payroll_import/utils/file_utils.py
@@ -0,0 +1,31 @@
+import os
+import base64
+
+
+EXTENSIONS = [".csv", ".xls", ".xlsx"]
+
+
+def get_file_extension(file_name):
+    """
+    Get the file extension from the file name.
+    """
+    if file_name:
+        return os.path.splitext(file_name)[-1]
+    return False
+
+
+def decode_file(file_contents):
+    """
+    Decode the file content.
+    """
+    return base64.b64decode(file_contents)
+
+
+def is_valid_extension(file_name):
+    """
+    Get the valid file extension.
+    """
+    ext = get_file_extension(file_name)
+    if ext not in EXTENSIONS:
+        return False
+    return ext[1:]
