e800e1d501f704c5430c1388a75d9587c536ab68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:23 +0000

    Add package carta-iam-sdk/0.0.1.dev1731606183/carta-iam-sdk-0.0.1.dev1731606183.tar.gz

---
+++ packages/carta-iam-sdk/carta-iam-sdk-0.0.1.dev1731606183.tar.gz/carta-iam-sdk-0.0.1.dev1731606183/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='carta-iam-sdk',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
