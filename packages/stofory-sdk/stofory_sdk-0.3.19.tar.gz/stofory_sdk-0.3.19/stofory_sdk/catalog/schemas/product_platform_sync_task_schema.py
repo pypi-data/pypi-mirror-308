f73906483349768f1480bc935741f3e27d047728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:34 +0000

    Add package stofory-sdk/0.3.19/stofory_sdk-0.3.19.tar.gz

---
+++ packages/stofory-sdk/stofory_sdk-0.3.19.tar.gz/stofory_sdk-0.3.19/stofory_sdk/catalog/schemas/product_platform_sync_task_schema.py
@@ -0,0 +1,121 @@
+from datetime import datetime
+from typing import Annotated
+
+from litestar.params import Parameter
+from msgspec import Struct
+
+from stofory_sdk.catalog.models.enums import SyncTaskStatus, SyncTaskType
+from stofory_sdk.catalog.schemas import ProductPlatformResponse
+
+
+class TaskMetaChange(Struct):
+    table_name: str
+    id: int
+    operation: SyncTaskType
+
+
+class TaskMeta(Struct):
+    product_platform: ProductPlatformResponse
+    changes: list[TaskMetaChange]
+
+
+class ProductPlatformSyncTaskCreateRequest(Struct, forbid_unknown_fields=True):
+    product_platform_id: Annotated[int, Parameter(title="Product Platform ID")]
+    task_type: Annotated[
+        SyncTaskType,
+        Parameter(
+            title="Task Type",
+            description="The task type."
+        )
+    ]
+    task_meta: Annotated[
+        TaskMeta,
+        Parameter(
+            required=False,
+            title="Task Meta",
+            description="The task meta."
+        )
+    ]
+    status: Annotated[
+        SyncTaskStatus,
+        Parameter(
+            title="Status",
+            description="The status."
+        )
+    ]
+    info: Annotated[
+        str | None,
+        Parameter(
+            required=False,
+            title="Info",
+            description="The info."
+        )
+    ] = None
+
+
+class ProductPlatformSyncTaskUpdateRequest(Struct, forbid_unknown_fields=True):
+    task_type: Annotated[
+        SyncTaskType,
+        Parameter(
+            title="Task Type",
+            description="The task type."
+        )
+    ]
+    task_meta: Annotated[
+        TaskMeta,
+        Parameter(
+            title="Task Meta",
+            description="The task meta."
+        )
+    ]
+    status: Annotated[
+        SyncTaskStatus,
+        Parameter(
+            title="Status",
+            description="The status."
+        )
+    ]
+    info: Annotated[
+        str | None,
+        Parameter(
+            title="Info",
+            description="The info."
+        )
+    ]
+
+
+class ProductPlatformSyncTaskResponse(Struct):
+    id: Annotated[int, Parameter(title="ID")]
+    product_platform_id: Annotated[int, Parameter(title="Product Platform ID")]
+    task_type: Annotated[
+        SyncTaskType,
+        Parameter(
+            title="Task Type",
+            description="The task type."
+        )
+    ]
+    task_meta: Annotated[
+        TaskMeta,
+        Parameter(
+            title="Task Meta",
+            description="The task meta."
+        )
+    ]
+    status: Annotated[
+        SyncTaskStatus,
+        Parameter(
+            title="Status",
+            description="The status."
+        )
+    ]
+    info: Annotated[
+        str | None,
+        Parameter(
+            required=False,
+            title="Info",
+            description="The info."
+        )
+    ]
+
+    created_at: Annotated[datetime, Parameter(title="Created At")]
+    updated_at: Annotated[datetime, Parameter(title="Updated At")]
