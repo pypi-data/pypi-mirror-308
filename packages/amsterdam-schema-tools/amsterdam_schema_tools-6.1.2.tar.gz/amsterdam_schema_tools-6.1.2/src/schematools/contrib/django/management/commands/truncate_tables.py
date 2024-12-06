a65dd32070af57861a35a54e4d2e2feb69af2212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:39 +0000

    Add package amsterdam-schema-tools/6.1.2/amsterdam_schema_tools-6.1.2.tar.gz

---
+++ packages/amsterdam-schema-tools/amsterdam_schema_tools-6.1.2.tar.gz/amsterdam_schema_tools-6.1.2/src/schematools/contrib/django/management/commands/truncate_tables.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from django.db import connection
+
+from . import BaseDatasetCommand
+
+
+class Command(BaseDatasetCommand):  # noqa: D101
+    help = "Truncates the tables of a given dataset."
+    requires_system_checks = []  # don't test URLs (which create models)
+
+    def handle(self, *args, **options):  # noqa: D102
+        db_tables = []
+        for dataset in self.get_datasets(options, enable_db=True, default_all=False):
+            db_tables.extend(
+                table.db_name
+                for table in dataset.schema.get_tables(include_nested=True, include_through=True)
+            )
+
+        with connection.cursor() as cursor:
+            for db_table in sorted(db_tables):
+                self.stdout.write(f"Truncating {db_table}")
+                cursor.execute(f"TRUNCATE {db_table}")
