3e686f686a846aa26cd2e8974b6a3fe490c35e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:49 +0000

    Add package hdate/0.11.1/hdate-0.11.1.tar.gz

---
+++ packages/hdate/hdate-0.11.1.tar.gz/hdate-0.11.1/tests/test_hdate_api.py
@@ -0,0 +1,218 @@
+"""
+These tests are based on the API calls made to hdate by homeassistant (and
+maybe other apps in the future).
+"""
+
+import sys
+from datetime import date, datetime, tzinfo
+from typing import cast
+
+from _pytest.capture import CaptureFixture
+
+from hdate import HDate, Location, Zmanim
+
+_ASTRAL = "astral" in sys.modules
+
+
+class TestHDateAPI:
+    """Test the HDate API provided in the README."""
+
+    def test_readme_example_english(self, capsys: CaptureFixture[str]) -> None:
+        """Test the README example in English."""
+
+        test_date = date(2016, 4, 18)
+        hdate = HDate(test_date, hebrew=False)
+        print(hdate)
+        captured = capsys.readouterr()
+        assert captured.out == "Monday 10 Nisan 5776\n"
+
+    def test_readme_example_hebrew(self, capsys: CaptureFixture[str]) -> None:
+        """Test the README example in Hebrew."""
+        test_date = date(2016, 4, 26)
+        hdate = HDate(test_date, hebrew=True)
+        print(hdate)
+        captured = capsys.readouterr()
+        assert captured.out == "יום שלישי י\"ח בניסן ה' תשע\"ו ג' בעומר חול המועד פסח\n"
+
+    def test_get_hebrew_date(self) -> None:
+        """Print the hebrew date."""
+        test_date = datetime(2018, 11, 2)
+        assert HDate(test_date).hebrew_date == 'כ"ד מרחשוון ה\' תשע"ט'
+        assert HDate(test_date, hebrew=False).hebrew_date == "24 Marcheshvan 5779"
+
+    def test_get_upcoming_parasha(self) -> None:
+        """Check that the upcoming parasha is correct."""
+        test_date = datetime(2018, 11, 2)
+        assert HDate(test_date).parasha == "חיי שרה"
+        assert HDate(test_date, hebrew=False).parasha == "Chayei Sara"
+
+    def test_get_upcoming_parasha_vezot_habracha(self) -> None:
+        """Check that the upcoming parasha is correct for vezot habracha."""
+        test_date = datetime(2018, 9, 30)
+        assert HDate(test_date).parasha == "וזאת הברכה"
+        assert HDate(test_date, hebrew=False).parasha == "Vezot Habracha"
+
+    def test_get_holiday_description(self) -> None:
+        """Check that the holiday description is correct."""
+        test_date = datetime(2018, 12, 3)
+        assert HDate(test_date).holiday_description == "חנוכה"
+        assert HDate(test_date, hebrew=False).holiday_description == "Chanukah"
+
+
+class TestZmanimAPI:
+    """Test the API provided in the README."""
+
+    def test_readme_example_hebrew(self, capsys: CaptureFixture[str]) -> None:
+        """Test for hebrew."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=date(2016, 4, 18), location=coord)
+        print(zman)
+        captured = capsys.readouterr()
+        if not _ASTRAL:
+            return
+        assert (
+            captured.out == "עלות השחר - 04:52:00\n"
+            "זמן טלית ותפילין - 05:18:00\n"
+            "הנץ החמה - 06:08:00\n"
+            'סוף זמן ק"ש מג"א - 08:46:00\n'
+            'סוף זמן ק"ש גר"א - 09:23:00\n'
+            'סוף זמן תפילה מג"א - 10:04:00\n'
+            'סוף זמן תפילה גר"א - 10:28:00\n'
+            "חצות היום - 12:40:00\n"
+            "מנחה גדולה - 13:10:30\n"
+            "מנחה קטנה - 16:25:30\n"
+            "פלג המנחה - 17:50:45\n"
+            "שקיעה - 19:12:00\n"
+            "צאת הכוכבים - 19:38:00\n"
+            "חצות הלילה - 00:40:00\n"
+        )
+
+    def test_readme_example_english(self, capsys: CaptureFixture[str]) -> None:
+        """Test for english."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=date(2016, 4, 18), location=coord, hebrew=False)
+        print(zman)
+        captured = capsys.readouterr()
+        if not _ASTRAL:
+            return
+        assert (
+            captured.out == "Alot HaShachar - 04:52:00\n"
+            "Talit & Tefilin's time - 05:18:00\n"
+            "Sunrise - 06:08:00\n"
+            'Shema EOT MG"A - 08:46:00\n'
+            'Shema EOT GR"A - 09:23:00\n'
+            'Tefila EOT MG"A - 10:04:00\n'
+            'Tefila EOT GR"A - 10:28:00\n'
+            "Midday - 12:40:00\n"
+            "Big Mincha - 13:10:30\n"
+            "Small Mincha - 16:25:30\n"
+            "Plag Mincha - 17:50:45\n"
+            "Sunset - 19:12:00\n"
+            "First stars - 19:38:00\n"
+            "Midnight - 00:40:00\n"
+        )
+
+    def test_issur_melacha_weekday(self) -> None:
+        """Test for issur melacha on a weekday."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=date(2018, 11, 12), location=coord)
+        assert not zman.issur_melacha_in_effect
+
+    def test_issur_melacha_shabbat_morning(self) -> None:
+        """Test for issur melacha on shabbat morning."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=datetime(2018, 11, 10, 9), location=coord)
+        assert zman.issur_melacha_in_effect
+
+    def test_issur_melacha_friday_morning(self) -> None:
+        """Test for issur melacha on friday morning."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=datetime(2018, 11, 9, 9, 45), location=coord)
+        assert not zman.issur_melacha_in_effect
+
+    def test_issur_melacha_friday_evening(self) -> None:
+        """Test for issur melacha on friday evening."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=datetime(2018, 11, 9, 16, 45), location=coord)
+        assert zman.issur_melacha_in_effect
+
+    def test_issur_melacha_motsaei_shabbat(self) -> None:
+        """Test for issur melacha on Motsaei shabbat."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=datetime(2018, 11, 10, 17, 45), location=coord)
+        assert not zman.issur_melacha_in_effect
+
+    def test_issur_melacha_shavuot_morning(self) -> None:
+        """Test for issur melacha on shavuot morning."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=datetime(2019, 6, 9, 9), location=coord)
+        assert zman.issur_melacha_in_effect
+
+    def test_issur_melacha_pesach_vi_morning(self) -> None:
+        """Test for issur melacha on erev shvii shel pesach morning."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=datetime(2019, 4, 25, 9, 45), location=coord)
+        assert not zman.issur_melacha_in_effect
+
+    def test_issur_melacha_shavuot_evening(self) -> None:
+        """Test for issur melacha on shavuot evening."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=datetime(2019, 6, 8, 21, 45), location=coord)
+        assert zman.issur_melacha_in_effect
+
+    def test_issur_melacha_motsaei_shavuot(self) -> None:
+        """Test for issur melacha on motsaei shavuot."""
+        coord = Location("פתח תקוה", 32.08707, 34.88747, "Asia/Jerusalem", 54)
+        zman = Zmanim(date=datetime(2019, 6, 9, 20, 30), location=coord)
+        assert not zman.issur_melacha_in_effect
+
+    def test_issur_melacha_pesach_ii_morning(self) -> None:
+        """Test for issur melacha on the second day of pesach in the diaspora."""
+        coord = Location(
+            name="New York",
+            latitude=40.7128,
+            longitude=-74.0060,
+            timezone="America/New_York",
+            diaspora=True,
+        )
+        zman = Zmanim(date=datetime(2019, 4, 21, 9), location=coord)
+        assert zman.issur_melacha_in_effect
+
+    def test_issur_melacha_pesach_ii_evening(self) -> None:
+        """Test for issur melacha on the eve of second day of pesach in the diaspora."""
+        coord = Location(
+            name="New York",
+            latitude=40.7128,
+            longitude=-74.0060,
+            timezone="America/New_York",
+            diaspora=True,
+        )
+        zman = Zmanim(date=datetime(2019, 4, 20, 21, 45), location=coord)
+        assert zman.issur_melacha_in_effect
+
+    def test_issur_melacha_motsaei_pesach_ii(self) -> None:
+        """Test for issur melacha on the end of second day of pesach in the diaspora."""
+        coord = Location(
+            name="New York",
+            latitude=40.7128,
+            longitude=-74.0060,
+            timezone="America/New_York",
+            diaspora=True,
+        )
+        zman = Zmanim(date=datetime(2019, 4, 21, 20, 30), location=coord)
+        assert not zman.issur_melacha_in_effect
+
+    def test_zmanim_localized_datetime(self) -> None:
+        """Test for issur melacha if datetime is localized."""
+        coord = Location(
+            name="New York",
+            latitude=40.7128,
+            longitude=-74.0060,
+            timezone="America/New_York",
+            diaspora=True,
+        )
+        _timezone = cast(tzinfo, coord.timezone)
+        zman = Zmanim(
+            date=datetime(2019, 4, 21, 20, 30, tzinfo=_timezone), location=coord
+        )
+        assert not zman.issur_melacha_in_effect
---
+++ packages/hdate/hdate-0.11.1.tar.gz/hdate-0.11.1/tests/test_zmanim.py
@@ -0,0 +1,315 @@
+"""Test Zmanim objects."""
+
+import datetime
+import random
+import sys
+from calendar import isleap
+from datetime import datetime as dt
+from datetime import timedelta as td
+from typing import Optional
+from zoneinfo import ZoneInfo
+
+import pytest
+
+from hdate import Zmanim
+from hdate.location import Location
+
+_ASTRAL = "astral" in sys.modules
+
+NYC_LAT = 40.7128
+NYC_LNG = -74.0060
+
+LONDON_LAT = 51.5074
+LONDON_LNG = -0.1278
+
+PUNTA_ARENAS_LAT = -53.1678  # Southern example
+PUNTA_ARENAS_LNG = -70.9167
+
+
+def compare_dates(
+    date1: Optional[datetime.datetime],
+    date2: Optional[datetime.datetime],
+    allow_grace: bool = False,
+) -> None:
+    """Compare 2 dates to be more or less equal."""
+    if not (date1 or date2):
+        assert date1 == date2
+    else:
+        grace = td(minutes=5 if (not _ASTRAL or allow_grace) else 0)
+        assert date1 is not None
+        assert date2 is not None
+        assert date1 - grace <= date2 <= date1 + grace
+
+
+def compare_times(
+    time1: datetime.time, time2: datetime.time, allow_grace: bool = False
+) -> None:
+    """Compare times to be equal."""
+    compare_dates(
+        dt.combine(dt.today(), time1), dt.combine(dt.today(), time2), allow_grace
+    )
+
+
+class TestZmanim:
+    """Zmanim tests"""
+
+    def test_bad_date(self) -> None:
+        """Check that a bad value argument to zmanim raises an error"""
+        with pytest.raises(TypeError):
+            Zmanim(date="bad value")
+
+    @pytest.mark.parametrize("execution_number", list(range(5)))
+    def test_same_doy_is_equal(
+        self, execution_number: int, random_date: datetime.date
+    ) -> None:
+        """Test two doy to be equal."""
+        print(f"Run number {execution_number}")
+        other_year = random.randint(500, 3000)
+        shift_day = datetime.timedelta(days=0)
+        this_date = random_date
+
+        if isleap(this_date.year) != isleap(other_year) and this_date > datetime.date(
+            this_date.year, 2, 28
+        ):
+            if isleap(other_year):
+                shift_day = datetime.timedelta(days=-1)
+            else:
+                shift_day = datetime.timedelta(days=1)
+
+        if (
+            isleap(this_date.year)
+            and not isleap(other_year)
+            and this_date.month == 2
+            and this_date.day == 29
+        ):
+            # Special case we can't simply replace the year as there's
+            # no leap day in the other year
+            other_date = datetime.date(other_year, 3, 1)
+        else:
+            other_date = this_date.replace(year=other_year) + shift_day
+
+        this_zmanim = Zmanim(this_date).get_utc_sun_time_full()
+        other_zmanim = Zmanim(other_date).get_utc_sun_time_full()
+        grace = 0 if not _ASTRAL else 14
+        for key, value in this_zmanim.items():
+            assert value - grace <= other_zmanim[key] <= value + grace, key
+
+    def test_extreme_zmanim(self) -> None:
+        """Test that Zmanim north to 50 degrees latitude is correct."""
+        day = datetime.date(2024, 6, 18)
+        compare_times(
+            Zmanim(
+                date=day,
+                location=Location(
+                    name="London",
+                    latitude=LONDON_LAT,
+                    longitude=LONDON_LNG,
+                    timezone="Europe/London",
+                    diaspora=True,
+                ),
+            )
+            .zmanim["sunset"]
+            .time(),
+            datetime.time(21, 22),
+            allow_grace=True,
+        )
+        compare_times(
+            Zmanim(
+                date=day,
+                location=Location(
+                    name="Punta Arenas",
+                    latitude=PUNTA_ARENAS_LAT,
+                    longitude=PUNTA_ARENAS_LNG,
+                    timezone="America/Punta_Arenas",
+                    diaspora=True,
+                ),
+            )
+            .zmanim["sunset"]
+            .time(),
+            datetime.time(17, 31),
+            allow_grace=True,
+        )
+
+    def test_using_tzinfo(self) -> None:
+        """Test tzinfo to be correct."""
+        day = datetime.date(2018, 9, 8)
+        timezone_str = "America/New_York"
+        timezone = ZoneInfo(timezone_str)
+        location_tz_str = Location(
+            name="New York",
+            latitude=NYC_LAT,
+            longitude=NYC_LNG,
+            timezone=timezone_str,
+            diaspora=True,
+        )
+        location = Location(
+            name="New York",
+            latitude=NYC_LAT,
+            longitude=NYC_LNG,
+            timezone=timezone,
+            diaspora=True,
+        )
+
+        compare_times(
+            Zmanim(date=day, location=location_tz_str).zmanim["first_stars"].time(),
+            datetime.time(19, 45),
+        )
+
+        compare_times(
+            Zmanim(date=day, location=location).zmanim["first_stars"].time(),
+            datetime.time(19, 45),
+        )
+
+    # Times are assumed for NYC.
+    CANDLES_TEST = [
+        (dt(2018, 9, 7, 13, 1), 18, dt(2018, 9, 7, 19, 0), False),
+        (dt(2018, 9, 7, 19, 4), 18, dt(2018, 9, 7, 19, 0), True),
+        (dt(2018, 9, 8, 13, 1), 18, None, True),
+        (dt(2018, 9, 19, 22, 1), 18, None, False),
+        (dt(2018, 9, 9, 16, 1), 20, dt(2018, 9, 9, 18, 55), False),
+        (dt(2018, 9, 9, 19, 30), 18, dt(2018, 9, 9, 18, 57), True),
+        # Candle lighting matches the time that would be havdalah.
+        (dt(2018, 9, 10, 8, 1), 18, dt(2018, 9, 10, 19, 55), True),
+        (dt(2018, 9, 10, 20, 20), 18, dt(2018, 9, 10, 19, 55), True),
+    ]
+
+    @pytest.mark.parametrize(
+        ["now", "offset", "candle_lighting", "melacha_assur"], CANDLES_TEST
+    )
+    def test_candle_lighting(
+        self,
+        now: datetime.datetime,
+        offset: int,
+        candle_lighting: Optional[datetime.datetime],
+        melacha_assur: bool,
+    ) -> None:
+        """Test candle lighting values."""
+        location_tz_str = Location(
+            name="New York",
+            latitude=NYC_LAT,
+            longitude=NYC_LNG,
+            timezone="America/New_York",
+            diaspora=True,
+        )
+        # Use a constant offset for Havdalah for unit test stability.
+        zmanim = Zmanim(
+            date=now,
+            location=location_tz_str,
+            candle_lighting_offset=offset,
+            havdalah_offset=42,
+        )
+        actual = zmanim.candle_lighting
+        if actual is not None:
+            actual = actual.replace(tzinfo=None)
+        compare_dates(actual, candle_lighting)
+        assert zmanim.issur_melacha_in_effect == melacha_assur
+
+    # Times are assumed for NYC.
+    HAVDALAH_TEST = [
+        (dt(2018, 9, 7, 13, 1), 42, None, False),
+        (dt(2018, 9, 7, 20, 1), 42, None, True),
+        (dt(2018, 9, 8, 13, 1), 42, dt(2018, 9, 8, 19, 59), True),
+        (dt(2018, 9, 8, 13, 1), 0, dt(2018, 9, 8, 19, 58), True),
+        (dt(2018, 9, 19, 22, 1), 18, dt(2018, 9, 19, 19, 16), False),
+        (dt(2018, 9, 9, 16, 1), 0, None, False),
+        (dt(2018, 9, 9, 19, 30), 0, None, True),
+        (dt(2018, 9, 11, 16, 1), 0, dt(2018, 9, 11, 19, 53), True),
+        # No havdalah in the middle of Yom Tov.
+        (dt(2018, 9, 10, 8, 1), 0, None, True),
+        (dt(2018, 9, 10, 20, 20), 0, None, True),
+    ]
+
+    @pytest.mark.parametrize(
+        ["now", "offset", "havdalah", "melacha_assur"], HAVDALAH_TEST
+    )
+    def test_havdalah(
+        self,
+        now: datetime.datetime,
+        offset: int,
+        havdalah: Optional[datetime.datetime],
+        melacha_assur: bool,
+    ) -> None:
+        """Test havdalah times."""
+        location_tz_str = Location(
+            name="New York",
+            latitude=NYC_LAT,
+            longitude=NYC_LNG,
+            timezone="America/New_York",
+            diaspora=True,
+        )
+        # Use a constant offset for Havdalah for unit test stability.
+        zmanim = Zmanim(date=now, location=location_tz_str, havdalah_offset=offset)
+        actual = zmanim.havdalah
+        if actual is not None:
+            actual = actual.replace(tzinfo=None)
+        compare_dates(actual, havdalah)
+        assert zmanim.issur_melacha_in_effect == melacha_assur
+
+    # Times are assumed for NYC.
+    MOTZEI_SHABBAT_CHAG_TEST = [
+        (dt(2018, 9, 7, 13, 1), 42, False),
+        (dt(2018, 9, 7, 20, 1), 42, False),
+        (dt(2018, 9, 8, 13, 1), 42, False),
+        (dt(2018, 9, 8, 22, 1), 0, True),
+        (dt(2018, 9, 9, 16, 1), 0, False),
+        (dt(2018, 9, 9, 19, 30), 0, False),
+        (dt(2018, 9, 10, 16, 1), 0, False),
+        (dt(2018, 9, 10, 19, 30), 0, False),
+        (dt(2018, 9, 11, 16, 1), 0, False),
+        (dt(2018, 9, 11, 20, 1), 0, True),
+        (dt(2018, 9, 19, 22, 1), 18, True),
+    ]
+
+    @pytest.mark.parametrize(
+        ["now", "offset", "motzei_shabbat_chag"], MOTZEI_SHABBAT_CHAG_TEST
+    )
+    def test_motzei_shabbat_chag(
+        self, now: datetime.datetime, offset: int, motzei_shabbat_chag: bool
+    ) -> None:
+        """Test motzei shabbat chag boolean is correct."""
+        location_tz_str = Location(
+            name="New York",
+            latitude=NYC_LAT,
+            longitude=NYC_LNG,
+            timezone="America/New_York",
+            diaspora=True,
+        )
+        # Use a constant offset for Havdalah for unit test stability.
+        zmanim = Zmanim(date=now, location=location_tz_str, havdalah_offset=offset)
+        assert zmanim.motzei_shabbat_chag == motzei_shabbat_chag
+
+    # Times are assumed for NYC.
+    EREV_SHABBAT_CHAG_TEST = [
+        (dt(2018, 9, 7, 13, 1), 42, True),  # fri
+        (dt(2018, 9, 7, 20, 1), 42, False),
+        (dt(2018, 9, 8, 13, 1), 42, False),  # sat
+        (dt(2018, 9, 8, 20, 1), 0, False),
+        (dt(2018, 9, 9, 16, 1), 0, True),  # erev rosh hashana
+        (dt(2018, 9, 9, 19, 30), 0, False),
+        (dt(2018, 9, 10, 16, 1), 0, False),  # rosh hashana I
+        (dt(2018, 9, 10, 19, 30), 0, False),
+        (dt(2018, 9, 11, 16, 1), 0, False),  # rosh hashana II
+        (dt(2018, 9, 11, 20, 1), 0, False),
+        (dt(2018, 9, 18, 13, 1), 18, True),  # erev yom kipur
+        (dt(2018, 9, 18, 22, 1), 18, False),
+        (dt(2018, 9, 19, 13, 1), 18, False),
+        (dt(2018, 9, 19, 22, 1), 18, False),
+    ]
+
+    @pytest.mark.parametrize(
+        ["now", "offset", "erev_shabbat_chag"], EREV_SHABBAT_CHAG_TEST
+    )
+    def test_erev_shabbat_hag(
+        self, now: datetime.datetime, offset: int, erev_shabbat_chag: bool
+    ) -> None:
+        """Test erev shabbat chag boolean is correct."""
+        location_tz_str = Location(
+            name="New York",
+            latitude=NYC_LAT,
+            longitude=NYC_LNG,
+            timezone="America/New_York",
+            diaspora=True,
+        )
+        # Use a constant offset for Havdalah for unit test stability.
+        zmanim = Zmanim(date=now, location=location_tz_str, havdalah_offset=offset)
+        assert zmanim.erev_shabbat_chag == erev_shabbat_chag
