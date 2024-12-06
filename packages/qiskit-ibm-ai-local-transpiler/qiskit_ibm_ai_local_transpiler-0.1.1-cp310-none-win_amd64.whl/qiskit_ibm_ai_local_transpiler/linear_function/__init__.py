dc39e3b70ac944dd5378101dd727caf75774d3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:02 +0000

    Add package qiskit-ibm-ai-local-transpiler/0.1.1/qiskit_ibm_ai_local_transpiler-0.1.1-cp310-none-win_amd64.whl

---
+++ packages/qiskit-ibm-ai-local-transpiler/qiskit_ibm_ai_local_transpiler-0.1.1-cp310-none-win_amd64.whl/qiskit_ibm_ai_local_transpiler/linear_function/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# (C) Copyright 2023 IBM. All Rights Reserved.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from .inference import LinearFunctionInference
+from .utils import (
+    linear_function_coupling_map_by_hash_dict as LINEAR_FUNCTION_COUPLING_MAPS_BY_HASHES_DICT,
+)
