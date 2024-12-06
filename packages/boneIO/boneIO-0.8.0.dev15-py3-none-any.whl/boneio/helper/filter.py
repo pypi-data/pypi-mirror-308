56b65f3164672326bc599a1dc3b49166790ee912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:50 +0000

    Add package boneIO/0.8.0.dev15/boneIO-0.8.0.dev15-py3-none-any.whl

---
+++ packages/boneIO/boneIO-0.8.0.dev15-py3-none-any.whl/boneio/helper/filter.py
@@ -0,0 +1,34 @@
+"""Filter class to adjust sensor values."""
+
+from __future__ import annotations
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+FILTERS = {
+    "offset": lambda x, y: x + y,
+    "round": lambda x, y: round(x, y),
+    "multiply": lambda x, y: x * y,
+    "filter_out": lambda x, y: None if x == y else x,
+    "filter_out_greater": lambda x, y: None if x > y else x,
+    "filter_out_lower": lambda x, y: None if x < y else x,
+}
+
+
+class Filter:
+    _filters = []
+
+    def _apply_filters(self, value: float, filters: list = []) -> float | None:
+        filters = filters if filters else self._filters
+        for filter in filters:
+            for k, v in filter.items():
+                if k not in FILTERS:
+                    _LOGGER.warning(
+                        "Filter %s doesn't exists. Fix it in config.", k
+                    )
+                    continue
+                if value is None:
+                    return None
+                value = FILTERS[k](value, v)
+        return value
