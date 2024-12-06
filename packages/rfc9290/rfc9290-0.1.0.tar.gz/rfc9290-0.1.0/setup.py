9c721dfe609122083b105377c49288a88a545863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:03 +0000

    Add package rfc9290/0.1.0/rfc9290-0.1.0.tar.gz

---
+++ packages/rfc9290/rfc9290-0.1.0.tar.gz/rfc9290-0.1.0/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="rfc9290",
+    version="0.1.0",
+    description="Simple encoder/decoder for RFC 9290 Concise Problem Details",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="JAG-UK",
+    url="https://github.com/JAG-UK/rfc9290",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "cbor2",
+    ],
+)
