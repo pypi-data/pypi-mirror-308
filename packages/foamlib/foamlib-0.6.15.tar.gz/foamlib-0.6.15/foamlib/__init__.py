c512fea90f162513e8b8a4e50ee39ebc842f30bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:15 +0000

    Add package foamlib/0.6.15/foamlib-0.6.15.tar.gz

---
+++ packages/foamlib/foamlib-0.6.15.tar.gz/foamlib-0.6.15/foamlib/__init__.py
@@ -0,0 +1,25 @@
+"""A Python interface for interacting with OpenFOAM."""
+
+__version__ = "0.6.15"
+
+from ._cases import (
+    AsyncFoamCase,
+    AsyncSlurmFoamCase,
+    CalledProcessError,
+    FoamCase,
+    FoamCaseBase,
+    FoamCaseRunBase,
+)
+from ._files import FoamFieldFile, FoamFile, FoamFileBase
+
+__all__ = [
+    "AsyncFoamCase",
+    "AsyncSlurmFoamCase",
+    "CalledProcessError",
+    "FoamFile",
+    "FoamCase",
+    "FoamCaseBase",
+    "FoamCaseRunBase",
+    "FoamFieldFile",
+    "FoamFileBase",
+]
---
+++ packages/foamlib/foamlib-0.6.15.tar.gz/foamlib-0.6.15/foamlib/_files/__init__.py
@@ -0,0 +1,8 @@
+from ._base import FoamFileBase
+from ._files import FoamFieldFile, FoamFile
+
+__all__ = [
+    "FoamFile",
+    "FoamFieldFile",
+    "FoamFileBase",
+]
