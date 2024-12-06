a4d8c39f26b05f9938be89f38b4aaa4ef76d6bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:05:43 +0000

    Add package deeporigin-data-sdk/0.1.0a19/deeporigin_data_sdk-0.1.0a19.tar.gz

---
+++ packages/deeporigin-data-sdk/deeporigin_data_sdk-0.1.0a19.tar.gz/deeporigin_data_sdk-0.1.0a19/src/deeporigin_data/types/create_file_upload_response.py
@@ -0,0 +1,41 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from typing_extensions import Literal
+
+from pydantic import Field as FieldInfo
+
+from .._models import BaseModel
+
+__all__ = ["CreateFileUploadResponse", "Data", "DataFile"]
+
+
+class DataFile(BaseModel):
+    id: str
+    """Deep Origin system ID."""
+
+    content_length: float = FieldInfo(alias="contentLength")
+
+    date_created: str = FieldInfo(alias="dateCreated")
+
+    name: str
+
+    status: Literal["ready", "archived"]
+
+    uri: str
+
+    content_type: Optional[str] = FieldInfo(alias="contentType", default=None)
+
+    created_by_user_drn: Optional[str] = FieldInfo(alias="createdByUserDrn", default=None)
+
+    date_updated: Optional[str] = FieldInfo(alias="dateUpdated", default=None)
+
+
+class Data(BaseModel):
+    file: DataFile
+
+    upload_url: str = FieldInfo(alias="uploadUrl")
+
+
+class CreateFileUploadResponse(BaseModel):
+    data: Data
