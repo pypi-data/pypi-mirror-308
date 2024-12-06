fd6ae97f6420f3eb6abc6dec8701a3375ef6b23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:29 +0000

    Add package light-agents/0.1.2/light_agents-0.1.2.tar.gz

---
+++ packages/light-agents/light_agents-0.1.2.tar.gz/light_agents-0.1.2/light_agents/config.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AppSettings(BaseSettings):
+    """Base settings."""
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",  # Change made here
+    )
+    GCP_PROJECT_ID: Optional[str] = None
+    GCP_REGION: Optional[str] = None
+    AWS_ACCESS_KEY: Optional[str] = None
+    AWS_SECRET_KEY: Optional[str] = None
+    OPENAI_API_KEY: Optional[str] = None
+
+
+appSettings = AppSettings()
