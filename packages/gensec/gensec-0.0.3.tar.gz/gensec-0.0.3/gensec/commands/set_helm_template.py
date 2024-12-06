4e8668443ebbdfc48ee70ab3b61bd3d312d185e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:21 +0000

    Add package gensec/0.0.3/gensec-0.0.3.tar.gz

---
+++ packages/gensec/gensec-0.0.3.tar.gz/gensec-0.0.3/gensec/commands/set_helm_template.py
@@ -0,0 +1,26 @@
+import shutil
+from pathlib import Path
+import pkg_resources
+
+def copy_chart(chart_template_path, helmchart_name):
+    shutil.copytree(chart_template_path, helmchart_name)
+
+def update_chart(helmchart_name):
+    for item in Path(helmchart_name).rglob('*'):
+        print(item)
+        if item.is_file():
+            # updating
+            with open(item, 'r') as read_file:
+                filedata = read_file.read()
+                filedata = filedata.replace('helm_template', helmchart_name)
+            with open(item, 'w') as write_file:
+                write_file.write(filedata)
+
+chart_template_name = 'templates/helm_template'
+
+def main_helmchart(helmchart_name):
+    chart_template_path_content = pkg_resources.resource_filename(__name__, chart_template_name)
+    copy_chart(chart_template_path_content, helmchart_name)
+    update_chart(helmchart_name)
+
+# main()
