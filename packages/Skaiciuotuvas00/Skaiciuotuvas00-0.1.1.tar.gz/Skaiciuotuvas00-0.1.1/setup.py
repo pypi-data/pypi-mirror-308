1579e58862618676ba9bdae206483357135e4e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:02 +0000

    Add package Skaiciuotuvas00/0.1.1/Skaiciuotuvas00-0.1.1.tar.gz

---
+++ packages/Skaiciuotuvas00/Skaiciuotuvas00-0.1.1.tar.gz/Skaiciuotuvas00-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Skaiciuotuvas00",
+    version="0.1.1",
+    author_email="ambrasas.arnoldas@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6"
+)
