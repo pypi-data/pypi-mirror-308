a68454acd0dd3e714595fbfe6883c31d0623ea44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:33 +0000

    Add package megabouts/0.1.1/megabouts-0.1.1-py3-none-any.whl

---
+++ packages/megabouts/megabouts-0.1.1-py3-none-any.whl/megabouts/config/__init__.py
@@ -0,0 +1,9 @@
+from .preprocessing_config import TrajPreprocessingConfig, TailPreprocessingConfig
+from .segmentation_config import TailSegmentationConfig, TrajSegmentationConfig
+
+__all__ = [
+    "TrajPreprocessingConfig",
+    "TailPreprocessingConfig",
+    "TailSegmentationConfig",
+    "TrajSegmentationConfig",
+]
