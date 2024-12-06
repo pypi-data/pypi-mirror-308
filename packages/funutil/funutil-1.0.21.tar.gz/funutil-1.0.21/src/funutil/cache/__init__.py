f914ecdf7c07ae5648fb850a565cde1dee751bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:58 +0000

    Add package funutil/1.0.21/funutil-1.0.21.tar.gz

---
+++ packages/funutil/funutil-1.0.21.tar.gz/funutil-1.0.21/src/funutil/cache/__init__.py
@@ -0,0 +1,14 @@
+from .box import cache, fifo_cache, lfu_cache, lru_cache, pkl_cache, rr_cache, ttl_cache
+from .core import PickleCache, cached_property
+
+__all__ = [
+    "lru_cache",
+    "PickleCache",
+    "pkl_cache",
+    "cache",
+    "cached_property",
+    "ttl_cache",
+    "lfu_cache",
+    "fifo_cache",
+    "rr_cache",
+]
