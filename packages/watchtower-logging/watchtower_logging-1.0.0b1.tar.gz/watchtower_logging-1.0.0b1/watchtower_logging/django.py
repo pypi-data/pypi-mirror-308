e6e19fe4821a02be46b7495352b2ead0cef84f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:12 +0000

    Add package watchtower-logging/1.0.0b1/watchtower_logging-1.0.0b1.tar.gz

---
+++ packages/watchtower-logging/watchtower_logging-1.0.0b1.tar.gz/watchtower_logging-1.0.0b1/watchtower_logging/django.py
@@ -0,0 +1,27 @@
+import watchtower_logging
+
+# this does not work, since the payload is not build in the handler
+
+class DjangoWatchTowerHandler(watchtower_logging.watchtower_logging.WatchTowerHandler):
+
+    def __init__(self):
+
+        from django.conf import settings
+
+        token = getattr(settings, 'WT_TOKEN', None)
+        protocol = getattr(settings, 'WT_PROTOCOL', 'https')
+        dev = getattr(settings, 'WT_DEV', False)
+        timeout = getattr(settings, 'WT_TIMEOUT', 1.0)
+        retry_count = getattr(settings, 'WT_NUM_RETRY', 1)
+        use_fallback = getattr(settings, 'WT_USE_FALLBACK', True)
+
+        super().__init__(
+            beam_id=settings.WT_BEAM_ID,
+            token=token,
+            protocol=protocol,
+            host=settings.WT_HOST,
+            flush_interval=-1,
+            dev=dev,
+            timeout=timeout,
+            retry_count=retry_count,
+            use_fallback=use_fallback)
