c16079e09c319b7cce44ed326e94d5d10fcf70c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:10 +0000

    Add package shore-pkg-geonda/0.1.2/shore_pkg_geonda-0.1.2.tar.gz

---
+++ packages/shore-pkg-geonda/shore_pkg_geonda-0.1.2.tar.gz/shore_pkg_geonda-0.1.2/README.md
@@ -0,0 +1,10 @@
+# shore
+Python wapper for OCEAN(FEFF) code.
+Calculates XAS.
+
+
+### The structure of the code:
+<img src="img/img2.png" alt="drawing" width="700"/>
+
+### The input object structure:
+<img src="img/img3.png" alt="drawing" width="700"/>
---
+++ packages/shore-pkg-geonda/shore_pkg_geonda-0.1.2.tar.gz/shore_pkg_geonda-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+    
+setup(
+    name='shore-pkg-geonda',  # Replace with your package name
+    version='0.1.2',          # Initial version
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=required,
+    author='Andrey Geondzhian',       # Your name
+    author_email='a.geondzhian@gmail.com',  # Your email
+    description='python wrapper for OCEAN-FEFF',
+    long_description=open('README.md').read(),  # Read from README file for long description
+    long_description_content_type='text/markdown',  # Format of the long description
+    url='https://github.com/geonda/shore',  # URL to your project
+)
