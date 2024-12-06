c0a335456924253435919dc9b192adaed1219b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:41 +0000

    Add package polotsk/1.0.30/polotsk-1.0.30.tar.gz

---
+++ packages/polotsk/polotsk-1.0.30.tar.gz/polotsk-1.0.30/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+# setup(
+#     name="polotsk",
+#     version="1.0.25",
+#     author="B",
+#     fullname="polotsk",
+#     description="This is polotsk module",
+#     long_description=readme(),
+#     long_description_content_type="text/markdown",
+#     # packages=find_packages(),
+#     # packages=[
+#     #     "polotsk",
+#     # ],
+#     classifiers=[
+#         "Programming Language :: Python :: 3.11",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+#     package_dir={"polotsk/": "package/src/"},
+#     # package_dir={"polotsk": "package/src"},
+#     # packages=find_packages(where="package/src"),
+#     keywords="polotsk python",
+#     python_requires=">=3.7",
+# )
+
+setup(
+    name="polotsk",
+    version="1.0.30",
+    # author="",
+    fullname="polotsk",
+    # description="This is polotsk module",
+    # long_description=readme(),
+    # long_description_content_type="text/markdown",
+    # packages=find_packages(),
+    packages=[
+        "polotsk",
+    ],
+    # classifiers=[
+    #     "Programming Language :: Python :: 3.11",
+    #     "License :: OSI Approved :: MIT License",
+    #     "Operating System :: OS Independent",
+    # ],
+    # package_dir={"polotsk": "package/src/"},
+    # packages=find_packages(where="package/src"),
+    # keywords="polotsk python",
+    # python_requires=">=3.7",
+)
