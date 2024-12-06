5de89ee699d185a9e1fc1ba26de63415ec8fcc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:05:25 +0000

    Add package arvi/0.1.23/arvi-0.1.23.tar.gz

---
+++ packages/arvi/arvi-0.1.23.tar.gz/arvi-0.1.23/arvi/translations.py
@@ -0,0 +1,31 @@
+import re
+
+STARS = {
+    '_51Peg': '51 Peg',
+    'Barnard': 'GJ699',
+    "Barnard's": 'GJ699',
+    'Ross128': 'Ross  128',
+    'Ross 128': 'Ross  128',
+}
+
+
+def translate(star, ngc=False, ic=False):
+    # known translations
+    if star in STARS:
+        return STARS[star]
+
+    # regex translations
+    if ngc:
+        NGC_match = re.match(r'NGC([\s\d]+)No([\s\d]+)', star)
+        if NGC_match:
+            cluster = NGC_match.group(1).replace(' ', '')
+            target = NGC_match.group(2).replace(' ', '')
+            return f'Cl* NGC {cluster} MMU {target}'
+    if ic:
+        IC_match = re.match(r'IC([\s\d]+)No([\s\d]+)', star)
+        if IC_match:
+            cluster = IC_match.group(1).replace(' ', '')
+            target = IC_match.group(2).replace(' ', '')
+            return f'Cl* IC {cluster} MMU {target}'
+
+    return star
