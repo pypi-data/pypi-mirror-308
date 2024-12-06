09b4e295518b007222c6a2ea2b93dde46486da34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:23 +0000

    Add package reticula/0.13.0/reticula-0.13.0-cp311-cp311-win_amd64.whl

---
+++ packages/reticula/reticula-0.13.0-cp311-cp311-win_amd64.whl/reticula/microcanonical_reference_models.py
@@ -0,0 +1,7 @@
+from ._reticula_ext.microcanonical_reference_models import (
+        degree_sequence_preserving_shuffling,
+        joint_degree_sequence_preserving_shuffling,
+        instant_event_shuffling, link_shuffling, connected_link_shuffling,
+        topology_constrained_link_shuffling, timeline_shuffling,
+        weight_constrained_timeline_shuffling,
+        activity_constrained_timeline_shuffling, inter_event_shuffling)
