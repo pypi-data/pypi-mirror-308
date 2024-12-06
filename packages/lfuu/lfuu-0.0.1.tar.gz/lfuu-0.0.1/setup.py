7d95ac2a3a8454eb513d912655a5f80e0a706d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:47 +0000

    Add package lfuu/0.0.1/lfuu-0.0.1.tar.gz

---
+++ packages/lfuu/lfuu-0.0.1.tar.gz/lfuu-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='lfuu',
+  version='0.0.1',
+  author='v1ll',
+  author_email='312lv5@gmail.com',
+  description='This is the simplest module for quick work with files.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/Vadosik653/libary_test.git',
+  packages=find_packages(),
+  install_requires=[''],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='files speedfiles ',
+  project_urls={
+    'GitHub': 'https://github.com/Vadosik653/'
+  },
+  python_requires='>=3.6'
+)
