fa873ecab9f3f35013fa7502a7d74808074a77fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:59 +0000

    Add package payme-pkg/3.0.7/payme_pkg-3.0.7.tar.gz

---
+++ packages/payme-pkg/payme_pkg-3.0.7.tar.gz/payme_pkg-3.0.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='payme-pkg',
+    version='3.0.7',
+    license='MIT',
+    author="Muhammadali Akbarov",
+    author_email='muhammadali17abc@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/Muhammadali-Akbarov/payme-pkg',
+    keywords='paymeuz paycomuz payme-merchant merchant-api subscribe-api payme-pkg payme-api',
+    install_requires=[
+        'requests==2.*',
+        "dataclasses==0.*;python_version<'3.7'",  # will only install on py3.6
+        'djangorestframework==3.*'
+      ],
+)
