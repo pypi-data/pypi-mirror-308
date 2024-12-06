ddebd7d657e773e6981d3b1b14aaea1f8d56f480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:35 +0000

    Add package linkup-sdk/0.1.2/linkup_sdk-0.1.2-py3-none-any.whl

---
+++ packages/linkup-sdk/linkup_sdk-0.1.2-py3-none-any.whl/linkup/__init__.py
@@ -0,0 +1,10 @@
+from .client import LinkupClient
+from .types import LinkupSearchResult, LinkupSearchResults, LinkupSource, LinkupSourcedAnswer
+
+__all__ = [
+    "LinkupClient",
+    "LinkupSearchResult",
+    "LinkupSearchResults",
+    "LinkupSource",
+    "LinkupSourcedAnswer",
+]
