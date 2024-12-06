e4431e80f931cb0a7f9e765f177dc3d8ecb80f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:24 +0000

    Add package reticula/0.13.0/reticula-0.13.0-pp310-pypy310_pp73-win_amd64.whl

---
+++ packages/reticula/reticula-0.13.0-pp310-pypy310_pp73-win_amd64.whl/include/reticula/reticula/ranges.hpp
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_RETICULA_RANGES_HPP_
+#define INCLUDE_RETICULA_RANGES_HPP_
+
+#include <ranges>
+
+namespace reticula {
+  namespace ranges = std::ranges;
+  namespace views = std::ranges::views;
+}  // namespace reticula
+
+#endif  // INCLUDE_RETICULA_RANGES_HPP_
