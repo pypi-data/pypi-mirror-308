27546e88a27c1165e56777f6770ef4aff161c97d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:01 +0000

    Add package PyWrikeAssist/0.1/pywrikeassist-0.1.tar.gz

---
+++ packages/PyWrikeAssist/pywrikeassist-0.1.tar.gz/pywrikeassist-0.1/PyWrikeAssist/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+from .gateways.oauth2gateway1 import OAuth2Gateway1
+from .wrike_export import wrike_export_main
+from .new_space import new_space_main
+from .create_delete_folders import create_delete_folders_main
+from .propagate_tasks import propagate_tasks_main
+from .create_update_tasks import create_update_tasks_main
+from .wrike_export_json import wrike_export_json_main
