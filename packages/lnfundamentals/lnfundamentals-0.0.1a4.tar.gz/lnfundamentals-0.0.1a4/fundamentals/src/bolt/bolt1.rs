f075993d633bb82aa67f6b641aceb7fce9e2ed1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:06:30 +0000

    Add package lnfundamentals/0.0.1a4/lnfundamentals-0.0.1a4.tar.gz

---
+++ packages/lnfundamentals/lnfundamentals-0.0.1a4.tar.gz/lnfundamentals-0.0.1a4/fundamentals/src/bolt/bolt1.rs
@@ -0,0 +1,54 @@
+// code generated with the lngen, please not edit this file.
+use std::io::{Read, Write};
+
+use fundamentals_derive::{DecodeWire, EncodeWire};
+#[cfg(feature = "pyo3")]
+use pyo3::pyclass;
+
+use crate::core::{FromWire, ToWire};
+use crate::prelude::*;
+
+#[cfg_attr(feature = "pyo3", pyclass)]
+#[derive(DecodeWire, EncodeWire, Debug, Clone)]
+pub struct Error {
+    #[msg_type = 17]
+    pub ty: u16,
+    pub channel_id: ChannelId,
+    pub data: BitFlag,
+}
+
+#[cfg_attr(feature = "pyo3", pyclass)]
+#[derive(DecodeWire, EncodeWire, Debug, Clone)]
+pub struct Init {
+    #[msg_type = 16]
+    pub ty: u16,
+    pub globalfeatures: BitFlag,
+    pub features: BitFlag,
+    pub init_tlvs: Stream,
+}
+
+#[cfg_attr(feature = "pyo3", pyclass)]
+#[derive(DecodeWire, EncodeWire, Debug, Clone)]
+pub struct Ping {
+    #[msg_type = 18]
+    pub ty: u16,
+    pub num_pong_bytes: u16,
+    pub ignored: BitFlag,
+}
+
+#[cfg_attr(feature = "pyo3", pyclass)]
+#[derive(DecodeWire, EncodeWire, Debug, Clone)]
+pub struct Pong {
+    #[msg_type = 19]
+    pub ty: u16,
+    pub ignored: BitFlag,
+}
+
+#[cfg_attr(feature = "pyo3", pyclass)]
+#[derive(DecodeWire, EncodeWire, Debug, Clone)]
+pub struct Warning {
+    #[msg_type = 1]
+    pub ty: u16,
+    pub channel_id: ChannelId,
+    pub data: BitFlag,
+}
