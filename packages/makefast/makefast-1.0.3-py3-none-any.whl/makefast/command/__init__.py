a737f4f51d44382de33333cb4995774c445d8fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:05:25 +0000

    Add package makefast/1.0.3/makefast-1.0.3-py3-none-any.whl

---
+++ packages/makefast/makefast-1.0.3-py3-none-any.whl/makefast/command/__init__.py
@@ -0,0 +1,6 @@
+from .create_route import CreateRoute
+from .create_model import CreateModel
+from .create_migration import CreateMigration
+from .create_scheme import CreateScheme
+from .create_enum import CreateEnum
+from .project_init import ProjectInit
