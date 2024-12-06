db05d627f91dc1d0d02cd1500ac870710c02cd18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:33 +0000

    Add package MagpyCAS/1.0.11/magpycas-1.0.11.tar.gz

---
+++ packages/MagpyCAS/magpycas-1.0.11.tar.gz/magpycas-1.0.11/Magpy/__init__.py
@@ -0,0 +1,12 @@
+# import Magmas 
+# import RandSampMags 
+from .Magmas import *
+from .RandSampMags import *
+from .MagmaPropertiesChecker import *
+
+__all__ = [
+    "Magmas",
+    "RandSampMags",
+    "MagmaPropertiesChecker"
+]
+
---
+++ packages/MagpyCAS/magpycas-1.0.11.tar.gz/magpycas-1.0.11/README.md
@@ -0,0 +1,5 @@
+# Magpy
+
+A primitive package for creating mathematical structures from magmas to abelian groups, and determining their properties.
+
+Official documentation coming soon...
