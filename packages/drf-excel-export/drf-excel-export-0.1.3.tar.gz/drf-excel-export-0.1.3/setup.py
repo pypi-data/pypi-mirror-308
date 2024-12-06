a42d708dfdbf76579e99ded3d2259e360270ad3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:05:05 +0000

    Add package drf-excel-export/0.1.3/drf-excel-export-0.1.3.tar.gz

---
+++ packages/drf-excel-export/drf-excel-export-0.1.3.tar.gz/drf-excel-export-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="drf-excel-export",
+    version="0.1.3",   
+    description="A tool to export Django REST Framework API documentation to Excel",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Donald Programmeur",
+    author_email="donaldtedom0@gmail.com",
+    url="https://github.com/donaldte/drf-excel-export",  # Link to your GitHub repo
+    packages=find_packages(),
+    install_requires=[
+        "django",
+        "djangorestframework",
+        "openpyxl",
+        "drf-spectacular", 
+        "drf-yasg", 
+        "pyyaml"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
