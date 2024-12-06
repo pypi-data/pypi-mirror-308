87fd3a7c80e8ec6dc8a85728b4cbffbd62e6518b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:24 +0000

    Add package capella-console-client/0.13.3/capella_console_client-0.13.3-py3-none-any.whl

---
+++ packages/capella-console-client/capella_console_client-0.13.3-py3-none-any.whl/capella_console_client/enumerations.py
@@ -0,0 +1,125 @@
+from enum import Enum, EnumMeta
+
+
+class MetaEnum(EnumMeta):
+    def __contains__(cls, item):
+        try:
+            cls(item)
+        except ValueError:
+            return False
+        return True
+
+
+class BaseEnum(Enum, metaclass=MetaEnum):
+    pass
+
+
+class ProductType(str, BaseEnum):
+    SLC = "SLC"
+    GEO = "GEO"
+    SICD = "SICD"
+    GEC = "GEC"
+    SIDD = "SIDD"
+    CPHD = "CPHD"
+    VS = "VS"
+    ACD = "ACD"
+
+
+class AssetType(str, BaseEnum):
+    # shared
+    preview = "preview"
+    thumbnail = "thumbnail"
+    # img product type assets
+    HH = "HH"
+    VV = "VV"
+    raster = "raster"
+    metadata = "metadata"
+    # analytic produc type assets
+    # VS
+    analytic_product = "analytic_product"
+    analytic_product_diag = "analytic_product_diag"
+    landmask = "landmask"
+    # ACD
+    changemap = "changemap"
+    # internal only
+    log = "log"
+    profile = "profile"
+    stats = "stats"
+    stats_plots = "stats_plots"
+
+
+class ProductClass(str, BaseEnum):
+    standard = "standard"
+    extended = "extended"
+    custom = "custom"
+
+
+class OrbitState(str, BaseEnum):
+    ascending = "ascending"
+    descending = "descending"
+    either = "either"
+
+
+class ObservationDirection(str, BaseEnum):
+    left = "left"
+    right = "right"
+    either = "either"
+
+
+class OrbitalPlane(int, BaseEnum):
+    fortyfive = 45
+    fiftythree = 53
+    ninetyseven = 97
+
+
+class InstrumentMode(str, BaseEnum):
+    stripmap = "stripmap"
+    spotlight = "spotlight"
+    sliding_spotlight = "sliding_spotlight"
+
+
+class TaskingRequestStatus(str, BaseEnum):
+    received = "received"
+    review = "review"
+    submitted = "submitted"
+    active = "active"
+    accepted = "accepted"
+    rejected = "rejected"
+    expired = "expired"
+    completed = "completed"
+    anomaly = "anomaly"
+    canceled = "canceled"
+    error = "error"
+    failed = "failed"
+
+
+class CollectionTier(str, BaseEnum):
+    urgent = "urgent"
+    priority = "priority"
+    standard = "standard"
+    flexible = "flexible"
+    internal = "internal"
+
+
+class RepeatCollectionTier(str, BaseEnum):
+    flexible = "flexible"
+    routine = "routine"
+    internal = "internal"
+
+
+class ArchiveHoldback(str, BaseEnum):
+    none = "none"
+    one_year = "1 year"
+    thirty_day = "30 day"
+    permanent = "permanent"
+
+
+class Polarization(str, BaseEnum):
+    HH = "HH"
+    VV = "VV"
+
+
+class LocalTimeOption(str, BaseEnum):
+    day = "day"
+    night = "night"
+    anytime = "anytime"
---
+++ packages/capella-console-client/capella_console_client-0.13.3-py3-none-any.whl/capella_console_client/session.py
@@ -0,0 +1,170 @@
+import base64
+from enum import Enum
+from getpass import getpass
+
+from typing import Optional, Tuple
+
+import httpx
+
+from capella_console_client.config import DEFAULT_TIMEOUT, CONSOLE_API_URL
+from capella_console_client.hooks import log_on_4xx_5xx, translate_error_to_exception
+from capella_console_client.logconf import logger
+from capella_console_client.exceptions import (
+    CapellaConsoleClientError,
+    AuthenticationError,
+    INVALID_TOKEN_ERROR_CODE,
+    NoRefreshTokenError,
+)
+from capella_console_client.version import __version__
+
+
+class AuthMethod(Enum):
+    BASIC = 1  # email/ password
+    TOKEN = 2  # JWT token
+
+
+class CapellaConsoleSession(httpx.Client):
+    def __init__(self, *args, **kwargs):
+        verbose = kwargs.pop("verbose", False)
+        search_url = kwargs.pop("search_url", None)
+        event_hooks = [translate_error_to_exception]
+        if verbose:
+            event_hooks.insert(0, log_on_4xx_5xx)
+
+        super(CapellaConsoleSession, self).__init__(
+            *args,
+            event_hooks={"response": event_hooks},
+            timeout=DEFAULT_TIMEOUT,
+            headers={
+                "Content-Type": "application/json; charset=utf-8",
+                "User-Agent": f"capella-console-client/{__version__}",
+            },
+            **kwargs,
+        )
+        self.customer_id = None
+        self.organization_id = None
+
+        self.search_url = search_url if search_url is not None else f"{self.base_url}/catalog/search"
+        self._refresh_token = None
+
+    def authenticate(
+        self,
+        email: Optional[str] = None,
+        password: Optional[str] = None,
+        token: Optional[str] = None,
+        no_token_check: bool = False,
+    ) -> None:
+        try:
+            basic_auth_provided = bool(email) and bool(password)
+            if not basic_auth_provided and not bool(token):
+                email, password = self._prompt_user_creds(email, password)  # type: ignore
+
+            auth_method = self._get_auth_method(email, password, token)
+            if auth_method == AuthMethod.BASIC:
+                self._basic_auth(email, password)  # type: ignore
+            elif auth_method == AuthMethod.TOKEN:
+                self._token_auth_check(token, no_token_check)  # type: ignore
+        except (httpx.HTTPStatusError, CapellaConsoleClientError):
+            message = {
+                AuthMethod.BASIC: "please check your credentials",
+                AuthMethod.TOKEN: "provided token invalid",
+            }[auth_method]
+
+            raise AuthenticationError(
+                f"Unable to authenticate with {self.base_url} ({auth_method}) - {message}"
+            ) from None
+
+        suffix = f"({self.base_url})" if self.base_url != CONSOLE_API_URL else ""
+        if auth_method == AuthMethod.TOKEN and no_token_check:
+            logger.info(f"successfully authenticated {suffix}")
+        else:
+            logger.info(f"successfully authenticated as {self.email} {suffix}")
+
+    def _prompt_user_creds(self, email: str, password: str) -> Tuple[str, str]:
+        """user credentials on console"""
+        if not email:
+            email = input(f"user on {self.base_url} (user@email.com): ").strip()
+        if not password:
+            password = getpass("password: ").strip()
+        return (email, password)
+
+    def _get_auth_method(self, email: Optional[str], password: Optional[str], token: Optional[str]) -> AuthMethod:
+        basic_auth_provided = bool(email) and bool(password)
+        has_token = bool(token)
+
+        if not has_token and not basic_auth_provided:
+            raise ValueError("please provide either email and password or token")
+
+        if has_token and basic_auth_provided:
+            logger.info("both token and email/ password provided ... using email/ password for authentication")
+
+        auth_method = AuthMethod.BASIC
+        if not basic_auth_provided:
+            auth_method = AuthMethod.TOKEN
+        return auth_method
+
+    def _basic_auth(self, email: str, password: str):
+        """
+        authenticate with Console API
+
+        returns jwt access token
+        """
+        basic_token = base64.b64encode(f"{email}:{password}".encode()).decode("utf-8")
+        resp = self.post("/token", headers={"Authorization": f"Basic {basic_token}"})
+        resp.raise_for_status()
+        response_body = resp.json()
+
+        self._set_auth_header(response_body["accessToken"])
+        self._refresh_token = response_body["refreshToken"]
+        self._cache_user_info()
+
+    def _set_auth_header(self, token: str):
+        token = token.strip()
+        if not token.startswith("Bearer"):
+            token = f"Bearer {token}"
+
+        self.headers["Authorization"] = token
+
+    def _cache_user_info(self):
+        """cache customer_id and organization_id - serves as test for successful auth"""
+        resp = self.get("/user")
+        resp.raise_for_status()
+
+        con = resp.json()
+        self.customer_id = con["id"]
+        self.organization_id = con["organizationId"]
+        self.email = con["email"]
+
+    def _token_auth_check(self, token: str, no_token_check: bool):
+        self._set_auth_header(token)
+        if not no_token_check:
+            self._cache_user_info()
+
+    def send(self, *fct_args, **kwargs):
+        """wrap httpx.Client.send for auto token_refresh"""
+        try:
+            ret = super().send(*fct_args, **kwargs)
+        except AuthenticationError as e:
+            # safeguard in case AuthenticationError get's improperly re-used
+            if e.code != INVALID_TOKEN_ERROR_CODE:
+                raise e
+
+            self.perform_token_refresh()
+
+            # retry request
+            orig_request = fct_args[0]
+            orig_request.headers["authorization"] = self.headers["authorization"]
+            logger.info(f"retrying {fct_args[0]}")
+            ret = super().send(*fct_args, **kwargs)
+        return ret
+
+    def perform_token_refresh(self):
+        if not self._refresh_token:
+            raise NoRefreshTokenError("No refresh token found") from None
+
+        resp = self.post("/token/refresh", json={"refreshToken": self._refresh_token})
+        con = resp.json()
+        self._set_auth_header(con["accessToken"])
+        if con["refreshToken"] != self._refresh_token:
+            self._refresh_token = con["refreshToken"]
+        logger.info("successfully refreshed access token")
