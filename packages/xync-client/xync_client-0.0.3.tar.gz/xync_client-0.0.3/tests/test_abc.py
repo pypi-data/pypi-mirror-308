e2eb5262665da8151e36f1744d1858a9c42797b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:45 +0000

    Add package xync-client/0.0.3/xync_client-0.0.3.tar.gz

---
+++ packages/xync-client/xync_client-0.0.3.tar.gz/xync_client-0.0.3/tests/test_abc.py
@@ -0,0 +1,8 @@
+import asyncio
+from xync_client.Abc import AgentClient
+
+async def test_payment_methods():
+    result = await AgentClient.payment_methods()
+    return result
+
+asyncio.run(test_payment_methods())
