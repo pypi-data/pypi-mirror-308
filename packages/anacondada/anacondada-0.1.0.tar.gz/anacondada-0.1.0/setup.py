88c2a07a3f24d1936ec40bbd3047c104aaa845e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:05:48 +0000

    Add package anacondada/0.1.0/anacondada-0.1.0.tar.gz

---
+++ packages/anacondada/anacondada-0.1.0.tar.gz/anacondada-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anacondada',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Tech',
+    author_email='techdobe150@gmail.com',
+    url='https://github.com/yourusername/my_python_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
