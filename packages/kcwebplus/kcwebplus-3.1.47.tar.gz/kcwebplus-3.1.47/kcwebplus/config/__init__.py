09877dafa98441be13334d95f22dd7da4690bd2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:16 +0000

    Add package kcwebplus/3.1.47/kcwebplus-3.1.47.tar.gz

---
+++ packages/kcwebplus/kcwebplus-3.1.47.tar.gz/kcwebplus-3.1.47/kcwebplus/config/__init__.py
@@ -0,0 +1,15 @@
+from .app import *
+kcwebplus={}
+kcwebplus['name']='kcwebplus'                             #项目的名称
+kcwebplus['version']='3.1.46'							#项目版本
+kcwebplus['description']=''       #项目的简单描述
+kcwebplus['long_description']=''     #项目详细描述
+kcwebplus['license']='MIT'                    #开源协议   mit开源
+kcwebplus['url']=''
+kcwebplus['author']='百里'  					 #名字
+kcwebplus['author_email']='fk1402936534@qq.com' 	     #邮件地址
+kcwebplus['maintainer']='百里' 						 #维护人员的名字
+kcwebplus['maintainer_email']='fk1402936534@qq.com'    #维护人员的邮件地址
+
+fdgrsgrsegsrsgrsbsdbftbrsbfdrtrtbdfsrsgr=kcwebplus['name'] #不要修改改参数，否则无法上传模块和插件
+
