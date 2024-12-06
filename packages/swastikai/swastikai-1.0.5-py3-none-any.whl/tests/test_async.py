0d2b20f2443e3f59c4cc655969901556d46fd42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:05:09 +0000

    Add package swastikai/1.0.5/swastikai-1.0.5-py3-none-any.whl

---
+++ packages/swastikai/swastikai-1.0.5-py3-none-any.whl/tests/test_async.py
@@ -0,0 +1,28 @@
+from swastikai.memory_async import personal_memory
+import asyncio
+
+
+async def test_get_request():
+    client = personal_memory(api_key="SWK_API_KEY")
+    paylod = {
+        "agent_details": {
+        "company_id": "Deloitte",
+        "department_id": "Analytics",
+        "team_id": "Dev Team",
+        "agent_id": "RAG_123",
+        "user_id": "Chirotpal",
+        "agent_type": "iA",
+        "agent_description": "This is a very helpful personal assisstant.",
+        "system_prompt": ""
+        },
+        "memories": "",
+        "last_user_prompt": "I love mangoes",
+        "last_ai_response": "",
+        "chat_history": [],
+        "memory_category": "personal_memory"
+        }
+
+    response = await client.retrieve_memory(paylod)
+    print(response)
+
+asyncio.run(test_get_request())
