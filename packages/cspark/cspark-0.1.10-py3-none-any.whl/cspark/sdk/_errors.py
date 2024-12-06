8d70f4fd2249c1894db81840a3041a0d3cd787ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:47 +0000

    Add package cspark/0.1.10/cspark-0.1.10-py3-none-any.whl

---
+++ packages/cspark/cspark-0.1.10-py3-none-any.whl/cspark/sdk/_errors.py
@@ -0,0 +1,216 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Any, Dict, Generic, Optional, TypeVar, cast
+
+from httpx import Headers
+
+__all__ = ['SparkError', 'SparkSdkError', 'SparkApiError', 'ErrorMessage']
+
+TReq = TypeVar('TReq')
+TResp = TypeVar('TResp')
+
+
+class TRequest(Generic[TReq]):
+    def __init__(self, url: str, method: str, headers: Headers, body: Optional[TReq]):
+        self.url = url
+        self.method = method
+        self.headers = headers
+        self.body = body
+
+
+class TResponse(Generic[TResp]):
+    def __init__(self, headers: Headers, body: TResp, raw: str):
+        self.headers = headers
+        self.body = body
+        self.raw = raw
+
+
+class SparkError(Exception):
+    """Base class for all SDK-related errors."""
+
+    def __init__(self, message: str, cause: Optional[Any] = None):
+        super().__init__()
+        self.name = self.__class__.__name__
+        self.message = message
+        self.cause = cause
+
+    def __str__(self):
+        error = f'{self.name}: {self.message}'
+        return f'{error} ({self.details})' if self.details else error
+
+    def __repr__(self):
+        return f'<{self.name}>'
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {'name': self.name, 'message': self.message, 'cause': self.cause}
+
+    @property
+    def details(self) -> str:
+        if isinstance(self.cause, Exception):
+            return str(self.cause)
+        elif isinstance(self.cause, str):
+            return self.cause
+        elif isinstance(self.cause, dict):
+            return str(self.cause)
+        return ''
+
+    @staticmethod
+    def sdk(message: str, cause: Optional[Any] = None) -> SparkSdkError:
+        return SparkSdkError(ErrorMessage(message, cause))
+
+    @staticmethod
+    def api(status: int, error: Dict[str, Any]) -> SparkApiError:
+        return SparkApiError.when(status, ErrorMessage.from_dict(error))
+
+
+class SparkSdkError(SparkError):
+    """
+    Base class for SDK-specific errors.
+
+    Usually thrown when an argument fails to comply with the expected format.
+    Because it's a client-side error, it will include in the majority of cases
+    the invalid entry as `cause`.
+
+    If the argument causes another process to fail, the `cause` will be referencing
+    that thrown `Exception`. The `timestamp` helps to define a hierarchy of errors since
+    the `cause` might be a `SparkError` as well.
+    """
+
+    def __init__(self, error: ErrorMessage):
+        super().__init__(error.message, error.cause)
+        self.timestamp = datetime.now().isoformat()
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {**super().to_dict(), 'timestamp': self.timestamp}
+
+
+class SparkApiError(SparkError):
+    """
+    Base class for errors related to the API.
+
+    When attempting to communicate with the API, the SDK will wrap any sort of failure
+    (any error during the round trip) into `SparkApiError`. The `status` is the HTTP
+    status code of the response, and the `request_id`, a unique identifier of the request.
+    """
+
+    def __init__(self, error: ErrorMessage, status: Optional[int] = None):
+        cause = ApiErrorCause.from_dict(error.cause) if isinstance(error.cause, dict) else None
+        super().__init__(f"{status or ''} {error.message}".strip(), cause)
+        self.status = status
+
+    @property
+    def request_id(self) -> str:
+        if self.cause and isinstance(self.cause, ApiErrorCause):
+            return self.cause.request.headers.get('x-request-id', '')
+        return ''  # Note: this should never happen if thrown by the SDK
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {**super().to_dict(), 'status': self.status}
+
+    @staticmethod
+    def when(status: int, error: ErrorMessage) -> SparkApiError:
+        if status == 0:
+            return InternetError(error, status)
+        elif status == 400:
+            return BadRequestError(error, status)
+        elif status == 401:
+            return UnauthorizedError(error, status)
+        elif status == 403:
+            return ForbiddenError(error, status)
+        elif status == 404:
+            return NotFoundError(error, status)
+        elif status == 409:
+            return ConflictError(error, status)
+        elif status == 415:
+            return UnsupportedMediaTypeError(error, status)
+        elif status == 422:
+            return UnprocessableEntityError(error, status)
+        elif status == 429:
+            return RateLimitError(error, status)
+        elif status == 500:
+            return InternalServerError(error, status)
+        elif status == 503:
+            return ServiceUnavailableError(error, status)
+        elif status == 504:
+            return GatewayTimeoutError(error, status)
+        else:
+            return UnknownApiError(error)
+
+
+class InternetError(SparkApiError):
+    status = 0
+
+
+class BadRequestError(SparkApiError):
+    status = 400
+
+
+class UnauthorizedError(SparkApiError):
+    status = 401
+
+
+class ForbiddenError(SparkApiError):
+    status = 403
+
+
+class NotFoundError(SparkApiError):
+    status = 404
+
+
+class ConflictError(SparkApiError):
+    status = 409
+
+
+class UnsupportedMediaTypeError(SparkApiError):
+    status = 415
+
+
+class UnprocessableEntityError(SparkApiError):
+    status = 422
+
+
+class RateLimitError(SparkApiError):
+    status = 429
+
+
+class InternalServerError(SparkApiError):
+    status = 500
+
+
+class ServiceUnavailableError(SparkApiError):
+    status = 503
+
+
+class GatewayTimeoutError(SparkApiError):
+    status = 504
+
+
+class UnknownApiError(SparkApiError):
+    status = None
+
+
+class ErrorMessage:
+    def __init__(self, message: str, cause: Optional[Any] = None):
+        self.message = message
+        self.cause = cause
+
+    @staticmethod
+    def from_dict(data: Dict[str, Any]) -> ErrorMessage:
+        message = cast(str, data.get('message'))
+        cause = cast(Optional[Any], data.get('cause'))
+        return ErrorMessage(message, cause)
+
+
+class ApiErrorCause(Generic[TReq, TResp]):
+    def __init__(self, request: TRequest[TReq], response: Optional[TResponse[TResp]] = None):
+        self.request = request
+        self.response = response
+
+    @staticmethod
+    def from_dict(data: Dict[str, Any]) -> ApiErrorCause:
+        req, res = data.get('request', {}), data.get('response')
+        request = TRequest(req.get('url', ''), req.get('method', ''), req.get('headers', {}), req.get('body', ''))
+        if not res:
+            return ApiErrorCause(request)
+        return ApiErrorCause(request, TResponse(res['headers'], res['body'], res['raw']))
---
+++ packages/cspark/cspark-0.1.10-py3-none-any.whl/cspark/sdk/resources/_base.py
@@ -0,0 +1,332 @@
+from __future__ import annotations
+
+import re
+import time
+from dataclasses import dataclass
+from types import TracebackType
+from typing import Any, Mapping, Optional, Union
+
+from httpx import URL, Client, Headers, Request, Response
+
+from .._config import Config
+from .._errors import SparkError
+from .._logger import get_logger
+from .._utils import StringUtils, get_retry_timeout, get_uuid, sanitize_uri
+from .._version import about, sdk_ua_header
+
+__all__ = ['ApiResource', 'UriParams', 'Uri', 'HttpResponse']
+
+
+class ApiResource:
+    def __init__(self, config: Config):
+        self.config = config
+        self._client = Client()
+        self.logger = get_logger(**config.logger.__dict__)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        self.close()
+
+    def close(self):
+        if not self._client.is_closed:
+            self._client.close()
+
+    @property
+    def default_headers(self):
+        return {
+            **self.config.extra_headers,
+            'User-Agent': about,
+            'x-spark-ua': sdk_ua_header,
+            'x-request-id': get_uuid(),
+            'x-tenant-name': self.config.base_url.tenant,
+        }
+
+    def request(
+        self,
+        url: Union[str, 'Uri'],
+        *,
+        method: str = 'GET',
+        headers: Mapping[str, str] = {},
+        params: Optional[Mapping[str, str]] = None,
+        body: Optional[Any] = None,
+        content: Optional[bytes] = None,
+        form=None,
+        files=None,
+    ) -> HttpResponse:
+        url = url.value if isinstance(url, Uri) else url
+        request = self._client.build_request(
+            method,
+            url,
+            params=params,
+            headers={**headers, **self.default_headers},
+            data=form,
+            json=body,
+            content=content,
+            files=files,
+            timeout=self.config.timeout / 1000,
+        )
+
+        self.logger.debug(f'{method} {url}')
+        return self.__fetch(request)
+
+    def __fetch(self, request: Request, retries: int = 0) -> HttpResponse:
+        request.headers.update(self.config.auth.as_header)
+        response = self._client.send(request)  # FIXME: handling errors (httpx.RequestError | httpx.HTTPStatusError)
+        status = response.status_code
+
+        if status >= 400:
+            if status == 401 and self.config.auth.type == 'oauth' and retries < self.config.max_retries:
+                self.config.auth.oauth.retrieve_token(self.config)  # pyright: ignore[reportOptionalMemberAccess]
+                return self.__fetch(request, retries + 1)
+
+            if (status == 408 or status == 429) and retries < self.config.max_retries:
+                self.logger.debug(f'retrying request due to status code {status}...')
+                delay = get_retry_timeout(retries, self.config.retry_interval)
+                time.sleep(delay)
+                return self.__fetch(request, retries + 1)
+
+            url = str(request.url)
+            raise SparkError.api(
+                status,
+                {'message': f'failed to fetch <{url}>', 'cause': _create_api_error_cause(request, response)},
+            )
+
+        # otherwise, ok response
+        http_response = {
+            'status': status,
+            'data': None,
+            'buffer': response.content,
+            'headers': response.headers,
+            'raw_request': request,
+            'raw_response': response,
+        }
+
+        content_type = response.headers.get('content-type', '')
+        if 'application/json' in content_type:
+            try:
+                http_response['data'] = response.json()
+            except Exception:
+                http_response['data'] = response.text
+        return HttpResponse(**http_response)
+
+
+def download(
+    url: str,
+    *,
+    method: str = 'GET',
+    headers: Mapping[str, str] = {},
+    params: Optional[Mapping[str, str]] = None,
+    body=None,
+    form=None,
+    timeout: Optional[float] = 60,
+):
+    with Client() as client:
+        request = client.build_request(
+            method,
+            url,
+            params=params,
+            headers=headers,
+            data=form,
+            json=body,
+            timeout=timeout,
+        )
+
+        response = client.send(request)
+        if response.status_code >= 400:
+            raise SparkError.api(response.status_code, _create_api_error_cause(request, response))
+
+        return HttpResponse(
+            status=response.status_code,
+            data=None,
+            buffer=response.content,
+            headers=response.headers,
+            raw_request=request,
+            raw_response=response,
+        )
+
+
+def _create_api_error_cause(request: Request, response: Response) -> dict[str, Any]:
+    return {
+        'request': {
+            'url': str(request.url),
+            'method': request.method,
+            'headers': request.headers,
+            'body': request.content,
+        },
+        'response': {
+            'headers': response.headers,
+            'body': response.text,
+            'raw': response.content,
+        },
+    }
+
+
+@dataclass
+class HttpResponse:
+    status: int
+    data: Union[None, Any, str]
+    buffer: bytes
+    headers: Headers
+    raw_request: Request
+    raw_response: Response
+
+    def copy_with(self, **kwargs) -> 'HttpResponse':
+        return HttpResponse(
+            status=kwargs.get('status', self.status),
+            data=kwargs.get('data', self.data),
+            buffer=kwargs.get('buffer', self.buffer),
+            headers=kwargs.get('headers', self.headers),
+            raw_request=kwargs.get('request', self.raw_request),
+            raw_response=kwargs.get('response', self.raw_response),
+        )
+
+
+@dataclass(frozen=True)
+class UriParams:
+    folder: Optional[str] = None
+    service: Optional[str] = None
+    service_id: Optional[str] = None
+    version: Optional[str] = None
+    version_id: Optional[str] = None
+    proxy: Optional[str] = None
+    public: Optional[bool] = False
+
+    @property
+    def service_uri(self) -> str:
+        """
+        Returns the service URI locator's short format if available.
+        folder/service[version?]
+        """
+        return Uri.encode(self, long=False)
+
+    def pick(self, *args: str) -> 'UriParams':
+        return UriParams(**{k: v for k, v in self.__dict__.items() if k in args})
+
+    def omit(self, *args: str) -> 'UriParams':
+        return UriParams(**{k: v for k, v in self.__dict__.items() if k not in args})
+
+    def encode(self, long: bool = True) -> str:
+        return Uri.encode(self, long=long)
+
+
+class Uri:
+    def __init__(self, url: URL):
+        self._url = url
+
+    @property
+    def value(self) -> str:
+        return str(self._url)
+
+    @staticmethod
+    def of(
+        uri: Optional[UriParams] = None,
+        *,
+        base_url: str = '',
+        version: str = 'api/v3',
+        endpoint: str = '',
+    ) -> 'Uri':
+        uri = uri or UriParams()
+        path = version
+        if uri.public:
+            path += '/public'
+
+        if uri.version_id:
+            path += f'/version/{uri.version_id}'
+        elif uri.service_id:
+            path += f'/service/{uri.service_id}'
+        elif uri.folder and uri.service:
+            path += f'/folders/{uri.folder}/services/{uri.service}'
+        elif uri.proxy:
+            path += f'/proxy/{sanitize_uri(uri.proxy)}'
+
+        if endpoint and not uri.proxy:
+            path += f'/{endpoint}'
+
+        try:
+            return Uri(URL(f'{base_url}/{path}'))
+        except Exception as cause:
+            raise SparkError.sdk('invalid URI params', uri) from cause
+
+    @staticmethod
+    def partial(
+        resource: str,
+        *,
+        base_url: str = '',
+        version: str = 'api/v3',
+        endpoint: str = '',
+    ) -> 'Uri':
+        try:
+            resource = sanitize_uri(resource)
+            if version:
+                resource = f'{version}/{resource}'
+            if endpoint:
+                resource += f'/{endpoint}'
+            resource = f'{base_url}/{resource}'
+
+            return Uri(URL(resource))
+        except SparkError as error:
+            raise SparkError.sdk(f'invalid service URI <{resource}>', error) from error
+        except Exception as cause:
+            raise SparkError(f'failed to build Spark endpoint from <{resource}>', cause) from cause
+
+    @staticmethod
+    def to_params(uri: Union[str, UriParams]) -> UriParams:
+        return Uri.decode(uri) if isinstance(uri, str) else uri
+
+    @staticmethod
+    def decode(uri: str) -> UriParams:
+        uri = re.sub('folders/', '', sanitize_uri(uri))
+        uri = re.sub('services/', '', uri)
+        match = re.match(r'^([^\/]+)\/([^[]+)(?:\[(.*?)\])?$', uri)
+        if match:
+            folder, service, version = match.groups()
+            if folder == 'version':
+                return UriParams(version_id=service)
+            if folder == 'service':
+                return UriParams(service_id=service)
+            if folder == 'proxy':
+                return UriParams(proxy=service)
+            version = None if StringUtils.is_empty(version) else version
+            return UriParams(folder=folder, service=service, version=version)
+        return UriParams()
+
+    @staticmethod
+    def encode(uri: UriParams, long: bool = True) -> str:
+        folder, service, version = uri.folder, uri.service, uri.version
+        if uri.proxy:
+            return f'proxy/{uri.proxy}'
+        if uri.version_id:
+            return f'version/{uri.version_id}'
+        if uri.service_id:
+            return f'service/{uri.service_id}'
+        if folder and service:
+            if long:
+                return f'folders/{folder}/services/{service}{f"[{version}]" if version else ""}'
+            return f'{folder}/{service}{f"[{version}]" if version else ""}'
+        return ''
+
+    @staticmethod
+    def validate(uri: Union[str, UriParams], message: Optional[str] = None) -> UriParams:
+        uri_params = Uri.to_params(uri)
+        if StringUtils.is_empty(uri_params.service_uri):
+            folder, service = uri_params.folder, uri_params.service
+            if folder and not service:
+                msg = message or 'service name is missing'
+            elif not folder and service:
+                msg = message or 'folder name is missing'
+            else:
+                msg = message or 'service uri locator is required'
+            msg += ' :: a uri needs to be of these formats: \n\t- "folder/service[?version]" \n\t-'
+            msg += ' "service/service_id" \n\t- "version/version_id" \n\t- "proxy/custom-endpoint"\n'
+            raise SparkError.sdk(msg, uri)
+        return uri_params
+
+    def __str__(self) -> str:
+        return self.value
