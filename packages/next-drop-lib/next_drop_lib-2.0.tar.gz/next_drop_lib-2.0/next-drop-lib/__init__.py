bc62112c343c6f93406ab8ba206dad43bb94a146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:06:07 +0000

    Add package next-drop-lib/2.0/next_drop_lib-2.0.tar.gz

---
+++ packages/next-drop-lib/next_drop_lib-2.0.tar.gz/next_drop_lib-2.0/next-drop-lib/__init__.py
@@ -0,0 +1,5 @@
+from .file_sender import FileSender
+from .file_receiver import FileReceiver
+from .nextdrop_general import NextDropGeneral
+
+__all__ = ["FileSender", "FileReceiver", "NextDropGeneral"]
