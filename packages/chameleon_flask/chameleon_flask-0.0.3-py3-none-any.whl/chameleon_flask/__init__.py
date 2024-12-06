b5d8532e16e9c34b748329b41f443cc4b1273b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:49 +0000

    Add package chameleon_flask/0.0.3/chameleon_flask-0.0.3-py3-none-any.whl

---
+++ packages/chameleon_flask/chameleon_flask-0.0.3-py3-none-any.whl/chameleon_flask/__init__.py
@@ -0,0 +1,10 @@
+"""chameleon-flask - Adds integration of the Chameleon template language to Flask and Quart."""
+
+__version__ = '0.0.3'
+__author__ = 'Michael Kennedy <michael@talkpython.fm>'
+__all__ = ['template', 'global_init', 'not_found', 'response', ]
+
+from .engine import global_init
+from .engine import not_found
+from .engine import response
+from .engine import template
