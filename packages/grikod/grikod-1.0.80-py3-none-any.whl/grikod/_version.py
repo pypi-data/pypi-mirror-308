c433ef0641a82da4db3fd5ae2f3dbad875204cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:00 +0000

    Add package grikod/1.0.80/grikod-1.0.80-py3-none-any.whl

---
+++ packages/grikod/grikod-1.0.80-py3-none-any.whl/grikod/_version.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+__version__ = "1.0.80"
+__license__ = "Açık Kaynak: Open Source, MIT, Apache License 2.0"
+__description__ = "Grikod (Gri Kod, Gray Code, Grey Code)"
+__author__ = "Mehmet Keçeci"
+__url__ = "https://github.com/KuantumBS/grikod"
+__dependencies__ = ["python>=3.8"]
