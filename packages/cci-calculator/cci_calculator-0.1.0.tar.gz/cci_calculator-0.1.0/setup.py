bc9d9da836c9177d09cfb558c34de2829a473b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:45 +0000

    Add package cci-calculator/0.1.0/cci_calculator-0.1.0.tar.gz

---
+++ packages/cci-calculator/cci_calculator-0.1.0.tar.gz/cci_calculator-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cci_calculator",
+    version="0.1.0",
+    description="Charlson Comorbidity Index calculator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    authors=[
+        "Sokolowski, Piotr",
+        "Siegel, Fabian"
+        ],
+    author_email="sokol.to@gmail.com",
+    url="https://github.com/psokolo/cci/tree/main/cci_calculator",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=['json', 'typing'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
