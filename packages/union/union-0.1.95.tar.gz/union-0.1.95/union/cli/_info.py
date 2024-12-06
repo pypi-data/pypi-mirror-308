a49aafdae367953df67bdc504c445ff005731e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:50 +0000

    Add package union/0.1.95/union-0.1.95.tar.gz

---
+++ packages/union/union-0.1.95.tar.gz/union-0.1.95/union/cli/_info.py
@@ -0,0 +1,31 @@
+import os
+from importlib.metadata import version
+from textwrap import dedent
+
+import rich
+import rich_click as click
+from rich.panel import Panel
+
+from union._config import _UNION_CONFIG, _get_config_obj
+
+
+@click.command("info")
+@click.pass_context
+def info(ctx: click.Context):
+    config_obj = _get_config_obj()
+    endpoint = config_obj.platform.endpoint
+
+    union_version = version("union")
+    flytekit_version = version("flytekit")
+    context = dedent(
+        f"""\
+    union is the CLI to interact with Union. Use the CLI to register, create and track task and workflow executions locally and remotely.
+
+    Union Version    : {union_version}
+    Flytekit Version : {flytekit_version}
+    Union Endpoint   : {endpoint}"""  # noqa
+    )
+    if _UNION_CONFIG.config_source:
+        context += f"{os.linesep}Config Source    : {_UNION_CONFIG.config_source}"
+
+    rich.print(Panel(context, title="Union CLI Info", padding=(1, 1)))
