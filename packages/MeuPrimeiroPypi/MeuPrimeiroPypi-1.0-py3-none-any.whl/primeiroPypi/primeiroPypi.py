bdf553f38b8a5cba2af90b93d6202434152e3b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:56 +0000

    Add package MeuPrimeiroPypi/1.0/MeuPrimeiroPypi-1.0-py3-none-any.whl

---
+++ packages/MeuPrimeiroPypi/MeuPrimeiroPypi-1.0-py3-none-any.whl/primeiroPypi/primeiroPypi.py
@@ -0,0 +1,20 @@
+# primeiroPypi.py
+
+def imprime_Hello_world:
+   """
+   Ele imprime hello world
+   Returns:
+       string: 'HELLO WORLD'.
+   """
+   retorno = "HELLO WORLD"
+   return retorno
+
+def faz_de_novo:
+   """
+   Faz de novo ué
+   Returns:
+       string: 'FIZ DE NOVO'.
+   """
+   retorno = "FIZ DE NOVO"
+   return retorno
+
