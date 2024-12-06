c32742438f8f8ce6a65907310f7eb907835b7309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:19 +0000

    Add package py-brainrot/0.1.0/py_brainrot-0.1.0-py3-none-any.whl

---
+++ packages/py-brainrot/py_brainrot-0.1.0-py3-none-any.whl/brainrot/src/brainrot/core.py
@@ -0,0 +1,43 @@
+#build in functions
+
+def yap(*args, **kwargs):
+    print(*args, **kwargs)
+
+def Lit(*args, **kwargs):
+    abs(*args, **kwargs)
+
+def pluh(*args, **kwargs):
+    exit(*args, **kwargs)
+
+def rizz_counter(*args, **kwargs):
+    len(*args, **kwargs)
+
+def ratio(*args, **kwargs):
+    sum(*args, **kwargs)
+
+def GOAT(*args, **kwargs):
+    int(*args, **kwargs)
+
+def glow_up(*args, **kwargs):
+    str(*args, **kwargs)
+
+def vibe_check(*args, **kwargs):
+    input(*args, **kwargs)
+
+def giga_chad(*args, **kwargs):
+    max(*args, **kwargs)
+
+def beta(*args, **kwargs):
+    min(*args, **kwargs)
+
+def simp_for(*args, **kwargs):
+    map(*args, **kwargs)
+
+def main_character(*args, **kwargs):
+    open(*args, **kwargs)
+
+def bounce(*args, **kwargs):
+    enumerate(*args, **kwargs)
+
+def reality_check(*args, **kwargs):
+    isinstance(*args, **kwargs)
