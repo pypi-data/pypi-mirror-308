3b124cabd54ef063fa823c9b2b38ce32e34ea91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:18 +0000

    Add package gerk/1.0.0/gerk-1.0.0.tar.gz

---
+++ packages/gerk/gerk-1.0.0.tar.gz/gerk-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+import codecs
+import os
+
+VERSION = "1.0.0"
+DESCRIPTION = "Generalized Explicit Runge-Kutta"
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="gerk",
+    version=VERSION,
+    author="Yasser Naji",
+    author_email="yfnaji@gmail.com",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["numpy"],
+    keywords=[
+        "runge-kutta", 
+        "runge", 
+        "kutta", 
+        "numerical", 
+        "integration", 
+        "approximation"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
