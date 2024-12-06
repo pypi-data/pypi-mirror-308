67ffce01c62e3a9d39cf8202a5b8ed7457779d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:45 +0000

    Add package baise-mondrs-kalkuliatorius/0.1.0/baise_mondrs_kalkuliatorius-0.1.0.tar.gz

---
+++ packages/baise-mondrs-kalkuliatorius/baise_mondrs_kalkuliatorius-0.1.0.tar.gz/baise_mondrs_kalkuliatorius-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="baise_mondrs_kalkuliatorius",
+    version="0.1.0",
+    author="",
+    email="",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6"
+)
