fe7fcb15ac9ad30fb62472bf1ae81bd579afa71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:53 +0000

    Add package morfoUz-kutubxona/3.2/morfouz_kutubxona-3.2.tar.gz

---
+++ packages/morfoUz-kutubxona/morfouz_kutubxona-3.2.tar.gz/morfouz_kutubxona-3.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+   name='morfoUz_kutubxona',
+   version='3.2',
+   author='Alisher Ismailov Shakirovich',
+   author_email='alisherismailov1991@gmail.com',
+   description='''ushbu kutubxona o`zbek tili uchun qo`llaniladi. 
+   Kutubxonadan foydalangan holda tadqiqotchi o`zbek morfologik tahlilni amalga oshirish mumkin. 
+   Kutubxona foydalanuvchidan gap/matn kiritishni so`raydi va 
+   kiritilgan matnni morfologik tahlil qilingan holatda natijasini ko`rsatadi''',
+   packages=setuptools.find_packages(),
+   license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
