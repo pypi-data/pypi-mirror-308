909044e63d6ff6f506b2be8360fa10f4b905eb87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:44 +0000

    Add package ephios/0.18.2/ephios-0.18.2.tar.gz

---
+++ packages/ephios/ephios-0.18.2.tar.gz/ephios-0.18.2/ephios/api/filters.py
@@ -0,0 +1,54 @@
+import django_filters
+from django.utils.translation import gettext_lazy as _
+from django_filters import FilterSet, IsoDateTimeFilter, ModelChoiceFilter
+from guardian.shortcuts import get_objects_for_user
+from rest_framework.filters import BaseFilterBackend
+
+from ephios.core.models import Event, EventType, LocalParticipation
+
+
+class ParticipationPermissionFilter(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        events = get_objects_for_user(request.user, "core.view_event")
+        return queryset.filter(shift__event__in=events)
+
+
+class ShiftPermissionFilter(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        events = get_objects_for_user(request.user, "core.view_event")
+        return queryset.filter(event__in=events)
+
+
+class StartEndTimeFilterSet(FilterSet):
+    start_time = django_filters.rest_framework.IsoDateTimeFromToRangeFilter(label=_("start time"))
+    end_time = django_filters.rest_framework.IsoDateTimeFromToRangeFilter(label=_("end time"))
+
+    start_gte = IsoDateTimeFilter(
+        field_name="start_time",
+        lookup_expr="gte",
+        label=_("start time greater than equals (deprecated, use start_time_after instead)"),
+    )
+    start_lte = IsoDateTimeFilter(
+        field_name="start_time",
+        lookup_expr="lte",
+        label=_("start time less than equals (deprecated, use start_time_before instead)"),
+    )
+
+
+class ParticipationFilterSet(StartEndTimeFilterSet):
+    event_type = ModelChoiceFilter(
+        field_name="shift__event__type", label=_("event type"), queryset=EventType.objects.all()
+    )
+
+    class Meta:
+        model = LocalParticipation
+        fields = ["state"]
+
+
+class EventFilterSet(StartEndTimeFilterSet):
+
+    class Meta:
+        model = Event
+        fields = [
+            "type",
+        ]
