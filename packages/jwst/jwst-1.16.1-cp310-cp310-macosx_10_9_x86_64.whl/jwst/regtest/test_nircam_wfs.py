33339e1ad72e581c8f7f60d1e3b4a95f9fcee4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:52 +0000

    Add package jwst/1.16.1/jwst-1.16.1-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/jwst/jwst-1.16.1-cp310-cp310-macosx_10_9_x86_64.whl/jwst/regtest/test_nircam_wfs.py
@@ -0,0 +1,72 @@
+import os
+from pathlib import Path
+import pytest
+from astropy.io.fits.diff import FITSDiff
+
+from jwst.stpipe import Step
+
+
+@pytest.fixture(scope="module")
+def run_pipelines(rtdata_module):
+    """Run the calwebb_wfs-image2 and calwebb_wfs-image3 pipelines
+       on NIRCam WFS&C images. The calwebb_wfs-image3 pipeline is
+       run twice: once with default params and a second time with
+       the do_refine option turned on."""
+
+    rtdata = rtdata_module
+
+    # Get the image2 ASN and members
+    rtdata.get_asn("nircam/wavefront/jw00632-o003_20191210t194815_wfs-image2_001_asn.json")
+
+    # Run the calwebb_wfs-image2 pipeline
+    args = ["jwst.pipeline.Image2Pipeline", rtdata.input]
+    Step.from_cmdline(args)
+
+    # Get the image3 ASN file only, not the members. The members were just
+    # produced by the calwebb_wfs-image2 pipeline run.
+    rtdata.get_data("nircam/wavefront/jw00632-o003_20191210t194815_wfs-image3_001_asn.json")
+
+    # Run the calwebb_wfs-image3 pipeline with no refinement (default)
+    args = ["calwebb_wfs-image3", rtdata.input]
+    Step.from_cmdline(args)
+
+    # Get the second version of the image3 ASN, which has a unique output
+    # product name. Only get the ASN file, not the members.
+    rtdata.get_data("nircam/wavefront/jw00632-o003_20191210t194815_wfs-image3_002_asn.json")
+
+    # Run the calwebb_wfs-image3 pipeline with refinement
+    args = ["calwebb_wfs-image3", rtdata.input, "--do_refine=True"]
+    Step.from_cmdline(args)
+
+    return rtdata
+
+
+@pytest.mark.bigdata
+def test_nicam_wfsimage_noextras(run_pipelines):
+    """Ensure that i2d files are not created"""
+    i2ds = list(Path('.').glob('*i2d*'))
+
+    assert not i2ds
+
+
+@pytest.mark.bigdata
+@pytest.mark.parametrize("output",[
+    "jw00632003002_03105_00001_nrca4_cal.fits",
+    "jw00632003002_03105_00002_nrca4_cal.fits",
+    "jw00632-o003_t001_nircam_f212n-wlp8-nrca4_wfscmb-05.fits",
+    "jw00632-o003_t001_nircam_f212n-wlp8-nrca4_wfscmb-05_refine.fits"],
+    ids=["cal1", "cal2", "wfscmb1", "wfscmb2"])
+def test_nircam_wfsimage(run_pipelines, fitsdiff_default_kwargs, output):
+    """Regression test of the calwebb_wfs-image2 and calwebb_wfs-image3
+       pipelines on a dithered pair of NIRCam images."""
+
+    # Run the pipeline and retrieve outputs
+    rtdata = run_pipelines
+    rtdata.output = output
+
+    # Get the truth files
+    rtdata.get_truth(os.path.join("truth/nircam/test_wfs-image2and3", output))
+
+    # Compare the results
+    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
+    assert diff.identical, diff.report()
---
+++ packages/jwst/jwst-1.16.1-cp310-cp310-macosx_10_9_x86_64.whl/jwst/regtest/test_nirspec_steps_spec2.py
@@ -0,0 +1,110 @@
+"""Regression tests for NIRSpec IFU"""
+import pytest
+
+from astropy.io.fits.diff import FITSDiff
+import numpy as np
+
+import stdatamodels.jwst.datamodels as dm
+
+from jwst.flatfield import FlatFieldStep
+from jwst.flatfield.flat_field import nirspec_ifu
+from jwst.pathloss import PathLossStep
+
+# Define artifactory source and truth
+INPUT_PATH = 'nirspec/ifu'
+TRUTH_PATH = 'truth/test_nirspec_ifu'
+
+
+@pytest.mark.bigdata
+def test_nirspec_ifu_user_supplied_flat(rtdata, fitsdiff_default_kwargs):
+    """Test using predefined interpolated flat"""
+    with dm.open(rtdata.get_data('nirspec/ifu/nrs_ifu_nrs1_assign_wcs.fits')) as data:
+        with dm.open(rtdata.get_data('nirspec/ifu/nrs_ifu_nrs1_interpolated_flat.fits')) as user_supplied_flat:
+            nirspec_ifu(data, None, None, None, None, user_supplied_flat=user_supplied_flat)
+            rtdata.output = 'ff_using_interpolated.fits'
+            data.write(rtdata.output)
+
+    rtdata.get_truth(TRUTH_PATH + '/' + 'ff_using_interpolated.fits')
+    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
+    assert diff.identical, diff.report()
+
+
+@pytest.mark.bigdata
+def test_flat_field_step_user_supplied_flat(rtdata, fitsdiff_default_kwargs):
+    """Test providing a user-supplied flat field to the FlatFieldStep"""
+    data = rtdata.get_data('nirspec/ifu/nrs_ifu_nrs1_assign_wcs.fits')
+    user_supplied_flat = rtdata.get_data('nirspec/ifu/nrs_ifu_nrs1_interpolated_flat.fits')
+
+    data_flat_fielded = FlatFieldStep.call(data, user_supplied_flat=user_supplied_flat)
+    rtdata.output = 'flat_fielded_step_user_supplied.fits'
+    data_flat_fielded.write(rtdata.output)
+    del data_flat_fielded
+
+    rtdata.get_truth(TRUTH_PATH + '/' + 'flat_fielded_step_user_supplied.fits')
+    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
+    assert diff.identical, diff.report()
+
+
+@pytest.mark.slow
+@pytest.mark.bigdata
+def test_ff_inv(rtdata, fitsdiff_default_kwargs):
+    """Test flat field inversion"""
+    with dm.open(rtdata.get_data('nirspec/ifu/nrs_ifu_nrs1_assign_wcs.fits')) as data:
+        flatted = FlatFieldStep.call(data)
+        unflatted = FlatFieldStep.call(flatted, inverse=True)
+
+    # flat fielding may set some new NaN values - ignore these in test
+    is_nan = np.isnan(unflatted.data)
+    assert np.allclose(data.data[~is_nan], unflatted.data[~is_nan]), 'Inversion failed'
+
+    # make sure NaNs are only at do_not_use pixels
+    assert np.all(unflatted.dq[is_nan] & dm.dqflags.pixel['DO_NOT_USE'])
+
+    # make sure NaNs at science pixels are also NaN in error and var arrays
+    assert np.all(np.isnan(unflatted.err[is_nan]))
+    assert np.all(np.isnan(unflatted.var_poisson[is_nan]))
+    assert np.all(np.isnan(unflatted.var_rnoise[is_nan]))
+    assert np.all(np.isnan(unflatted.var_flat[is_nan]))
+
+
+@pytest.mark.slow
+@pytest.mark.bigdata
+def test_pathloss_corrpars(rtdata):
+    """Test PathLossStep using correction_pars"""
+    with dm.open(rtdata.get_data('nirspec/ifu/nrs1_flat_field.fits')) as data:
+        pls = PathLossStep()
+        corrected = pls.run(data)
+
+        pls.use_correction_pars = True
+        corrected_corrpars = pls.run(data)
+
+    assert np.allclose(corrected.data, corrected_corrpars.data, equal_nan=True)
+
+
+@pytest.mark.slow
+@pytest.mark.bigdata
+def test_pathloss_inverse(rtdata):
+    """Test PathLossStep using correction_pars"""
+    with dm.open(rtdata.get_data('nirspec/ifu/nrs1_flat_field.fits')) as data:
+        pls = PathLossStep()
+        corrected = pls.run(data)
+
+        pls.inverse = True
+        corrected_inverse = pls.run(corrected)
+        non_nan = ~np.isnan(corrected_inverse.data)
+
+    assert np.allclose(corrected.data[non_nan], corrected_inverse.data[non_nan])
+
+
+@pytest.mark.slow
+@pytest.mark.bigdata
+def test_pathloss_source_type(rtdata):
+    """Test PathLossStep forcing source type"""
+    with dm.open(rtdata.get_data('nirspec/ifu/nrs1_flat_field.fits')) as data:
+        pls = PathLossStep()
+        pls.source_type = 'extended'
+        pls.run(data)
+
+    assert np.allclose(pls.correction_pars.data,
+                       pls.correction_pars.pathloss_uniform,
+                       equal_nan=True)
