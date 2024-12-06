9b56599532d3af0f2ea07adda519e6c99a28eb87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:06:09 +0000

    Add package adhawk/6.4/adhawk-6.4.tar.gz

---
+++ packages/adhawk/adhawk-6.4.tar.gz/adhawk-6.4/adhawkapi/types/__init__.py
@@ -0,0 +1,37 @@
+'''This module defines all of the APIs to Adhawk's embedded devices'''
+
+from .error import (
+    CommunicationError,
+    Error,
+    PortNotFoundError,
+    RecoverableCommunicationError
+)
+from .publicapi import (
+    REQUEST_TIMEOUT,
+    AckCodes,
+    APIRequestError,
+    BlobType,
+    BlobVersion,
+    CameraResolution,
+    CameraType,
+    CameraUserSettings,
+    EventControlBit,
+    Events,
+    EyeTrackingStreamBits,
+    EyeTrackingStreamData,
+    EyeTrackingStreamTypes,
+    FeatureStreamBits,
+    FeatureStreamData,
+    FeatureStreamTypes,
+    EyeMask,
+    LogMode,
+    MarkerSequenceMode,
+    PacketType,
+    ProcedureType,
+    PropertyType,
+    StreamControlBit,
+    StreamRates,
+    SystemControlType,
+    SystemInfo,
+    errormsg
+)
