934fe43f17adc1c8da9b9d659882d04f54e793d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:56 +0000

    Add package qiskit-ibm-ai-local-transpiler/0.1.2/qiskit_ibm_ai_local_transpiler-0.1.2-cp311-none-win_amd64.whl

---
+++ packages/qiskit-ibm-ai-local-transpiler/qiskit_ibm_ai_local_transpiler-0.1.2-cp311-none-win_amd64.whl/qiskit_ibm_ai_local_transpiler/permutation/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# (C) Copyright 2023 IBM. All Rights Reserved.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from .inference import PermutationInference
+from .utils import PERMUTATION_COUPLING_MAPS_BY_HASHES_DICT
