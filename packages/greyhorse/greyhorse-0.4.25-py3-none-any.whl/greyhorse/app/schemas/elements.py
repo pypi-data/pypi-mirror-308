aa63990e2bcb546ef282e8cc1a4a2c27cc54346a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:24 +0000

    Add package greyhorse/0.4.25/greyhorse-0.4.25-py3-none-any.whl

---
+++ packages/greyhorse/greyhorse-0.4.25-py3-none-any.whl/greyhorse/app/schemas/elements.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+from pydantic import AliasChoices, BaseModel, Field, model_validator
+
+from ..abc.controllers import Controller
+from ..abc.services import Service
+
+
+class CtrlConf(BaseModel, frozen=True):
+    type_: type[Controller] = Field(validation_alias=AliasChoices('type'))
+    name: str | None = None
+    args: dict[str, Any] = Field(default_factory=dict)
+    resources: list[type] = Field(default_factory=list)
+
+    @model_validator(mode='before')
+    def _setup_name(self: dict[str, Any]) -> dict:
+        if 'name' not in self:
+            self['name'] = self['type'].__name__
+        return self
+
+
+class SvcConf(BaseModel, frozen=True):
+    type_: type[Service] = Field(validation_alias=AliasChoices('type'))
+    name: str | None = None
+    args: dict[str, Any] = Field(default_factory=dict)
+    resources: list[type] = Field(default_factory=list)
+
+    @model_validator(mode='before')
+    def _setup_name(self: dict[str, Any]) -> dict:
+        if 'name' not in self:
+            self['name'] = self['type'].__name__
+        return self
