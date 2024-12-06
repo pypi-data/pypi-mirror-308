9af14be9741f9000e2e552b680e7f4efd4ba0f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:01 +0000

    Add package documente_shared/0.1.5/documente_shared-0.1.5.tar.gz

---
+++ packages/documente_shared/documente_shared-0.1.5.tar.gz/documente_shared-0.1.5/documente_shared/domain/enums.py
@@ -0,0 +1,10 @@
+from documente_shared.domain.base_enum import BaseEnum
+
+
+class DocumentProcessStatus(BaseEnum):
+    PENDING = 'PENDING'
+    ENQUEUED = 'ENQUEUED'
+    PROCESSING = 'PROCESSING'
+    COMPLETED = 'COMPLETED'
+    FAILED = 'FAILED'
+
