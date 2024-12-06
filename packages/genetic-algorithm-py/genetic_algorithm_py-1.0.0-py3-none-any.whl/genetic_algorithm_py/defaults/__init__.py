901d5a5c9d29fd35d4d4e3ac5db09aade0e31af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:42 +0000

    Add package genetic-algorithm-py/1.0.0/genetic_algorithm_py-1.0.0-py3-none-any.whl

---
+++ packages/genetic-algorithm-py/genetic_algorithm_py-1.0.0-py3-none-any.whl/genetic_algorithm_py/defaults/__init__.py
@@ -0,0 +1,4 @@
+from .fitness_function import MaximizeOnesFitness, MinimizeDistanceFitness, WeightedSumFitness
+from .selection import RouletteWheelSelection, TournamentSelection, StochasticUniversalSampling
+from .crossover import OnePointCrossover, UniformCrossover
+from .mutation import SwapMutation, GaussianMutation, PolynomialMutation
