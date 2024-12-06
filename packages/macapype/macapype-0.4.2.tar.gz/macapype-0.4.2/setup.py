b9914bb5ba8ca677927ed9f0f11e8707a1804fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:08 +0000

    Add package macapype/0.4.2/macapype-0.4.2.tar.gz

---
+++ packages/macapype/macapype-0.4.2.tar.gz/macapype-0.4.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+from setuptools import find_packages, setup
+
+
+required_packages = [
+    "nipype", "nilearn", "networkx",
+    "pybids", "scikit-image", "nibabel",
+    "numpy", "brain-slam"]
+
+verstr = "unknown"
+try:
+    verstrline = open('macapype/_version.py', "rt").read()
+except EnvironmentError:
+    pass # Okay, there is no version file.
+else:
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        verstr = mo.group(1)
+    else:
+        raise RuntimeError("unable to find version in yourpackage/_version.py")
+
+print("Will not build conda module")
+
+setup(
+    name="macapype",
+    version=verstr,
+    packages=find_packages(),
+    author="macatools team",
+    description="Pipeline for anatomic processing for macaque",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='BSD 3',
+    entry_points={
+        'console_scripts': ['segment_pnh = workflows.segment_pnh:main']},
+    install_requires=required_packages,
+    include_package_data=True)
