5fe90e5936ee9cee67d56790edf2091a1b81d722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:52 +0000

    Add package pytamaro/1.0.0/pytamaro-1.0.0-py3-none-any.whl

---
+++ packages/pytamaro/pytamaro-1.0.0-py3-none-any.whl/pytamaro/__init__.py
@@ -0,0 +1,24 @@
+"""
+(Re-)Export names for:
+- basic types (Graphic and Color)
+- color names
+- operations
+- primitves
+- I/O
+from this top-level module.
+"""
+
+
+from pytamaro.color import *
+from pytamaro.color_functions import *
+from pytamaro.color_names import *
+from pytamaro.graphic import *
+from pytamaro.io import *
+from pytamaro.operations import *
+from pytamaro.point import *
+from pytamaro.point_names import *
+from pytamaro.primitives import *
+
+LANGUAGE = "en"
+
+__version__ = "0.7.3"
