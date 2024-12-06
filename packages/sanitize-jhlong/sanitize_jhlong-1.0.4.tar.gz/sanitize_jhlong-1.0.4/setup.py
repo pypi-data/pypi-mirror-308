45af6463209ddc5b266989921ce10aa9a436dcbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:06:54 +0000

    Add package sanitize-jhlong/1.0.4/sanitize_jhlong-1.0.4.tar.gz

---
+++ packages/sanitize-jhlong/sanitize_jhlong-1.0.4.tar.gz/sanitize_jhlong-1.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sanitize_jhlong',
+    version='1.0.4',
+    description='处理计时数据的一个函数',
+    long_description=open("README.md", encoding="utf-8").read(), # 从 README.md 文件读取长描述
+    long_description_content_type="text/markdown",  # 指定描述文件的格式
+    py_modules=['sanitize_jhlong'],
+    author='jhlong',
+    author_email='jhlong2024@163.com',
+)
+
