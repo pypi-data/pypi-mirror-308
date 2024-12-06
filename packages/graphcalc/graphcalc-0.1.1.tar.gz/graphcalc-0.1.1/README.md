cadd4183c65570c4a08cc9c47936cb5e17411fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:26 +0000

    Add package graphcalc/0.1.1/graphcalc-0.1.1.tar.gz

---
+++ packages/graphcalc/graphcalc-0.1.1.tar.gz/graphcalc-0.1.1/README.md
@@ -0,0 +1,21 @@
+# graphcalc
+
+## Overview
+
+`graphcalc` is a Python package for performing a variety of graph computations, including maximum clique detection, chromatic number calculation, and vertex cover identification. It is built on top of `networkx` and provides efficient implementations of fundamental graph theory algorithms.
+
+## Features
+
+- **Maximum Clique**: Finds the maximum clique in a given graph.
+- **Chromatic Number**: Computes the minimum number of colors required for graph coloring.
+- **Vertex and Edge Cover**: Determines vertex and edge covers.
+- **Matching and Independence**: Calculates maximum matching and independent sets.
+
+## Installation
+
+To install `graphcalc`, make sure you have Python 3.7 or higher, then install it:
+
+```bash
+pip install graphcalc
+```
+
---
+++ packages/graphcalc/graphcalc-0.1.1.tar.gz/graphcalc-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Read requirements from requirements.txt
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="graphcalc",
+    version="0.1.1",
+    author="Randy Davila",
+    author_email="rrd6@rice.edu",
+    description="A Python package for graph computation functions",
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/randydavila/graphcalc",
+    packages=find_packages(),
+    install_requires=requirements,  # Use requirements from the file
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    keywords="graph theory, networkx, graph computation",
+)
