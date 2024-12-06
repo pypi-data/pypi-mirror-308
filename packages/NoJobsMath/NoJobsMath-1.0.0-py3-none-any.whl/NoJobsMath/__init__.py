e5215b3b932f29d249173bf628da4b06a8df0e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:21 +0000

    Add package NoJobsMath/1.0.0/NoJobsMath-1.0.0-py3-none-any.whl

---
+++ packages/NoJobsMath/NoJobsMath-1.0.0-py3-none-any.whl/NoJobsMath/__init__.py
@@ -0,0 +1,14 @@
+__copyright__    = 'Copyright (C) 2024 No Jobs Technologies'
+__version__      = '1.0.0'
+__license__      = 'MIT'
+__author__       = 'No Jobs Technologies'
+__author_email__ = 'javacommmons@gmail.com'
+__url__          = 'https://github.com/no-jobs/no-jobs-math'
+__all__ = ['NoJobsMath']
+
+class NoJobsMath:
+    def __init__(self):
+        pass
+    def add2(self, x, y):
+        answer = x + y
+        return answer
