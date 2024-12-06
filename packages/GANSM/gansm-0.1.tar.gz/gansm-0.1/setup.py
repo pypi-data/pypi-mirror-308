aee4a166bb9ab108224ef710a24fb515d698ce39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:17 +0000

    Add package GANSM/0.1/gansm-0.1.tar.gz

---
+++ packages/GANSM/gansm-0.1.tar.gz/gansm-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "GANSM",
+    version = '0.1',
+    packages = find_packages(),
+    install_requires=[
+        "torch>=1.9.0",
+        "torchvision>=0.10.0",
+        "tensorboard>=2.5.0",
+    ],
+)
