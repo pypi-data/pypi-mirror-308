c98b637abfbcfe4f854e0fd4bc078f6e465131dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:58 +0000

    Add package lenlab/8.0b2/lenlab-8.0b2-py3-none-any.whl

---
+++ packages/lenlab/lenlab-8.0b2-py3-none-any.whl/lenlab/flash.py
@@ -0,0 +1,32 @@
+import logging
+from importlib import resources
+
+from PySide6.QtSerialPort import QSerialPort, QSerialPortInfo
+
+import lenlab
+
+from .bsl import BootstrapLoader, Programmer
+from .launchpad import find_vid_pid
+from .loop import Loop
+from .terminal import Terminal
+
+logger = logging.getLogger(__name__)
+
+
+def flash():
+    firmware_bin = (resources.files(lenlab) / "lenlab_fw.bin").read_bytes()
+
+    port_infos = QSerialPortInfo.availablePorts()
+    matches = find_vid_pid(port_infos)  # let's test discovery and give all launchpad ports
+    if not matches:
+        logger.error("No Launchpad found")
+        return
+
+    programmer = Programmer([BootstrapLoader(Terminal(QSerialPort(port_info))) for port_info in matches])
+    programmer.message.connect(logger.info)
+    programmer.error.connect(logger.error)
+
+    programmer.program(firmware_bin)
+    loop = Loop()
+    event = loop.run_until(programmer.success, programmer.error, timeout=600)
+    assert event, "At least one bootstrap loader did neither emit an error nor the success signal"
