44d61b6c5ba9581cbbe2ff507acc8296c5819003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:15 +0000

    Add package zhinst-qcodes/0.5.5/zhinst_qcodes-0.5.5.tar.gz

---
+++ packages/zhinst-qcodes/zhinst_qcodes-0.5.5.tar.gz/zhinst_qcodes-0.5.5/src/zhinst/qcodes/__init__.py
@@ -0,0 +1,51 @@
+"""QCoDeS Drivers for Zurich Instruments devices."""
+
+from zhinst.qcodes.session import ZISession
+from zhinst.qcodes.device_creator import (
+    HDAWG,
+    MFLI,
+    MFIA,
+    PQSC,
+    SHFQA,
+    SHFQC,
+    SHFSG,
+    UHFLI,
+    UHFQA,
+    ZIDevice,
+    HF2,
+)
+
+from zhinst.toolkit import (
+    Waveforms,
+    CommandTable,
+    Sequence,
+    PollFlags,
+    AveragingMode,
+    SHFQAChannelMode,
+)
+
+try:
+    from zhinst.qcodes._version import version as __version__
+except ModuleNotFoundError:
+    pass
+
+__all__ = [
+    "ZISession",
+    "HDAWG",
+    "MFLI",
+    "MFIA",
+    "PQSC",
+    "SHFQA",
+    "SHFQC",
+    "SHFSG",
+    "UHFLI",
+    "UHFQA",
+    "ZIDevice",
+    "HF2",
+    "Waveforms",
+    "CommandTable",
+    "Sequence",
+    "PollFlags",
+    "AveragingMode",
+    "SHFQAChannelMode",
+]
