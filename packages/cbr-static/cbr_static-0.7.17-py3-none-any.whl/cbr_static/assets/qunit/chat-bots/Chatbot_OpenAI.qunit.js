fe2c55b13a324975f4e38e17cde0cc1419395ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:22 +0000

    Add package cbr-static/0.7.17/cbr_static-0.7.17-py3-none-any.whl

---
+++ packages/cbr-static/cbr_static-0.7.17-py3-none-any.whl/cbr_static/assets/qunit/chat-bots/Chatbot_OpenAI.qunit.js
@@ -0,0 +1,22 @@
+import Chatbot_OpenAI   from '../../webc/chat-bot/Chatbot_OpenAI.js'
+import WebC__Target_Div from "../../webc/utils/WebC__Target_Div.js";
+
+QUnit.module('Chatbot_OpenAI', function(hooks) {
+
+    let chatbot_openai
+    let target_div
+
+    hooks.before(async (assert) => {
+        chatbot_openai = Chatbot_OpenAI
+        target_div = WebC__Target_Div.add_to_body().build({width: "50%"})        
+        target_div.append_child(chatbot_openai)
+        //await webc_events_viewer.load_datatables_css() 
+    });
+ 
+
+    QUnit.test('constructor', (assert) => {
+        assert.ok(true)
+        //assert.ok(chatbot_openai instanceof Chatbot_OpenAI)
+        //assert.ok(Chatbot_OpenAI.prototype instanceof Web_Component);
+    })
+})
