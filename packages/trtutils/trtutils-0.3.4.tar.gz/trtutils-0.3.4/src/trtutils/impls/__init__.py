09254ed69ef661bcddc3c7dae5898dd69400e308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:21 +0000

    Add package trtutils/0.3.4/trtutils-0.3.4.tar.gz

---
+++ packages/trtutils/trtutils-0.3.4.tar.gz/trtutils-0.3.4/src/trtutils/impls/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2024 Justin Davis (davisjustin302@gmail.com)
+#
+# MIT License
+"""
+Submodule of implementations for TRTModels.
+
+Submodules
+----------
+common
+    Implementations which are generic to underlying models.
+yolo
+    Implementations of YOLO object detectors.
+
+"""
+
+from __future__ import annotations
+
+import contextlib
+
+from . import common
+
+__all__ = ["common"]
+
+# import yolo models
+with contextlib.suppress(ImportError):
+    from . import yolo
+
+    __all__ += ["yolo"]
