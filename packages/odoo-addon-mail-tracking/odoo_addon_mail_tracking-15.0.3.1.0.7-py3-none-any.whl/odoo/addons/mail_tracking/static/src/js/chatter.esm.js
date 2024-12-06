e713d373e1b1da4ed75c94b84bebad90c5d37ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:14 +0000

    Add package odoo-addon-mail-tracking/15.0.3.1.0.7/odoo_addon_mail_tracking-15.0.3.1.0.7-py3-none-any.whl

---
+++ packages/odoo-addon-mail-tracking/odoo_addon_mail_tracking-15.0.3.1.0.7-py3-none-any.whl/odoo/addons/mail_tracking/static/src/js/chatter.esm.js
@@ -0,0 +1,35 @@
+/** @odoo-module **/
+import {
+    registerFieldPatchModel,
+    registerInstancePatchModel,
+} from "@mail/model/model_core";
+import {attr} from "@mail/model/model_field";
+
+registerInstancePatchModel(
+    "mail.chatter",
+    "mail/static/src/models/chatter/chatter.esm.js",
+    {
+        async refresh() {
+            this._super(...arguments);
+            this.thread.refreshMessagefailed();
+        },
+        toggleMessageFailedBoxVisibility() {
+            this.update({
+                isMessageFailedBoxVisible: !this.isMessageFailedBoxVisible,
+            });
+        },
+        _onThreadIdOrThreadModelChanged() {
+            this._super(...arguments);
+            this.thread.refreshMessagefailed();
+        },
+    }
+);
+registerFieldPatchModel(
+    "mail.chatter",
+    "mail/static/src/models/chatter/chatter.esm.js",
+    {
+        isMessageFailedBoxVisible: attr({
+            default: true,
+        }),
+    }
+);
