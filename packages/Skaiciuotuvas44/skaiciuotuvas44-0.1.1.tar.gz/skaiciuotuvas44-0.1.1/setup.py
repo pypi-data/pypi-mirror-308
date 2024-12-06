4ab5d67150d5c5d28823128823d72ff173875fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:06:33 +0000

    Add package Skaiciuotuvas44/0.1.1/skaiciuotuvas44-0.1.1.tar.gz

---
+++ packages/Skaiciuotuvas44/skaiciuotuvas44-0.1.1.tar.gz/skaiciuotuvas44-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Skaiciuotuvas44",
+    version="0.1.1",
+    author_email="balvakokay@gmail.com",
+    email="balvakokay@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=0.0.1"
+)
