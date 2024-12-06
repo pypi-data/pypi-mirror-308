8e9e66a733cbc1481052ebee40eef5dddc493b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:07:21 +0000

    Add package zutool/0.2.0/zutool-0.2.0.tar.gz

---
+++ packages/zutool/zutool-0.2.0.tar.gz/zutool-0.2.0/zutool/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from .api import get_otenki_asp, get_pain_status, get_weather_point, get_weather_status
+
+__version__ = "0.2.0"
+__all__ = (
+    "get_otenki_asp",
+    "get_pain_status",
+    "get_weather_point",
+    "get_weather_status",
+    "set_weather_point",
+)
