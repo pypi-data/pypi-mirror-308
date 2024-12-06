666003a141acb937a4a1576d1075322c0a224e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:49 +0000

    Add package shuffle-sdk/0.0.2/shuffle_sdk-0.0.2.tar.gz

---
+++ packages/shuffle-sdk/shuffle_sdk-0.0.2.tar.gz/shuffle_sdk-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shuffle_sdk',  # Name of the package
+    version='0.0.2',  
+    description='The SDK used for Shuffle',  
+    py_modules=["shuffle_sdk"],  # Name of the module
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown',  
+    author='Fredrik Saito Odegaardstuen',  
+    author_email='frikky@shuffler.io',  
+    url='https://github.com/shuffle/shuffle',  
+    packages=find_packages(),  
+    install_requires=[  
+        'requests',
+    ],
+    classifiers=[  
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',  # Specify Python version requirements
+)
