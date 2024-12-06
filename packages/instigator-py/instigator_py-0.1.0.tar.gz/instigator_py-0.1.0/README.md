2a472eacfef5b5597a1ebac4e309242d799b336b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:51 +0000

    Add package instigator-py/0.1.0/instigator_py-0.1.0.tar.gz

---
+++ packages/instigator-py/instigator_py-0.1.0.tar.gz/instigator_py-0.1.0/README.md
@@ -0,0 +1,7 @@
+# Instigator Py
+
+This project generates templates and provides a CLI interface.
+
+Create your own package called instigator_py with an interactive CLI that prompts for project details (like dxh-py), here’s a guide to building and publishing it on PyPI.
+
+The package will help users set up boilerplate configurations for a Django or Python project by answering questions and generating a folder structure and files based on those inputs.
---
+++ packages/instigator-py/instigator_py-0.1.0.tar.gz/instigator_py-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="instigator_py",
+    version="0.1.0",
+    description="CLI tool for creating boilerplate projects",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Sachin",
+    author_email="schnaror@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "instigator_py=instigator_py.cli:main",
+        ],
+    },
+)
+
+
