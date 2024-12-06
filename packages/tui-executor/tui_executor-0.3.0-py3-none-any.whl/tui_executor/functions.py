89f79b13b0ff46b9e6042543890def596921dee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:47 +0000

    Add package tui-executor/0.3.0/tui_executor-0.3.0-py3-none-any.whl

---
+++ packages/tui-executor/tui_executor-0.3.0-py3-none-any.whl/tui_executor/functions.py
@@ -0,0 +1,107 @@
+"""
+This module provides functionality to find specific functions, i.e. functions that are decorated
+with the `@exec_task` or `@exec_recurring_task` decorators. The former, also called 'ui_button_functions'
+are tasks that are associated with a button in the UI, while the latter are functions that are periodically
+executed in the background.
+"""
+__all__ = [
+    "find_ui_functions",
+    "get_ui_button_functions",
+    "get_ui_recurring_functions",
+]
+
+import importlib
+import inspect
+import sys
+import traceback
+from typing import Callable
+from typing import Dict
+from typing import List
+
+from textual import log
+
+from .tasks import TaskKind
+
+
+def get_ui_button_functions(module_path: str) -> Dict[str, Callable]:
+    """
+    Returns a dictionary with function names as keys and the callable function as their value.
+    The functions are intended to be used as UI button callable, i.e. a UI can automatically
+    identify these functions and assign them to a `clicked` action of a button.
+
+    Args:
+        module_path: string containing a fully qualified module name
+    """
+    return find_ui_functions(
+        module_path,
+        lambda x: x.__ui_kind__ & TaskKind.BUTTON
+    )
+
+
+def get_ui_recurring_functions(module_path: str) -> Dict[str, Callable]:
+    """
+    Returns a dictionary with function names as keys and the callable function as their value.
+    The functions are intended to be used as recurring callables, i.e. the GUI will call these
+    functions from a QTimer when the timer times out.
+
+    Args:
+        module_path: string containing a fully qualified module name
+    """
+    return find_ui_functions(
+        module_path,
+        lambda x: x.__ui_kind__ & TaskKind.RECURRING
+    )
+
+
+def find_ui_functions(module_path: str, predicate: Callable = None) -> Dict[str, Callable]:
+    """
+    Returns a dictionary with function names as keys and the callable function as their value.
+    The predicate is a function that returns True or False depending on some required conditions
+    for the functions that are returned.
+
+    Args:
+        module_path: string containing a fully qualified module name
+        predicate: condition to select and return the function
+    """
+    predicate = predicate if predicate is not None else lambda x: True
+    mod = importlib.import_module(module_path)
+
+    return {
+        name: member
+        for name, member in inspect.getmembers(mod)
+        if inspect.isfunction(member) and hasattr(member, "__ui_kind__") and predicate(member)
+    }
+
+
+def run_function(func: Callable, args: List, kwargs: Dict, runnable_type: int = None,
+                 notify: Callable = lambda x: ...):
+
+    runnable_type = runnable_type or func.__ui_runnable__
+
+    try:
+        response = func(*args, **kwargs)
+    except Exception as exc:
+        # TODO: This shall be sent to the Output console with proper formatting
+        notify(f"Caught {exc.__class__.__name__}: {exc}")
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback_details = traceback.extract_tb(exc_traceback)
+        err_msg = ""
+        for filename, lineno, fn, text in traceback_details:
+            err_msg += (
+                f"In File    : {filename}\n"
+                f"At Line    : {lineno}\n"
+                f"In Function: {fn}\n"
+                f"Code       : {text}\n"
+                f"Exception  : {exc_value}\n"
+            )
+        notify(err_msg)
+    else:
+        parameters = ""
+        if args:
+            parameters = ', '.join(args)
+        if kwargs:
+            if parameters:
+                parameters += ', '
+            parameters += ', '.join([f"{k}={v}" for k, v in kwargs.items()])
+
+        notify(f"run_function: {func.__name__}({parameters}) -> {response = }")
---
+++ packages/tui-executor/tui_executor-0.3.0-py3-none-any.whl/tui_executor/panels.py
@@ -0,0 +1,167 @@
+from __future__ import annotations
+
+import importlib
+from typing import Dict
+
+from rich.console import RenderableType
+from textual.app import ComposeResult
+from textual.containers import VerticalScroll
+from textual.widget import Widget
+from textual.widgets import Collapsible
+from textual.widgets import Label
+from textual.widgets import RichLog
+from textual.widgets import Static
+from textual.widgets import TabPane
+
+from tui_executor.funcpars import Parameter
+from tui_executor.funcpars import get_parameters
+from tui_executor.functions import get_ui_button_functions
+from tui_executor.modules import get_ui_modules
+from tui_executor.tasks import TaskButton
+from tui_executor.utils import format_datetime
+
+
+class ArgumentsPanel(Widget):
+    """
+    A panel that lays out input fields for function parameters. The type of input field
+    is determined by the type hint for the parameter. If no type hint is given, `str` is used
+    as a default.
+
+    The input fields for the function parameters are pre-filled with the default value if that
+    default is available.
+
+    The panel contains two buttons at the lower right:
+
+    - a button to execute the task with the arguments filled into the input fields -> Run.
+    - a button to close the panel -> Close.
+
+    The class is called ArgumentsPanel instead of ParametersPanel because the values that you fill
+    are the arguments to a function.
+    """
+
+    def __init__(self, button: TaskButton):
+        super().__init__()
+
+        self._button = button
+        self._pars: Dict[str, Parameter] = get_parameters(button.function)
+
+    def compose(self) -> ComposeResult:
+
+        with VerticalScroll():
+            for name, parameter in self._pars.items():
+                yield Label(name)
+
+
+class ModulePanel(Static):
+    """
+    A collapsible panel that contains a button for each task that is defined in that module.
+
+    The task buttons are generated from the decorated functions in a Python module. One Python
+    module (`*.py` file) will correspond to one ModulePanel. The functions are decorated with
+    @exec_task (or the deprecated @exec_ui).
+
+    The title of the collapsible widget is the module name unless the variable UI_MODULE_DISPLAY_NAME
+    was defined in the module.
+    """
+    def __init__(self, name: str, module_path: str):
+        super().__init__(name=name)
+
+        self.module_path = module_path
+        self.functions = get_ui_button_functions(module_path=module_path)
+
+        self.log.info(f"ModulePanel: {self.module_path = }, {self.functions = }")
+
+    def compose(self) -> ComposeResult:
+
+        # Our functions are all decorated functions, decorated with the @exec_ui or @exec_task.
+        # Since we have used functools.wraps(), all our functions have the attribute __wrapped__
+        # which points to the original function. What we need is the first line of the function
+        # in the module file, because we want the functions to be sorted in the order they appear
+        # in the source code file and not alphabetically.
+
+        with Collapsible(title=self.name) as section:
+            mod = importlib.import_module(self.module_path)
+            section.tooltip = mod.__doc__
+            for func_name, func in sorted(self.functions.items(), key=lambda x: x[1].__ui_lineno__):
+                yield TaskButton(func_name, func)
+
+    def is_empty(self):
+        return len(self.functions) == 0
+
+
+class PackagePanel(TabPane):
+    """
+    A TAB pane that contains all task buttons for each module in the package. The buttons are organised
+    in a collapsible ModulePanel. Since there can be quite a lot of task buttons when the module panels
+    are expanded.
+
+    All (sub-)packages are TABs in the TUI. Sub-packages only go one level deep, and we start from the
+    module path. So, if our package hierarchy is `tasks.shared.unit_tests` where this package contains
+    Python module files (`*.py`), then module_path will be `tasks.shared` and this will generate the TAB
+    `unit_tests` (unless there is a `UI_TAB_DISPLAY_NAME` defined in the `__init__.py` of that package).
+
+    If the module_path doesn't have any sub-packages,
+    Args:
+        title: The title of the TabPane (will be displayed in a TabLabel)
+        module_path: the full dotted module path for this package
+    """
+    def __init__(self, title: str, module_path: str):
+        super().__init__(title=title)
+
+        self.module_path = module_path
+        self.modules = get_ui_modules(module_path=module_path)
+        self.panels = {}
+
+        self.log.info(f"PackagePanel: {self.module_path = }, {self.modules = }")
+
+    def compose(self) -> ComposeResult:
+
+        with VerticalScroll():
+
+            self.panels = self._create_module_panels()
+            for panel_name in sorted(self.panels):
+                yield self.panels[panel_name]
+
+    def is_empty(self):
+        return len(self.modules) == 0
+
+    def _create_module_panels(self):
+        """
+        Creates all collapsible panels for the modules in this package. The reason to do this before adding them to
+        the TabPane is that the panels now can be sorted before adding.
+
+        Returns:
+            A dictionary containing all Collapsible ModulePanel with the display name as their key.
+        """
+
+        panels = {}
+
+        for module_name, module in self.modules.items():
+            display_name, dotted_path = module
+            self.log.info(f"PackagePanel: {module_name = }, {display_name = }, {dotted_path = }")
+            panel = ModulePanel(name=display_name, module_path=dotted_path)
+            if not panel.is_empty():
+                panels[display_name] = panel
+
+        return panels
+
+
+class ConsoleOutput(RichLog):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.level_width = 10
+        """The column width of the logging level, used to properly align log messages."""
+        self.info = f"[green]{'INFO':{self.level_width}}[/]"
+        self.warning = f"[dark_orange]{'WARNING':{self.level_width}}[/]"
+        self.error = f"[red1]{'ERROR':{self.level_width}}[/]"
+
+    def write_log_info(self, msg: RenderableType | str):
+        self.write(f"{format_datetime(fmt='%Y-%m-%d %H:%M:%S')}: {self.info} {msg}")
+
+    def write_log_warning(self, msg: RenderableType | str):
+        self.write(f"{format_datetime(fmt='%Y-%m-%d %H:%M:%S')}: {self.warning} {msg}")
+
+    def write_log_error(self, msg: RenderableType | str):
+        self.write(f"{format_datetime(fmt='%Y-%m-%d %H:%M:%S')}: {self.error} {msg}")
