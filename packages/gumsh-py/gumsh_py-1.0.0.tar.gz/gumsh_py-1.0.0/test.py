90d8fa433d0e3c9ec7fc97d91ce366916b64fa93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:04 +0000

    Add package gumsh-py/1.0.0/gumsh_py-1.0.0.tar.gz

---
+++ packages/gumsh-py/gumsh_py-1.0.0.tar.gz/gumsh_py-1.0.0/test.py
@@ -0,0 +1,7 @@
+from gumpy import gumpy
+
+# gumpy.choose(prompt="favorite languages?", options=["python", "javascript", "java", "c++", "c#"])
+# gumpy.spin(command="sleep 5", show_output=False, spinner="dot", text="Sleeping...")
+# print(gumpy.confirm(prompt="Please?"))
+# gumpy.get_input(prompt="Enter your name:", placeholder="John Doe")
+gumpy.log(text="Hello, World!", level="info", time="ansic")
