f1e262921128057ae8f6af640c40f9a261a39298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:34 +0000

    Add package instabase-aihub/0.6.0/instabase_aihub-0.6.0-py3-none-any.whl

---
+++ packages/instabase-aihub/instabase_aihub-0.6.0-py3-none-any.whl/aihub/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from aihub.api.queries_api import QueriesApi
+from aihub.api.batches_api import BatchesApi
+from aihub.api.conversations_api import ConversationsApi
+from aihub.api.jobs_api import JobsApi
+from aihub.api.runs_api import RunsApi
+
