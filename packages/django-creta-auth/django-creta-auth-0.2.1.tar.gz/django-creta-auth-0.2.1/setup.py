f60927f59f56c4351d5884b054107ad921ced9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:34 +0000

    Add package django-creta-auth/0.2.1/django-creta-auth-0.2.1.tar.gz

---
+++ packages/django-creta-auth/django-creta-auth-0.2.1.tar.gz/django-creta-auth-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-creta-auth",
+    version="0.2.1",
+    packages=find_packages(),
+    install_requires=[
+        "django>=3.0",
+        "requests",
+    ],
+    author="Runners Co., Ltd.",
+    author_email="dev@runners.im",
+    description="A boilerplate Django app package for easy integration",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/RUNNERS-IM/django-creta-auth",
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
