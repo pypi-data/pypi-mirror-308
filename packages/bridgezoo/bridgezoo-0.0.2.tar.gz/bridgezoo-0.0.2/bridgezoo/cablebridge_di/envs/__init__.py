05b46103dc05d9330a5589abccf68ea9ae5432ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:06:25 +0000

    Add package bridgezoo/0.0.2/bridgezoo-0.0.2.tar.gz

---
+++ packages/bridgezoo/bridgezoo-0.0.2.tar.gz/bridgezoo-0.0.2/bridgezoo/cablebridge_di/envs/__init__.py
@@ -0,0 +1,8 @@
+from gymnasium.envs.registration import register
+
+
+register(
+    id='cable_bridge_di-v0',
+    entry_point='bridgezoo.cablebridge_di.envs.cablebridge_base:CableBridgeBase',
+    max_episode_steps=200,
+)
