6c47e7c489f0ca098e6c54f447d783657089ff72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:07:01 +0000

    Add package refuel/0.15.12/refuel-0.15.12-py3-none-any.whl

---
+++ packages/refuel/refuel-0.15.12-py3-none-any.whl/refuel/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+import os
+import refuel
+
+
+@pytest.fixture
+def refuel_client():
+    options = {
+        "api_key": os.environ.get("REFUEL_TEST_API_KEY"),
+        "project": "test_sdk",
+    }
+    return refuel.init(**options)
