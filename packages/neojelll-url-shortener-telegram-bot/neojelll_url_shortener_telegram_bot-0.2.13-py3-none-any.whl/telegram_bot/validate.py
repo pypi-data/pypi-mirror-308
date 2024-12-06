9437020361b35cc0a9c06a715b4239d3323ded00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:02 +0000

    Add package neojelll-url-shortener-telegram-bot/0.2.13/neojelll_url_shortener_telegram_bot-0.2.13-py3-none-any.whl

---
+++ packages/neojelll-url-shortener-telegram-bot/neojelll_url_shortener_telegram_bot-0.2.13-py3-none-any.whl/telegram_bot/validate.py
@@ -0,0 +1,30 @@
+async def is_valid_message(message: str) -> dict | str:
+	lst: list[str] = message.split()
+
+	if len(lst) > 3:
+		return 'Вы ввели слишком много параметров или поставили лишний пробел в сообщении попробуйте еще раз'
+	
+	if len(lst) == 1:
+		return {'url': lst[0]}
+	
+	elif len(lst) == 2:
+		if not lst[1].isalnum():
+			return "Похоже что то не так с префиксом попробуйте еще раз"
+		return {'url': lst[0], 'prefix': lst[1]}
+	
+	else:
+		if lst[1] == '-':
+			if not lst[2].isdigit():
+				return 'Похоже что то не так с последним параметром (это должно быть число) попробуте еще раз'
+			return {'url': lst[0], 'expiration': lst[2]}
+			
+		else:
+
+			if not lst[1].isalnum():
+				return "Похоже что то не так с префиксом попробуйте еще раз"
+	
+			elif not lst[2].isdigit():
+				return 'Похоже что то не так с последним параметром (это должно быть число) попробуте еще раз'
+		
+			else:
+				return {'url': lst[0], 'prefix': lst[1], 'expiration': lst[2]}
