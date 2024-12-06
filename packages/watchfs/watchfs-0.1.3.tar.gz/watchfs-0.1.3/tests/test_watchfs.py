b3e5d6a249f62d245957bfcd40abe36152edc591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:46 +0000

    Add package watchfs/0.1.3/watchfs-0.1.3.tar.gz

---
+++ packages/watchfs/watchfs-0.1.3.tar.gz/watchfs-0.1.3/tests/test_watchfs.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import tomllib
+from pathlib import Path
+
+from watchfs import __version__
+
+with Path("pyproject.toml").open("rb") as f:
+    project_info = tomllib.load(f)
+
+
+def test_version():
+    assert __version__ == project_info["project"]["version"]
