1541f4c3fa75521144128f0f2da0e4e46047600d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:19 +0000

    Add package buco-db-controller/0.1.98/buco_db_controller-0.1.98.tar.gz

---
+++ packages/buco-db-controller/buco_db_controller-0.1.98.tar.gz/buco_db_controller-0.1.98/buco_db_controller/models/fixture_stats.py
@@ -0,0 +1,16 @@
+
+
+class FixtureStats:
+    def __init__(self, fixture_id: int, home: dict, away: dict):
+        self.fixture_id: int = fixture_id
+        self.home: dict = home
+        self.away: dict = away
+
+    @classmethod
+    def from_dict(cls, response):
+        fixture_id = response['parameters']['fixture']
+
+        home = response['data']['home']
+        away = response['data']['away']
+
+        return cls(fixture_id=fixture_id, home=home, away=away)
---
+++ packages/buco-db-controller/buco_db_controller-0.1.98.tar.gz/buco_db_controller-0.1.98/buco_db_controller/models/teams_stats.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from buco_db_controller.models.league import League
+
+
+class TeamStats:
+    def __init__(
+            self,
+            fixture_id: int,
+            season: int,
+            date: datetime,
+
+            league: League,
+            league_round: str,
+            team_id: int,
+
+            team_stats: dict
+    ):
+        self.fixture_id: int = fixture_id
+        self.date: datetime = date
+        self.season: int = season
+        self.team_id: int = team_id
+        self.league: League = league
+        self.league_round: str = league_round
+        self.team_stats: dict = team_stats
+
+    @classmethod
+    def from_dict(cls, response):
+        fixture_id = response['parameters']['fixture']
+
+        date = response['parameters']['date']
+        season = response['parameters']['season']
+        league_round = response['parameters']['round']
+        team_id = response['parameters']['team']
+
+        data = response['data']
+
+        return cls(
+            fixture_id=fixture_id,
+            season=season,
+            date=datetime.strptime(date, '%Y-%m-%d'),
+
+            league=League(league_id=data['league']['id'], name=data['league']['name']),
+            league_round=league_round,
+            team_id=team_id,
+
+            team_stats=data,
+        )
