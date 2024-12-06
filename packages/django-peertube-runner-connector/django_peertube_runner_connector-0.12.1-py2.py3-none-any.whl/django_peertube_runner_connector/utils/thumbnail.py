603075c42041b8792ec6515bab4faa7535329b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:15 +0000

    Add package django-peertube-runner-connector/0.12.1/django_peertube_runner_connector-0.12.1-py2.py3-none-any.whl

---
+++ packages/django-peertube-runner-connector/django_peertube_runner_connector-0.12.1-py2.py3-none-any.whl/django_peertube_runner_connector/utils/thumbnail.py
@@ -0,0 +1,38 @@
+"""Thumbnail utilities."""
+
+import tempfile
+
+import ffmpeg
+
+from django_peertube_runner_connector.models import Video, VideoFile
+from django_peertube_runner_connector.storage import video_storage
+
+from .ffprobe import get_video_stream
+from .files import get_video_directory
+
+
+def build_video_thumbnails(video=Video, video_file=VideoFile, existing_probe=None):
+    """Create a video thumbnails with ffmpeg and save it to a file."""
+    video_url = video_storage.url(video_file.filename)
+
+    if get_video_stream(video_url, existing_probe=existing_probe) is None:
+        return None
+
+    thumbnail_filename = get_video_directory(video, "thumbnail.jpg")
+
+    with tempfile.NamedTemporaryFile(suffix=".jpg") as temp_file:
+        output_path = temp_file.name
+
+        input_stream = ffmpeg.input(video_url)
+
+        output_stream = ffmpeg.filter(
+            input_stream,
+            "select",
+            "gte(n, 0)",
+        ).output(output_path, vframes=1)
+
+        ffmpeg.run(output_stream, overwrite_output=True, quiet=True)
+        with open(output_path, "rb") as thumbnail_file:
+            thumbnail_filename = video_storage.save(thumbnail_filename, thumbnail_file)
+
+        return thumbnail_filename
