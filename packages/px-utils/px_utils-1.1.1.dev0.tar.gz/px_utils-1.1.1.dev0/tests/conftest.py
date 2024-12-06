7f56d380030c8cfa20406261eed810c82ae131f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:40 +0000

    Add package px-utils/1.1.1.dev0/px_utils-1.1.1.dev0.tar.gz

---
+++ packages/px-utils/px_utils-1.1.1.dev0.tar.gz/px_utils-1.1.1.dev0/tests/conftest.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def tmp_dir(tmp_path):
+    r = tmp_path / "root"
+    a = r / "subdir-a"
+    ac = a / "subdir-ac"
+    ac.mkdir(parents=True)
+    b = r / "subdir-b"
+    b.mkdir()
+    (a / "a-file.txt").touch()
+    (a / "a-picture.png").touch()
+    (b / "b-file.txt").touch()
+    (b / "b-picture.png").touch()
+    (ac / "ac-file.txt").touch()
+    (ac / "ac-picture.png").touch()
+    cwd = Path.cwd()
+    os.chdir(tmp_path.absolute())
+    yield tmp_path
+    os.chdir(cwd)
