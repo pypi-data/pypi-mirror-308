69fcfd847a876cdf0bd66fa66f53338c0efda3fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:20 +0000

    Add package polotsk/1.0.36/polotsk-1.0.36-py3-none-any.whl

---
+++ packages/polotsk/polotsk-1.0.36-py3-none-any.whl/polotsk-admin/main.py
@@ -0,0 +1,29 @@
+from optparse import OptionParser
+from os import path
+from shutil import copytree
+
+
+def main():
+    usage = "Params: -a/--app app"
+    parser = OptionParser(usage=usage)
+    parser.add_option(
+        "-a",
+        "--app",
+        dest="app",
+        help="Application name",
+        metavar="FILE",
+    )
+    (options, _) = parser.parse_args()
+    if not options.app:
+        print(usage)
+        exit(1)
+    from_dir = path.join(path.dirname(path.abspath(__file__)), "app")
+    to_dir = path.join(path.abspath("./"), str(options.app))
+    if path.exists(to_dir):
+        print(f"Dir `{to_dir}` already exists")
+        exit(1)
+    copytree(
+        from_dir,
+        to_dir,
+        False,
+    )
