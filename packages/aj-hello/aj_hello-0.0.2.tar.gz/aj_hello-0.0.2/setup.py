767ff9f7bef769e775db56d684953cc60bb6b0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:05:20 +0000

    Add package aj-hello/0.0.2/aj_hello-0.0.2.tar.gz

---
+++ packages/aj-hello/aj_hello-0.0.2.tar.gz/aj_hello-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aj_hello",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="Akbar Jorayev",
+    author_email="akbarjonjorayev001@gmail.com",
+)
