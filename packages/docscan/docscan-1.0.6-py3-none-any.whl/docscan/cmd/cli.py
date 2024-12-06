69d908932c386068380411adc9169eee43e8e959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:05 +0000

    Add package docscan/1.0.6/docscan-1.0.6-py3-none-any.whl

---
+++ packages/docscan/docscan-1.0.6-py3-none-any.whl/docscan/cmd/cli.py
@@ -0,0 +1,26 @@
+import glob
+import imghdr
+import os
+import sys
+
+import click
+import rembg
+
+from ..doc import scan
+
+
+@click.command()
+@click.argument(
+    "input", default=(None if sys.stdin.isatty() else "-"), type=click.File("rb")
+)
+@click.argument(
+    "output",
+    default=(None if sys.stdin.isatty() else "-"),
+    type=click.File("wb", lazy=True),
+)
+def main(input, output):
+    output.write(scan(rembg.remove(input.read())))
+
+
+if __name__ == "__main__":
+    main()
