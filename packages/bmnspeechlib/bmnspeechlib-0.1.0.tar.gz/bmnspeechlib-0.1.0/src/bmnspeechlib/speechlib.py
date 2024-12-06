41d2ade86ceeffc1baada09b5ed0a1fa7f3806e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:19 +0000

    Add package bmnspeechlib/0.1.0/bmnspeechlib-0.1.0.tar.gz

---
+++ packages/bmnspeechlib/bmnspeechlib-0.1.0.tar.gz/bmnspeechlib-0.1.0/src/bmnspeechlib/speechlib.py
@@ -0,0 +1,268 @@
+from .core_analysis import (core_analysis)
+from .hf_access import ACCESS_TOKEN
+from .re_encode import (re_encode)
+from .convert_to_mono import (convert_to_mono)
+from .convert_to_wav import (convert_to_wav)
+
+class Transcriptor:
+
+    def __init__(self, file, log_folder, language, modelSize, voices_folder=None, quantization=False, max_speakers=10, pyannote_model="pyannote/speaker-diarization@2.1", hf_token=ACCESS_TOKEN, skip_diarization=False):
+        '''transcribe a wav file 
+        
+            arguments:
+
+            file: name of wav file with extension ex: file.wav
+
+            log_folder: name of folder where transcript will be stored
+
+            language: language of wav file
+
+            modelSize: tiny, small, medium, large, large-v1, large-v2, large-v3 (bigger model is more accurate but slow!!)
+
+            voices_folder: folder containing subfolders named after each speaker with speaker voice samples in them. This will be used for speaker recognition
+
+            quantization: whether to use int8 quantization or not (default=False)
+
+            see documentation: https://github.com/Navodplayer1/speechlib
+        
+            
+        supported languages:  
+        #### Afrikaans
+        "af",
+        #### Amharic
+        "am",
+        #### Arabic
+        "ar",
+        #### Assamese
+        "as",
+        #### Azerbaijani
+        "az",
+        #### Bashkir
+        "ba",
+        #### Belarusian
+        "be",
+        #### Bulgarian
+        "bg",
+        #### Bengali
+        "bn",
+        #### Tibetan
+        "bo",
+        #### Breton
+        "br",
+        #### Bosnian
+        "bs",
+        #### Catalan
+        "ca",
+        #### Czech
+        "cs",
+        #### Welsh
+        "cy",
+        #### Danish
+        "da",
+        #### German
+        "de",
+        #### Greek
+        "el",
+        #### English
+        "en",
+        #### Spanish
+        "es",
+        #### Estonian
+        "et",
+        #### Basque
+        "eu",
+        #### Persian
+        "fa",
+        #### Finnish
+        "fi",
+        #### Faroese
+        "fo",
+        #### French
+        "fr",
+        #### Galician
+        "gl",
+        #### Gujarati
+        "gu",
+        #### Hausa
+        "ha",
+        #### Hawaiian
+        "haw",
+        #### Hebrew
+        "he",
+        #### Hindi
+        "hi",
+        #### Croatian
+        "hr",
+        #### Haitian
+        "ht",
+        #### Hungarian
+        "hu",
+        #### Armenian
+        "hy",
+        #### Indonesian
+        "id",
+        #### Icelandic
+        "is",
+        #### Italian
+        "it",
+        #### Japanese
+        "ja",
+        #### Javanese
+        "jw",
+        #### Georgian
+        "ka",
+        #### Kazakh
+        "kk",
+        #### Khmer
+        "km",
+        #### Kannada
+        "kn",
+        #### Korean
+        "ko",
+        #### Latin
+        "la",
+        #### Luxembourgish
+        "lb",
+        #### Lingala
+        "ln",
+        #### Lao
+        "lo",
+        #### Lithuanian
+        "lt",
+        #### Latvian
+        "lv",
+        #### Malagasy
+        "mg",
+        #### Maori
+        "mi",
+        #### Macedonian
+        "mk",
+        #### Malayalam
+        "ml",
+        #### Mongolian
+        "mn",
+        #### Marathi
+        "mr",
+        #### Malay
+        "ms",
+        #### Maltese
+        "mt",
+        #### Burmese
+        "my",
+        #### Nepali
+        "ne",
+        #### Dutch
+        "nl",
+        #### Norwegian Nynorsk
+        "nn",
+        #### Norwegian
+        "no",
+        #### Occitan
+        "oc",
+        #### Punjabi
+        "pa",
+        #### Polish
+        "pl",
+        #### Pashto
+        "ps",
+        #### Portuguese
+        "pt",
+        #### Romanian
+        "ro",
+        #### Russian
+        "ru",
+        #### Sanskrit
+        "sa",
+        #### Sindhi
+        "sd",
+        #### Sinhalese
+        "si",
+        #### Slovak
+        "sk",
+        #### Slovenian
+        "sl",
+        #### Shona
+        "sn",
+        #### Somali
+        "so",
+        #### Albanian
+        "sq",
+        #### Serbian
+        "sr",
+        #### Sundanese
+        "su",
+        #### Swedish
+        "sv",
+        #### Swahili
+        "sw",
+        #### Tamil
+        "ta",
+        #### Telugu
+        "te",
+        #### Tajik
+        "tg",
+        #### Thai
+        "th",
+        #### Turkmen
+        "tk",
+        #### Tagalog
+        "tl",
+        #### Turkish
+        "tr",
+        #### Tatar
+        "tt",
+        #### Ukrainian
+        "uk",
+        #### Urdu
+        "ur",
+        #### Uzbek
+        "uz",
+        #### Vietnamese
+        "vi",
+        #### Yiddish
+        "yi",
+        #### Yoruba
+        "yo",
+        #### Chinese
+        "zh",
+        #### Cantonese
+        "yue",
+        '''
+        self.file = file
+        self.voices_folder = voices_folder
+        self.language = language
+        self.log_folder = log_folder
+        self.modelSize = modelSize
+        self.quantization = quantization
+        self.max_speakers = max_speakers
+        self.pyannote_model = pyannote_model
+        self.hf_token = hf_token
+        self.skip_diarization = skip_diarization
+
+    def transcribe(self):
+        segments_list, entry, overlaps = core_analysis(self.file, self.voices_folder, self.log_folder, self.language, self.modelSize, self.quantization, self.max_speakers, self.pyannote_model, self.hf_token, self.skip_diarization)
+        return segments_list, entry, overlaps
+
+class PreProcessor:
+    '''
+    class for preprocessing audio files.
+
+    methods:
+
+    re_encode(file) -> re-encode file to 16-bit PCM encoding  
+
+    convert_to_mono(file) -> convert file from stereo to mono  
+
+    mp3_to_wav(file) -> convert mp3 file to wav format  
+
+    '''
+
+    def re_encode(self, file):
+        re_encode(file)
+    
+    def convert_to_mono(self, file):
+        convert_to_mono(file)
+
+    def convert_to_wav(self, file):
+        path = convert_to_wav(file)
+        return path
