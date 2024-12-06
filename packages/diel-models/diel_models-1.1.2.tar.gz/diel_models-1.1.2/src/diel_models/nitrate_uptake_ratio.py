78f67b0b49a375e3a5d1c72e9a6da0e4236c6304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:20 +0000

    Add package diel-models/1.1.2/diel_models-1.1.2.tar.gz

---
+++ packages/diel-models/diel_models-1.1.2.tar.gz/diel_models-1.1.2/src/diel_models/nitrate_uptake_ratio.py
@@ -0,0 +1,58 @@
+from typing import List
+
+from cobra import Model
+from diel_models.pipeline import Step
+
+
+class NitrateUptakeRatioCalibrator(Step):
+
+    def __init__(self, model: Model, id_nitrate_uptake_reaction_day: List[str],
+                 id_nitrate_uptake_reaction_night: List[str]) -> None:
+        """
+        Constructor
+
+        Parameters
+        ----------
+        model: cobra.Model
+            Metabolic model
+        id_nitrate_uptake_reaction_day: List[str]
+            Identification of day nitrate uptake reaction(s) - in case of for example, multi tissue models.
+        id_nitrate_uptake_reaction_night: List[str]
+            Identification of night nitrate uptake reaction(s) - in case of for example, multi tissue models.
+        """
+        self.model: Model = model
+        self.id_nitrate_uptake_reaction_day: List[str] = id_nitrate_uptake_reaction_day
+        self.id_nitrate_uptake_reaction_night: List[str] = id_nitrate_uptake_reaction_night
+
+    def ratio_set(self) -> None:
+        """
+        This function establishes a 3:2 ratio of nitrate uptake between day and night, respectively.
+
+        Raises:
+            ValueError: If the nitrate uptake reaction ID is not present in the given model.
+        """
+        for nitrate_uptake_day_reaction, nitrate_uptake_night_reaction in \
+                zip(self.id_nitrate_uptake_reaction_day, self.id_nitrate_uptake_reaction_night):
+            if self.model.reactions.has_id(nitrate_uptake_day_reaction) and \
+                    self.model.reactions.has_id(nitrate_uptake_night_reaction):
+                nitrate_uptake_reaction_day = self.model.reactions.get_by_id(nitrate_uptake_day_reaction)
+                nitrate_uptake_reaction_night = self.model.reactions.get_by_id(nitrate_uptake_night_reaction)
+
+                same_flux = self.model.problem.Constraint(
+                    nitrate_uptake_reaction_day.flux_expression * 2 -
+                    nitrate_uptake_reaction_night.flux_expression * 3, lb=0, ub=0)
+                self.model.add_cons_vars(same_flux)
+
+            else:
+                raise ValueError("id_nitrate_uptake_reaction not present in the model that was given.")
+
+    def run(self) -> Model:
+        """
+        Executes the method of the class NitrateUptakeRatioCalibrator.
+        """
+        self.ratio_set()
+
+        return self.model
+
+    def validate(self) -> None:
+        pass
