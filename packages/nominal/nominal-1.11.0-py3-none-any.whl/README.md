6ee98024becaa9a17bae7c84560d4bbfef14f31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:17 +0000

    Add package nominal/1.11.0/nominal-1.11.0-py3-none-any.whl

---
+++ packages/nominal/nominal-1.11.0-py3-none-any.whl/README.md
@@ -0,0 +1,19 @@
+# ⬖ `nominal`
+
+Nominal Python SDK for test data, storage, and compute.
+
+### 📖 Docs
+View on github pages: [nominal-io.github.io/nominal-client/](https://nominal-io.github.io/nominal-client/).
+
+### 📦 Install
+```sh
+pip install nominal
+```
+
+### 💻 Usage
+
+```py
+import nominal as nm
+```
+
+See examples in the [documentation](https://nominal-io.github.io/nominal-client/usage/examples).
---
+++ packages/nominal/nominal-1.11.0-py3-none-any.whl/nominal/core/run.py
@@ -0,0 +1,251 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from datetime import datetime
+from types import MappingProxyType
+from typing import Iterable, Mapping, Protocol, Sequence, cast
+
+from typing_extensions import Self
+
+from nominal._api.combined import attachments_api, scout, scout_catalog, scout_run_api
+from nominal.core._clientsbunch import HasAuthHeader
+from nominal.core._utils import HasRid, rid_from_instance_or_string, update_dataclass
+from nominal.core.attachment import Attachment, _iter_get_attachments
+from nominal.core.connection import Connection
+from nominal.core.dataset import Dataset, _get_datasets
+from nominal.core.log import LogSet
+from nominal.core.video import Video
+from nominal.ts import IntegralNanosecondsUTC, _SecondsNanos
+
+
+@dataclass(frozen=True)
+class Run(HasRid):
+    rid: str
+    name: str
+    description: str
+    properties: Mapping[str, str]
+    labels: Sequence[str]
+    start: IntegralNanosecondsUTC
+    end: IntegralNanosecondsUTC | None
+    run_number: int
+
+    _clients: _Clients = field(repr=False)
+
+    class _Clients(Dataset._Clients, Attachment._Clients, HasAuthHeader, Protocol):
+        @property
+        def attachment(self) -> attachments_api.AttachmentService: ...
+        @property
+        def catalog(self) -> scout_catalog.CatalogService: ...
+        @property
+        def run(self) -> scout.RunService: ...
+
+    @property
+    def nominal_url(self) -> str:
+        """Returns a link to the page for this Run in the Nominal app"""
+        # TODO (drake): move logic into _from_conjure() factory function to accomodate different URL schemes
+        return f"https://app.gov.nominal.io/runs/{self.run_number}"
+
+    def add_dataset(self, ref_name: str, dataset: Dataset | str) -> None:
+        """Add a dataset to this run.
+
+        Datasets map "ref names" (their name within the run) to a Dataset (or dataset rid). The same type of datasets
+        should use the same ref name across runs, since checklists and templates use ref names to reference datasets.
+        """
+        self.add_datasets({ref_name: dataset})
+
+    def add_log_set(self, ref_name: str, log_set: LogSet | str) -> None:
+        """Add a log set to this run.
+
+        Log sets map "ref names" (their name within the run) to a Log set (or log set rid).
+        """
+        self.add_log_sets({ref_name: log_set})
+
+    def add_log_sets(self, log_sets: Mapping[str, LogSet | str]) -> None:
+        """Add multiple log sets to this run.
+
+        Log sets map "ref names" (their name within the run) to a Log set (or log set rid).
+        """
+        data_sources = {
+            ref_name: scout_run_api.CreateRunDataSource(
+                data_source=scout_run_api.DataSource(log_set=rid_from_instance_or_string(log_set)),
+                series_tags={},
+                offset=None,
+            )
+            for ref_name, log_set in log_sets.items()
+        }
+        self._clients.run.add_data_sources_to_run(self._clients.auth_header, data_sources, self.rid)
+
+    def add_datasets(self, datasets: Mapping[str, Dataset | str]) -> None:
+        """Add multiple datasets to this run.
+
+        Datasets map "ref names" (their name within the run) to a Dataset (or dataset rid). The same type of datasets
+        should use the same ref name across runs, since checklists and templates use ref names to reference datasets.
+        """
+        # TODO(alkasm): support series tags & offset
+        data_sources = {
+            ref_name: scout_run_api.CreateRunDataSource(
+                data_source=scout_run_api.DataSource(dataset=rid_from_instance_or_string(dataset)),
+                series_tags={},
+                offset=None,
+            )
+            for ref_name, dataset in datasets.items()
+        }
+        self._clients.run.add_data_sources_to_run(self._clients.auth_header, data_sources, self.rid)
+
+    def _iter_list_datasets(self) -> Iterable[tuple[str, Dataset]]:
+        run = self._clients.run.get_run(self._clients.auth_header, self.rid)
+        dataset_rids_by_ref_name = {}
+        for ref_name, source in run.data_sources.items():
+            if source.data_source.type == "dataset":
+                dataset_rid = cast(str, source.data_source.dataset)
+                dataset_rids_by_ref_name[ref_name] = dataset_rid
+        datasets_by_rids = {
+            ds.rid: Dataset._from_conjure(self._clients, ds)
+            for ds in _get_datasets(self._clients.auth_header, self._clients.catalog, dataset_rids_by_ref_name.values())
+        }
+        for ref_name, rid in dataset_rids_by_ref_name.items():
+            dataset = datasets_by_rids[rid]
+            yield (ref_name, dataset)
+
+    def list_datasets(self) -> Sequence[tuple[str, Dataset]]:
+        """List the datasets associated with this run.
+        Returns (ref_name, dataset) pairs for each dataset.
+        """
+        return list(self._iter_list_datasets())
+
+    def add_attachments(self, attachments: Iterable[Attachment] | Iterable[str]) -> None:
+        """Add attachments that have already been uploaded to this run.
+
+        `attachments` can be `Attachment` instances, or attachment RIDs.
+        """
+        rids = [rid_from_instance_or_string(a) for a in attachments]
+        request = scout_run_api.UpdateAttachmentsRequest(attachments_to_add=rids, attachments_to_remove=[])
+        self._clients.run.update_run_attachment(self._clients.auth_header, request, self.rid)
+
+    def remove_attachments(self, attachments: Iterable[Attachment] | Iterable[str]) -> None:
+        """Remove attachments from this run.
+        Does not remove the attachments from Nominal.
+
+        `attachments` can be `Attachment` instances, or attachment RIDs.
+        """
+        rids = [rid_from_instance_or_string(a) for a in attachments]
+        request = scout_run_api.UpdateAttachmentsRequest(attachments_to_add=[], attachments_to_remove=rids)
+        self._clients.run.update_run_attachment(self._clients.auth_header, request, self.rid)
+
+    def _iter_list_attachments(self) -> Iterable[Attachment]:
+        run = self._clients.run.get_run(self._clients.auth_header, self.rid)
+        for a in _iter_get_attachments(self._clients.auth_header, self._clients.attachment, run.attachments):
+            yield Attachment._from_conjure(self._clients, a)
+
+    def list_attachments(self) -> Sequence[Attachment]:
+        return list(self._iter_list_attachments())
+
+    def archive(self) -> None:
+        """Archive this run.
+        Archived runs are not deleted, but are hidden from the UI.
+        """
+        self._clients.run.archive_run(self._clients.auth_header, self.rid)
+
+    def update(
+        self,
+        *,
+        name: str | None = None,
+        start: datetime | IntegralNanosecondsUTC | None = None,
+        end: datetime | IntegralNanosecondsUTC | None = None,
+        description: str | None = None,
+        properties: Mapping[str, str] | None = None,
+        labels: Sequence[str] | None = None,
+    ) -> Self:
+        """Replace run metadata.
+        Updates the current instance, and returns it.
+        Only the metadata passed in will be replaced, the rest will remain untouched.
+
+        Note: This replaces the metadata rather than appending it. To append to labels or properties, merge them before
+        calling this method. E.g.:
+
+            new_labels = ["new-label-a", "new-label-b"]
+            for old_label in run.labels:
+                new_labels.append(old_label)
+            run = run.update(labels=new_labels)
+        """
+        request = scout_run_api.UpdateRunRequest(
+            description=description,
+            labels=None if labels is None else list(labels),
+            properties=None if properties is None else dict(properties),
+            start_time=None if start is None else _SecondsNanos.from_flexible(start).to_scout_run_api(),
+            end_time=None if end is None else _SecondsNanos.from_flexible(end).to_scout_run_api(),
+            title=name,
+            assets=[],
+        )
+        response = self._clients.run.update_run(self._clients.auth_header, request, self.rid)
+        run = self.__class__._from_conjure(self._clients, response)
+        update_dataclass(self, run, fields=self.__dataclass_fields__)
+        return self
+
+    def remove_data_sources(
+        self,
+        *,
+        ref_names: Sequence[str] | None = None,
+        data_sources: Sequence[Connection | Dataset | Video | str] | None = None,
+    ) -> None:
+        """Remove data sources from this run.
+
+        The list data_sources can contain Connection, Dataset, Video instances, or rids as string.
+        """
+        ref_names = ref_names or []
+        data_source_rids = {rid_from_instance_or_string(ds) for ds in data_sources or []}
+
+        conjure_run = self._clients.run.get_run(self._clients.auth_header, self.rid)
+
+        data_sources_to_keep = {
+            ref_name: scout_run_api.CreateRunDataSource(
+                data_source=rds.data_source,
+                series_tags=rds.series_tags,
+                offset=rds.offset,
+            )
+            for ref_name, rds in conjure_run.data_sources.items()
+            if ref_name not in ref_names
+            and (rds.data_source.dataset or rds.data_source.connection or rds.data_source.video) not in data_source_rids
+        }
+
+        response = self._clients.run.update_run(
+            self._clients.auth_header,
+            scout_run_api.UpdateRunRequest(
+                assets=[],
+                data_sources=data_sources_to_keep,
+            ),
+            self.rid,
+        )
+        run = self.__class__._from_conjure(self._clients, response)
+        update_dataclass(self, run, fields=self.__dataclass_fields__)
+
+    def add_connection(self, ref_name: str, connection: Connection | str) -> None:
+        """Add a connection to this run.
+
+        Ref_name maps "ref name" (the name within the run) to a Connection (or connection rid). The same type of
+        connection should use the same ref name across runs, since checklists and templates use ref names to reference
+        connections.
+        """
+        # TODO(alkasm): support series tags & offset
+        data_sources = {
+            ref_name: scout_run_api.CreateRunDataSource(
+                data_source=scout_run_api.DataSource(connection=rid_from_instance_or_string(connection)),
+                series_tags={},
+                offset=None,
+            )
+        }
+        self._clients.run.add_data_sources_to_run(self._clients.auth_header, data_sources, self.rid)
+
+    @classmethod
+    def _from_conjure(cls, clients: _Clients, run: scout_run_api.Run) -> Self:
+        return cls(
+            rid=run.rid,
+            name=run.title,
+            description=run.description,
+            properties=MappingProxyType(run.properties),
+            labels=tuple(run.labels),
+            start=_SecondsNanos.from_scout_run_api(run.start_time).to_nanoseconds(),
+            end=(_SecondsNanos.from_scout_run_api(run.end_time).to_nanoseconds() if run.end_time else None),
+            run_number=run.run_number,
+            _clients=clients,
+        )
