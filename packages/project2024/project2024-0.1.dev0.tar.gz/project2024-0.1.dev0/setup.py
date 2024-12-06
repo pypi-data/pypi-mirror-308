761e553edde0fabde1b8c9e98c3c96cb1bacc5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:17 +0000

    Add package project2024/0.1.dev0/project2024-0.1.dev0.tar.gz

---
+++ packages/project2024/project2024-0.1.dev0.tar.gz/project2024-0.1.dev0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name= 'project2024' ,
+	version = '0.1dev' ,
+	packages = ['project2024' ,] ,
+	license = 'UIC',
+	long_description = open("README.txt").read(),
+
+	
+)
