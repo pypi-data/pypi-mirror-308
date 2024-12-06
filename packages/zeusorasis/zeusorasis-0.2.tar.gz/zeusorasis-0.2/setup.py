970bef14931ba4d7b1a1baae1814ab590d1f87bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:07 +0000

    Add package zeusorasis/0.2/zeusorasis-0.2.tar.gz

---
+++ packages/zeusorasis/zeusorasis-0.2.tar.gz/zeusorasis-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeusorasis',
+    version='0.2',
+    description='Library to detect eye movements',
+    author='Robinson Gauci',
+    packages=find_packages(),
+)
---
+++ packages/zeusorasis/zeusorasis-0.2.tar.gz/zeusorasis-0.2/zeusorasis/__init__.py
@@ -0,0 +1,2 @@
+from .process_batch_code import process_batch
+from .process_sightpro_code import process_sightpro
