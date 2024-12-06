1634a23338d56ae1bfdc9c1947bb3c284c278a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:13 +0000

    Add package XeprAPI/0.61.1/XeprAPI-0.61.1-py3-none-any.whl

---
+++ packages/XeprAPI/XeprAPI-0.61.1-py3-none-any.whl/XeprAPI/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# (c) Copyright 2014, Bruker BioSpin
+#
+# Modified by Sam Schott, University of Cambridge 2019.
+# Modified by Hugo Karas, ETH Zurich 2023.
+#
+
+from .main import (
+    Xepr, getXeprInstances,
+    DatasetError, DimensionError, ExperimentError, ParameterError,
+)
+
+
+__version__ = "0.61.1"
+__author__ = "Bruker Biospin"
