81da0d3bd13279528b3c8edbd391c7c153c8b836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:28 +0000

    Add package json-schema-mapper/0.0.2/json_schema_mapper-0.0.2-py3-none-any.whl

---
+++ packages/json-schema-mapper/json_schema_mapper-0.0.2-py3-none-any.whl/json_schema_mapper/type_mapper/dict_open_mapper.py
@@ -0,0 +1,35 @@
+from typing import Dict, Any, List
+
+from json_schema_mapper.mapper_types import MappedType
+from json_schema_mapper.type_mapper.base_type_mapper import BaseTypeMapper
+
+
+class DictOpenMapper(BaseTypeMapper):
+    """Dict open character Mapper."""
+
+    def __init__(self, mapped_type_stack: List[MappedType]):
+        super().__init__(mapped_type_stack)
+
+    def perform_mapping_on_list(self, json_schema: str) -> str:
+        dict_open_index = json_schema.find('{')
+        new_json_schema = json_schema[dict_open_index + 1:]
+
+        attr_name = None
+
+        if dict_open_index:
+            attr_name = json_schema[:dict_open_index].split(':')[0]
+
+        new_mapped_type = MappedType(
+            attr_name=attr_name,
+            attr_type=Dict[str, Any]
+        )
+
+        try:
+            last_mapped_type = self._current_mapped_type
+            last_mapped_type.add_to_mapped_children(new_mapped_type)
+        except KeyError:
+            pass
+
+        self._add_mapped_type_to_stack(new_mapped_type)
+
+        return new_json_schema
