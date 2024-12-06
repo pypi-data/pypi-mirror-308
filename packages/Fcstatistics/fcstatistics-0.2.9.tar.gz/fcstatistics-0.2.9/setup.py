31ccc6350d22933b22248bbf814751890cd6575f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:51 +0000

    Add package Fcstatistics/0.2.9/fcstatistics-0.2.9.tar.gz

---
+++ packages/Fcstatistics/fcstatistics-0.2.9.tar.gz/fcstatistics-0.2.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+# Cython으로 빌드할 확장 모듈 정의
+extensions = [
+    Extension("Fcstatistics.Fc", ["Fcstatistics/Fc_test.pyx"]),
+]
+
+# setup 함수 정의
+setup(
+    name="Fcstatistics",
+    version="0.2.9",
+    long_description=open("README.md", encoding="utf-8").read(),
+    ext_modules=cythonize(extensions),    # Cython 컴파일 활성화
+    package_data={
+        "Fcstatistics": ["*.pxd", "*.c", "*.h", "*.pyd"],  # 포함할 파일 확장자 명시
+    },
+    include_package_data=True,
+)
