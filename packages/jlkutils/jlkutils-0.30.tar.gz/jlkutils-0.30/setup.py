ea8ba5a78a35a91b601661b89878d5486ec670ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:06 +0000

    Add package jlkutils/0.30/jlkutils-0.30.tar.gz

---
+++ packages/jlkutils/jlkutils-0.30.tar.gz/jlkutils-0.30/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jlkutils',
+    version='0.30',
+    packages=find_packages(),
+    install_requires=[
+        "cryptography", "opencv-python", "openai", "ollama",
+    ],   author='Jaron Kramer',
+    description='Allgemeine Library für allen moglichen Praktischen Stuff',
+    url='https://github.com/jkramer5103/jlkutils',  # Optional: URL zu deinem Projekt
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
