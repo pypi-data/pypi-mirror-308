2ef34d9a9b012c3e4b61e0437c7f2793af0c0fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:30 +0000

    Add package cadnaPromise/0.1.9/cadnapromise-0.1.9.tar.gz

---
+++ packages/cadnaPromise/cadnapromise-0.1.9.tar.gz/cadnapromise-0.1.9/cadnaPromise/extra/run_unix_copy.sh
@@ -0,0 +1,5 @@
+cp flexfloat.h $CADNA_PATH/include
+cp flexfloat.hpp $CADNA_PATH/include
+cp flexfloat_config.h $CADNA_PATH/include
+cp flexfloat_config.h.in $CADNA_PATH/include
+cp libflexfloat.a $CADNA_PATH/lib
