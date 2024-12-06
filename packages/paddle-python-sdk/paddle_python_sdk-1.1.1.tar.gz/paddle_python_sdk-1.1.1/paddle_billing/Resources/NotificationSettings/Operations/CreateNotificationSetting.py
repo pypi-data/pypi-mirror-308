484a93b3c644c30829948b65a49ea92369df9e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:32 +0000

    Add package paddle-python-sdk/1.1.1/paddle_python_sdk-1.1.1.tar.gz

---
+++ packages/paddle-python-sdk/paddle_python_sdk-1.1.1.tar.gz/paddle_python_sdk-1.1.1/paddle_billing/Resources/NotificationSettings/Operations/CreateNotificationSetting.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from paddle_billing.Operation import Operation
+from paddle_billing.Undefined import Undefined
+from paddle_billing.Entities.Events import EventTypeName
+from paddle_billing.Entities.NotificationSettings import NotificationSettingType
+
+
+@dataclass
+class CreateNotificationSetting(Operation):
+    description: str
+    destination: str
+    subscribed_events: list[EventTypeName]
+    type: NotificationSettingType
+    include_sensitive_fields: bool
+    api_version: int | Undefined = Undefined()
---
+++ packages/paddle-python-sdk/paddle_python_sdk-1.1.1.tar.gz/paddle_python_sdk-1.1.1/paddle_billing/Resources/NotificationSettings/Operations/UpdateNotificationSetting.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from paddle_billing.Operation import Operation
+from paddle_billing.Undefined import Undefined
+from paddle_billing.Entities.Events import EventTypeName
+
+
+@dataclass
+class UpdateNotificationSetting(Operation):
+    description: str | Undefined = Undefined()
+    destination: str | Undefined = Undefined()
+    active: bool | Undefined = Undefined()
+    api_version: int | Undefined = Undefined()
+    include_sensitive_fields: bool | Undefined = Undefined()
+    subscribed_events: list[EventTypeName] | Undefined = Undefined()
