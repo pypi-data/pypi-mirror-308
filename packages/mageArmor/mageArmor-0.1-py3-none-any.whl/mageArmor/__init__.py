b5c54073028830506e0edac46515d6d81893f485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:44 +0000

    Add package mageArmor/0.1/mageArmor-0.1-py3-none-any.whl

---
+++ packages/mageArmor/mageArmor-0.1-py3-none-any.whl/mageArmor/__init__.py
@@ -0,0 +1,3 @@
+from .mageArmor import guard_cast, normalize_input, too_much_input, log_suspicious_activity
+
+__all__ = ['guard_cast', 'normalize_input', 'too_much_input', 'log_suspicious_activity']
