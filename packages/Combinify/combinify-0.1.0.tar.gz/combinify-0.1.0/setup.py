27709aaaa8c387f53f23ee8a07ad7819cdff14fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:46 +0000

    Add package Combinify/0.1.0/combinify-0.1.0.tar.gz

---
+++ packages/Combinify/combinify-0.1.0.tar.gz/combinify-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Combinify",  
+    version="0.1.0",   
+    description="A combinatory package",  
+    author="Your Name",  # Your name
+    author_email="gomezbarrosojavier@example.com", 
+    url="https://github.com/jgomezba/Combinify",
+    packages=find_packages(),
+    install_requires=[  "itertools"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
