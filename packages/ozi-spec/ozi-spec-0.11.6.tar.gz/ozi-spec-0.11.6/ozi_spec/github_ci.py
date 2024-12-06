58c65587c4e740fb108019b9b08c0b598f90277c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:50 +0000

    Add package ozi-spec/0.11.6/ozi-spec-0.11.6.tar.gz

---
+++ packages/ozi-spec/ozi-spec-0.11.6.tar.gz/ozi-spec-0.11.6/ozi_spec/github_ci.py
@@ -0,0 +1,47 @@
+# ozi/spec/ci.py
+# Part of the OZI Project.
+# See LICENSE.txt for license information.
+# SPDX-License-Identifier: Unlicense
+"""Continuous integration specification."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from ozi_spec.base import Default
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class Publish(Default):
+    """Publishing patterns for packaged project."""
+
+    include: tuple[str, ...] = ('*.tar.gz', '*.whl', 'sig/*')
+    version: str = '@github_publish_version@'
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class Draft(Default):
+    """Draft release patterns for packaged project."""
+
+    version: str = '@github_draft_version@'
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class Release(Default):
+    """Release patterns for packaged project."""
+
+    version: str = '@github_release_version@'
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class GenerateProvenance(Default):
+    """SLSA provenance generator metadata."""
+
+    version: str = '@github_slsa_version@'
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class Checkpoint(Default):
+    """Checkpoint suites to run."""
+
+    suites: tuple[str, ...] = ('dist', 'lint', 'test')
+    version: str = '@github_checkpoint_version@'
