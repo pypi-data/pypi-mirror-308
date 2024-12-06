2ca12434dd00ae369f256b9d7ba072760e7d5358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:38 +0000

    Add package kvdeveloper/2024.1.8/kvdeveloper-2024.1.8.tar.gz

---
+++ packages/kvdeveloper/kvdeveloper-2024.1.8.tar.gz/kvdeveloper-2024.1.8/kvdeveloper/config.py
@@ -0,0 +1,35 @@
+import typer
+from pathlib import Path
+
+app = typer.Typer()
+def_dir = Path(__file__).parent
+
+# Configuration settings
+TEMPLATES = {
+    "blank": "Basic structure with a SampleScreen component.",
+    "nav_toolbar": "Navigation and toolbar screens with HomeScreen and LoginScreen components.",
+    "nav_dock": "Navigation and toolbar screens with BottomNavigation, HomeScreen, LoginScreen and SettingsScreen components.",
+}
+TEMPLATES_DIR = f"{def_dir}/templates"
+STRUCTURES = {
+    "none": "No specific structure",
+    "MVC": "Model-View-Controller architecture.",
+}
+STRUCTURES_DIR = f"{def_dir}/structures"
+DEFAULT_TEMPLATE = "blank"
+DEFAULT_STRUCTURE = "none"
+
+VIEW_BASE = f"{def_dir}/view_base"
+
+IMAGE_LIBRARY = f"{def_dir}/assets/image_library"
+
+BUILD_DIR = f"{def_dir}/build_files"
+
+LAYOUTS_DIR = f"{def_dir}/layouts"
+
+COMPONENTS = {
+    "Container": "A responsive container with pre-defined padding calculations.",
+    "ITCard": "(Image Title Description Card) A responsive boostrap like card with image aspect-ratio calculations.",
+    "ResponsiveGrid": " A responsive grid with pre-defined column calculations.",
+}
+COMPONENTS_DIR = f"{def_dir}/components"
