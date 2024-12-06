bd3a7e5eaa2711db7c1bc51f7d05eb55558db3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:31:27 +0000

    Add package unitycatalog_client/1.0.0/unitycatalog_client-1.0.0-py3-none-any.whl

---
+++ packages/unitycatalog_client/unitycatalog_client-1.0.0-py3-none-any.whl/unitycatalog_client/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from unitycatalog_client.api.catalogs_api import CatalogsApi
+from unitycatalog_client.api.functions_api import FunctionsApi
+from unitycatalog_client.api.grants_api import GrantsApi
+from unitycatalog_client.api.model_versions_api import ModelVersionsApi
+from unitycatalog_client.api.registered_models_api import RegisteredModelsApi
+from unitycatalog_client.api.schemas_api import SchemasApi
+from unitycatalog_client.api.tables_api import TablesApi
+from unitycatalog_client.api.temporary_credentials_api import TemporaryCredentialsApi
+from unitycatalog_client.api.volumes_api import VolumesApi
+
