2a4dd56886d82825ef7a8db371f2a887063759d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:24 +0000

    Add package streamlit-chat-prompt/0.1.0/streamlit_chat_prompt-0.1.0.tar.gz

---
+++ packages/streamlit-chat-prompt/streamlit_chat_prompt-0.1.0.tar.gz/streamlit_chat_prompt-0.1.0/setup.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-chat-prompt",
+    version="0.1.0",
+    author="Tyler House",
+    author_email="26489166+tahouse@users.noreply.github.com",
+    description="Streamlit component that allows you to create a chat prompt with paste and image attachment support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tahouse/streamlit-chat-prompt",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+        "pydantic >= 2",
+    ],
+    extras_require={
+        "devel": [
+            "wheel",
+            "pytest==7.4.0",
+            "playwright==1.48.0",
+            "requests==2.31.0",
+            "pytest-playwright-snapshot==1.0",
+            "pytest-rerunfailures==12.0",
+            "twine~=5.1",
+        ]
+    },
+)
