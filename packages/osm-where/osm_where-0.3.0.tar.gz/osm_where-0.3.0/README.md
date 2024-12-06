279534297383a4c6cf721635e0d26120c4f9a4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:06 +0000

    Add package osm-where/0.3.0/osm_where-0.3.0.tar.gz

---
+++ packages/osm-where/osm_where-0.3.0.tar.gz/osm_where-0.3.0/README.md
@@ -0,0 +1,26 @@
+osm_where
+=========
+
+Simple tool to get [GeoURI](https://geouri.org/) for given
+location identified by URL or by its name (not 100% reliable, as
+we don’t have any interface to choose between multiple options).
+
+Currently defaults to looking at Ukraine.
+
+**Quick Start**:
+```bash
+# Get help message
+python src/osm_where.py --help
+
+# Query by URL
+python src/osm_where.py https://www.openstreetmap.org/node/4126283179
+
+# Query by place name
+python src/osm_where.py --name Kropyvnytskyi
+```
+
+All issues, questions, complaints, or (even better!) patches
+should be send via email to
+[~mcepl/devel@lists.sr.ht](mailto:~mcepl/devel@lists.sr.ht) email
+list (for patches use [git
+send-email](https://git-send-email.io/)).
---
+++ packages/osm-where/osm_where-0.3.0.tar.gz/osm_where-0.3.0/src/osm_where.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import argparse
+import urllib.parse
+
+import xdg.BaseDirectory
+
+from OSMPythonTools.cachingStrategy import CachingStrategy, JSON
+from OSMPythonTools.api import Api
+from OSMPythonTools.overpass import Overpass
+
+from shapely.geometry import shape
+
+CachingStrategy.use(JSON, cacheDir=xdg.BaseDirectory.save_cache_path("osm_where"))
+
+import logging
+
+logging.getLogger("OSMPythonTools").setLevel(logging.ERROR)
+
+__version__ = '0.3.0'
+
+def parse_name(name, area, lang):
+    overpass = Overpass()
+
+    result = overpass.query(
+        f'area["ISO3166-1"="{area}"][admin_level=2];relation["name:{lang}"="{name}"](area);out center;'
+    )
+    if len(result.elements()) > 0:
+        bakhmut = result.elements()[0]
+        return bakhmut.centerLat(), bakhmut.centerLon()
+    else:
+        return None
+
+
+def parse_URL(url):
+    api = Api()
+
+    way = api.query(url.path)
+    geom = shape(way.geometry())
+    return geom.centroid.y, geom.centroid.x
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="osm_where", description="Get geo: URI for given locality from OSM"
+    )
+    parser.add_argument("url", nargs="?", help="researched location")
+    parser.add_argument("-a", "--area", default="UA", help="ISO 3166 area code")
+    parser.add_argument("-l", "--lang", default="en", help="ISO 639-1 language code")
+    args = parser.parse_args()
+
+    parse_url = urllib.parse.urlparse(args.url)
+
+    if all((parse_url.scheme, parse_url.netloc)):
+        geo = parse_URL(parse_url)
+    else:
+        geo = parse_name(args.url, args.area, args.lang)
+
+    if geo:
+        print(f"geo:{geo[0]:.7f},{geo[1]:.7f}")
+
+
+if __name__ == "__main__":
+    main()
