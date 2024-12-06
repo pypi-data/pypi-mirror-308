3c9d0d58abe97214d340678609d26240ba005230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:58 +0000

    Add package hubspace-da/1.0.10/hubspace_da-1.0.10-py3-none-any.whl

---
+++ packages/hubspace-da/hubspace_da-1.0.10-py3-none-any.whl/hubspace_da/__init__.py
@@ -0,0 +1,16 @@
+import pathlib
+from datetime import datetime as dt
+
+import pkg_resources
+
+PACKAGE_NAME = 'hubspace_da'
+
+try:
+    __version__ = pkg_resources.get_distribution(PACKAGE_NAME).version
+except:
+    # version based on the mod timestamp of the most current updated python code file
+    file_list = list(pathlib.Path(__file__).parent.glob('**/*.py'))
+    ver_date = dt(2000,1,1,0,0,0,0)
+    for file_nm in file_list:
+        ver_date = max(ver_date, dt.fromtimestamp(file_nm.stat().st_mtime))
+    __version__ = f'{ver_date.year}.{ver_date.month}.{ver_date.day}'
