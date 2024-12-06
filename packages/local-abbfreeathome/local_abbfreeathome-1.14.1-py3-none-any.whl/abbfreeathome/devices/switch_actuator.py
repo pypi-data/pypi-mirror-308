949ce67fff6b5444db1260664b1819e8c40d9b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:45 +0000

    Add package local-abbfreeathome/1.14.1/local_abbfreeathome-1.14.1-py3-none-any.whl

---
+++ packages/local-abbfreeathome/local_abbfreeathome-1.14.1-py3-none-any.whl/abbfreeathome/devices/switch_actuator.py
@@ -0,0 +1,139 @@
+"""Free@Home SwitchActuator Class."""
+
+import enum
+from typing import Any
+
+from ..api import FreeAtHomeApi
+from ..bin.pairing import Pairing
+from .base import Base
+
+
+class SwitchActuatorForceCommand(enum.Enum):
+    """An Enum class for the force commands."""
+
+    unknown = None
+    deactivate = "0"
+    force_off = "2"
+    force_on = "3"
+
+
+class SwitchActuatorForceState(enum.Enum):
+    """An Enum class for the force states."""
+
+    unknown = None
+    deactivated = "0"
+    forced_on = "4"
+    forced_off = "5"
+
+
+class SwitchActuator(Base):
+    """Free@Home SwitchActuator Class."""
+
+    _state_refresh_output_pairings: list[Pairing] = [
+        Pairing.AL_INFO_FORCE,
+        Pairing.AL_INFO_ON_OFF,
+    ]
+
+    def __init__(
+        self,
+        device_id: str,
+        device_name: str,
+        channel_id: str,
+        channel_name: str,
+        inputs: dict[str, dict[str, Any]],
+        outputs: dict[str, dict[str, Any]],
+        parameters: dict[str, dict[str, Any]],
+        api: FreeAtHomeApi,
+        floor_name: str | None = None,
+        room_name: str | None = None,
+    ) -> None:
+        """Initialize the Free@Home SwitchActuator class."""
+        self._state: bool | None = None
+        self._forced: SwitchActuatorForceState = SwitchActuatorForceState.unknown
+
+        super().__init__(
+            device_id,
+            device_name,
+            channel_id,
+            channel_name,
+            inputs,
+            outputs,
+            parameters,
+            api,
+            floor_name,
+            room_name,
+        )
+
+    @property
+    def state(self) -> bool | None:
+        """Get the state of the switch."""
+        return self._state
+
+    @property
+    def forced(self) -> str | None:
+        """Get the forced state of the switch."""
+        return self._forced.name
+
+    async def turn_on(self):
+        """Turn on the switch."""
+        await self._set_switching_datapoint("1")
+        self._state = True
+
+    async def turn_off(self):
+        """Turn on the switch."""
+        await self._set_switching_datapoint("0")
+        self._state = False
+
+    async def set_forced(self, value: SwitchActuatorForceCommand):
+        """Set the forced-option on the switch."""
+
+        if value is not SwitchActuatorForceCommand.unknown:
+            await self._set_force_datapoint(str(value.value))
+
+            if value is SwitchActuatorForceCommand.deactivate:
+                self._forced = SwitchActuatorForceState.deactivated
+            elif value is SwitchActuatorForceCommand.force_off:
+                self._forced = SwitchActuatorForceState.forced_off
+            elif value is SwitchActuatorForceCommand.force_on:
+                self._forced = SwitchActuatorForceState.forced_on
+
+    def _refresh_state_from_output(self, output: dict[str, Any]) -> bool:
+        """
+        Refresh the state of the device from a given output.
+
+        This will return whether the state was refreshed as a boolean value.
+        """
+        if output.get("pairingID") == Pairing.AL_INFO_ON_OFF.value:
+            self._state = output.get("value") == "1"
+            return True
+        if output.get("pairingID") == Pairing.AL_INFO_FORCE.value:
+            try:
+                self._forced = SwitchActuatorForceState(output.get("value"))
+            except ValueError:
+                self._forced = SwitchActuatorForceState.unknown
+            return True
+        return False
+
+    async def _set_switching_datapoint(self, value: str):
+        """Set the switching datapoint on the api."""
+        _switch_input_id, _switch_input_value = self.get_input_by_pairing(
+            pairing=Pairing.AL_SWITCH_ON_OFF
+        )
+        return await self._api.set_datapoint(
+            device_id=self.device_id,
+            channel_id=self.channel_id,
+            datapoint=_switch_input_id,
+            value=value,
+        )
+
+    async def _set_force_datapoint(self, value: str):
+        """Set the force datapoint on the api."""
+        _force_input_id, _force_input_value = self.get_input_by_pairing(
+            pairing=Pairing.AL_FORCED
+        )
+        return await self._api.set_datapoint(
+            device_id=self.device_id,
+            channel_id=self.channel_id,
+            datapoint=_force_input_id,
+            value=value,
+        )
