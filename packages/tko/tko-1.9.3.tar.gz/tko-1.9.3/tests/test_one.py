45bfb2822edecd94068c5744952f7016bf7cfca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:18 +0000

    Add package tko/1.9.3/tko-1.9.3.tar.gz

---
+++ packages/tko/tko-1.9.3.tar.gz/tko-1.9.3/tests/test_one.py
@@ -0,0 +1,29 @@
+# the inclusion of the tests module is not meant to offer best practices for
+# testing in general, but rather to support the `find_packages` example in
+# setup.py that excludes installing the "tests" package
+
+import unittest
+
+
+from tko.game.task import TaskParser, Task
+
+
+class TestSimple(unittest.TestCase):
+
+    def test_reading_task(self):
+        task: Task = TaskParser.parse_line("- [ ] [tarefa um](wiki/tarefa.md)", 0)
+        assert task.key == "wiki/tarefa.md"
+        assert task.link == "wiki/tarefa.md"
+        assert task.self_grade == 0
+        assert task.title == "tarefa um"
+
+    def test_coding_local_task(self):
+        task: Task = TaskParser.parse_line("- [ ] [minha @boneca quebrou](base/boneca/Readme.md)", 0)
+        assert task.key == "boneca"
+        assert task.link == "base/boneca/Readme.md"
+        assert task.self_grade == 0
+        assert task.title == "minha @boneca quebrou"
+
+
+if __name__ == '__main__':
+    unittest.main()
