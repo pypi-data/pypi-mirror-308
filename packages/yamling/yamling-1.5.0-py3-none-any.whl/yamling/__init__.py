bcb7895e78f3bb7f079dfd2fadb6f3a2a1fad082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:35 +0000

    Add package yamling/1.5.0/yamling-1.5.0-py3-none-any.whl

---
+++ packages/yamling/yamling-1.5.0-py3-none-any.whl/yamling/__init__.py
@@ -0,0 +1,27 @@
+__version__ = "1.5.0"
+
+import yaml
+from yamling.yaml_loaders import load_yaml, load_yaml_file, get_loader, YAMLInput
+from yamling.load_universal import load, load_file, ParsingError
+from yamling.yaml_dumpers import dump_yaml
+from yamling.dump_universal import DumpingError, dump, dump_file
+from yamling.yamlparser import YAMLParser
+
+YAMLError = yaml.YAMLError  # Reference for external libs that need to catch this error
+
+
+__all__ = [
+    "load_yaml",
+    "dump_yaml",
+    "dump",
+    "dump_file",
+    "YAMLError",
+    "load_yaml_file",
+    "get_loader",
+    "load",
+    "load_file",
+    "ParsingError",
+    "DumpingError",
+    "YAMLInput",
+    "YAMLParser",
+]
