874bc08fb431750ac404306891ed420f2f011c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:02 +0000

    Add package craterstats/3.0.22/craterstats-3.0.22-py3-none-any.whl

---
+++ packages/craterstats/craterstats-3.0.22-py3-none-any.whl/craterstats/Craterpdf.py
@@ -0,0 +1,178 @@
+#  Copyright (c) 2021, Greg Michael
+#  Licensed under BSD 3-Clause License. See LICENSE.txt for details.
+
+import sys
+
+import numpy as np
+from scipy.integrate import simps
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+import craterstats as cst
+import craterstats.gm as gm
+
+
+class Craterpdf:
+    """
+    Create and plot age uncertainty distribution function
+
+    """
+
+    def __init__(self, pf,cf,cc,d_range,k=None,bcc=False,n_samples=3000):
+        """
+        :param pf: Productionfn object
+        :param cf: Chronologyfn object
+        :param cc: Cratercount object
+        :param d_range: diameter range (km)
+        :param k: number of craters in range (normally not required - calculated from cc)
+        :param bcc: buffered crater count?
+        :param n_samples: number of samples for likelihood curve
+
+        """
+
+        x=np.linspace(-10, 5, n_samples) #additive change to a0 (equidistant in fitting space)
+        a0=cf.a0(1.)
+        self.ts = cf.t(a0=a0+x)
+        self.dt = self.ts - np.roll(self.ts, 1)
+        self.dt[0] = self.dt[1]
+
+        if k is not None:
+            self.k=k
+        elif cc.diam:
+            d = [e for e in cc.diam if d_range[0] <= e < d_range[1]]
+            self.k = len(d)
+        else:   # we have a binned-only count
+            b = cc['binned']
+            q = np.where(b['d_min'] >= d_range[0] and b['d_max'] <= d_range[1])
+            self.k = np.sum(b['.n_event'][q])
+
+        if bcc: #buffered count
+            if cc.perimeter is None:
+                sys.exit('Error: buffered-poisson calculation requires polygon perimeter in source file')
+            ns=500 #samples of PF for integration
+            d=np.linspace(d_range[0], d_range[1], ns)
+            F=pf.F(d,a0)
+            y=(cc.area+d*cc.perimeter/2.+np.pi*d**2/8.)*F
+            I1=simps(y,d)
+            lam=I1
+        else: #standard count
+            Ncum=pf.evaluate("cumulative",d_range,a0)
+            Ninc=(Ncum[0]-Ncum[1])*cc.area # no expected on area with phi(1 Ga)
+            lam=Ninc
+
+        pdf0 = gm.poisson(self.k, lam * 10 ** x)
+        pdf0 = pdf0.astype(float)
+        self.pdf = pdf0/np.sum(pdf0*self.dt)
+        self.cdf = np.cumsum(self.pdf*self.dt)
+
+
+    def t(self,cum_fraction):
+        """
+        Return time for interpolated percentiles
+
+        :param cum_fraction: percentile as fraction
+        :return: times
+        """
+        return np.interp(cum_fraction,self.cdf,self.ts)
+
+    def median1sigma(self):
+        """
+        Return times for median and 1-sigma percentiles
+
+        :return: times
+        """
+        return self.t([.5, .1586, .8413])  # median/1-sigma gaussian-equivalent percentiles
+
+    def plot(self,ax=None,pt_size=9,color='0',t_range=[],logscale=False, max_ticks=3, violin=False):
+        """
+        Set up uncertainty distribution plot
+
+        :param ax: matplotlib axes object
+        :param pt_size: character point size
+        :param color: colour
+        :return: none
+        """
+        if not ax:
+            ax = plt.subplot(1,1,1)
+
+        linewidth=.5 * pt_size / 9
+        t=self.t([.003,.16,.5,.84,.997])
+        p=np.searchsorted(self.ts,t)-1
+
+        if not logscale:
+            if t_range:
+                max_t = t_range[1]
+            else:
+                max_t = np.max(t)
+            xt = gm.ticks(np.array([0.,max_t]), max_ticks)
+            #2024-07-25 don't understand what this was for:
+            # for i,e in enumerate(xt):
+            #     if e<=max_t: max_i=i
+            max_i=-1
+
+            max_text = cst.str_age(xt[max_i], simple=True)
+            a = max_text.split(' ')
+            xt_units = float(a[0]) / xt[max_i] * xt
+            xt_label = ['{:g}'.format(e) for e in xt_units]
+            xt_label[max_i] = "      " + xt_label[max_i] + " " + a[1]  # add unit to last label, e.g. "Ga"
+
+        else: #logscale
+            if not t_range:
+                t_range=gm.range(t)
+                xt0=list(range(np.floor(np.log10(t_range[0])).astype(int),np.ceil(np.log10(t_range[1])).astype(int)+1))
+            else:
+                xt0= ([np.log10(t_range[0])]
+                    +list(range(np.floor(np.log10(t_range[0])).astype(int)+1,np.ceil(np.log10(t_range[1])).astype(int)))
+                    +[np.log10(t_range[1])])
+
+            xt = [10 ** e for e in xt0]
+            xt_label = [cst.str_age(e, simple=True) for e in xt]
+            ax.set_xscale('log')
+
+        if violin:
+            reflectx = lambda x: np.concatenate([x, x[::-1]])
+            reflecty = lambda x: np.concatenate([x, -x[::-1]])
+            ax.plot(reflectx(self.ts), reflecty(self.pdf),lw=linewidth *1.5, color=color)
+            ax.fill_between(reflectx(self.ts[p[1]:p[3]]), reflecty(self.pdf[p[1]:p[3]]), 0, color=color, alpha=0.35, edgecolor=color,
+                            lw=linewidth)  # '.7'
+            ax.fill_between(reflectx(self.ts[p[2]:p[2] + 1]), reflecty(self.pdf[p[2]:p[2] + 1]), edgecolor=color, lw=linewidth, alpha=.5)
+        else:
+            ax.plot(self.ts, self.pdf, lw=linewidth * 1.5, color=color)
+            ax.fill_between(self.ts[p[1]:p[3]], self.pdf[p[1]:p[3]], 0, color=color, alpha=0.35, edgecolor=color,
+                            lw=linewidth)  # '.7'
+            ax.fill_between(self.ts[p[2]:p[2] + 1], self.pdf[p[2]:p[2] + 1], edgecolor=color, lw=linewidth, alpha=.5)
+
+        ax.patch.set_facecolor('none') # make region transparent over background graphics
+        ax.get_yaxis().set_visible(False)
+        for e in ['right','left','top']: ax.spines[e].set_visible(False)
+        ax.spines['bottom'].set_linewidth(linewidth)
+        ax.spines['bottom'].set_color(color)
+
+        ax.set_xticks(xt)
+        ax.set_xlim(xt[0], t_range[1] if t_range else xt[-1],auto=False)
+
+        ax.tick_params(axis='x', which='both', width=linewidth, length=pt_size * .2, pad=pt_size * .1, color=color)
+        ax.tick_params(axis='x', which='minor', length=pt_size * .1)
+        if not logscale: ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
+        ax.set_xticklabels(xt_label,fontsize=pt_size*.7, color=color)#,horizontalalignment='left')
+
+    def offset(self,left):
+        """
+        Calculate offset for correct spacing of age text from mini-plot peak, in +ve x-direction
+
+        :param left: left-aligned plot?
+        :return: offset as fraction of mini-plot width
+        """
+        t=self.t(.997)
+        max_ticks=3
+        xt= gm.ticks(np.append(0, t), max_ticks)
+        q=np.where(self.pdf > np.max(self.pdf)/2)
+        edges=self.ts[[q[0][0],q[0][-1]]]
+        norm=edges/np.max(xt)
+        offset=(1-norm[1])-.1 if left else -norm[0]+.1
+        return offset
+
+
+
+
---
+++ packages/craterstats/craterstats-3.0.22-py3-none-any.whl/craterstats/Craterplot.py
@@ -0,0 +1,210 @@
+#  Copyright (c) 2021, Greg Michael
+#  Licensed under BSD 3-Clause License. See LICENSE.txt for details.
+
+import numpy as np
+from matplotlib.textpath import TextPath
+
+import craterstats as cst
+import craterstats.gm as gm
+
+
+class Craterplot:
+    """
+    Specify and draw a single overplot: either a crater data series, or an age estimate of one
+    """
+
+    def __init__(self,*args,**kwargs):
+        self.UpdateSettings({
+            'cratercount':None, # if not provided, will create from 'source'
+            'source':'',
+            'name':'',
+            'range':np.array([0.,np.inf]),   #unconstrained
+            'type':'data',
+            'error_bars':1,
+            'hide':0,
+            'colour':0,
+            'psym':0,
+            'binning':'pseudo-log',
+            'age_left':0,
+            'display_age':1,
+            'resurf':0,
+            'resurf_showall':0,
+            'isochron':0,
+            'offset_age':[0.,0.],
+            },*args,kwargs)
+
+    def UpdateSettings(self,*args,**kwargs): #pass either dictionary or keywords
+        a = {k: v for d in args for k, v in d.items()}
+        a.update(**kwargs)
+        for k, v in a.items():
+            if k in ('range', 'offset_age'): v = [float(e) for e in v]
+            if k in ('error_bars',
+                     'hide',
+                     'age_left',
+                     'display_age',
+                     'resurf',
+                     'resurf_showall',
+                     'isochron',
+                     'colour',
+                     'psym',
+                     ):
+                v = int(v)
+            setattr(self, k, v)
+        if not self.cratercount and self.source:
+            self.cratercount = cst.Cratercount(self.source)
+        if not self.source and self.cratercount:
+            self.source = self.cratercount.filename
+
+
+    def calculate_age(self,cps):
+        """
+        Calculate age
+
+        :param cps: Craterplotset instance
+        """
+        if self.type in ['poisson','b-poisson']:
+            pf_range=cps.pf.range
+            r0=np.clip(self.range,pf_range[0],pf_range[1])
+            self.pdf=cst.Craterpdf(cps.pf, cps.cf, self.cratercount, r0, bcc=self.type == 'b-poisson')
+            #self.t=self.pdf.t([.5,.1586,.8413]) #median/1-sigma gaussian-equivalent percentiles
+            self.t = self.pdf.median1sigma()  # median/1-sigma gaussian-equivalent percentiles
+            self.a0=cps.cf.a0(self.t)
+            self.n=self.pdf.k
+            self.n_event=self.n
+
+        else:
+            if self.type=='c-fit':
+                p0=self.cratercount.getplotdata("cumulative",self.binning, range=self.range,
+                    resurfacing=self.resurf_showall if self.resurf and self.type == 'c-fit' else None,
+                    pf=cps.pf)
+            elif self.type=='d-fit':
+                p0=self.cratercount.getplotdata("differential",self.binning,range=self.range,pf=cps.pf)
+
+            self.n=p0['n']    #override with number used in fit (range may be a bit different because of d_min)
+            self.n_event=p0['n_event']
+            self.a0=cps.pf.fit(p0)
+            self.t = [cps.cf.t(a0=e) for e in self.a0]
+            self.bin_range=p0['bin_range']
+
+        self.n_d = cps.pf.evaluate("cumulative", cps.ref_diameter, self.a0[0])
+
+
+
+    def overplot(self,cps):
+        """
+        Add overplot elements into figure
+
+        :param cps: Craterplotset instance
+        :return: none
+        """
+        if not self.cratercount or self.hide: return
+
+        p = self.cratercount.getplotdata(cps.presentation, self.binning, range=self.range,
+             resurfacing=self.resurf_showall if self.resurf and self.type == 'c-fit' else None,
+             pf=cps.pf)
+
+        self.n=p['n']
+        self.n_event=p['n_event']
+        legend_label = []
+
+        if self.error_bars:
+            cps.ax.errorbar(np.log10(p['d']),p['y'],yerr=p['err'],fmt='none',linewidth=.7*cps.sz_ratio,ecolor=cps.grey[0])
+
+        if self.type in ['c-fit','d-fit','poisson','b-poisson']:
+            self.calculate_age(cps)
+
+            if self.isochron:
+                iso=cps.pf.getisochron(cps.presentation,self.a0[0],cps.ef)
+                cps.ax.plot(np.log10(iso['d']), iso['y'], label=None,color=cps.grey[0], lw=.4*cps.sz_ratio, zorder=.9)
+
+            expansion=np.array([.99,1.01])
+            fit=cps.pf.getplotdata(cps.presentation,self.a0[0],range=self.range*expansion)
+            cps.ax.plot(np.log10(fit['d']), fit['y'], label='fit', color=cps.palette[self.colour], lw=.7*cps.sz_ratio)
+
+            if self.display_age:
+                st=cst.str_age(self.t[0], self.t[2] - self.t[0], self.t[0] - self.t[1], cps.sig_figs, mu=cps.mu)
+                xy = cps.data_to_axis((np.log10(fit['d'][0]),fit['y'][0]))
+                x,y = xy + 0.02*np.ones(2)*(-1 if self.age_left else 1) +np.array(self.offset_age)/(cps.decades[0]*20) #(cps.decades[0]*10).
+                cps.ax.text(x,y,st,transform=cps.ax.transAxes,
+                            color=cps.palette[self.colour],size=cps.scaled_pt_size*1.2,
+                            horizontalalignment='right' if self.age_left else 'left',)
+
+                if self.type in ['poisson','b-poisson']:
+                    text_extent = TextPath((0, 0), st, size=cps.scaled_pt_size*1.2).get_extents()
+                    h,w=text_extent.height,text_extent.width
+                    f = 1 / (cps.cm2inch * (cps.position[2] - cps.position[0]) * 100) #conversion for axes coord
+                    offset = self.pdf.offset(self.age_left)          # normalised units of mini-plot width in +x direction
+                    box = np.array([.12, .05])*cps.sz_ratio          # dimensions of plot box
+
+                    if self.age_left:  # offset from string write position
+                        dx = -(f * w + .03) + (-1 + offset) * box[0]
+                    else:
+                        dx = f * w + .03 + offset * box[0]
+                    dy = f * h / 2
+
+                    pos = np.array([x+dx, y-dy, x+dx + box[0], y-dy + box[1]])
+                    pos2=cps.axis_to_fig(pos)
+                    pos3=np.concatenate([pos2[0:2],pos2[2:4]-pos2[0:2]])
+                    ax = cps.fig.add_axes(pos3)
+                    self.pdf.plot(ax,pt_size=cps.scaled_pt_size,color=cps.palette[self.colour])
+
+            if 'c' in cps.legend:
+                if self.cratercount.buffered:
+                    legend_label += ['{:.1f}'.format(self.n_event)]
+                else:
+                    if np.abs(self.n_event - self.n) < .001:
+                        legend_label += ['{:0g}'.format(self.n)]
+                    else:
+                        legend_label += ['{0:.1f} (of {1:d})'.format(self.n,self.n_event)]
+                legend_label[-1] += " craters"
+            if 'r' in cps.legend:
+                if not self.cratercount.prebinned and self.type in ['poisson','b-poisson']:
+                    r=self.range
+                else:
+                    r=gm.range(self.cratercount.generate_bins(self.binning,self.range,expand=False))
+                legend_label += [cst.str_diameter_range(r)]
+            if 'N' in cps.legend:
+                legend_label += ['N({0:0g})'.format(cps.ref_diameter) +'$=' + gm.scientific_notation(self.n_d, sf=3) + '$ km$^{-2}$']
+
+
+        if self.type=='data':
+            if 'n' in cps.legend:
+                legend_label+=[self.name if self.name!='' else gm.filename(self.source, "n")]
+            if 'a' in cps.legend:
+                legend_label+=['$' + gm.scientific_notation(self.cratercount.area, sf=3) + '$ km$^{2}$']
+            if 'p' in cps.legend:
+                if self.cratercount.perimeter:
+                    legend_label += ['$' + gm.scientific_notation(self.cratercount.perimeter, sf=3) + '$ km']
+
+        cps.ax.plot(np.log10(p['d']),p['y'],label=', '.join(legend_label) if legend_label else None,
+                    **cps.marker_def[self.psym],ls='',color=cps.palette[self.colour])
+
+
+
+    def get_data_range(self,cps):
+        """
+        Return data range of Craterplot
+
+        :param cps: Craterplotset instance
+        :return: (d_min,d_max,y_min,y_max)
+        """
+        p = self.cratercount.getplotdata(cps.presentation, self.binning, range=self.range,
+             resurfacing=self.resurf_showall if self.resurf and self.type == 'c-fit' else None,
+             pf=cps.pf)
+        return gm.range(p['d']) + gm.range(p['y'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
