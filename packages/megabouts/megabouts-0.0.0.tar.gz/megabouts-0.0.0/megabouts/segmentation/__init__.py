4758a8a3925dc0f587947001caddf8b15363af25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:57 +0000

    Add package megabouts/0.0.0/megabouts-0.0.0.tar.gz

---
+++ packages/megabouts/megabouts-0.0.0.tar.gz/megabouts-0.0.0/megabouts/segmentation/__init__.py
@@ -0,0 +1,8 @@
+from .segmentation import (
+    TailSegmentation,
+    TrajSegmentation,
+    Segmentation,
+    SegmentationResult,
+)
+
+__all__ = ["TailSegmentation", "TrajSegmentation", "Segmentation", "SegmentationResult"]
