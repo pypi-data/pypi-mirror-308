a9b5b05e03b73a9ae7f00f0d6d486607110e01df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:45 +0000

    Add package lilliepy/0.4.0/lilliepy-0.4.0.tar.gz

---
+++ packages/lilliepy/lilliepy-0.4.0.tar.gz/lilliepy-0.4.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lilliepy',  
+    version='0.4.0',  
+    packages=find_packages(),  
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    package_data={
+        'lilliepy': ['bin.bat', 'bin.sh']
+    },
+    include_package_data=True
+)
