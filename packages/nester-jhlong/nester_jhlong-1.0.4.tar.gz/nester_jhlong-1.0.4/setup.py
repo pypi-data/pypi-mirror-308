ab4c11441b904f9a0fce58eef17306965fbbcb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:04 +0000

    Add package nester-jhlong/1.0.4/nester_jhlong-1.0.4.tar.gz

---
+++ packages/nester-jhlong/nester_jhlong-1.0.4.tar.gz/nester_jhlong-1.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='nester_jhlong',
+    version='1.0.4',
+    description='A simple printer of nested lists',
+    py_modules=['nester_jhlong'],
+    author='jhlong',
+    author_email='jhlong2024@163.com',
+)
