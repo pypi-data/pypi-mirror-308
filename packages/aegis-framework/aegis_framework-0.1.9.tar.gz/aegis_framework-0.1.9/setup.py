9e7c83b444abcb1c235b809ee01d188cfb58921e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:15 +0000

    Add package aegis-framework/0.1.9/aegis_framework-0.1.9.tar.gz

---
+++ packages/aegis-framework/aegis_framework-0.1.9.tar.gz/aegis_framework-0.1.9/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aegis_framework",  # Name of your package (should be unique on PyPI)
+    version="0.1.9",  # Version of your package
+    description="A framework for creating multi-agent colonies",  # Short description
+    author="Metis Analytics",  # Your name or organization
+    author_email="cjohnson@metisos.com",  # Your email address
+    url="https://github.com/metisos/aegis_framework.git",  # URL for your project
+    packages=find_packages(include=["aegis_framework", "aegis_framework.*"]),  # Explicitly find packages under aegis_framework
+    install_requires=[
+        "flask",
+        "flask-socketio",
+        "fuzzywuzzy",
+        "python-socketio",
+        "schedule"
+    ],  # List your package's dependencies
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # License for your package
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
