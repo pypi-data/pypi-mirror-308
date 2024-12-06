f714ffbf589dd00dc64ecc6c896e0dd8e0adfaf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:00 +0000

    Add package unicorn-binance-websocket-api/2.9.0/unicorn_binance_websocket_api-2.9.0-cp312-cp312-musllinux_1_1_i686.whl

---
+++ packages/unicorn-binance-websocket-api/unicorn_binance_websocket_api-2.9.0-cp312-cp312-musllinux_1_1_i686.whl/unicorn_binance_websocket_api/connection_settings.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# File: unicorn_binance_websocket_api/connection_settings.py
+#
+# Part of ‘UNICORN Binance WebSocket API’
+# Project website: https://www.lucit.tech/unicorn-binance-websocket-api.html
+# Github: https://github.com/LUCIT-Systems-and-Development/unicorn-binance-websocket-api
+# Documentation: https://unicorn-binance-websocket-api.docs.lucit.tech
+# PyPI: https://pypi.org/project/unicorn-binance-websocket-api
+# LUCIT Online Shop: https://shop.lucit.services/software
+#
+# License: LSOSL - LUCIT Synergetic Open Source License
+# https://github.com/LUCIT-Systems-and-Development/unicorn-binance-websocket-api/blob/master/LICENSE
+#
+# Author: LUCIT Systems and Development
+#
+# Copyright (c) 2019-2024, LUCIT Systems and Development (https://www.lucit.tech)
+# All rights reserved.
+
+from enum import Enum
+
+import sys
+if sys.version_info >= (3, 9):
+    from typing import Type
+    MAX_SUBSCRIPTIONS_PER_STREAM: Type[int] = int
+    WEBSOCKET_BASE_URI: Type[str] = str
+    WEBSOCKET_API_BASE_URI: Type[str] = str
+else:
+    MAX_SUBSCRIPTIONS_PER_STREAM = int
+    WEBSOCKET_BASE_URI = str
+    WEBSOCKET_API_BASE_URI = str
+
+
+class Exchanges(str, Enum):
+    BINANCE = "binance.com"
+    BINANCE_TESTNET = "binance.com-testnet"
+    BINANCE_MARGIN = "binance.com-margin"
+    BINANCE_MARGIN_TESTNET = "binance.com-margin-testnet"
+    BINANCE_ISOLATED_MARGIN = "binance.com-isolated_margin"
+    BINANCE_ISOLATED_MARGIN_TESTNET = "binance.com-isolated_margin-testnet"
+    BINANCE_FUTURES = "binance.com-futures"
+    BINANCE_COIN_FUTURES = "binance.com-coin_futures"
+    BINANCE_FUTURES_TESTNET = "binance.com-futures-testnet"
+    BINANCE_US = "binance.us"
+    TRBINANCE = "trbinance.com"
+    BINANCE_ORG = "binance.org"
+    BINANCE_ORG_TESTNET = "binance.org-testnet"
+
+
+DEX_EXCHANGES = [Exchanges.BINANCE_ORG, Exchanges.BINANCE_ORG_TESTNET]
+CEX_EXCHANGES = [
+    Exchanges.BINANCE,
+    Exchanges.BINANCE_TESTNET,
+    Exchanges.BINANCE_MARGIN,
+    Exchanges.BINANCE_MARGIN_TESTNET,
+    Exchanges.BINANCE_ISOLATED_MARGIN,
+    Exchanges.BINANCE_ISOLATED_MARGIN_TESTNET,
+    Exchanges.BINANCE_FUTURES,
+    Exchanges.BINANCE_COIN_FUTURES,
+    Exchanges.BINANCE_FUTURES_TESTNET,
+    Exchanges.BINANCE_US,
+    Exchanges.TRBINANCE,
+]
+
+# only python 3.9+
+# CONNECTION_SETTINGS: dict[str, Tuple[MAX_SUBSCRIPTIONS_PER_STREAM, WEBSOCKET_BASE_URI, WEBSOCKET_API_BASE_URI]] = {
+
+CONNECTION_SETTINGS = {
+    Exchanges.BINANCE: (1024, "wss://stream.binance.com:9443/", "wss://ws-api.binance.com/ws-api/v3"),
+    Exchanges.BINANCE_TESTNET: (1024, "wss://testnet.binance.vision/", "wss://testnet.binance.vision/ws-api/v3"),
+    Exchanges.BINANCE_MARGIN: (1024, "wss://stream.binance.com:9443/", None),
+    Exchanges.BINANCE_MARGIN_TESTNET: (1024, "wss://testnet.binance.vision/", None),
+    Exchanges.BINANCE_ISOLATED_MARGIN: (1024, "wss://stream.binance.com:9443/", None),
+    Exchanges.BINANCE_ISOLATED_MARGIN_TESTNET: (1024, "wss://testnet.binance.vision/", None),
+    Exchanges.BINANCE_FUTURES: (200, "wss://fstream.binance.com/", "wss://ws-fapi.binance.com/ws-fapi/v1"),
+    Exchanges.BINANCE_FUTURES_TESTNET: (200, "wss://stream.binancefuture.com/", "wss://testnet.binancefuture.com/ws-fapi/v1"),
+    Exchanges.BINANCE_COIN_FUTURES: (200, "wss://dstream.binance.com/", None),
+    Exchanges.BINANCE_US: (1024, "wss://stream.binance.us:9443/", None),
+    Exchanges.TRBINANCE: (1024, "wss://stream-cloud.trbinance.com/", None),
+    Exchanges.BINANCE_ORG: (1024, "wss://dex.binance.org/api/", None),
+    Exchanges.BINANCE_ORG_TESTNET: (1024, "wss://testnet-dex.binance.org/api/", None),
+}
---
+++ packages/unicorn-binance-websocket-api/unicorn_binance_websocket_api-2.9.0-cp312-cp312-musllinux_1_1_i686.whl/unicorn_binance_websocket_api/restserver.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ¯\_(ツ)_/¯
+#
+# File: unicorn_binance_websocket_api/restserver.py
+#
+# Part of ‘UNICORN Binance WebSocket API’
+# Project website: https://www.lucit.tech/unicorn-binance-websocket-api.html
+# Github: https://github.com/LUCIT-Systems-and-Development/unicorn-binance-websocket-api
+# Documentation: https://unicorn-binance-websocket-api.docs.lucit.tech
+# PyPI: https://pypi.org/project/unicorn-binance-websocket-api
+# LUCIT Online Shop: https://shop.lucit.services/software
+#
+# License: LSOSL - LUCIT Synergetic Open Source License
+# https://github.com/LUCIT-Systems-and-Development/unicorn-binance-websocket-api/blob/master/LICENSE
+#
+# Author: LUCIT Systems and Development
+#
+# Copyright (c) 2019-2024, LUCIT Systems and Development (https://www.lucit.tech)
+# All rights reserved.
+
+from flask_restful import Resource
+import logging
+
+__logger__: logging.getLogger = logging.getLogger("unicorn_binance_websocket_api")
+
+logger = __logger__
+
+
+class BinanceWebSocketApiRestServer(Resource):
+    """
+    Provide a REST API server 
+    
+    Description:
+    https://github.com/LUCIT-Systems-and-Development/unicorn-binance-websocket-api/wiki/UNICORN-Monitoring-API-Service
+
+    :param handler_binance_websocket_api_manager: Provide the handler of the binance_websocket_api_manager
+    :type handler_binance_websocket_api_manager: function
+    :param warn_on_update: set to 'False' to avoid a warning on available updates
+    :type warn_on_update: bool
+    """
+    def __init__(self, handler_binance_websocket_api_manager, warn_on_update=True):
+        self.manager = handler_binance_websocket_api_manager
+        self.warn_on_update = warn_on_update
+
+    def get(self, statusformat, checkcommandversion=False):
+        """
+        Get the status of the 'UNICORN Binance WebSocket API Manager'
+
+        :param statusformat: Choose the format for the export (e.g. 'icinga')
+        :type statusformat: str
+
+        :param checkcommandversion: Control if there is a new version of the check_command available!
+        :type checkcommandversion: bool
+
+        :return: status message of 'UNICORN Binance WebSocket API Manager'
+        :rtype: list (status string, http status code)
+        """
+        if statusformat == "icinga":
+            logger.info(f"BinanceWebSocketApiRestServer.get({statusformat}, {str(checkcommandversion)}) - 200")
+            return self.manager.get_monitoring_status_icinga(check_command_version=checkcommandversion,
+                                                             warn_on_update=self.warn_on_update), 200
+        else:
+            logger.error(f"BinanceWebSocketApiRestServer.get({statusformat}, {str(checkcommandversion)}) - Service not"
+                          f"found!")
+            return "service not found", 404
