eceb6d19ae2528ce3721606f3e38dacace51e3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:24 +0000

    Add package al-dag-kits/1.0.5/al_dag_kits-1.0.5.tar.gz

---
+++ packages/al-dag-kits/al_dag_kits-1.0.5.tar.gz/al_dag_kits-1.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='al_dag_kits',
+    version='1.0.5',
+    description='Custom utilities and triggers for Airflow DAGs',
+    author='Zeming lim',
+    author_email='zeming.lim@applovin.com',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+        'apache-airflow-providers-google==10.21.0',
+        'google-cloud-dataproc>=3.1.0',
+        'apache-airflow-providers-cncf-kubernetes==8.3.3',
+    ],
+)
