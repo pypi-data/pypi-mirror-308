1510a83280b202276e3804b8bdd04764b7d90e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:06:15 +0000

    Add package letsbuilda-pypi/5.2.0/letsbuilda_pypi-5.2.0-py3-none-any.whl

---
+++ packages/letsbuilda-pypi/letsbuilda_pypi-5.2.0-py3-none-any.whl/letsbuilda/pypi/sync_client.py
@@ -0,0 +1,49 @@
+"""The sync client."""
+
+from http import HTTPStatus
+from typing import Final, Self
+
+import xmltodict
+from httpx import Client
+
+from .exceptions import PackageNotFoundError
+from .models import JSONPackageMetadata, Package, RSSPackageMetadata
+
+
+class PyPIServices:
+    """A class for interacting with PyPI."""
+
+    NEWEST_PACKAGES_FEED_URL: Final[str] = "https://pypi.org/rss/packages.xml"
+    PACKAGE_UPDATES_FEED_URL: Final[str] = "https://pypi.org/rss/updates.xml"
+
+    def __init__(self: Self, http_client: Client) -> None:
+        self.http_client = http_client
+
+    def get_rss_feed(self: Self, feed_url: str) -> list[RSSPackageMetadata]:
+        """Get the new packages RSS feed."""
+        response_text = self.http_client.get(feed_url).text
+        rss_data = xmltodict.parse(response_text)["rss"]["channel"]["item"]
+        return [RSSPackageMetadata.build_from(package_data) for package_data in rss_data]
+
+    def get_package_json_metadata(
+        self: Self,
+        package_title: str,
+        package_version: str | None = None,
+    ) -> JSONPackageMetadata:
+        """Get metadata for a package."""
+        if package_version is not None:
+            url = f"https://pypi.org/pypi/{package_title}/{package_version}/json"
+        else:
+            url = f"https://pypi.org/pypi/{package_title}/json"
+        response = self.http_client.get(url)
+        if response.status_code == HTTPStatus.NOT_FOUND:
+            raise PackageNotFoundError(package_title, package_version)
+        return JSONPackageMetadata.from_dict(response.json())
+
+    def get_package_metadata(
+        self: Self,
+        package_title: str,
+        package_version: str | None = None,
+    ) -> Package:
+        """Get metadata for a package."""
+        return Package.from_json_api_data(self.get_package_json_metadata(package_title, package_version))
