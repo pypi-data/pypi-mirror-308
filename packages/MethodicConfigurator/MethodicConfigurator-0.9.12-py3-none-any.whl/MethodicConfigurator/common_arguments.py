3091fe16b8ce0fa49eab3bcc7924cacf52678e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:35 +0000

    Add package MethodicConfigurator/0.9.12/MethodicConfigurator-0.9.12-py3-none-any.whl

---
+++ packages/MethodicConfigurator/MethodicConfigurator-0.9.12-py3-none-any.whl/MethodicConfigurator/common_arguments.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+This file is part of Ardupilot methodic configurator. https://github.com/ArduPilot/MethodicConfigurator
+
+SPDX-FileCopyrightText: 2024 Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+"""
+
+from MethodicConfigurator import _
+from MethodicConfigurator.internationalization import LANGUAGE_CHOICES
+from MethodicConfigurator.version import VERSION
+
+
+def add_common_arguments_and_parse(parser):
+    parser.add_argument(
+        "--loglevel",
+        type=str,
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help=_("Logging level (default is %(default)s)."),
+    )
+    parser.add_argument(
+        "-v", "--version", action="version", version=f"%(prog)s {VERSION}", help=_("Display version information and exit.")
+    )
+    parser.add_argument(
+        "--language",
+        type=str,
+        default=LANGUAGE_CHOICES[0],
+        choices=LANGUAGE_CHOICES,
+        help=_("User interface language (default is %(default)s)."),
+    )
+    return parser.parse_args()
