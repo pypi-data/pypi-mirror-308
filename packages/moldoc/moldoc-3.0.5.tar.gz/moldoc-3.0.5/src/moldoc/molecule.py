cde0d3ec7bad93b036796831ad72fbf00e908203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:34 +0000

    Add package moldoc/3.0.5/moldoc-3.0.5.tar.gz

---
+++ packages/moldoc/moldoc-3.0.5.tar.gz/moldoc-3.0.5/src/moldoc/molecule.py
@@ -0,0 +1,31 @@
+"""Molecular rendering."""
+
+from moldoc._internal.molecule import (
+    Atom,
+    AtomConfig,
+    Bond,
+    Color,
+    Material,
+    MeshLambertMaterial,
+    MeshNormalMaterial,
+    MeshPhongMaterial,
+    MeshPhysicalMaterial,
+    MeshStandardMaterial,
+    Molecule,
+    MoleculeConfig,
+)
+
+__all__ = [
+    "Atom",
+    "AtomConfig",
+    "Bond",
+    "Color",
+    "Material",
+    "MeshLambertMaterial",
+    "MeshNormalMaterial",
+    "MeshPhongMaterial",
+    "MeshPhysicalMaterial",
+    "MeshStandardMaterial",
+    "Molecule",
+    "MoleculeConfig",
+]
