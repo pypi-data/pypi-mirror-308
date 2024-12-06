7709d6f0a5f7a87167e2ba8915c6a7cb974e7cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:18 +0000

    Add package torchsystem/0.1.1/torchsystem-0.1.1.tar.gz

---
+++ packages/torchsystem/torchsystem-0.1.1.tar.gz/torchsystem-0.1.1/torchsystem/__init__.py
@@ -0,0 +1,9 @@
+from pybondi import Session
+from pybondi import Repository
+from torchsystem.aggregate import Aggregate
+from torchsystem.aggregate import Loader
+from torchsystem.loaders import Loaders
+from torchsystem.storage import Models, Criterions, Optimizer, Datasets
+from torchsystem.compiler import Compiler
+from torchsystem.commands import Command
+from torchsystem.events import Event
