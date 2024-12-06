ef9d00a635dc57c6affade26987275813624655a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:18 +0000

    Add package shib-config-green/0.0.30/shib_config_green-0.0.30.tar.gz

---
+++ packages/shib-config-green/shib_config_green-0.0.30.tar.gz/shib_config_green-0.0.30/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shib-config-green",
+    version="0.0.30",
+    description="Used to update and manage the Shibboleth Service Provider Directory in Green Production for" 
+                "Terra Dotta",
+    packages=find_packages(),
+    author="JonathanM",
+    author_email="jonathan.mckay@terradotta.com",
+    install_requires=["pyodbc>=5.2.0", "requests>=2.32.3"],
+    extras_require={
+        "dev": ["setuptools>=75.5.0", "pytest>=8.3.3"],
+    },
+    entry_points={
+        'console_scripts': [
+            'run-shib = mgmt_scripts.run:main',
+        ]
+    },
+    python_requires=">=3.12",
+)
