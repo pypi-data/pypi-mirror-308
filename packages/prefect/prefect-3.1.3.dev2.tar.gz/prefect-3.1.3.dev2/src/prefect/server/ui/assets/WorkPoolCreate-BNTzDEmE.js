d7da1a4e0ea18344d2c2d5e2ac598a3d4e920250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:23 +0000

    Add package prefect/3.1.3.dev2/prefect-3.1.3.dev2.tar.gz

---
+++ packages/prefect/prefect-3.1.3.dev2.tar.gz/prefect-3.1.3.dev2/src/prefect/server/ui/assets/WorkPoolCreate-BNTzDEmE.js
@@ -0,0 +1,2 @@
+import{d as r,c as s,g as e,h as n,o as l,j as o,k as a,d0 as u,d1 as _}from"./index--BXsRDnW.js";import{u as c}from"./usePageTitle-CvzYXBv_.js";const h=r({__name:"WorkPoolCreate",setup(p){return c("Create Work Pool"),(d,f)=>{const t=n("p-layout-default");return l(),s(t,null,{header:e(()=>[o(a(u))]),default:e(()=>[o(a(_))]),_:1})}}});export{h as default};
+//# sourceMappingURL=WorkPoolCreate-BNTzDEmE.js.map
