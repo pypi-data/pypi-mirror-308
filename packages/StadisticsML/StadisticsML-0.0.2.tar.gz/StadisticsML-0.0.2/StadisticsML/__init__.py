b1d2a39bb9febc6021ea5d941a5f4760772326d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:06:15 +0000

    Add package StadisticsML/0.0.2/StadisticsML-0.0.2.tar.gz

---
+++ packages/StadisticsML/StadisticsML-0.0.2.tar.gz/StadisticsML-0.0.2/StadisticsML/__init__.py
@@ -0,0 +1,5 @@
+from .Funciones import detec_outliers 
+from .Funciones import cal_random_probability
+from .Funciones import durbin_watson
+from .Funciones import calculate_dL
+from .Funciones import calculate_dU
