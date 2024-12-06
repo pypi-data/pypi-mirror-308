f2ba1be0eef080f7f376f801adeb61e221b41479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:06:57 +0000

    Add package is-thirteen-yuva47/0.2/is-thirteen-yuva47-0.2.tar.gz

---
+++ packages/is-thirteen-yuva47/is-thirteen-yuva47-0.2.tar.gz/is-thirteen-yuva47-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="is-thirteen-yuva47",
+    version="0.2",
+    author="Yuvaraj",
+    author_email="yuvarj68@gmail.com",
+    description="A simple package to check if a number is thirteen",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yuva47/is-thirteen",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
