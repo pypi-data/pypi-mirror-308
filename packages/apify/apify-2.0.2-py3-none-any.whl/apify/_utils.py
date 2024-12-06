1206e5e27435b7db3045cf863becadf8705330cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:15 +0000

    Add package apify/2.0.2/apify-2.0.2-py3-none-any.whl

---
+++ packages/apify/apify-2.0.2-py3-none-any.whl/apify/_utils.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import builtins
+import sys
+from importlib import metadata
+
+
+def get_system_info() -> dict:
+    python_version = '.'.join([str(x) for x in sys.version_info[:3]])
+
+    system_info: dict[str, str | bool] = {
+        'apify_sdk_version': metadata.version('apify'),
+        'apify_client_version': metadata.version('apify-client'),
+        'crawlee_version': metadata.version('crawlee'),
+        'python_version': python_version,
+        'os': sys.platform,
+    }
+
+    if is_running_in_ipython():
+        system_info['is_running_in_ipython'] = True
+
+    return system_info
+
+
+def is_running_in_ipython() -> bool:
+    return getattr(builtins, '__IPYTHON__', False)
