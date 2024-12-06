410b88af2c681c81e0953eaae508cddf6fda0e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:40 +0000

    Add package flo-ai/0.0.4rc1/flo_ai-0.0.4rc1-py3-none-any.whl

---
+++ packages/flo-ai/flo_ai-0.0.4rc1-py3-none-any.whl/flo_ai/models/flo_team.py
@@ -0,0 +1,18 @@
+from flo_ai.models.flo_member import FloMember
+from flo_ai.yaml.config import TeamConfig
+
+
+class FloTeam:
+    def __init__(self, team_config: TeamConfig, members: list[FloMember]) -> None:
+        self.name = team_config.name
+        self.config = team_config
+        self.members = members
+
+    class Builder:
+        def __init__(self, team_config: TeamConfig, members: list[FloMember]) -> None:
+            self.team_config = team_config
+            self.members = members
+            self.member_names = list(map(lambda x: x.name, self.members))
+
+        def build(self):
+            return FloTeam(team_config=self.team_config, members=self.members)
