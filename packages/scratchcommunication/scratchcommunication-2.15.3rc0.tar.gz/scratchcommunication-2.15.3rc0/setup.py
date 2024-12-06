68ded907b36dd26958b73a7c839f5f1b77a435b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:35 +0000

    Add package scratchcommunication/2.15.3rc0/scratchcommunication-2.15.3rc0.tar.gz

---
+++ packages/scratchcommunication/scratchcommunication-2.15.3rc0.tar.gz/scratchcommunication-2.15.3rc0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+    
+VERSION = '2.15.3c'
+
+setup(
+    name='scratchcommunication',
+    version=VERSION,
+    author='Simon Gilde',
+    author_email='simon.c.gilde@gmail.com',
+    description='A python module for communicating with scratch projects',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/thecommcraft/scratchcommunication',
+    packages=find_packages(exclude=[]),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    keywords=['scratch', 'api'],
+    install_requires=[
+        'requests',
+        'websocket-client',
+        'func-timeout',
+        'pycryptodome',
+        'attrs',
+        'browsercookie',
+        'cryptography',
+        'super-session-keys',
+        'weakreflist',
+    ],
+    python_requires='>=3.11',
+)
