5e76dbbc094a99c1b79a887505f2411aeec29eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:57 +0000

    Add package rithm/14.5.0/rithm-14.5.0.tar.gz

---
+++ packages/rithm/rithm-14.5.0.tar.gz/rithm-14.5.0/src/big_int/not.rs
@@ -0,0 +1,29 @@
+use std::ops::Not;
+
+use crate::big_int::digits::InvertComponents;
+
+use super::types::BigInt;
+
+impl<Digit: InvertComponents, const DIGIT_BITNESS: usize> Not
+    for &BigInt<Digit, DIGIT_BITNESS>
+{
+    type Output = BigInt<Digit, DIGIT_BITNESS>;
+
+    fn not(self) -> Self::Output {
+        let (sign, digits) =
+            Digit::invert_components::<DIGIT_BITNESS>(self.sign, &self.digits);
+        Self::Output { sign, digits }
+    }
+}
+
+impl<Digit: InvertComponents, const DIGIT_BITNESS: usize> Not
+    for BigInt<Digit, DIGIT_BITNESS>
+{
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        let (sign, digits) =
+            Digit::invert_components::<DIGIT_BITNESS>(self.sign, &self.digits);
+        Self { sign, digits }
+    }
+}
