0f775dd0228bad076bd610e8aaa96f08ef2e17b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:46 +0000

    Add package doku-python-library/1.1.59/doku_python_library-1.1.59.tar.gz

---
+++ packages/doku-python-library/doku_python_library-1.1.59.tar.gz/doku_python_library-1.1.59/doku_python_library/src/model/direct_debit/account_unbinding_additional_info_request.py
@@ -0,0 +1,10 @@
+
+class AccountUnbindingAdditionalInfoRequest:
+
+    def __init__(self, channel: str) -> None:
+        self.channel = channel
+    
+    def json(self) -> dict:
+        return {
+            "channel": self.channel
+        }
