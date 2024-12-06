fdb783876a923ce014c71538455027bad75cd0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:32 +0000

    Add package ncert-learn/2.7.2/ncert_learn-2.7.2.tar.gz

---
+++ packages/ncert-learn/ncert_learn-2.7.2.tar.gz/ncert_learn-2.7.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ncert_learn",                   # Replace with your package name
+    version="2.7.2",                     # Initial version
+    author="Muhammed Shafin P",
+    author_email="hejhdiss@example.com",
+    description="A module for learning in ncerts cs class 12 python and mysql",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url="https://github.com/hejhdiss/ncert_learn",  # Add your GitHub repository URL here
+    project_urls={
+        "Bug Tracker": "https://github.com/hejhdiss/ncert_learn/issues",
+        "Documentation": "https://hejhdiss.github.io/ncert_learn-website/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'mysql-connector-python',
+        'pytest',
+        'tox',# Add this line to include the MySQL connector
+    ],
+)
