03cf87ccbf8e09ac4d3f98c2c9bdbcb5c599181a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:31 +0000

    Add package queue-confirmer/0.0.6/queue_confirmer-0.0.6.tar.gz

---
+++ packages/queue-confirmer/queue_confirmer-0.0.6.tar.gz/queue_confirmer-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.6'
+DESCRIPTION = 'Handling server and client information'
+LONG_DESCRIPTION = 'A package that allows you to handle server and client interaction.'
+
+setup(
+    name="queue_confirmer",
+    version=VERSION,
+    author="MichaeCarlsonDev (Michael Carlson)",
+    author_email="<soprano19522@gmail.com>",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'sockets', 'solar car', 'threading'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
