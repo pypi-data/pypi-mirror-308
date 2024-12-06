99c7cd21acdc893ca9ffcb75e6ac8af026fae1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:11 +0000

    Add package target-apprise/0.1.0/target_apprise-0.1.0.tar.gz

---
+++ packages/target-apprise/target_apprise-0.1.0.tar.gz/target_apprise-0.1.0/target_apprise/target.py
@@ -0,0 +1,36 @@
+"""Apprise target class."""
+
+from singer_sdk.target_base import Target
+from singer_sdk import typing as th
+
+from target_apprise.sinks import (
+    AppriseSink,
+)
+
+
+class TargetApprise(Target):
+    """Sample target for Apprise."""
+
+    name = "target-apprise"
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "uris",
+            th.ArrayType(th.StringType),
+            description="Array of apprise URIs,"
+            + "checkout https://github.com/caronc/apprise",
+            required=True,
+        ),
+        th.Property(
+            "uri_replacement",
+            th.BooleanType,
+            description=(
+                "If enabled, allows for uris to be dynamically configured. Any fields "
+                "in the record that have a name beginning with `_sdc_replace_`, will "
+                "have their value substituted in for a matching string in the URI. See "
+                "an example [here](#dynamically-providing-target-emails)."
+            ),
+            required=True,
+            default=False,
+        ),
+    ).to_dict()
+    default_sink_class = AppriseSink
