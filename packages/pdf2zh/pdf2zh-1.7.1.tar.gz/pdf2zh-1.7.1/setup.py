41aa05d11cef3e3bfebdd2a297ac3c67b8872e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:04 +0000

    Add package pdf2zh/1.7.1/pdf2zh-1.7.1.tar.gz

---
+++ packages/pdf2zh/pdf2zh-1.7.1.tar.gz/pdf2zh-1.7.1/setup.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from pdf2zh import __version__, __author__
+from setuptools import setup
+
+root_dir = Path(__file__).parent
+with open(root_dir / "README.md", encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name="pdf2zh",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    description="Latex PDF Translator",
+    license="MIT",
+    version=__version__,
+    author=__author__,
+    author_email="byaidux@gmail.com",
+    url="https://github.com/Byaidu/PDFMathTranslate",
+    packages=["pdf2zh"],
+    install_requires=[
+        "charset-normalizer",
+        "cryptography",
+        "requests",
+        "pymupdf",
+        "tqdm",
+        "tenacity",
+        "doclayout-yolo",
+        "numpy",
+        "ollama",
+        "deepl",
+        "openai",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pdf2zh=pdf2zh.pdf2zh:main',
+        ]
+    },
+)
