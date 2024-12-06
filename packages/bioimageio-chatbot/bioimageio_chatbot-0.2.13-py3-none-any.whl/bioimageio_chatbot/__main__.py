026d3e4ad38c36e59999fac008cf3d66d3bb592f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:51 +0000

    Add package bioimageio-chatbot/0.2.13/bioimageio_chatbot-0.2.13-py3-none-any.whl

---
+++ packages/bioimageio-chatbot/bioimageio_chatbot-0.2.13-py3-none-any.whl/bioimageio_chatbot/__main__.py
@@ -0,0 +1,89 @@
+import sys
+import argparse
+import asyncio
+import subprocess
+import os
+from bioimageio_chatbot.knowledge_base import load_knowledge_base
+
+def start_server(args):
+    if args.login_required:
+        os.environ["BIOIMAGEIO_LOGIN_REQUIRED"] = "true"
+    else:
+        os.environ["BIOIMAGEIO_LOGIN_REQUIRED"] = "false"
+    # get current file path so we can get the path of apps under the same directory
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    command = [
+        sys.executable,
+        "-m",
+        "hypha.server",
+        f"--host={args.host}",
+        f"--port={args.port}",
+        f"--public-base-url={args.public_base_url}",
+        f"--static-mounts=/chat:{current_dir}/static",
+        "--startup-functions=bioimageio_chatbot.chatbot:register_chat_service"
+    ]
+    subprocess.run(command)
+
+def connect_server(args):
+    from bioimageio_chatbot.chatbot import connect_server
+    if args.login_required:
+        os.environ["BIOIMAGEIO_LOGIN_REQUIRED"] = "true"
+    else:
+        os.environ["BIOIMAGEIO_LOGIN_REQUIRED"] = "false"
+    server_url = args.server_url
+    loop = asyncio.get_event_loop()
+    loop.create_task(connect_server(server_url))
+    loop.run_forever()
+
+def create_knowledge_base(args):
+    from bioimageio_chatbot.knowledge_base import create_vector_knowledge_base
+    create_vector_knowledge_base(args.output_dir)
+
+def init(args):
+    knowledge_base_path = os.environ.get("BIOIMAGEIO_KNOWLEDGE_BASE_PATH", "./bioimageio-knowledge-base")
+    assert knowledge_base_path is not None, "Please set the BIOIMAGEIO_KNOWLEDGE_BASE_PATH environment variable to the path of the knowledge base."
+    if not os.path.exists(knowledge_base_path):
+        print(f"The knowledge base is not found at {knowledge_base_path}, will download it automatically.")
+        os.makedirs(knowledge_base_path, exist_ok=True)
+    docs_store_dict = load_knowledge_base(knowledge_base_path)
+    
+    print("Databases loaded in the knowledge base:")
+    for key in docs_store_dict.keys():
+        print(f" - {key}")
+
+def main():
+    parser = argparse.ArgumentParser(description="BioImage.IO Chatbot utility commands.")
+    
+    subparsers = parser.add_subparsers()
+
+    # Init command
+    parser_init = subparsers.add_parser("init")
+    parser_init.set_defaults(func=init)
+
+    # Start server command
+    parser_start_server = subparsers.add_parser("start-server")
+    parser_start_server.add_argument("--host", type=str, default="0.0.0.0")
+    parser_start_server.add_argument("--port", type=int, default=9000)
+    parser_start_server.add_argument("--public-base-url", type=str, default="")
+    parser_start_server.add_argument("--login-required", action="store_true")
+    parser_start_server.set_defaults(func=start_server)
+    
+    # Connect server command
+    parser_connect_server = subparsers.add_parser("connect-server")
+    parser_connect_server.add_argument("--server-url", default="https://ai.imjoy.io")
+    parser_connect_server.add_argument("--login-required", action="store_true")
+    parser_connect_server.set_defaults(func=connect_server)
+    
+    # Create knowledge base command
+    parser_create_kb = subparsers.add_parser("create-knowledge-base")
+    parser_create_kb.add_argument("--output-dir", default="./bioimageio-knowledge-base")
+    parser_create_kb.set_defaults(func=create_knowledge_base)
+    
+    args = parser.parse_args()
+    if hasattr(args, 'func'):
+        args.func(args)
+    else:
+        parser.print_help()
+        
+if __name__ == '__main__':
+    main()
---
+++ packages/bioimageio-chatbot/bioimageio_chatbot-0.2.13-py3-none-any.whl/bioimageio_chatbot/utils.py
@@ -0,0 +1,116 @@
+
+import requests
+import yaml
+import os
+from tqdm import tqdm
+from pydantic import BaseModel, Field
+from typing import Callable, Optional
+import typing
+from inspect import signature
+from typing import Any, Callable, Dict, Optional
+from bioimageio_chatbot.jsonschema_pydantic import json_schema_to_pydantic_model
+from schema_agents import schema_tool
+
+def get_manifest():
+    # If no manifest is provided, download from the repo
+    if not os.path.exists("./knowledge-base-manifest.yaml"):
+        print("Downloading the knowledge base manifest...")
+        response = requests.get("https://raw.githubusercontent.com/bioimage-io/bioimageio-chatbot/main/knowledge-base-manifest.yaml")
+        assert response.status_code == 200
+        with open("./knowledge-base-manifest.yaml", "wb") as f:
+            f.write(response.content)
+    
+    return yaml.load(open("./knowledge-base-manifest.yaml", "r"), Loader=yaml.FullLoader)
+
+
+def download_file(url, filename):
+    response = requests.get(url, stream=True)
+    file_size = int(response.headers.get('content-length', 0))
+
+    # Initialize the progress bar
+    progress = tqdm(response.iter_content(1024), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
+    
+    with open(filename, 'wb') as f:
+        for data in progress:
+            # Update the progress bar
+            progress.update(len(data))
+            f.write(data)
+
+
+def extract_schemas(function):
+    sig = signature(function)
+    positional_annotation = [
+        p.annotation
+        for p in sig.parameters.values()
+        if p.kind == p.POSITIONAL_OR_KEYWORD
+    ][0]
+    output_schemas = (
+        [sig.return_annotation]
+        if not isinstance(sig.return_annotation, typing._UnionGenericAlias)
+        else list(sig.return_annotation.__args__)
+    )
+    input_schemas = (
+        [positional_annotation]
+        if not isinstance(positional_annotation, typing._UnionGenericAlias)
+        else list(positional_annotation.__args__)
+    )
+    return input_schemas, output_schemas
+
+class ChatbotExtension(BaseModel):
+    """Chatbot extension."""
+
+    id: str
+    name: str
+    description: str
+    tools: Optional[Dict[str, Any]] = {}
+    get_schema: Optional[Callable] = None
+    get_state: Optional[Callable] = None
+    info: Optional[Dict[str, Any]] = {}
+
+class LegacyChatbotExtension(BaseModel):
+    """A class that defines the interface for a user extension"""
+    name: str = Field(..., description="The name of the extension")
+    description: str = Field(..., description="A description of the extension")
+    get_schema: Optional[Callable] = Field(None, description="A function that returns the schema for the extension")
+    execute: Callable = Field(..., description="The extension's execution function")
+    schema_class: Optional[BaseModel] = Field(None, description="The schema class for the extension")
+
+def convert_to_dict(obj):
+    if isinstance(obj, BaseModel):
+        return obj.dict()
+    if isinstance(obj, dict):
+        return {k: convert_to_dict(v) for k, v in obj.items()}
+    if isinstance(obj, list):
+        return [convert_to_dict(v) for v in obj]
+    return obj
+
+
+async def legacy_extension_to_tool(extension: LegacyChatbotExtension):
+    if extension.get_schema:
+        schema = await extension.get_schema()
+        extension.schema_class = json_schema_to_pydantic_model(schema)
+    else:
+        input_schemas, _ = extract_schemas(extension.execute)
+        extension.schema_class = input_schemas[0]
+
+    assert extension.schema_class, f"Extension {extension.name} has no valid schema class."
+
+    # NOTE: Right now, the first arguments has to be req
+    async def execute(req: extension.schema_class):
+        print("Executing extension:", extension.name, req)
+        # req = extension.schema_class.parse_obj(req)
+        result = await extension.execute(req)
+        return convert_to_dict(result)
+
+    execute.__name__ = extension.name
+
+    if extension.get_schema:
+        execute.__doc__ = schema['description']
+    
+    if not execute.__doc__:
+        # if extension.execute is partial
+        if hasattr(extension.execute, "func"):
+            execute.__doc__ = extension.execute.func.__doc__ or extension.description
+        else:
+            execute.__doc__ = extension.execute.__doc__ or extension.description
+    return schema_tool(execute)
