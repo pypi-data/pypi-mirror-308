1dd0bf8b51c508f9c4bf5787fe924f2308c2ca0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:29 +0000

    Add package polarstar/0.2.0/polarstar-0.2.0-py3-none-any.whl

---
+++ packages/polarstar/polarstar-0.2.0-py3-none-any.whl/polarstar/__init__.py
@@ -0,0 +1,19 @@
+"""Polar Star.
+
+STAR is a Python library focused on simplifying the automation of scientific experiments,
+including data collection and hardware control. While STAR is designed to integrate with POLAR,
+it can also be used independently for various scientific applications.
+
+Modules:
+- plate: Defines the Plate class for handling data and automating optical measurements.
+
+Exports:
+- Plate: Main class representing a plate.
+- load_plate: Function to load a saved plate configuration from a file.
+"""
+
+from .plate import Plate
+from .plate import load_plate
+
+
+__all__ = ["Plate", "load_plate"]
