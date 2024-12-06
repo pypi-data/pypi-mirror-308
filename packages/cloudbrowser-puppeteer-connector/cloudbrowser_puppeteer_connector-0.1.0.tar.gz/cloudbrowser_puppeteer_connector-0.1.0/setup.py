4b6bc7bfe3c16014572a05d9436eaae9a27fa49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:01 +0000

    Add package cloudbrowser-puppeteer-connector/0.1.0/cloudbrowser_puppeteer_connector-0.1.0.tar.gz

---
+++ packages/cloudbrowser-puppeteer-connector/cloudbrowser_puppeteer_connector-0.1.0.tar.gz/cloudbrowser_puppeteer_connector-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloudbrowser-puppeteer-connector",
+    version="0.1.0",
+    author="cloudbrowser",
+    author_email="contact@cloudbrowser.ai",
+    description="Client for connection with CloudBrowser.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
