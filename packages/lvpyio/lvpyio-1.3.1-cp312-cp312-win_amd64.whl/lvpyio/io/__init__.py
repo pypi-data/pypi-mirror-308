425a5a1a02d52615f0bbd4ccf6b0f021ee6fdac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:47 +0000

    Add package lvpyio/1.3.1/lvpyio-1.3.1-cp312-cp312-win_amd64.whl

---
+++ packages/lvpyio/lvpyio-1.3.1-cp312-cp312-win_amd64.whl/lvpyio/io/__init__.py
@@ -0,0 +1,10 @@
+#############################################################################
+#                                                                           #
+#           Copyright (C) LaVision GmbH.  All Rights Reserved.              #
+#                                                                           #
+#############################################################################
+
+from .read_buffer import read_buffer
+from .write_buffer import write_buffer
+from .set import read_set, write_set, is_multiset, _create_set
+from .particles import read_particles, write_particles
---
+++ packages/lvpyio/lvpyio-1.3.1-cp312-cp312-win_amd64.whl/lvpyio/validate/validate_common.py
@@ -0,0 +1,15 @@
+#############################################################################
+#                                                                           #
+#           Copyright (C) LaVision GmbH.  All Rights Reserved.              #
+#                                                                           #
+#############################################################################
+
+import numpy as np
+
+
+scalar_types = {
+    np.dtype("double"),
+    np.dtype("single"),
+    np.dtype("int32"),
+    np.dtype("uint16"),
+}
