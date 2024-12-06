1e59887fecf986784c2a13f84b928abd05846d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:13 +0000

    Add package buco-db-controller/0.1.104/buco_db_controller-0.1.104.tar.gz

---
+++ packages/buco-db-controller/buco_db_controller-0.1.104.tar.gz/buco_db_controller-0.1.104/buco_db_controller/models/team_ratings.py
@@ -0,0 +1,15 @@
+
+
+class TeamRatings:
+    def __init__(self, league_id: int, season: int, team_ratings: dict):
+        self.league_id: int = league_id
+        self.season: int = season
+        self.team_ratings: dict = team_ratings
+
+    @classmethod
+    def from_dict(cls, response: dict) -> 'TeamRatings':
+        league_id = response['parameters']['league']
+        season = response['parameters']['season']
+        team_ratings = response['data']
+
+        return cls(league_id=league_id, season=season, team_ratings=team_ratings)
