ed4e2a4434409c031b0aae4bf6619fc675156732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:05:46 +0000

    Add package search-engine-tool-vito1317/0.7.0/search_engine_tool_vito1317-0.7.0.tar.gz

---
+++ packages/search-engine-tool-vito1317/search_engine_tool_vito1317-0.7.0.tar.gz/search_engine_tool_vito1317-0.7.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="search-engine-tool-vito1317",
+    version="0.7.0",
+    packages=find_packages(),
+    install_requires=[
+        "selenium",
+        "BeautifulSoup4"
+    ],
+
+    author="vito",
+    author_email="vito95311@gmail.com",
+    description="搜索引擎API Bing / Google / Yahoo ",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/vito1317/search-engine-tool-vito1317",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
