681d6f8ef11505ba9f2fa9f128eb3eb685c06083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:13 +0000

    Add package pyproject-installer/0.5.5/pyproject_installer-0.5.5-py3-none-any.whl

---
+++ packages/pyproject-installer/pyproject_installer-0.5.5-py3-none-any.whl/pyproject_installer/deps_cmd/collectors/__init__.py
@@ -0,0 +1,36 @@
+from .pep517 import Pep517Collector
+from .pep518 import Pep518Collector
+from .pip_reqfile import PipReqFileCollector
+from .metadata import MetadataCollector
+from .poetry import PoetryCollector
+from .tox import ToxCollector
+from .hatch import HatchCollector
+from .pdm import PdmCollector
+from .pipenv import PipenvCollector
+from .pep735 import Pep735Collector
+
+
+__all__ = ["get_collector", "SUPPORTED_COLLECTORS"]
+
+SUPPORTED_COLLECTORS = {
+    cls.name: cls
+    for cls in [
+        Pep517Collector,
+        Pep518Collector,
+        MetadataCollector,
+        PipReqFileCollector,
+        PoetryCollector,
+        ToxCollector,
+        HatchCollector,
+        PdmCollector,
+        PipenvCollector,
+        Pep735Collector,
+    ]
+}
+
+
+def get_collector(name):
+    try:
+        return SUPPORTED_COLLECTORS[name]
+    except KeyError:
+        return None
