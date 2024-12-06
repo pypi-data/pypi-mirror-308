8e71937c42ef83549f5a397e786aad66df977a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:05 +0000

    Add package athena-pysdk/0.1.3/athena_pysdk-0.1.3-py3-none-any.whl

---
+++ packages/athena-pysdk/athena_pysdk-0.1.3-py3-none-any.whl/athena_pysdk/fastapi/__init__.py
@@ -0,0 +1,4 @@
+from athena_pysdk.fastapi.utils.tool_pinpoint import middleware, CommonPlugin
+
+
+__all__ = ['middleware', 'CommonPlugin']
---
+++ packages/athena-pysdk/athena_pysdk-0.1.3-py3-none-any.whl/athena_pysdk/fastapi/utils/tool_pinpoint.py
@@ -0,0 +1,23 @@
+import os
+from starlette_context.middleware import ContextMiddleware
+from pinpointPy import set_agent, use_thread_local_context
+from pinpointPy.Fastapi import async_monkey_patch_for_pinpoint, CommonPlugin
+from starlette.middleware import Middleware
+from pinpointPy.Fastapi import PinPointMiddleWare
+
+use_thread_local_context()
+async_monkey_patch_for_pinpoint()
+
+app_id = os.getenv('pinpoint_id', 'dev_pysdk')
+app_name = os.getenv('pinpoint_appname', 'dev_pysdk')
+collector_agent_ip = os.getenv('collector_agent_ip')
+collector_agent_port = os.getenv('collector_agent_port')
+collect_agent_host = 'tcp:{}:{}'.format(collector_agent_ip, collector_agent_port)
+print(app_id, app_name, collect_agent_host)
+
+middleware = [
+    Middleware(ContextMiddleware),
+    Middleware(PinPointMiddleWare)
+]
+
+set_agent(app_id, app_name, collect_agent_host, -1, True)
