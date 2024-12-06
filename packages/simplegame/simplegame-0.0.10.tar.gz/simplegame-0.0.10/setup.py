248dd7c0a89499809e19292ce25993655af95d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:05:00 +0000

    Add package simplegame/0.0.10/simplegame-0.0.10.tar.gz

---
+++ packages/simplegame/simplegame-0.0.10.tar.gz/simplegame-0.0.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='simplegame',
+    version='0.0.10',
+    description='Package for creating simple games',
+    url='https://github.com/szotms/simplegame',
+    author='Michal Szot',
+    author_email='szot.net@gmail.com',
+    license='MIT',
+    packages=['simplegame'],
+    zip_safe=False,
+    install_requires=[
+       'Pillow==11.0.0',
+       'pygame==2.5.2'
+    ]
+)
