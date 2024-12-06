28053a573372cc67151f055404fc3a80d5617814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:13 +0000

    Add package KeyisBClient/1.4.0.1.1/keyisbclient-1.4.0.1.1.tar.gz

---
+++ packages/KeyisBClient/keyisbclient-1.4.0.1.1.tar.gz/keyisbclient-1.4.0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+name = '${LibName}'
+
+setup(
+    name=name,
+    version='${version}',
+    author="KeyisB",
+    author_email="keyisb.pip@gmail.com",
+    description=name,
+    long_description='',
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/KeyisB/libs/tree/main/{name}",
+    package_dir={'': f'{name}'.replace('-','_')},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    license="MMB License v1.0",
+    install_requires= '${install_requires}',
+)
