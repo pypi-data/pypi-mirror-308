6341113dacbc9983ad214f00210ee6c33eb743b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:34 +0000

    Add package natal/0.7.5/natal-0.7.5-py3-none-any.whl

---
+++ packages/natal/natal-0.7.5-py3-none-any.whl/natal/classes.py
@@ -0,0 +1,155 @@
+from natal.const import *
+from math import floor
+
+
+class MovableBody(Body):
+    """
+    Represents a movable celestial body with additional properties.
+    """
+
+    degree: float = 0
+    speed: float = 0
+    normalized_degree: float = 0
+
+    @property
+    def signed_deg(self) -> int:
+        """
+        Degree in sign, between 0 and 29.
+
+        Returns:
+            int: The degree in sign.
+        """
+        return int(self.degree % 30)
+
+    @property
+    def minute(self) -> int:
+        """
+        Minute of the body, between 0 and 59.
+
+        Returns:
+            int: The minute of the body.
+        """
+        minutes = (self.degree % 30 - self.signed_deg) * 60
+        return floor(minutes)
+
+    @property
+    def retro(self) -> bool:
+        """
+        Retrograde status.
+
+        Returns:
+            bool: True if retrograde, False otherwise.
+        """
+        return self.speed < 0
+
+    @property
+    def rx(self) -> str:
+        """
+        Retrograde symbol.
+
+        Returns:
+            str: The retrograde symbol if retrograde, empty string otherwise.
+        """
+        return "℞" if self.retro else ""
+
+    @property
+    def sign(self) -> SignMember:
+        """
+        Return sign name, symbol, element, quality, and polarity.
+
+        Returns:
+            SignMember: The sign member.
+        """
+        idx = int(self.degree // 30)
+        return SIGN_MEMBERS[idx]
+
+    @property
+    def dms(self) -> str:
+        """
+        Degree Minute Second representation of the position.
+
+        Returns:
+            str: The DMS representation.
+        """
+        op = [f"{self.signed_deg:02d}°", f"{self.minute:02d}'"]
+        if self.rx:
+            op.append(self.rx)
+        return "".join(op)
+
+    @property
+    def signed_dms(self) -> str:
+        """
+        Degree Minute representation with sign.
+
+        Returns:
+            str: The signed DMS representation.
+        """
+        op = [f"{self.signed_deg:02d}°", self.sign.symbol, f"{self.minute:02d}'"]
+        if self.rx:
+            op.append(self.rx)
+        return "".join(op)
+
+
+class Planet(MovableBody):
+    """
+    Represents a planet.
+    """
+
+    ...
+
+
+class Extra(MovableBody):
+    """
+    Represents an extra celestial body (e.g. Moon's Node and Asteroids).
+    """
+
+    ...
+
+
+class Vertex(MovableBody):
+    """
+    Represents a vertex (Asc, Dsc, MC, IC).
+    """
+
+    ...
+
+
+class Aspectable(MovableBody):
+    """
+    Represents a celestial body that can form aspects.
+    """
+
+    ...
+
+
+class Sign(SignMember):
+    """
+    alias to SignMember
+    """
+
+    ...
+
+
+class House(MovableBody):
+    """
+    Represents a house.
+    """
+
+    ruler: str = None
+    ruler_sign: str = None
+    ruler_house: int = None
+    classic_ruler: str = None
+    classic_ruler_sign: str = None
+    classic_ruler_house: int = None
+
+
+class Aspect(DotDict):
+    """
+    Represents an aspect between two celestial bodies.
+    """
+
+    body1: Aspectable
+    body2: Aspectable
+    aspect_member: AspectMember
+    applying: bool = None
+    orb: float = None
---
+++ packages/natal/natal-0.7.5-py3-none-any.whl/natal/utils.py
@@ -0,0 +1,64 @@
+"""utility functions for natal"""
+
+from datetime import datetime
+from natal.config import Config
+from typing import Iterable
+
+
+def color_hex(name: str, config: Config = Config()) -> str:
+    """
+    Get color hex from name and Config instance, default to 'natal_config.yml'.
+
+    Args:
+        name (str): The name of the color.
+        config (Config): The configuration instance.
+
+    Returns:
+        str: The hex value of the color.
+    """
+    return getattr(config.colors, name)
+
+
+def pairs[T](iterable: Iterable[T]) -> list[tuple[T, T]]:
+    """
+    Generate pairs of elements from an iterable.
+
+    Args:
+        iterable (Iterable[T]): The input iterable.
+
+    Returns:
+        list[tuple[T, T]]: A list of tuples, each containing a pair of elements.
+    """
+    output = []
+    for i in range(len(iterable)):
+        for j in range(i + 1, len(iterable)):
+            output.append((iterable[i], iterable[j]))
+    return output
+
+
+def member_of[T](const: list[T], name: str) -> T:
+    """
+    Get a member from a list of constants by name.
+
+    Args:
+        const (list[T]): The list of constants.
+        name (str): The name of the member.
+
+    Returns:
+        T: The member with the specified name.
+    """
+    idx: int = const["name"].index(name)
+    return {prop: const[prop][idx] for prop in const.model_fields}
+
+
+def str_to_dt(dt_str: str) -> datetime:
+    """
+    Convert a string to a datetime object.
+
+    Args:
+        dt_str (str): The datetime string.
+
+    Returns:
+        datetime: The corresponding datetime object.
+    """
+    return datetime.strptime(dt_str, "%Y-%m-%d %H:%M")
