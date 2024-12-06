19cf758c5a3ccedc61c8e7ed030738eb557ea318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:23 +0000

    Add package carta-authz/0.0.1.dev1731606180/carta-authz-0.0.1.dev1731606180.tar.gz

---
+++ packages/carta-authz/carta-authz-0.0.1.dev1731606180.tar.gz/carta-authz-0.0.1.dev1731606180/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='carta-authz',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
