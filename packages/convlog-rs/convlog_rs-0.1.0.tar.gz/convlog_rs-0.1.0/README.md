4f454713f14c062415bd7d69cb7c0790907f8624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:12 +0000

    Add package convlog-rs/0.1.0/convlog_rs-0.1.0.tar.gz

---
+++ packages/convlog-rs/convlog_rs-0.1.0.tar.gz/convlog_rs-0.1.0/README.md
@@ -0,0 +1,3 @@
+# convlog
+
+A rust binding for [convlog](https://github.com/Equim-chan/mjai-reviewer/tree/master/convlog)
---
+++ packages/convlog-rs/convlog_rs-0.1.0.tar.gz/convlog_rs-0.1.0/convlog/__init__.py
@@ -0,0 +1,20 @@
+from . import _convlog
+
+__all__ = (
+    "__version__",
+    "TenhouLogParsingError",
+    "TenhouToMjaiError",
+    "JsonSerializationError",
+    "JsonParsingError",
+    "tenhou_to_mjai",
+)
+
+# VERSION is set in Cargo.toml
+VERSION = _convlog.__version__
+__version__ = _convlog.__version__
+TenhouLogParsingError = _convlog.TenhouLogParsingError
+TenhouToMjaiError = _convlog.TenhouToMjaiError
+JsonSerializationError = _convlog.JsonSerializationError
+JsonParsingError = _convlog.JsonParsingError
+
+tenhou_to_mjai = _convlog.tenhou_to_mjai
