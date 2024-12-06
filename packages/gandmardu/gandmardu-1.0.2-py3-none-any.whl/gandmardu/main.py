05094c58405c065a85fc22f01b17fba444d62598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:06:33 +0000

    Add package gandmardu/1.0.2/gandmardu-1.0.2-py3-none-any.whl

---
+++ packages/gandmardu/gandmardu-1.0.2-py3-none-any.whl/gandmardu/main.py
@@ -0,0 +1,11 @@
+def print_hello_world_code():
+    # The code that would go into the gand.py file
+    hello_world_code = 'print("Hello, gandu World!")\n'
+    
+    # Print the code to the terminal
+    print(f"Here is the code that would go into 'gand.py':\n")
+    print(hello_world_code)
+
+# Function to execute when the script is run directly
+if __name__ == "__main__":
+    print_hello_world_code()
