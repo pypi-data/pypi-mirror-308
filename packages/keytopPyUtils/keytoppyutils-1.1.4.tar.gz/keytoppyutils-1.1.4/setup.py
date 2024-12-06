15e350197624f9b876814bb97e584b4094cc6bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:03 +0000

    Add package keytopPyUtils/1.1.4/keytoppyutils-1.1.4.tar.gz

---
+++ packages/keytopPyUtils/keytoppyutils-1.1.4.tar.gz/keytoppyutils-1.1.4/setup.py
@@ -0,0 +1,27 @@
+# kt-py-redis\setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='keytopPyUtils',
+    version='1.1.4',
+    packages=find_packages(),
+    install_requires=[
+        'markdown',
+        'bs4',
+        'python-docx',
+        'weasyprint',
+        'esdk-obs-python'
+    ],
+    author='zhangjukai',
+    author_email='zhangjukai@keytop.com.cn',
+    description='keytop python utils',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
