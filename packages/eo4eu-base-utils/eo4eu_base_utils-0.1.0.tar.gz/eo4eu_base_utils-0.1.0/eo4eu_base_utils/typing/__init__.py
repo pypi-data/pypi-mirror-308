e8782fdaed46ee60e1e7700b1529e5fda210436d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:06 +0000

    Add package eo4eu-base-utils/0.1.0/eo4eu_base_utils-0.1.0.tar.gz

---
+++ packages/eo4eu-base-utils/eo4eu_base_utils-0.1.0.tar.gz/eo4eu_base_utils-0.1.0/eo4eu_base_utils/typing/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from typing import Self, Any, Optional, Callable, Iterator, List, Dict
+except Exception:
+    from typing_extensions import Self, Any, Optional, Callable, Iterator, List, Dict
