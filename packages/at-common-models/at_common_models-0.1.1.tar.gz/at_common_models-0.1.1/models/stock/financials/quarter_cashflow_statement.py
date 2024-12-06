801d6b97c0bea09bdbfc3e0fd24a36e8a41260d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:41 +0000

    Add package at-common-models/0.1.1/at_common_models-0.1.1.tar.gz

---
+++ packages/at-common-models/at_common_models-0.1.1.tar.gz/at_common_models-0.1.1/models/stock/financials/quarter_cashflow_statement.py
@@ -0,0 +1,4 @@
+from models.stock.financials.base_models import BaseCashflowStatementModel
+
+class QuarterCashflowStatementModel(BaseCashflowStatementModel):
+    __tablename__ = "stock_financials_quarter_cashflow_statements"
