ad67251c6696adb30aacdd6cd4c234a506b785dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:05:04 +0000

    Add package libbs/2.7.0/libbs-2.7.0.tar.gz

---
+++ packages/libbs/libbs-2.7.0.tar.gz/libbs-2.7.0/libbs/artifacts/__init__.py
@@ -0,0 +1,28 @@
+from .formatting import TomlHexEncoder, ArtifactFormat
+from .artifact import Artifact
+from .comment import Comment
+from .decompilation import Decompilation
+from .enum import Enum
+from .func import Function, FunctionHeader, FunctionArgument
+from .global_variable import GlobalVariable
+from .patch import Patch
+from .stack_variable import StackVariable
+from .struct import Struct, StructMember
+from .context import Context
+from .typedef import Typedef
+
+ART_NAME_TO_CLS = {
+    Function.__name__: Function,
+    FunctionHeader.__name__: FunctionHeader,
+    FunctionArgument.__name__: FunctionArgument,
+    StackVariable.__name__: StackVariable,
+    Comment.__name__: Comment,
+    GlobalVariable.__name__: GlobalVariable,
+    Enum.__name__: Enum,
+    Struct.__name__: Struct,
+    StructMember.__name__: StructMember,
+    Patch.__name__: Patch,
+    Decompilation.__name__: Decompilation,
+    Context.__name__: Context,
+    Typedef.__name__: Typedef,
+}
