59872730c41b5722a69905af4be86e48eb1d71af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:06 +0000

    Add package pyPCoA/0.1.4.2/pyPCoA-0.1.4.2.tar.gz

---
+++ packages/pyPCoA/pyPCoA-0.1.4.2.tar.gz/pyPCoA-0.1.4.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyPCoA',
+    version='0.1.4.2',
+    description='A package for Principal Coordinates Analysis (PCoA) with Jaccard and Bray-Curtis distances.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yuki Hamaguchi',
+    author_email='Yuki_Hamaguchi@suntory.co.jp',
+    url='https://github.com/YukiHSun/pyPCoA',
+    packages=find_packages(),
+    include_package_data=True,  # データファイルを含める設定
+    package_data={
+        'pyPCoA': ['data/*.csv'],  # dataディレクトリ内のすべてのCSVファイルを含める
+    },
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'openpyxl',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyPCoA = pyPCoA.core:main',  
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
