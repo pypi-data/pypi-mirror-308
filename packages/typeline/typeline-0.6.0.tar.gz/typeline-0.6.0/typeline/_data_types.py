3a50eb96fe09827f4ed40b582fdccc119ce72a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:16 +0000

    Add package typeline/0.6.0/typeline-0.6.0.tar.gz

---
+++ packages/typeline/typeline-0.6.0.tar.gz/typeline-0.6.0/typeline/_data_types.py
@@ -0,0 +1,31 @@
+from dataclasses import Field
+from types import UnionType
+from typing import Any
+from typing import ClassVar
+from typing import Protocol
+from typing import TypeAlias
+from typing import TypeVar
+from typing import cast
+
+
+class DataclassInstance(Protocol):
+    """A protocol for objects that are dataclass instances."""
+
+    __dataclass_fields__: ClassVar[dict[str, Field[Any]]]
+
+
+JsonType: TypeAlias = dict[str, "JsonType"] | list["JsonType"] | str | int | float | bool | None
+"""A JSON-like data type."""
+
+RecordType = TypeVar("RecordType", bound=DataclassInstance)
+"""The type variable for the records we will be reading and writing from delimited text data."""
+
+
+def build_union(*types: type) -> type | UnionType:
+    """Pass-through a singular type or build a union type if multiple types are provided."""
+    if len(types) == 1:
+        return types[0]
+    union: UnionType | type = types[0]
+    for t in types[1:]:
+        union |= t
+    return cast(UnionType, union)
