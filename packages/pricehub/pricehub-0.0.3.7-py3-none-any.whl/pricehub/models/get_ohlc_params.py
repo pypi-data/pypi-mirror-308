077ab54aba6710d3dffefc8a14f243b0524bd96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:00 +0000

    Add package pricehub/0.0.3.7/pricehub-0.0.3.7-py3-none-any.whl

---
+++ packages/pricehub/pricehub-0.0.3.7-py3-none-any.whl/pricehub/models/get_ohlc_params.py
@@ -0,0 +1,49 @@
+""" GetOhlcParams model. """
+
+from typing import Any
+
+import arrow
+from pydantic import BaseModel, field_validator, model_validator
+
+from pricehub.models import Interval
+
+
+class GetOhlcParams(BaseModel):
+    """
+    Get OHLC parameters.
+    """
+
+    broker: "Broker"  # type: ignore[name-defined]
+    symbol: str
+    interval: Interval
+    start: arrow.Arrow
+    end: arrow.Arrow
+
+    class Config:
+        """
+        Pydantic configuration.
+        """
+
+        arbitrary_types_allowed = True
+
+    @field_validator("start", "end", mode="before")
+    def convert_to_arrow(cls, value: Any) -> arrow.Arrow:
+        """
+        Convert the 'start' and 'end' values to Arrow objects.
+        :param value:
+        :return:
+        """
+        try:
+            return arrow.get(value)
+        except Exception as e:
+            raise ValueError(f"Invalid date format for value '{value}': {e}") from e
+
+    @model_validator(mode="after")
+    def check_start_before_end(self) -> "GetOhlcParams":
+        """
+        Check that the 'start' date is before the 'end' date.
+        :return:
+        """
+        if self.start > self.end:
+            raise ValueError("The 'start' date must be before the 'end' date.")
+        return self
