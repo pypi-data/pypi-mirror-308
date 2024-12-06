dae4e3b3cd79bffb720497ce2fd1f19e72681276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:39 +0000

    Add package ncert-learn/2.6.0/ncert_learn-2.6.0.tar.gz

---
+++ packages/ncert-learn/ncert_learn-2.6.0.tar.gz/ncert_learn-2.6.0/ncert_learn/__init__.py
@@ -0,0 +1,25 @@
+# ncert_learn/__init__.py
+__version__ = "2.6.0"
+
+from .intfncs import checkprime,factors,len_fibonacci,checkarmstrong,reverse,checkpalindrome,checkstrong,checkniven,prime,armstrong,strong,niven,palindrome,len_armstrong,len_strong,len_niven,len_palindrome,checkeven,checkodd,checkzero,checknegative,checkpositive
+from .stkdict import clearstackdict,createstackdict,pushstackdict,popstackdict,peekstackdict,displaymodestackdict# Import functions
+from .stklist import createstacklst,clearstacklst,pushstacklst,popstacklst,peekstacklst,displaymodestacklst
+from .dat import studyexamples,studyfunctions,studyshort,studymysqlpython,notes,basics,mysqlnotes
+from .area import areaofcircle,areaofpolygon,areaofrectangle,areaofsquare,areaoftriangle
+from .conversions import integertobinary,integertooctal,integertohexadecimal,binarytointeger
+from .mysql import mysqlconnect,mysqlconnectwithdatabase,mysqlshowdatabases,mysqlcreatedatabase,mysqlshowtables,mysqlshowtablesfromdatabase,mysqldescribetable,mysqldescribetablefromdatabase,mysqlfetchalltable,mysqlfetchalltablefromdatabase,mysqlcreatetable,mysqltableinsert,mysqlrowcounttablefromdatabase,mysqlexecutequery
+from .sort import bubblesort,insertionsort
+from .text import istextfile,copytextfromonetoanother,opentextfile,addlinetofile,readtextfile,cleartextfile,readspecificline
+from .os import getexecutablepath,cpucount,listdir,listdirfrompath,osname,processorname,isnetworkconnected,getpythoninterpreter
+from .packageinstall import install_library,install_librariesfromlist
+from .support import supportemail,feedbackemail,bugtrackerurl,githuburl,documentationurl,authorname
+from .time import currenttime,processtime,monotonic,threadtime
+# Optional: List functions to include in 'import *' calls
+__all__ = ['mysqlconnectwithdatabase','exampleusage','checkprime','factors','len_fibonacci','checkarmstrong','reverse','checkpalindrome','checkstrong','checkniven','prime','armstrong','strong','niven','palindrome','len_armstrong','len_strong','len_niven','len_palindrome','checkeven','checkodd','checkzero','checknegative','checkpositive','clearstackdict','createstackdict','pushstackdict','popstackdict','peekstackdict','displaymodestackdict','createstacklst','clearstacklst','pushstacklst','popstacklst','peekstacklst','displaymodestacklst','studyexamples','studyfunctions','studyshort','studymysqlpython','notes','basics','extendednotes','mysqlnotes'
+           ,'bubblesort','insertionsort','mysqlconnect','mysqlshowdatabases','mysqlcreatedatabase','mysqlshowtables','mysqlshowtablesfromdatabase','mysqldescribetable','mysqldescribetablefromdatabase','mysqlfetchalltable','mysqlfetchalltablefromdatabase','mysqlcreatetable','mysqltableinsert','mysqlrowcounttablefromdatabase','mysqlexecutequery',
+           'integertobinary','integertooctal','integertohexadecimal','binarytointeger','areaofcircle','areaofpolygon','areaofrectangle','areaofsquare','areaoftriangle',
+           'istextfile','copytextfromonetoanother','opentextfile','addlinetofile','readtextfile','cleartextfile','readspecificline',
+           'getexecutablepath','cpucount','listdir','listdirfrompath','osname','processorname','isnetworkconnected','getpythoninterpreter',
+           'install_library','install_librariesfromlist',
+           'supportemail','feedbackemail','bugtrackerurl','githuburl','documentationurl','authorname',
+           'currenttime','processtime','monotonic','threadtime']
---
+++ packages/ncert-learn/ncert_learn-2.6.0.tar.gz/ncert_learn-2.6.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ncert_learn",                   # Replace with your package name
+    version="2.6.0",                     # Initial version
+    author="Muhammed Shafin P",
+    author_email="hejhdiss@example.com",
+    description="A module for learning in ncerts cs class 12 python and mysql",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url="https://github.com/hejhdiss/ncert_learn",  # Add your GitHub repository URL here
+    project_urls={
+        "Bug Tracker": "https://github.com/hejhdiss/ncert_learn/issues",
+        "Documentation": "https://github.com/hejhdiss/ncert_learn#readme",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'mysql-connector-python',  # Add this line to include the MySQL connector
+    ],
+)
