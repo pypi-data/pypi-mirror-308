6706a001250dceb53ee9956ee1b70727cd040e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:52 +0000

    Add package python-bitpredict/1.4/python_bitpredict-1.4.tar.gz

---
+++ packages/python-bitpredict/python_bitpredict-1.4.tar.gz/python_bitpredict-1.4/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='python-bitpredict',
+ version='1.4',
+ author="Bitpredict",
+ author_email="admin@bitpredict.com",
+ description="This package for serving api's in python",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
