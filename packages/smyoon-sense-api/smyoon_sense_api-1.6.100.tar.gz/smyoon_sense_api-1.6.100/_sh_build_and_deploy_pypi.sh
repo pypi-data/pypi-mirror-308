1146d69bd2940106d0321dc9ff3e3c87a82d5924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:54 +0000

    Add package smyoon-sense-api/1.6.100/smyoon_sense_api-1.6.100.tar.gz

---
+++ packages/smyoon-sense-api/smyoon_sense_api-1.6.100.tar.gz/smyoon_sense_api-1.6.100/_sh_build_and_deploy_pypi.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+echo
+echo '[VERSION AT src/smyoon_sense_api/__init__.py]'
+grep __version__ src/smyoon_sense_api/__init__.py
+
+echo
+
+echo '[VERSION AT pyproject.toml]'
+grep version pyproject.toml
+echo
+
+echo '[VERSION AT README.md]'
+grep version README.md
+echo
+
+echo 'Do versions match? (y/n)'
+read -r ANSWER
+
+echo "${ANSWER}"
+
+if [ "${ANSWER}" = "n" ]; then
+  exit
+fi
+
+sudo apt install python3.8
+sudo apt install python3.8-venv
+
+python3.8 --version
+python3.8 -m pip install --upgrade pip
+python3.8 -m pip install --upgrade build
+
+rm -rf dist
+mkdir dist
+
+rm -rf .gitignore
+rm -rf .idea
+
+python3.8 -m build
+git reset --hard
+
+python3.8 -m pip install --upgrade twine
+
+#cp -f ~/.pypirc_for_project_smyoonear ~/.pypirc
+python3.8 -m twine upload dist/* --verbose --repository smyoon-sense-api
+#rm -rf ~/.pypirc
