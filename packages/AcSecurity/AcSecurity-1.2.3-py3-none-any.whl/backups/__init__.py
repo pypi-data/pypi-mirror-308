70740a7c3866b14565c07ed702409408151793c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:05:28 +0000

    Add package AcSecurity/1.2.3/AcSecurity-1.2.3-py3-none-any.whl

---
+++ packages/AcSecurity/AcSecurity-1.2.3-py3-none-any.whl/backups/__init__.py
@@ -0,0 +1,11 @@
+"""
+AcSecurity package.
+
+This package provides functionality to scan applications for vulnerabilities
+and code quality issues.
+"""
+
+from src.scanner import AcSecurity
+__all__ = ['AcSecurity']
+__author__ = 'Austin Cabler'
+__version__ = '1.1.3'
---
+++ packages/AcSecurity/AcSecurity-1.2.3-py3-none-any.whl/backups/run_scanner.py
@@ -0,0 +1,6 @@
+# run_scanner.py
+
+from src.scanner import main
+
+if __name__ == "__main__":
+    main()
