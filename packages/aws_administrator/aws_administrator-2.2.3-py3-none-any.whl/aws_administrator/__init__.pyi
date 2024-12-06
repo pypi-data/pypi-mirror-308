75577325d8b6dca805588fb9e2ec116cf4acee39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:43 +0000

    Add package aws_administrator/2.2.3/aws_administrator-2.2.3-py3-none-any.whl

---
+++ packages/aws_administrator/aws_administrator-2.2.3-py3-none-any.whl/aws_administrator/__init__.pyi
@@ -0,0 +1,11 @@
+__all__ = ['aws_actions_get', 'aws_sqs_get', 'aws_sso_assign', 'aws_sso_get', 'aws_sso_json', 'aws_sso_mappings', 'aws_sso_memberships', 'aws_sso_update']
+
+# Names in __all__ with no definition:
+#   aws_actions_get
+#   aws_sqs_get
+#   aws_sso_assign
+#   aws_sso_get
+#   aws_sso_json
+#   aws_sso_mappings
+#   aws_sso_memberships
+#   aws_sso_update
