c77ad1651f336d1bb682e3c269ed9d68643672bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:47 +0000

    Add package nshtrainer/1.0.0b24/nshtrainer-1.0.0b24.tar.gz

---
+++ packages/nshtrainer/nshtrainer-1.0.0b24.tar.gz/nshtrainer-1.0.0b24/src/nshtrainer/configs/callbacks/debug_flag/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+__codegen__ = True
+
+from nshtrainer.callbacks.debug_flag import CallbackConfigBase as CallbackConfigBase
+from nshtrainer.callbacks.debug_flag import (
+    DebugFlagCallbackConfig as DebugFlagCallbackConfig,
+)
---
+++ packages/nshtrainer/nshtrainer-1.0.0b24.tar.gz/nshtrainer-1.0.0b24/src/nshtrainer/configs/callbacks/finite_checks/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+__codegen__ = True
+
+from nshtrainer.callbacks.finite_checks import CallbackConfigBase as CallbackConfigBase
+from nshtrainer.callbacks.finite_checks import (
+    FiniteChecksCallbackConfig as FiniteChecksCallbackConfig,
+)
