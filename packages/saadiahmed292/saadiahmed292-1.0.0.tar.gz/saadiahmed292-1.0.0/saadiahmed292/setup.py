ff5265cee36dbf17208480c31af623817db6f3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:07 +0000

    Add package saadiahmed292/1.0.0/saadiahmed292-1.0.0.tar.gz

---
+++ packages/saadiahmed292/saadiahmed292-1.0.0.tar.gz/saadiahmed292-1.0.0/saadiahmed292/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rocket_pack',  
+    version='0.1',
+    description='A package to simulate rockets and shuttles',
+    long_description=open('README.md').read(),
+    author='islem',
+    author_email='ic.ourred@esi-sba.dz',
+    packages=['rocket_pack'],
+    url='https://pypi.org/user/Islem_ord/',  
+    license='LICENSE.txt',
+    long_description_content_type="text/markdown",
+    install_requires=[]
+)
---
+++ packages/saadiahmed292/saadiahmed292-1.0.0.tar.gz/saadiahmed292-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='saadiahmed292',  # Updated to a unique name
+    version='1.0.0',
+    author='Saadi Ahmed',
+    author_email='ah.saadi@esi-sba.dz',
+    packages=['saadiahmed292'],  # This should be the name of your package folder
+    url='http://pypi.python.org/pypi/saadiahmed292/',
+    license='LICENSE.txt',
+    description='An awesome package for demonstration purposes',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[],  # Add dependencies here if needed
+)
