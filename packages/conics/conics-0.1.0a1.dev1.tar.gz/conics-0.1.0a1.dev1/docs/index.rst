dd80359fc855024b4230d225c2410087d5145f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:30 +0000

    Add package conics/0.1.0a1.dev1/conics-0.1.0a1.dev1.tar.gz

---
+++ packages/conics/conics-0.1.0a1.dev1.tar.gz/conics-0.1.0a1.dev1/docs/index.rst
@@ -0,0 +1,26 @@
+conics
+======
+
+Conics is a library for dealing with conic sections (or simply `conics`).
+
+Features
+--------
+
+* Construction of conics
+* Geometric transformations
+* Computing intersections of conics
+* 5-D pose estimation of the supporting plane of an ellipse
+
+.. toctree::
+   :maxdepth: 4
+   :hidden:
+
+   readme
+   usage
+   bibliography
+   license
+
+Indices and tables
+==================
+* :ref:`genindex`
+* :ref:`search`
---
+++ packages/conics/conics-0.1.0a1.dev1.tar.gz/conics-0.1.0a1.dev1/tests/test_ellipse.py
@@ -0,0 +1,36 @@
+#
+# conics - Python library for dealing with conics
+#
+# Copyright 2024 Sergiu Deitsch <sergiu.deitsch@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from conics import Ellipse
+from conics.fitting import fit_dlt
+from conics.fitting import fit_nievergelt
+import numpy as np
+
+
+def test_ellipse_fitting():
+    pts = np.array([[1, 2, 5, 7, 9, 3, 6, 8], [7, 6, 8, 7, 5, 7, 2, 4]])
+
+    C = fit_dlt(pts)
+    C = fit_nievergelt(pts, type='ellipse', scale=True)
+    e = Ellipse.from_conic(C)
+    e = Ellipse([4.84, 4.979], [3.391, 3.391], 0)
+
+    # print(e.center, e.major_minor, e.alpha)
+
+    e1 = e.refine(pts)
+    print(e1.center, e1.major_minor, e1.alpha)
