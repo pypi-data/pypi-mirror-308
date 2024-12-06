8939b0868d556468b000c9e7e1a8aecea0ea3920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:06:01 +0000

    Add package sherbystats/0.0.27/sherbystats-0.0.27.tar.gz

---
+++ packages/sherbystats/sherbystats-0.0.27.tar.gz/sherbystats-0.0.27/setup.py
@@ -0,0 +1,55 @@
+import setuptools
+
+
+setuptools.setup(
+    name="sherbystats",
+    version="0.0.27",
+    author="Ryan Gosselin",
+    author_email="ryan.gosselin@usherbrooke.ca",
+    url="https://www.usherbrooke.ca/gchimiquebiotech/departement/professeurs/ryan-gosselin/",
+    packages=["sherbystats"],
+    description="Ryan @ UdeS",
+    long_description="\
+    \n\
+    \n\
+    \nGCB140. Statistiques en ingénierie\
+    \nanova\
+    \ndoe\
+    \mlr\
+    \nxlsread\
+    \n\
+    \n\
+    \nGCH755. Apprentissage machine données multivariées\
+    \ncolorspectra\
+    \ndtw\
+    \ndtw_multi\
+    \nlags\
+    \nlda\
+    \nmlr\
+    \nnormplot\
+    \npca\
+    \npca_trou\
+    \npcr\
+    \npls\
+    \vif\
+    \vip\
+    \nxlsread\
+    \n\
+    \n\
+    \nGCH757. Planification et analyse des expériences\
+    \nanova\
+    \nbinaire\
+    \nbinaire3D\
+    \ndoe\
+    \mlr\
+    \ternaire\
+    \nxlsread\
+    \n\
+    \n",
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
