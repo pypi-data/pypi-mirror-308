31398e177be930b635af992c5c7fc027466b7393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:52 +0000

    Add package kcwebplus/3.1.50/kcwebplus-3.1.50.tar.gz

---
+++ packages/kcwebplus/kcwebplus-3.1.50.tar.gz/kcwebplus-3.1.50/kcwebplus/index/controller/__init__.py
@@ -0,0 +1,14 @@
+# import re
+# def error(e,data):
+#     pass
+#     # try:
+#     #     plug=str(e).split("has no attribute")[1]
+#     #     plug=re.sub("[' ]","",plug)
+#     # except:
+#     #     plug=''
+#     # print('e',e)
+#     # print('plug',plug)
+#     # header={"Content-Type":"application/json; charset=utf-8","Access-Control-Allow-Origin":"*"}
+#     # header['Location']="/intapp/index/plug/index/intapp/"+plug
+#     # return '{"code":1,"msg":"您访问的地址不存在","data":"'+str(e)+'"}','302 Found',header 
+from . import index
