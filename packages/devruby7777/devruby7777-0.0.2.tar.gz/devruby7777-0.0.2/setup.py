b9182741a7a8bcd4876ca1269e478ee78e8476ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:06 +0000

    Add package devruby7777/0.0.2/devruby7777-0.0.2.tar.gz

---
+++ packages/devruby7777/devruby7777-0.0.2.tar.gz/devruby7777-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="devruby7777", # Replace with your own username
+    version="0.0.2",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dev-ruby/dev-ruby",
+    packages=setuptools.find_packages(),
+    package_data={
+        'your_package': ['main.py'],  # main.py 포함
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
