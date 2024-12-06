a84809401e4c13749c4d90aab93467aebd097ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:10 +0000

    Add package sixtynine/2.0/sixtynine-2.0.tar.gz

---
+++ packages/sixtynine/sixtynine-2.0.tar.gz/sixtynine-2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sixtynine',
+    version='2.0',
+    packages=find_packages(),
+    install_requires=[],
+    package_data={'sixtynine': ['*.zip', '*.py']},
+)
