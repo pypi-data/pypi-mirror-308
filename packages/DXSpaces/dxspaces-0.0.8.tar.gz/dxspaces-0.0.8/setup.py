5f04f58ef44ceb2aeb50a6576246a03a82b463d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:28 +0000

    Add package DXSpaces/0.0.8/dxspaces-0.0.8.tar.gz

---
+++ packages/DXSpaces/dxspaces-0.0.8.tar.gz/dxspaces-0.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='DXSpaces',
+      version='0.0.8',
+      description='Client library for RESTful DataSpaces',
+      long_description="Placeholder - README Pending",
+      long_description_content_type="text/x-rst",
+      author='Philip Davis',
+      author_email='philip.davis@sci.utah.edu',
+      url='https://github.com/sci-ndp/dxspaces',
+      install_requires=requirements,
+      packages=['dxspaces'],
+     )
