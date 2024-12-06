8fa32c46e0545e9a7681503c3f257a70e7e866a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:56 +0000

    Add package stookwijzer/1.5.1/stookwijzer-1.5.1.tar.gz

---
+++ packages/stookwijzer/stookwijzer-1.5.1.tar.gz/stookwijzer-1.5.1/stookwijzer/example.py
@@ -0,0 +1,27 @@
+"""Example usage of the Stookwijzer API."""
+import aiohttp
+import asyncio
+import stookwijzerapi
+
+async def main():
+    session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False))
+
+    x, y = await stookwijzerapi.Stookwijzer.async_transform_coordinates(session, 52.123456, 6.123456)
+    print(f"x:               {x}")
+    print(f"y:               {y}")
+
+    if x and y:
+        sw = stookwijzerapi.Stookwijzer(session, x, y)
+        await sw.async_update()
+        print()
+        print(f"advice:          {sw.advice}")
+        print(f"windspeed bft:   {sw.windspeed_bft}")
+        print(f"windspeed ms:    {sw.windspeed_ms}")
+        print(f"lki:             {sw.lki}")
+        print()
+        print(f"forecast_advice: {sw.forecast_advice}")
+    
+    await session.close()
+    
+if __name__ == "__main__":
+    asyncio.run(main())
