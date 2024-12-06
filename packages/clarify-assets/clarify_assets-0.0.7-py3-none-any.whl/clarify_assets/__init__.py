b5172f5b4380bb4f694bfdffba31a5907c570087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:29 +0000

    Add package clarify-assets/0.0.7/clarify_assets-0.0.7-py3-none-any.whl

---
+++ packages/clarify-assets/clarify_assets-0.0.7-py3-none-any.whl/clarify_assets/__init__.py
@@ -0,0 +1,6 @@
+from clarify_assets.broker.broker import AssetBroker
+from clarify_assets.models.asset import Asset
+from clarify_assets.models.attribute import Attribute
+from pyclarify import Item
+
+__version__ = "0.0.7"
