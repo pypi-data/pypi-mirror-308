afc3ac41f76c4ca5ee42f539d586fb4b8b65a8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:47 +0000

    Add package modelica-builder/0.6.0/modelica_builder-0.6.0.tar.gz

---
+++ packages/modelica-builder/modelica_builder-0.6.0.tar.gz/modelica_builder-0.6.0/modelica_builder/config.py
@@ -0,0 +1,10 @@
+# :copyright (c) URBANopt, Alliance for Sustainable Energy, LLC, and other contributors.
+# See also https://github.com/urbanopt/modelica-builder/blob/develop/LICENSE.md
+
+# global config variables
+# If true, then each inserted component/annotation argument will be indented on a new line
+INDENT_INSERTED_COMPONENT_ARGS = False
+INDENT_INSERTED_ANNOTATION_ARGS = False
+
+# used whenever text is inserted on a new line (e.g. new component or connect)
+INDENTATION = '  '
