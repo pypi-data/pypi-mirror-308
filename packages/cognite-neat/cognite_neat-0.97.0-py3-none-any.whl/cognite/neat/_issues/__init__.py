340ef1acb29c3db598f2761a1a46c1dc32f2ba70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:19 +0000

    Add package cognite-neat/0.97.0/cognite_neat-0.97.0-py3-none-any.whl

---
+++ packages/cognite-neat/cognite_neat-0.97.0-py3-none-any.whl/cognite/neat/_issues/__init__.py
@@ -0,0 +1,24 @@
+"""This is module contains all the Neat Exceptions (Errors) and Warnings as well
+as some helper classes to handle them like NeatIssueList"""
+
+from ._base import (
+    DefaultWarning,
+    IssueList,
+    MultiValueError,
+    NeatError,
+    NeatIssue,
+    NeatIssueList,
+    NeatWarning,
+    catch_warnings,
+)
+
+__all__ = [
+    "NeatIssue",
+    "NeatError",
+    "NeatWarning",
+    "DefaultWarning",
+    "NeatIssueList",
+    "IssueList",
+    "MultiValueError",
+    "catch_warnings",
+]
