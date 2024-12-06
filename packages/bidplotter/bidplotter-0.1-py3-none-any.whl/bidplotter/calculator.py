3d0968a5339266da10d0df42d562b3f41d7de47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:04 +0000

    Add package bidplotter/0.1/bidplotter-0.1-py3-none-any.whl

---
+++ packages/bidplotter/bidplotter-0.1-py3-none-any.whl/bidplotter/calculator.py
@@ -0,0 +1,37 @@
+from .plotting import plot_bidding_progress
+import matplotlib.pyplot as plt
+
+class BidCalculator:
+    def __init__(self, starting_price, bid_increment, duration_in_minutes):
+        self.starting_price = starting_price
+        self.bid_increment = bid_increment
+        self.duration_in_minutes = duration_in_minutes
+
+    def calculate_final_price(self):
+        # Convert duration to seconds
+        duration_in_seconds = self.duration_in_minutes * 60
+        
+        # Calculate total number of bids
+        time_between_bids = 10  # Assume each bid happens every 10 seconds
+        total_bids = duration_in_seconds // time_between_bids
+        
+        # Calculate price increase
+        price_increase = self.bid_increment * total_bids
+        predicted_final_price = self.starting_price + price_increase
+        
+        return predicted_final_price
+
+    def simulate_bidding(self):
+        # Simulate bidding over time for plotting
+        duration_in_seconds = self.duration_in_minutes * 60
+        time_between_bids = 10
+        total_bids = duration_in_seconds // time_between_bids
+        
+        times = [i * time_between_bids for i in range(total_bids)]
+        prices = [self.starting_price + self.bid_increment * i for i in range(total_bids)]
+        
+        return times, prices
+
+    def plot_bidding_progress(self):
+        times, prices = self.simulate_bidding()
+        plot_bidding_progress(times, prices)  # Pass data to plotting function
