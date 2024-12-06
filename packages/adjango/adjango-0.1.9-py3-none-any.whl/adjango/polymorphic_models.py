9194e62401c4f71c2ebf440435a5481994f3a008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:03 +0000

    Add package adjango/0.1.9/adjango-0.1.9-py3-none-any.whl

---
+++ packages/adjango/adjango-0.1.9-py3-none-any.whl/adjango/polymorphic_models.py
@@ -0,0 +1,15 @@
+# polymorphic_models.py
+try:
+    from polymorphic.models import PolymorphicModel
+
+    from adjango.managers.polymorphic import APolymorphicManager
+    from adjango.services.base import ABaseService
+
+
+    class APolymorphicModel(PolymorphicModel, ABaseService):
+        objects = APolymorphicManager()
+
+        class Meta:
+            abstract = True
+except ImportError:
+    pass
