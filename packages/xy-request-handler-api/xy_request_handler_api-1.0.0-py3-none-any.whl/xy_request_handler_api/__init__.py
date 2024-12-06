1e37a57537d1e137abb72cf44c5163eb5e9e3c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:17 +0000

    Add package xy-request-handler-api/1.0.0/xy_request_handler_api-1.0.0-py3-none-any.whl

---
+++ packages/xy-request-handler-api/xy_request_handler_api-1.0.0-py3-none-any.whl/xy_request_handler_api/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+__author__ = "余洋"
+__doc__ = "__init__"
+"""
+  * @File    :   __init__.py
+  * @Time    :   2023/04/29 19:15:56
+  * @Author  :   余洋
+  * @Version :   1.0
+  * @Contact :   yuyangit.0515@qq.com
+  * @License :   (C)Copyright 2019-2023, Ship of Ocean
+  * @Desc    :   None
+"""
