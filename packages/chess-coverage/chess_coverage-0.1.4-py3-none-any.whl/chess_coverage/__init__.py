112d03f3fd5f87b0886380c647d9e3ecfbdb1b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:05:06 +0000

    Add package chess-coverage/0.1.4/chess_coverage-0.1.4-py3-none-any.whl

---
+++ packages/chess-coverage/chess_coverage-0.1.4-py3-none-any.whl/chess_coverage/__init__.py
@@ -0,0 +1,7 @@
+# The root of the entire project
+from chess_coverage.chess_coverage import Coverage
+
+# This is the version number of your code.  This will change each time you make
+# a new release.  The most popular approach is to use Semantic Versioning,
+# described at https://semver.org/
+__version__ = "0.1.4"
