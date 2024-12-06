838d47cdee2143da2a0fb7117d218d30bd0c8dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:22 +0000

    Add package superagentx-handlers/0.1.4/superagentx_handlers-0.1.4.tar.gz

---
+++ packages/superagentx-handlers/superagentx_handlers-0.1.4.tar.gz/superagentx_handlers-0.1.4/superagentx_handlers/ecommerce/__init__.py
@@ -0,0 +1,5 @@
+from .amazon import AmazonHandler
+from .fake_amazon import FakeAmazonHandler
+from .fake_flipkart import FakeFlipkartHandler
+from .flipkart import FlipkartHandler
+from .walmart import WalmartHandler
