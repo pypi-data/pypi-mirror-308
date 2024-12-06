d6d1b39cd07af59b98555a27dfb82c371c219c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:55 +0000

    Add package fieldsurveys/1.0.2/fieldsurveys-1.0.2.tar.gz

---
+++ packages/fieldsurveys/fieldsurveys-1.0.2.tar.gz/fieldsurveys-1.0.2/README.md
@@ -0,0 +1,16 @@
+# Field surveys App
+
+![](https://mellifluous-kelpie-496610.netlify.app/fieldsurveys_app.png)
+
+We created an interactive web app using `Shiny for Python` to streamline multi-taxon field sampling and data collection. The app enables designing standardized surveys for various organisms like arthropods, birds, and plants.
+
+Accessible on internet-connected devices, it provides a user-friendly interface for field sampling, data storage, and analysis, catering to researchers, citizen scientists, students, and others, regardless of technical expertise or location.
+
+## Set-up your application
+
+Follow the instructions in the [documentation](https://karangattu.github.io/fieldsurveys/) to set up the app for your use case.
+
+
+## Issues/Feature requests
+
+If you encounter any issues or have any feature requests, please feel free to open an issue on the GitHub repository [here](https://github.com/karangattu/fieldsurveys/issues)
---
+++ packages/fieldsurveys/fieldsurveys-1.0.2.tar.gz/fieldsurveys-1.0.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fieldsurveys",
+    version="1.0.2",
+    author="Karan Gathani",
+    author_email="karan.gathani+fieldsurveysapp@posit.co",
+    packages=find_packages(),
+    description="A Python package for creating and managing field surveys.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/karangattu/fieldsurveys",
+    include_package_data=True,
+    package_data={"fieldsurveys": ["*"]},
+    entry_points={"console_scripts": ["fieldsurveys = fieldsurveys:copy_app_files"]},
+    setup_requires=["wheel"],
+    install_requires=[
+        "beautifulsoup4==4.12.3",
+        "cryptography==43.0.1",
+        "faicons==0.2.2",
+        "geopy==2.4.1",
+        "gspread==6.1.2",
+        "gspread-dataframe==4.0.0",
+        "htmltools==0.5.3",
+        "pandas==2.2.3",
+        "plotly==5.24.1", 
+        "pytz==2024.2",
+        "PyYAML==6.0.2",
+        "requests==2.32.3",
+        "rsconnect_python==1.24.0",
+        "shiny==1.1.0",
+        "shinyswatch==0.7.0",
+        "timezonefinder==6.5.3",
+        "us==3.2.0",
+        "Wikipedia-API==0.7.1",
+        "google-api-python-client==2.151.0"
+    ],
+    extras_require={
+        "tests": [
+            "playwright==1.47.0",
+            "pytest==8.3.3",
+            "pytest-mock==3.14.0",
+            "pytest-playwright==0.5.2",
+        ]
+    },
+)
