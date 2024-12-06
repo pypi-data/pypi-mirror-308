753f5d424bacff7f50c3be42f1e1fada62964e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:05:09 +0000

    Add package base-aux/0.0.15/base_aux-0.0.15-py3-none-any.whl

---
+++ packages/base-aux/base_aux-0.0.15-py3-none-any.whl/base_aux/pyqt/__init__.py
@@ -0,0 +1,64 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+# VERSION = (0, 0, 2)   # del blank lines
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#     # AUX
+#     # TYPES
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .gui import (
+    # BASE
+    Gui,
+    # AUX
+    # TYPES
+    TYPE__SIZE_TUPLE,
+    # EXX
+)
+from .signals import (
+    # BASE
+    SignalsTemplate,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .th import (
+    # BASE
+    HeaderViewCB,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .tm import (
+    # BASE
+    Headers,
+    TableModelTemplate,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .hl import (
+    # BASE
+    format_make,
+    HlStyle,
+    HlStyles,
+    Highlighter,
+    # AUX
+    HlStylesPython,
+    HlStylesMultiline,
+    HlStylesExample,
+    start_example,
+    # TYPES
+    # EXX
+)
+
+# =====================================================================================================================
+
---
+++ packages/base-aux/base_aux-0.0.15-py3-none-any.whl/base_aux/pyqt/_explore.py
@@ -0,0 +1,12 @@
+# DON'T DELETE!
+# useful to start smth without pytest and not to run in main script!
+
+from base_aux.pyqt import *
+
+
+class Example(Gui):
+    START = True
+    HL_STYLES = HlStylesExample()
+
+
+Example()
