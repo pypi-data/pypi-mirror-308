7747eb9672dce35440beead872d2680cc4454b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:50 +0000

    Add package py-package-test-kh-notAB/0.4/py_package_test_kh_notab-0.4.tar.gz

---
+++ packages/py-package-test-kh-notAB/py_package_test_kh_notab-0.4.tar.gz/py_package_test_kh_notab-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py_package_test_kh_notAB',
+    version='0.4',
+    packages= find_packages(),
+    install_requires=[
+        #none
+    ]
+
+)
