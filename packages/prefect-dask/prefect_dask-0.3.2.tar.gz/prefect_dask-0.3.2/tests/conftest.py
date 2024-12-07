5eace97e576c3bc3cf5e684121e732ab9e9b66c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:37 +0000

    Add package prefect-dask/0.3.2/prefect_dask-0.3.2.tar.gz

---
+++ packages/prefect-dask/prefect_dask-0.3.2.tar.gz/prefect_dask-0.3.2/tests/conftest.py
@@ -0,0 +1,47 @@
+import asyncio
+import logging
+import sys
+
+import pytest
+
+from prefect.testing.utilities import prefect_test_harness
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prefect_db():
+    """
+    Sets up test harness for temporary DB during test runs.
+    """
+    with prefect_test_harness():
+        yield
+
+
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """
+    Redefine the event loop to support session/module-scoped fixtures;
+    see https://github.com/pytest-dev/pytest-asyncio/issues/68
+    When running on Windows we need to use a non-default loop for subprocess support.
+    """
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+    policy = asyncio.get_event_loop_policy()
+
+    loop = policy.new_event_loop()
+
+    # configure asyncio logging to capture long running tasks
+    asyncio_logger = logging.getLogger("asyncio")
+    asyncio_logger.setLevel("WARNING")
+    asyncio_logger.addHandler(logging.StreamHandler())
+    loop.set_debug(True)
+    loop.slow_callback_duration = 0.25
+
+    try:
+        yield loop
+    finally:
+        loop.close()
+
+    # Workaround for failures in pytest_asyncio 0.17;
+    # see https://github.com/pytest-dev/pytest-asyncio/issues/257
+    policy.set_event_loop(loop)
