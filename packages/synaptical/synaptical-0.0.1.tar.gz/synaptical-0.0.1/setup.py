153ee697ab778d17f8256e33d6316eb8d1a1a7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:11:25 +0000

    Add package synaptical/0.0.1/synaptical-0.0.1.tar.gz

---
+++ packages/synaptical/synaptical-0.0.1.tar.gz/synaptical-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(
+    name="synaptical",
+    description="synaptical",
+    version="0.0.1",
+    author="shobrook",
+    author_email="shobrookj@gmail.com"
+)
