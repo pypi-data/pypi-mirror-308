a9b443c33a404b2a90d30d393f608e736a597f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:56 +0000

    Add package varphi-parsing-tools/0.0.0/varphi_parsing_tools-0.0.0.tar.gz

---
+++ packages/varphi-parsing-tools/varphi_parsing_tools-0.0.0.tar.gz/varphi_parsing_tools-0.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="varphi_parsing_tools",
+    version="0.0.0",
+    description="Lexer and parser for the Varphi programming language.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Hassan El-Sheikha",
+    author_email="hassan.elsheikha@utoronto.ca",
+    url="https://github.com/hassanelsheikha/varphi_parsing_tools",
+    packages=find_packages(),
+    install_requires=[
+        "antlr4-python3-runtime==4.13.2",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
