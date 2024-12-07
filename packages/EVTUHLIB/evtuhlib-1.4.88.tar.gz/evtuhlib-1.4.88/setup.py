681994495b3aee28ada47a2afebe458d5ae53024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:54 +0000

    Add package EVTUHLIB/1.4.88/evtuhlib-1.4.88.tar.gz

---
+++ packages/EVTUHLIB/evtuhlib-1.4.88.tar.gz/evtuhlib-1.4.88/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='EVTUHLIB',
+  version='1.4.88',
+  author='Evtushenko Vladislav',
+  author_email='evtuh545@gmail.com',
+  description='...',
+  long_description_content_type='text/markdown',
+  url='https://sweet-store.ru',
+  packages=find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='evtuh evtuhlib ',
+  python_requires='>=3.6'
+)
