68808e342fb719bf61158d2b7bf3de5c94422af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:16 +0000

    Add package xmlcoll/1.8.0/xmlcoll-1.8.0-py3-none-any.whl

---
+++ packages/xmlcoll/xmlcoll-1.8.0-py3-none-any.whl/xmlcoll/__about__.py
@@ -0,0 +1,16 @@
+"""
+A package of python routines to work with data in XML format of samples.
+"""
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__version__",
+    "__author__",
+    "__copyright__",
+]
+
+__title__ = "xmlcoll"
+__summary__ = "Python project to work with xml data of a collection of items."
+__version__ = "1.8.0"
+__author__ = "Clemson University"
+__copyright__ = "Clemson University, 2022-2024"
