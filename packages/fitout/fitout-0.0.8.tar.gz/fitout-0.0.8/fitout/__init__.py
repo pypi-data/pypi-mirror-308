d7f3c5c942517263da8a485b239ae3b843a8ba14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:11:22 +0000

    Add package fitout/0.0.8/fitout-0.0.8.tar.gz

---
+++ packages/fitout/fitout-0.0.8.tar.gz/fitout-0.0.8/fitout/__init__.py
@@ -0,0 +1,8 @@
+"""A Python library to extract FitBit Google Takeout data."""
+# Semantic Versioning according to https://semver.org/spec/v2.0.0.html
+__version__ = "v0.0.8"  # fix: Documenting basic sleep API.
+
+
+from .helpers import *
+from .datasources import *
+from .importers import *
