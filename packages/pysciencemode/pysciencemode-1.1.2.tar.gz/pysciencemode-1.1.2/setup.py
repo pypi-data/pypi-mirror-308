688b1f877ae4e8def00951d2c92728c234a39d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:33 +0000

    Add package pysciencemode/1.1.2/pysciencemode-1.1.2.tar.gz

---
+++ packages/pysciencemode/pysciencemode-1.1.2.tar.gz/pysciencemode-1.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="pyScienceMode",
+    version="1.1",
+    packages=["pyScienceMode"],
+    url="https://github.com/s2mLab/pyScienceMode.git",
+    license="",
+    author="S2M Lab",
+    author_email="kevin.co@umontreal.ca",
+    description="Python interface to control the Rehastim2 and RehastimP24 devices.",
+)
