776d15c3cc8889e888ca53032f1724499266fb06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:28 +0000

    Add package gs2-cdk/1.0.84/gs2-cdk-1.0.84.tar.gz

---
+++ packages/gs2-cdk/gs2-cdk-1.0.84.tar.gz/gs2-cdk-1.0.84/src/gs2_cdk/exchange/model/__init__.py
@@ -0,0 +1,29 @@
+# Copyright 2016- Game Server Services, Inc. or its affiliates. All Rights
+# Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+from .Namespace import Namespace
+from .options.NamespaceOptions import NamespaceOptions
+from .RateModel import RateModel
+from .options.RateModelOptions import RateModelOptions
+from .enum.RateModelTimingType import RateModelTimingType
+from .options.RateModelTimingTypeIsImmediateOptions import RateModelTimingTypeIsImmediateOptions
+from .options.RateModelTimingTypeIsAwaitOptions import RateModelTimingTypeIsAwaitOptions
+from .IncrementalRateModel import IncrementalRateModel
+from .options.IncrementalRateModelOptions import IncrementalRateModelOptions
+from .enum.IncrementalRateModelCalculateType import IncrementalRateModelCalculateType
+from .options.IncrementalRateModelCalculateTypeIsLinearOptions import IncrementalRateModelCalculateTypeIsLinearOptions
+from .options.IncrementalRateModelCalculateTypeIsPowerOptions import IncrementalRateModelCalculateTypeIsPowerOptions
+from .options.IncrementalRateModelCalculateTypeIsGs2ScriptOptions import IncrementalRateModelCalculateTypeIsGs2ScriptOptions
+from .LogCost import LogCost
+from .options.LogCostOptions import LogCostOptions
+from .CurrentMasterData import CurrentMasterData
