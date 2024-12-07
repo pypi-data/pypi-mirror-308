5418543cb9f207acd593a6a08b4b21820ce0c3ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:05 +0000

    Add package my-bot-library/0.0.3/my_bot_library-0.0.3.tar.gz

---
+++ packages/my-bot-library/my_bot_library-0.0.3.tar.gz/my_bot_library-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+classifiers = [
+    'Development Status :: Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Ubuntu',
+    'License :: OSI Approved :: MIT License',
+    'Programmming Language :: Python :: 3'
+]
+
+setup(
+    name='my_bot_library',
+    version='0.0.3',
+    description='Andromeda Python library',
+    long_description=open('README.md').read() + '\n\n' +
+    open('CHANGELOG.txt').read(),
+    url='',
+    author='Susheela',
+    license='MIT',
+    #classifiers=classifiers,
+    keywords='andromeda',
+    packages=find_packages(),
+)
