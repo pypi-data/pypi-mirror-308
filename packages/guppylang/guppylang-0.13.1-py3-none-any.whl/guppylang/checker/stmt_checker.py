2517b9b2d70ca52242b8d7b3a9f346fdc9358e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:32 +0000

    Add package guppylang/0.13.1/guppylang-0.13.1-py3-none-any.whl

---
+++ packages/guppylang/guppylang-0.13.1-py3-none-any.whl/guppylang/checker/stmt_checker.py
@@ -0,0 +1,199 @@
+"""Type checking code for statements.
+
+Operates on statements in a basic block after CFG construction. In particular, we
+assume that statements involving control flow (i.e. if, while, break, and return
+statements) have been removed during CFG construction.
+
+After checking, we return a desugared statement where all sub-expression have been type
+annotated.
+"""
+
+import ast
+from collections.abc import Sequence
+
+from guppylang.ast_util import AstVisitor, with_loc, with_type
+from guppylang.cfg.bb import BB, BBStatement
+from guppylang.checker.core import Context, FieldAccess, Variable
+from guppylang.checker.errors.generic import UnsupportedError
+from guppylang.checker.errors.type_errors import (
+    AssignFieldTypeMismatchError,
+    AssignNonPlaceHelp,
+    AttributeNotFoundError,
+    MissingReturnValueError,
+    WrongNumberOfUnpacksError,
+)
+from guppylang.checker.expr_checker import ExprChecker, ExprSynthesizer
+from guppylang.error import GuppyError, GuppyTypeError, InternalGuppyError
+from guppylang.nodes import NestedFunctionDef, PlaceNode
+from guppylang.span import Span, to_span
+from guppylang.tys.parsing import type_from_ast
+from guppylang.tys.subst import Subst
+from guppylang.tys.ty import NoneType, StructType, TupleType, Type
+
+
+class StmtChecker(AstVisitor[BBStatement]):
+    ctx: Context
+    bb: BB | None
+    return_ty: Type | None
+
+    def __init__(
+        self, ctx: Context, bb: BB | None = None, return_ty: Type | None = None
+    ) -> None:
+        assert not return_ty or not return_ty.unsolved_vars
+        self.ctx = ctx
+        self.bb = bb
+        self.return_ty = return_ty
+
+    def check_stmts(self, stmts: Sequence[BBStatement]) -> list[BBStatement]:
+        return [self.visit(s) for s in stmts]
+
+    def _synth_expr(self, node: ast.expr) -> tuple[ast.expr, Type]:
+        return ExprSynthesizer(self.ctx).synthesize(node)
+
+    def _check_expr(
+        self, node: ast.expr, ty: Type, kind: str = "expression"
+    ) -> tuple[ast.expr, Subst]:
+        return ExprChecker(self.ctx).check(node, ty, kind)
+
+    def _check_assign(self, lhs: ast.expr, ty: Type, node: ast.stmt) -> ast.expr:
+        """Helper function to check assignments with patterns."""
+        match lhs:
+            # Easiest case is if the LHS pattern is a single variable.
+            case ast.Name(id=x):
+                var = Variable(x, ty, lhs)
+                self.ctx.locals[x] = var
+                return with_loc(lhs, with_type(ty, PlaceNode(place=var)))
+
+            # The LHS could also be a field `expr.field`
+            case ast.Attribute(value=value, attr=attr):
+                # Unfortunately, the `attr` is just a string,  not an AST node, so we
+                # have to compute its span by hand. This is fine since linebreaks are
+                # not allowed in the identifier following the `.`
+                span = to_span(lhs)
+                attr_span = Span(span.end.shift_left(len(attr)), span.end)
+                value, struct_ty = self._synth_expr(value)
+                if (
+                    not isinstance(struct_ty, StructType)
+                    or attr not in struct_ty.field_dict
+                ):
+                    raise GuppyTypeError(
+                        AttributeNotFoundError(attr_span, struct_ty, attr)
+                    )
+                field = struct_ty.field_dict[attr]
+                # TODO: In the future, we could infer some type args here
+                if field.ty != ty:
+                    # TODO: Get hold of a span for the RHS and use a regular
+                    #  `TypeMismatchError` instead (maybe with a custom hint).
+                    raise GuppyTypeError(
+                        AssignFieldTypeMismatchError(attr_span, ty, field)
+                    )
+                if not isinstance(value, PlaceNode):
+                    # For now we complain if someone tries to assign to something that
+                    # is not a place, e.g. `f().a = 4`. This would only make sense if
+                    # there is another reference to the return value of `f`, otherwise
+                    # the mutation cannot be observed. We can start supporting this once
+                    # we have proper reference semantics.
+                    err = UnsupportedError(
+                        value, "Assigning to this expression", singular=True
+                    )
+                    err.add_sub_diagnostic(AssignNonPlaceHelp(None, field))
+                    raise GuppyError(err)
+                if not field.ty.linear:
+                    raise GuppyError(
+                        UnsupportedError(
+                            attr_span, "Mutation of classical fields", singular=True
+                        )
+                    )
+                place = FieldAccess(value.place, struct_ty.field_dict[attr], lhs)
+                return with_loc(lhs, with_type(ty, PlaceNode(place=place)))
+
+            # The only other thing we support right now are tuples
+            case ast.Tuple(elts=elts) as lhs:
+                tys = ty.element_types if isinstance(ty, TupleType) else [ty]
+                n, m = len(elts), len(tys)
+                if n != m:
+                    if n > m:
+                        span = Span(to_span(elts[m]).start, to_span(elts[-1]).end)
+                    else:
+                        span = to_span(lhs)
+                    raise GuppyTypeError(WrongNumberOfUnpacksError(span, m, n))
+                lhs.elts = [
+                    self._check_assign(pat, el_ty, node)
+                    for pat, el_ty in zip(elts, tys, strict=True)
+                ]
+                return with_type(ty, lhs)
+
+            # TODO: Python also supports assignments like `[a, b] = [1, 2]` or
+            #  `a, *b = ...`. The former would require some runtime checks but
+            #  the latter should be easier to do (unpack and repack the rest).
+            case _:
+                raise GuppyError(
+                    UnsupportedError(lhs, "This assignment pattern", singular=True)
+                )
+
+    def visit_Assign(self, node: ast.Assign) -> ast.Assign:
+        if len(node.targets) > 1:
+            # This is the case for assignments like `a = b = 1`
+            raise GuppyError(UnsupportedError(node, "Multi assignments"))
+
+        [target] = node.targets
+        node.value, ty = self._synth_expr(node.value)
+        node.targets = [self._check_assign(target, ty, node)]
+        return node
+
+    def visit_AnnAssign(self, node: ast.AnnAssign) -> ast.stmt:
+        if node.value is None:
+            raise GuppyError(UnsupportedError(node, "Variable declarations"))
+        ty = type_from_ast(node.annotation, self.ctx.globals, self.ctx.generic_params)
+        node.value, subst = self._check_expr(node.value, ty)
+        assert not ty.unsolved_vars  # `ty` must be closed!
+        assert len(subst) == 0
+        target = self._check_assign(node.target, ty, node)
+        return with_loc(node, ast.Assign(targets=[target], value=node.value))
+
+    def visit_AugAssign(self, node: ast.AugAssign) -> ast.stmt:
+        bin_op = with_loc(
+            node, ast.BinOp(left=node.target, op=node.op, right=node.value)
+        )
+        assign = with_loc(node, ast.Assign(targets=[node.target], value=bin_op))
+        return self.visit_Assign(assign)
+
+    def visit_Expr(self, node: ast.Expr) -> ast.stmt:
+        # An expression statement where the return value is discarded
+        node.value, _ = self._synth_expr(node.value)
+        return node
+
+    def visit_Return(self, node: ast.Return) -> ast.stmt:
+        if not self.return_ty:
+            raise InternalGuppyError("return_ty required to check return stmt!")
+
+        if node.value is not None:
+            node.value, subst = self._check_expr(
+                node.value, self.return_ty, "return value"
+            )
+            assert len(subst) == 0  # `self.return_ty` is closed!
+        elif not isinstance(self.return_ty, NoneType):
+            raise GuppyTypeError(MissingReturnValueError(node, self.return_ty))
+        return node
+
+    def visit_NestedFunctionDef(self, node: NestedFunctionDef) -> ast.stmt:
+        from guppylang.checker.func_checker import check_nested_func_def
+
+        if not self.bb:
+            raise InternalGuppyError("BB required to check nested function def!")
+
+        func_def = check_nested_func_def(node, self.bb, self.ctx)
+        self.ctx.locals[func_def.name] = Variable(func_def.name, func_def.ty, func_def)
+        return func_def
+
+    def visit_If(self, node: ast.If) -> None:
+        raise InternalGuppyError("Control-flow statement should not be present here.")
+
+    def visit_While(self, node: ast.While) -> None:
+        raise InternalGuppyError("Control-flow statement should not be present here.")
+
+    def visit_Break(self, node: ast.Break) -> None:
+        raise InternalGuppyError("Control-flow statement should not be present here.")
+
+    def visit_Continue(self, node: ast.Continue) -> None:
+        raise InternalGuppyError("Control-flow statement should not be present here.")
---
+++ packages/guppylang/guppylang-0.13.1-py3-none-any.whl/guppylang/tys/builtin.py
@@ -0,0 +1,259 @@
+from collections.abc import Sequence
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Literal, TypeGuard
+
+import hugr.std
+import hugr.std.collections
+from hugr import tys as ht
+
+from guppylang.ast_util import AstNode
+from guppylang.definition.common import DefId
+from guppylang.definition.ty import OpaqueTypeDef, TypeDef
+from guppylang.error import GuppyError, InternalGuppyError
+from guppylang.experimental import check_lists_enabled
+from guppylang.tys.arg import Argument, ConstArg, TypeArg
+from guppylang.tys.const import ConstValue
+from guppylang.tys.errors import WrongNumberOfTypeArgsError
+from guppylang.tys.param import ConstParam, TypeParam
+from guppylang.tys.ty import (
+    FunctionType,
+    NoneType,
+    NumericType,
+    OpaqueType,
+    TupleType,
+    Type,
+)
+
+if TYPE_CHECKING:
+    from guppylang.checker.core import Globals
+
+
+@dataclass(frozen=True)
+class CallableTypeDef(TypeDef):
+    """Type definition associated with the builtin `Callable` type.
+
+    Any impls on functions can be registered with this definition.
+    """
+
+    name: Literal["Callable"] = field(default="Callable", init=False)
+
+    def check_instantiate(
+        self, args: Sequence[Argument], globals: "Globals", loc: AstNode | None = None
+    ) -> FunctionType:
+        # Callable types are constructed using special logic in the type parser
+        raise InternalGuppyError("Tried to `Callable` type via `check_instantiate`")
+
+
+@dataclass(frozen=True)
+class _TupleTypeDef(TypeDef):
+    """Type definition associated with the builtin `tuple` type.
+
+    Any impls on tuples can be registered with this definition.
+    """
+
+    name: Literal["tuple"] = field(default="tuple", init=False)
+
+    def check_instantiate(
+        self, args: Sequence[Argument], globals: "Globals", loc: AstNode | None = None
+    ) -> TupleType:
+        # We accept any number of arguments. If users just write `tuple`, we give them
+        # the empty tuple type. We just have to make sure that the args are of kind type
+        args = [
+            # TODO: Better error location
+            TypeParam(0, f"T{i}", can_be_linear=True).check_arg(arg, loc).ty
+            for i, arg in enumerate(args)
+        ]
+        return TupleType(args)
+
+
+@dataclass(frozen=True)
+class _NoneTypeDef(TypeDef):
+    """Type definition associated with the builtin `None` type.
+
+    Any impls on None can be registered with this definition.
+    """
+
+    name: Literal["None"] = field(default="None", init=False)
+
+    def check_instantiate(
+        self, args: Sequence[Argument], globals: "Globals", loc: AstNode | None = None
+    ) -> NoneType:
+        if args:
+            raise GuppyError(WrongNumberOfTypeArgsError(loc, 0, len(args), "None"))
+        return NoneType()
+
+
+@dataclass(frozen=True)
+class _NumericTypeDef(TypeDef):
+    """Type definition associated with the builtin numeric types.
+
+    Any impls on numerics can be registered with these definitions.
+    """
+
+    ty: NumericType
+
+    def check_instantiate(
+        self, args: Sequence[Argument], globals: "Globals", loc: AstNode | None = None
+    ) -> NumericType:
+        if args:
+            raise GuppyError(WrongNumberOfTypeArgsError(loc, 0, len(args), self.name))
+        return self.ty
+
+
+@dataclass(frozen=True)
+class _ListTypeDef(OpaqueTypeDef):
+    """Type definition associated with the builtin `list` type.
+
+    We have a custom definition to disable usage of lists unless experimental features
+    are enabled.
+    """
+
+    def check_instantiate(
+        self, args: Sequence[Argument], globals: "Globals", loc: AstNode | None = None
+    ) -> OpaqueType:
+        check_lists_enabled(loc)
+        return super().check_instantiate(args, globals, loc)
+
+
+def _list_to_hugr(args: Sequence[Argument]) -> ht.Type:
+    # Type checker ensures that we get a single arg of kind type
+    [arg] = args
+    assert isinstance(arg, TypeArg)
+    # Linear elements are turned into an optional to enable unsafe indexing.
+    # See `ListGetitemCompiler` for details.
+    elem_ty = ht.Option(arg.ty.to_hugr()) if arg.ty.linear else arg.ty.to_hugr()
+    return hugr.std.collections.list_type(elem_ty)
+
+
+def _array_to_hugr(args: Sequence[Argument]) -> ht.Type:
+    # Type checker ensures that we get a two args
+    [ty_arg, len_arg] = args
+    assert isinstance(ty_arg, TypeArg)
+    assert isinstance(len_arg, ConstArg)
+
+    # Linear elements are turned into an optional to enable unsafe indexing.
+    # See `ArrayGetitemCompiler` for details.
+    # Same also for classical arrays, see https://github.com/CQCL/guppylang/issues/629
+    elem_ty = ht.Option(ty_arg.ty.to_hugr())
+
+    array = hugr.std.PRELUDE.get_type("array")
+    return array.instantiate([len_arg.to_hugr(), ht.TypeTypeArg(elem_ty)])
+
+
+def _sized_iter_to_hugr(args: Sequence[Argument]) -> ht.Type:
+    [ty_arg, len_arg] = args
+    assert isinstance(ty_arg, TypeArg)
+    assert isinstance(len_arg, ConstArg)
+    return ty_arg.ty.to_hugr()
+
+
+callable_type_def = CallableTypeDef(DefId.fresh(), None)
+tuple_type_def = _TupleTypeDef(DefId.fresh(), None)
+none_type_def = _NoneTypeDef(DefId.fresh(), None)
+bool_type_def = OpaqueTypeDef(
+    id=DefId.fresh(),
+    name="bool",
+    defined_at=None,
+    params=[],
+    always_linear=False,
+    to_hugr=lambda _: ht.Bool,
+)
+nat_type_def = _NumericTypeDef(
+    DefId.fresh(), "nat", None, NumericType(NumericType.Kind.Nat)
+)
+int_type_def = _NumericTypeDef(
+    DefId.fresh(), "int", None, NumericType(NumericType.Kind.Int)
+)
+float_type_def = _NumericTypeDef(
+    DefId.fresh(), "float", None, NumericType(NumericType.Kind.Float)
+)
+list_type_def = _ListTypeDef(
+    id=DefId.fresh(),
+    name="list",
+    defined_at=None,
+    params=[TypeParam(0, "T", can_be_linear=True)],
+    always_linear=False,
+    to_hugr=_list_to_hugr,
+)
+array_type_def = OpaqueTypeDef(
+    id=DefId.fresh(),
+    name="array",
+    defined_at=None,
+    params=[
+        TypeParam(0, "T", can_be_linear=True),
+        ConstParam(1, "n", NumericType(NumericType.Kind.Nat)),
+    ],
+    always_linear=False,
+    to_hugr=_array_to_hugr,
+)
+sized_iter_type_def = OpaqueTypeDef(
+    id=DefId.fresh(),
+    name="SizedIter",
+    defined_at=None,
+    params=[
+        TypeParam(0, "T", can_be_linear=True),
+        ConstParam(1, "n", NumericType(NumericType.Kind.Nat)),
+    ],
+    always_linear=False,
+    to_hugr=_sized_iter_to_hugr,
+)
+
+
+def bool_type() -> OpaqueType:
+    return OpaqueType([], bool_type_def)
+
+
+def int_type() -> NumericType:
+    return NumericType(NumericType.Kind.Int)
+
+
+def list_type(element_ty: Type) -> OpaqueType:
+    return OpaqueType([TypeArg(element_ty)], list_type_def)
+
+
+def array_type(element_ty: Type, length: int) -> OpaqueType:
+    nat_type = NumericType(NumericType.Kind.Nat)
+    return OpaqueType(
+        [TypeArg(element_ty), ConstArg(ConstValue(nat_type, length))], array_type_def
+    )
+
+
+def sized_iter_type(iter_type: Type, size: int) -> OpaqueType:
+    nat_type = NumericType(NumericType.Kind.Nat)
+    return OpaqueType(
+        [TypeArg(iter_type), ConstArg(ConstValue(nat_type, size))], sized_iter_type_def
+    )
+
+
+def is_bool_type(ty: Type) -> bool:
+    return isinstance(ty, OpaqueType) and ty.defn == bool_type_def
+
+
+def is_list_type(ty: Type) -> bool:
+    return isinstance(ty, OpaqueType) and ty.defn == list_type_def
+
+
+def is_array_type(ty: Type) -> TypeGuard[OpaqueType]:
+    return isinstance(ty, OpaqueType) and ty.defn == array_type_def
+
+
+def is_sized_iter_type(ty: Type) -> TypeGuard[OpaqueType]:
+    return isinstance(ty, OpaqueType) and ty.defn == sized_iter_type_def
+
+
+def get_element_type(ty: Type) -> Type:
+    assert isinstance(ty, OpaqueType)
+    assert ty.defn == list_type_def
+    (arg,) = ty.args
+    assert isinstance(arg, TypeArg)
+    return arg.ty
+
+
+def get_iter_size(ty: Type) -> int:
+    assert isinstance(ty, OpaqueType)
+    assert ty.defn == sized_iter_type_def
+    match ty.args:
+        case [_, ConstArg(ConstValue(value=int(size)))]:
+            return size
+        case _:
+            raise InternalGuppyError("Unexpected type args")
