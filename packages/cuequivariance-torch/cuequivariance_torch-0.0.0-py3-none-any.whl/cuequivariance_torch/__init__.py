cb348eb89267dbfb6cb8587d94b28f9eac52310b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:28 +0000

    Add package cuequivariance-torch/0.0.0/cuequivariance_torch-0.0.0-py3-none-any.whl

---
+++ packages/cuequivariance-torch/cuequivariance_torch-0.0.0-py3-none-any.whl/cuequivariance_torch/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES
+# SPDX-License-Identifier: Apache-2.0
+import importlib.resources
+
+__version__ = (
+    importlib.resources.files(__package__).joinpath("VERSION").read_text().strip()
+)
+
+__all__ = []
