06422ee24d4869770cfb114a33ec2671b604b207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:53 +0000

    Add package simple-translate/0.1.6/simple_translate-0.1.6.tar.gz

---
+++ packages/simple-translate/simple_translate-0.1.6.tar.gz/simple_translate-0.1.6/test/test.py
@@ -0,0 +1,28 @@
+import pytest
+from simple_translate import SimpleTranslate
+
+translator = SimpleTranslate()
+
+def test_translation_uz_to_en():
+    translated_text = translator.translate("salom", "en", "uz")
+    assert translated_text == "hello"
+
+def test_translation_en_to_uz():
+    translated_text = translator.translate("how are you", "uz", "en")
+    assert translated_text == "qalaysiz"
+
+def test_translation_en_to_fr():
+    translated_text = translator.translate("apple", "fr", "en")
+    assert translated_text == "pomme"
+
+def test_translation_en_to_kr():
+    translated_text = translator.translate("apple", "ko", "en")
+    assert translated_text == "사과"
+
+def test_supported_languages():
+    languages = translator.get_supported_languages()
+    assert languages == languages
+
+def test_supported_languages_uz():
+    languages = translator.get_supported_languages()
+    assert "uz" in languages, "Uzbek (uz) is not in the list of supported languages"
