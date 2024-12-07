7a621dbb4e52e325f4d3bf29b7bf906d2d7a6777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:07 +0000

    Add package agentboard/0.0.1/agentboard-0.0.1-py3-none-any.whl

---
+++ packages/agentboard/agentboard-0.0.1-py3-none-any.whl/agentboard/base.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/06/27
+
+class BaseAPI(object):
+    """docstring for ClassName"""
+    def __init__(self, configs):
+        self.configs = configs
+        
+    def api(self, kwargs):
+        """
+            Args:
+                kwargs: dict, 
+            Return:
+                res_dict: dict
+        """
+        # input
+        # output
+        res_dict={}
+        return res_dict
