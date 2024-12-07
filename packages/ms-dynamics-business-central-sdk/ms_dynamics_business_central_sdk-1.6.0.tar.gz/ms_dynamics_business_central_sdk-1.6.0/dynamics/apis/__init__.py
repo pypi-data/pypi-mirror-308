620a9de854ff5659af7084a69d4534f69b0a684e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:37 +0000

    Add package ms-dynamics-business-central-sdk/1.6.0/ms_dynamics_business_central_sdk-1.6.0.tar.gz

---
+++ packages/ms-dynamics-business-central-sdk/ms_dynamics_business_central_sdk-1.6.0.tar.gz/ms_dynamics_business_central_sdk-1.6.0/dynamics/apis/__init__.py
@@ -0,0 +1,27 @@
+from .companies import Companies
+from .vendors import Vendors
+from .accounts import Accounts
+from .invoices import PurchaseInvoices
+from .journal import Journals
+from .journal_line_items import JournalLineItem
+from .invoice_line_items import PurchaseInvoiceLineItems
+from .attachments import Attachments
+from .employees import Employees
+from .locations import Locations
+from .dimensions import Dimensions
+from .bank_accounts import BankAccounts
+
+__all__ = [
+    'Companies',
+    'Vendors',
+    'Accounts',
+    'PurchaseInvoices',
+    'Journals',
+    'JournalLineItem',
+    'PurchaseInvoiceLineItems',
+    'Attachments',
+    'Employees',
+    'Locations',
+    'Dimensions',
+    'BankAccounts',
+]
