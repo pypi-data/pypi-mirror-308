fe9d70b1285410fbc5b88b4094c7d771e991debc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:18 +0000

    Add package django-hstore-widget/0.0.14/django_hstore_widget-0.0.14.tar.gz

---
+++ packages/django-hstore-widget/django_hstore_widget-0.0.14.tar.gz/django_hstore_widget-0.0.14/django_hstore_widget/forms.py
@@ -0,0 +1,18 @@
+import json
+import logging
+
+from django.contrib.postgres.forms import HStoreField
+
+from .widgets import HStoreFormWidget
+
+
+logger = logging.getLogger("django_hstore_widget")
+
+
+class HStoreFormField(HStoreField):
+    widget = HStoreFormWidget
+
+    def clean(self, value):
+        if not value:
+            value = "{}"
+        return json.loads(value)
