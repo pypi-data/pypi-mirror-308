97f1aa7cbb7c64bc9059ce19095384813699b5f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:16 +0000

    Add package uiwiz/0.1.58/uiwiz-0.1.58.tar.gz

---
+++ packages/uiwiz/uiwiz-0.1.58.tar.gz/uiwiz-0.1.58/uiwiz/__init__.py
@@ -0,0 +1,6 @@
+import uiwiz.ui as ui
+from uiwiz.app import UiwizApp
+from uiwiz.element import Element
+from uiwiz.page_route import PageRouter
+
+__all__ = ["ui", "UiwizApp", "Element", "PageRouter"]
