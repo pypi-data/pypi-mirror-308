9f6d0299b067aa15bd3857bb4946dcee8a83b465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:25 +0000

    Add package collective.formsupport.counter/1.0.0rc2/collective_formsupport_counter-1.0.0rc2.tar.gz

---
+++ packages/collective.formsupport.counter/collective_formsupport_counter-1.0.0rc2.tar.gz/collective_formsupport_counter-1.0.0rc2/src/collective/formsupport/counter/interfaces.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Module where all interfaces, events and exceptions live."""
+
+from collective.volto.formsupport.interfaces import ICollectiveVoltoFormsupportLayer
+
+
+class ICollectiveFormsupportCounterLayer(ICollectiveVoltoFormsupportLayer):
+    """Marker interface that defines a browser layer."""
