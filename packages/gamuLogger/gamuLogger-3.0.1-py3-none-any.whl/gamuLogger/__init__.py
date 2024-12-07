9e51e666af2e30118fabe9ffac469873277c686e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:22 +0000

    Add package gamuLogger/3.0.1/gamuLogger-3.0.1-py3-none-any.whl

---
+++ packages/gamuLogger/gamuLogger-3.0.1-py3-none-any.whl/gamuLogger/__init__.py
@@ -0,0 +1,4 @@
+from .gamuLogger import (LEVELS, SENSITIVE_LEVELS, TERMINAL_TARGETS, Logger,
+                         Module, Target, chrono, critical, debug, debugFunc,
+                         deepDebug, deepDebugFunc, error, info, message,
+                         warning)
