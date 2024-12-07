2722655ddacc56a8ca4dead24cf70f9eafe02f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:11 +0000

    Add package pprl-client/0.3.3/pprl_client-0.3.3-py3-none-any.whl

---
+++ packages/pprl-client/pprl_client-0.3.3-py3-none-any.whl/pprl_client/__init__.py
@@ -0,0 +1,9 @@
+from .lib import transform, match, mask, compute_attribute_stats, AttributeStats
+
+__all__ = [
+    "transform",
+    "match",
+    "mask",
+    "compute_attribute_stats",
+    "AttributeStats",
+]
