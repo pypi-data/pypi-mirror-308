71fd04714279e13e1a82511c1876a14d656365bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:53 +0000

    Add package costo/0.0.7/costo-0.0.7.tar.gz

---
+++ packages/costo/costo-0.0.7.tar.gz/costo-0.0.7/src/costo/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+__author__  = "Teddy Chantrait"
+__email__   = "teddy.chantrait@gmail.com"
+__status__  = "Development"
+__date__    = "2024-11-15 10:10:48"
+__version__ = "0.0.7"
