c0e785df5bb0d0c8130f54ccaef0e1295db0c469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:10 +0000

    Add package jay-sdk-test/0.0.20/jay_sdk_test-0.0.20.tar.gz

---
+++ packages/jay-sdk-test/jay_sdk_test-0.0.20.tar.gz/jay_sdk_test-0.0.20/jay_sdk_test/api/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import apis into api package
+from jay_sdk_test.api.admin_api import AdminApi
+from jay_sdk_test.api.auth_api import AuthApi
+from jay_sdk_test.api.datasets_api import DatasetsApi
+from jay_sdk_test.api.default_api import DefaultApi
+from jay_sdk_test.api.elastic_search_api import ElasticSearchApi
+from jay_sdk_test.api.instruments_api import InstrumentsApi
+from jay_sdk_test.api.jobs_api import JobsApi
+from jay_sdk_test.api.logbooks_api import LogbooksApi
+from jay_sdk_test.api.origdatablocks_api import OrigdatablocksApi
+from jay_sdk_test.api.policies_api import PoliciesApi
+from jay_sdk_test.api.proposals_api import ProposalsApi
+from jay_sdk_test.api.published_data_api import PublishedDataApi
+from jay_sdk_test.api.samples_api import SamplesApi
+from jay_sdk_test.api.user_identities_api import UserIdentitiesApi
+from jay_sdk_test.api.users_api import UsersApi
+
