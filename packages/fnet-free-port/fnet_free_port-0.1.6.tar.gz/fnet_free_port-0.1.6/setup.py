1b538f426237f6d9dd157ab396728d6501100d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:54 +0000

    Add package fnet-free-port/0.1.6/fnet_free_port-0.1.6.tar.gz

---
+++ packages/fnet-free-port/fnet_free_port-0.1.6.tar.gz/fnet_free_port-0.1.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fnet_free_port",
+    version="0.1.6",
+    packages=["fnet_free_port","fnet_free_port/lib","fnet_free_port/cli"],
+    package_dir={"": "."},
+    entry_points={
+        "console_scripts": [
+            "fnet_free_port=fnet_free_port.cli:main",
+        ],
+    },
+    python_requires=">=3.12",
+    install_requires=[
+      
+    ],
+    # Optional metadata, to be added later:
+    # author="justai.pro",  # Your name (optional)
+    # author_email="devops@justai.pro",  # Your email address (optional)
+    # description="A Python dependency analyzer for import parsing and metadata extraction.",
+    # long_description=open("README.md").read(),
+    # long_description_content_type="text/markdown",
+    # url="https://gitlab.com/fnetai/py-import-parser",
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: MIT License",
+    #     # "Operating System :: OS Independent",
+    # ],
+    # include_package_data=True,  # Include non-Python files (e.g., YAML files) (optional)
+)
