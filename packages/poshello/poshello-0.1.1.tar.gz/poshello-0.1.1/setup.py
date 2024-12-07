40859baa54f2d9187fb93d1bff57e8c472647777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:16 +0000

    Add package poshello/0.1.1/poshello-0.1.1.tar.gz

---
+++ packages/poshello/poshello-0.1.1.tar.gz/poshello-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='poshello',
+    version='0.1.1',
+    description='A example Python package',
+    url='',
+    author='LEE Taehoon',
+    author_email='taehoon@poscodx.com',
+    license='MIT',
+    packages=['poshello'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
