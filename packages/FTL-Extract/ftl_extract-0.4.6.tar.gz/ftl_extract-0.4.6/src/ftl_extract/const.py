bdd8cddc1e3d2f2718b34d8a06bafea9c0ab0963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:16 +0000

    Add package FTL-Extract/0.4.6/ftl_extract-0.4.6.tar.gz

---
+++ packages/FTL-Extract/ftl_extract-0.4.6.tar.gz/ftl_extract-0.4.6/src/ftl_extract/const.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Final, Literal
+
+
+I18N_LITERAL: Final[Literal["i18n"]] = "i18n"
+GET_LITERAL: Final[Literal["get"]] = "get"
+PATH_LITERAL: Final[Literal["_path"]] = "_path"
+IGNORE_ATTRIBUTES: Final[frozenset[str]] = frozenset(
+    {"set_locale", "use_locale", "use_context", "set_context"},
+)
+IGNORE_KWARGS: Final[frozenset[str]] = frozenset()
+DEFAULT_FTL_FILE: Final[Path] = Path("_default.ftl")
