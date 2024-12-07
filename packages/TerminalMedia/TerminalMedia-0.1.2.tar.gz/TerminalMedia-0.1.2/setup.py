c4ae3a7e2479247a93409d6f210a025eaa56015b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:03 +0000

    Add package TerminalMedia/0.1.2/TerminalMedia-0.1.2.tar.gz

---
+++ packages/TerminalMedia/TerminalMedia-0.1.2.tar.gz/TerminalMedia-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TerminalMedia',
+    version='0.1.2',
+    description='A tool to display images and videos in the terminal',
+    author='Snape-max',
+    author_email='ssnape@qq.com',
+    url='https://github.com/Snape-max/TerminalMedia',  # 你的项目主页
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'terminalmedia=tmt.terminalmedia:main',
+            'icat=tmt.imgcat:main',
+            'vcat=tmt.videocat:main',
+        ],
+    },
+    install_requires=[
+        'Pillow',
+        'opencv-python'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # 选择合适的许可证
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
