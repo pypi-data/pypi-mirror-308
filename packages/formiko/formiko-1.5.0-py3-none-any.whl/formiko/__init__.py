3d179de3dcee9062aa3e3c443b411fd02af41bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:33 +0000

    Add package formiko/1.5.0/formiko-1.5.0-py3-none-any.whl

---
+++ packages/formiko/formiko-1.5.0-py3-none-any.whl/formiko/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "1.5.0"
+__copyright__ = "(c) 2024"
+__author__ = "Ondřej Tůma <mcbig@zeropage.cz>"
+__url__ = "https://github.com/ondratu/formiko"
+__comment__ = \
+    "Formiko is reStructuredText and MarkDown editor and live previewer."
