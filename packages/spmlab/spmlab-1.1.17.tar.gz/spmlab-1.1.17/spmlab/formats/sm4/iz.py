e32b997c73cb94ff42c95ecbc3b75f62d7879de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:33 +0000

    Add package spmlab/1.1.17/spmlab-1.1.17.tar.gz

---
+++ packages/spmlab/spmlab-1.1.17.tar.gz/spmlab-1.1.17/spmlab/formats/sm4/iz.py
@@ -0,0 +1,8 @@
+from xarray import Dataset
+from ._abstract import SpectralData
+
+class IZData(SpectralData):
+    def __init__(self, src: str, raw: Dataset):
+        super().__init__(src, raw)
+        self.iz = self.raw.Current
+        self.coords = self.get_unique_coords(zip(self.iz.RHK_SpecDrift_Xcoord, self.iz.RHK_SpecDrift_Ycoord))
