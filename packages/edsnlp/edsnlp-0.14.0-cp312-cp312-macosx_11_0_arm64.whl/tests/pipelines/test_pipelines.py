c2a5ee44260f79a78e02ecf0857c220bd1d6420f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:40 +0000

    Add package edsnlp/0.14.0/edsnlp-0.14.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/edsnlp/edsnlp-0.14.0-cp312-cp312-macosx_11_0_arm64.whl/tests/pipelines/test_pipelines.py
@@ -0,0 +1,16 @@
+def test_pipelines(doc):
+    assert len(doc.ents) == 3
+    patient, _, anomalie = doc.ents
+
+    assert not patient._.negation
+    assert anomalie._.negation
+
+    assert not doc[0]._.history
+
+
+def test_import_all():
+    import edsnlp.pipes
+
+    for name in dir(edsnlp.pipes):
+        if not name.startswith("_") and "endlines" not in name:
+            getattr(edsnlp.pipes, name)
