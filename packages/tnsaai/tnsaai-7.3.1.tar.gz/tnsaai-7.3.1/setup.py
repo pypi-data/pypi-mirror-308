755ede67a306c5b5bfc6f0fb329b9fb9bc2e642e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:11:24 +0000

    Add package tnsaai/7.3.1/tnsaai-7.3.1.tar.gz

---
+++ packages/tnsaai/tnsaai-7.3.1.tar.gz/tnsaai-7.3.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tnsaai',
+    version='7.3.1',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow>=2.0',  # Adjust according to your dependencies
+        'numpy',
+        'six',
+    ],
+    license='NGen2Community License',  # Your custom license
+    description='A transformer model with advanced features for casual language modeling.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='TNSA AI',
+    author_email='tnsa.company@gmail.com',
+    url='https://github.com/TnsaAi/tnsa.stable.curiosity',  # Replace with actual URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
