33a0c9c3579e9c1f512d326ef307c570834fce08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:29 +0000

    Add package schemon-python-logger/0.0.14/schemon-python-logger-0.0.14.tar.gz

---
+++ packages/schemon-python-logger/schemon-python-logger-0.0.14.tar.gz/schemon-python-logger-0.0.14/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+VERSION = "0.0.14"
+
+
+setup(
+    name="schemon-python-logger",
+    version=VERSION,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    license="Apache License 2.0",
+    license_files=["LICENSE"],  # Specify the license file
+    install_requires=[
+        "pyspark==3.5.0",
+        "tabulate==0.9.0",
+    ],
+    entry_points={},
+    python_requires=">=3.9",
+    include_package_data=True,  # Include package data specified in MANIFEST.in
+    package_data={},
+    exclude_package_data={},
+)
