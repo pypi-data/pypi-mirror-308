e09539afcc96a7c10d4cdd6c0abb1936d0a76953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:10 +0000

    Add package costo/0.0.8/costo-0.0.8-py3-none-any.whl

---
+++ packages/costo/costo-0.0.8-py3-none-any.whl/costo/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+__author__  = "Teddy Chantrait"
+__email__   = "teddy.chantrait@gmail.com"
+__status__  = "Development"
+__date__    = "2024-11-15 13:29:28"
+__version__ = "0.0.8"
