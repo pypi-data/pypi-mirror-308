6a14c275c2b0a74cc6b264d9896f64749eec7775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:56 +0000

    Add package qiskit-ibm-ai-local-transpiler/0.1.3/qiskit_ibm_ai_local_transpiler-0.1.3-cp312-none-win_amd64.whl

---
+++ packages/qiskit-ibm-ai-local-transpiler/qiskit_ibm_ai_local_transpiler-0.1.3-cp312-none-win_amd64.whl/qiskit_ibm_ai_local_transpiler/utils/transpile.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# (C) Copyright 2022 IBM. All Rights Reserved.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Transpile utils"""
+from qiskit import QuantumCircuit
+from networkx.algorithms import isomorphism
+
+
+def check_transpiling(circ, cmap):
+    """Checks if a given circuit follows a specific coupling map"""
+    for cc in circ:
+        if cc.operation.num_qubits == 2:
+            q_pair = tuple(circ.find_bit(qi).index for qi in cc.qubits)
+            if (
+                q_pair not in cmap
+                and q_pair[::-1] not in cmap
+                and list(q_pair) not in cmap
+                and list(q_pair[::-1]) not in cmap
+            ):
+                return False
+    return True
+
+
+def check_topology_synthesized_circuit(
+    circuit: QuantumCircuit,
+    coupling_map: list[list[int]],
+):
+    """Check whether a synthesized circuit follows a coupling map and respects topology"""
+    return check_transpiling(circuit, coupling_map)
+
+
+def find_symmetries(G):
+    return sorted(
+        [d[i] for i in range(G.number_of_nodes())]
+        for d in isomorphism.GraphMatcher(G, G).subgraph_isomorphisms_iter()
+    )
