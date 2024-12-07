c1f58f76fc937d1d1063ae6d96e6ee44f2421052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:50 +0000

    Add package neptune-fetcher/0.9.2b0/neptune_fetcher-0.9.2b0-py3-none-any.whl

---
+++ packages/neptune-fetcher/neptune_fetcher-0.9.2b0-py3-none-any.whl/neptune_fetcher/util.py
@@ -0,0 +1,79 @@
+#
+# Copyright (c) 2023, Neptune Labs Sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import abc
+import contextlib
+import os
+import warnings
+from typing import (
+    Any,
+    Optional,
+    Type,
+    Union,
+)
+
+
+def getenv_int(name: str, default: int, *, positive=True) -> int:
+    value = os.environ.get(name)
+    if value is None:
+        return default
+
+    try:
+        value = int(value)
+        if positive and value <= 0:
+            raise ValueError
+    except ValueError:
+        raise ValueError(f"Environment variable {name} must be a positive integer, got '{value}'")
+
+    return value
+
+
+class ProgressBarCallback(contextlib.AbstractContextManager):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        ...
+
+    @abc.abstractmethod
+    def update(self, *, by: int, total: Optional[int] = None) -> None:
+        ...
+
+
+ProgressBarType = Union[bool, Type[ProgressBarCallback]]
+
+
+class NeptuneException(Exception):
+    def __eq__(self, other: Any) -> bool:
+        if type(other) is type(self):
+            return super().__eq__(other) and str(self).__eq__(str(other))
+        else:
+            return False
+
+    def __hash__(self) -> int:
+        return hash((super().__hash__(), str(self)))
+
+
+class NeptuneWarning(Warning):
+    pass
+
+
+warnings.simplefilter("once", category=NeptuneWarning)
+
+
+def escape_nql_criterion(criterion):
+    """
+    Escape backslash and (double-)quotes in the string, to match what the NQL engine expects.
+    """
+
+    return criterion.replace("\\", r"\\").replace('"', r"\"")
