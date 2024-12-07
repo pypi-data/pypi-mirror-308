16b0a202935717ac4d14e376a0bafcf400e306c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:23 +0000

    Add package spsdk-pkcs11/0.1.1/spsdk_pkcs11-0.1.1-py3-none-any.whl

---
+++ packages/spsdk-pkcs11/spsdk_pkcs11-0.1.1-py3-none-any.whl/spsdk_pkcs11/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2024 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+"""Top-level package for PKCS#11 Signature Provider."""
+
+__author__ = """NXP"""
+__email__ = "michal.starecek@nxp.com"
+__version__ = "0.1.1"
+
+from .provider import PKCS11SP
+
+__all__ = ["PKCS11SP"]
