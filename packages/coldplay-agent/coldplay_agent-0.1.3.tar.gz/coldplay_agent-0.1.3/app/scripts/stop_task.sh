d2b17f595c5501dc6c4c1fc711e83e234e11e37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:13 +0000

    Add package coldplay-agent/0.1.3/coldplay_agent-0.1.3.tar.gz

---
+++ packages/coldplay-agent/coldplay_agent-0.1.3.tar.gz/coldplay_agent-0.1.3/app/scripts/stop_task.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+reboot_needed=0
+
+script_run_url_new=$1
+task_id=$2
+
+# bash fonts colors
+red='\e[31m'
+yellow='\e[33m'
+gray='\e[90m'
+green='\e[92m'
+blue='\e[94m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+_red() { echo -e ${red}$@${none}; }
+_blue() { echo -e ${blue}$@${none}; }
+_cyan() { echo -e ${cyan}$@${none}; }
+_green() { echo -e ${green}$@${none}; }
+_yellow() { echo -e ${yellow}$@${none}; }
+_magenta() { echo -e ${magenta}$@${none}; }
+_red_bg() { echo -e "\e[41m$@${none}"; }
+
+is_err=$(_red_bg 错误：)
+is_warn=$(_red_bg 警告：)
+is_info=$(_red_bg 提示：)
+
+err() {
+    echo -e "\n$is_err $@\n" && exit 1
+}
+
+warn() {
+    echo -e "\n$is_warn $@\n"
+}
+
+info() {
+    echo -e "\n$is_info $@\n"
+}
+
+check_err() {
+    if [[ $? != 0 ]]; then echo -e "\n$is_err $@\n" && exit 1; fi
+}
+
+if [[ $(lsb_release -rs) != "20.04" || $(lsb_release -is) != "Ubuntu" || $(uname -m) != "x86_64" ]]; then
+    err "仅支持 ${yellow}(Ubuntu 20.04 和 x86_64 架构)${none}"
+fi
+
+stop_task() {
+    info "关闭训练"
+    ps aux | grep "python $script_run_url_new --task=pointfoot_rough --task_id=$task_id" | grep -v grep | awk '{print $2}' | xargs kill -9
+
+    check_err "${yellow}关闭失败"
+    info "${yellow}关闭成功"
+}
+
+stop_task
+
+
