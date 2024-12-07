45f2fb0534702c7b1365e69caded8b19ac3d044b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:25 +0000

    Add package robolson/0.3.93/robolson-0.3.93-py3-none-any.whl

---
+++ packages/robolson/robolson-0.3.93-py3-none-any.whl/rob/project.py
@@ -0,0 +1,20 @@
+# to do
+# git
+#   git hook
+# pytest
+# typer
+# logger
+# toml config/database
+# other database
+import survey
+import typer
+
+app = typer.Typer()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    app()
