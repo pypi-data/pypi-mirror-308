66ca62506335952801198676515fe57bb50c85bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:23 +0000

    Add package pyshad/0.0.1/pyshad-0.0.1-py3-none-any.whl

---
+++ packages/pyshad/pyshad-0.0.1-py3-none-any.whl/pyshad/methods/groups/group_preview_by_join_link.py
@@ -0,0 +1,14 @@
+import rubpy
+
+class GroupPreviewByJoinLink:
+    async def group_preview_by_join_link(
+            self: "rubpy.Client",
+            link: str,
+    ):
+        if '/' in link:
+            link = link.split('/')[-1]
+
+        return await self.builder('groupPreviewByJoinLink',
+                                  input={
+                                      'hash_link': link,
+                                  })
