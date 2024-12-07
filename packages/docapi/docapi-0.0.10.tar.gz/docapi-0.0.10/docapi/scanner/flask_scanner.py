864f074babedeca11d1ea3ddf2e8f1bff6409019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:36 +0000

    Add package docapi/0.0.10/docapi-0.0.10.tar.gz

---
+++ packages/docapi/docapi-0.0.10.tar.gz/docapi-0.0.10/docapi/scanner/flask_scanner.py
@@ -0,0 +1,59 @@
+import sys
+from pathlib import Path
+import hashlib
+import inspect
+import importlib
+
+
+def scan(server_path):
+    server_path = Path(server_path)
+    server_dir = server_path.parent
+    sys.path.insert(0, str(server_dir))
+
+    package = server_path.stem
+    package = importlib.import_module(package)
+    sys.path.pop(0)
+    code = inspect.getsource(package)
+
+    for line in code.split('\n'):
+        if 'Flask(__name__)' in line:
+            app_name = line.split('=')[0].strip()
+            break
+
+    app = getattr(package, app_name)
+    structures = {}
+
+    for rule in app.url_map.iter_rules():
+        view_func = app.view_functions[rule.endpoint]
+        path = str(Path(inspect.getfile(view_func)).resolve())
+
+        if path.endswith('/site-packages/flask/app.py'):
+            continue
+
+        comments = inspect.getcomments(view_func)
+        code = inspect.getsource(view_func)
+
+        if comments is not None:
+            code = comments + code
+
+        md5 = hashlib.md5(code.encode('utf-8')).hexdigest()
+
+        if path not in structures:
+            structures[path] = []
+
+        structures[path].append({
+            'url': rule.rule,
+            'md5': md5,
+            'code': code
+        })
+
+    structures = _sort_structures(structures)
+    return structures
+
+
+def _sort_structures(structures):
+    new_structures = {}
+    for path, item_list in structures.items():
+        new_structures[path] = sorted(item_list, key=lambda x: x['url'])
+
+    return new_structures
