97e4facadefc83cbac46f2cc51d274ccbd659130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:13 +0000

    Add package nonebot_plugin_remake/0.4.4/nonebot_plugin_remake-0.4.4.tar.gz

---
+++ packages/nonebot_plugin_remake/nonebot_plugin_remake-0.4.4.tar.gz/nonebot_plugin_remake-0.4.4/README.md
@@ -0,0 +1,48 @@
+# nonebot-plugin-remake
+
+适用于 [Nonebot2](https://github.com/nonebot/nonebot2) 的人生重开模拟器
+
+这垃圾人生一秒也不想待了？立即重开！
+
+
+### 安装
+
+- 使用 nb-cli
+
+```
+nb plugin install nonebot_plugin_remake
+```
+
+- 使用 pip
+
+```
+pip install nonebot_plugin_remake
+```
+
+
+### 使用
+
+#### 触发方式：
+
+**以下命令需要加[命令前缀](https://nonebot.dev/docs/appendices/config#command-start-和-command-separator) (默认为`/`)，可自行设置为空**
+
+```
+@机器人 remake/liferestart/人生重开/人生重来
+```
+
+
+#### 示例：
+
+<div align="left">
+  <img src="https://s2.loli.net/2023/08/02/25YjUFKwvnWisNr.jpg" width="400" />
+  <img src="https://s2.loli.net/2023/08/02/b4L7WvnAyHeYUPZ.jpg" width="400" />
+</div>
+
+
+### 特别感谢
+
+- [VickScarlet/lifeRestart](https://github.com/VickScarlet/lifeRestart) やり直すんだ。そして、次はうまくやる。
+
+- [cc004/lifeRestart-py](https://github.com/cc004/lifeRestart-py) lifeRestart game in python
+
+- [DaiShengSheng/lifeRestart_bot](https://github.com/DaiShengSheng/lifeRestart_bot) 适用于HoshinoBot下的人生重来模拟器插件
---
+++ packages/nonebot_plugin_remake/nonebot_plugin_remake-0.4.4.tar.gz/nonebot_plugin_remake-0.4.4/nonebot_plugin_remake/utils.py
@@ -0,0 +1,37 @@
+import re
+
+from nonebot.log import logger
+
+
+class DummyList(list):
+    def __init__(self, lst: list[int]):
+        super().__init__(lst)
+
+    def __contains__(self, obj: object) -> bool:
+        if isinstance(obj, set):
+            for x in self:
+                if x in obj:
+                    return True
+            return False
+        return super().__contains__(obj)
+
+
+def parse_condition(cond: str):
+    reg_attr = re.compile("[A-Z]{3}")
+    cond2 = (
+        reg_attr.sub(
+            lambda m: f'getattr(x, "{m.group()}")', cond.replace("AEVT", "AVT")
+        )
+        .replace("?[", " in DummyList([")
+        .replace("![", "not in DummyList([")
+        .replace("]", "])")
+        .replace("|", " or ")
+    )
+    while True:
+        try:
+            func = eval(f"lambda x: {cond2}")
+            func.__doc__ = cond2
+            return func
+        except Exception:
+            logger.warning(f"[WARNING] missing ) in {cond}")
+            cond2 += ")"
