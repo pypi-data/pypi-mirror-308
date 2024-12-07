e4674c03f3bca765d9eb922b18717c2d9eab5d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:15 +0000

    Add package wxw/1.0.5/wxw-1.0.5.tar.gz

---
+++ packages/wxw/wxw-1.0.5.tar.gz/wxw-1.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wxw',
+    version='1.0.5',
+    keywords=['pip', 'wxw'],
+    description='A library for wxw',
+    long_description="Includes some ways to work with pictures, add qt utils",
+    author='weixianwei',
+    author_email='weixianwei0129@gmail.com',
+    url='https://github.com/weixianwei0129/mylibrary',
+
+    packages=find_packages(),
+    platforms="any",
+    install_requires=["numpy<2.0.0", "psutil", "opencv-python", "matplotlib", "Pillow", "einops", "PyYaml"]
+)
