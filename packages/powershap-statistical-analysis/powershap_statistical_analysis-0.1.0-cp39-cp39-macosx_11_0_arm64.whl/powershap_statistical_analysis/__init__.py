9b9fe419ecf433415c5c9e3052940418380a1741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:59 +0000

    Add package powershap-statistical-analysis/0.1.0/powershap_statistical_analysis-0.1.0-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/powershap-statistical-analysis/powershap_statistical_analysis-0.1.0-cp39-cp39-macosx_11_0_arm64.whl/powershap_statistical_analysis/__init__.py
@@ -0,0 +1,5 @@
+from .powershap_statistical_analysis import *
+
+__doc__ = powershap_statistical_analysis.__doc__
+if hasattr(powershap_statistical_analysis, "__all__"):
+    __all__ = powershap_statistical_analysis.__all__
