6e211d2b0d01e24f148bf3c8566f49cd4342a90a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:50 +0000

    Add package pppybox/0.0.15/pppybox-0.0.15.tar.gz

---
+++ packages/pppybox/pppybox-0.0.15.tar.gz/pppybox-0.0.15/src/pybox/__init__.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2024-present Junlin Zhou <jameszhou2108@hotmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from pybox.local import LocalPyBox, LocalPyBoxManager
+from pybox.remote import RemotePyBox, RemotePyBoxManager
+from pybox.schema import PyBoxOut
+
+__all__ = [
+    "LocalPyBox",
+    "LocalPyBoxManager",
+    "RemotePyBox",
+    "RemotePyBoxManager",
+    "PyBoxOut",
+]
---
+++ packages/pppybox/pppybox-0.0.15.tar.gz/pppybox-0.0.15/src/pybox/base.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+import logging
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pybox.schema import PyBoxOut
+
+logger = logging.getLogger(__name__)
+
+
+class BasePyBox(ABC):
+    """A PyBox is an interface to a kernel that can execute code."""
+
+    @abstractmethod
+    def run(self, code: str, **kwargs) -> PyBoxOut:
+        """Execute code in the PyBox and return the result.
+
+        Args:
+            code (str): code to execute
+
+        Returns:
+            PyBoxOut: result of the code execution
+
+        Raises:
+            TimeoutError: if the code execution times out
+        """
+
+    async def arun(self, code: str, **kwargs) -> PyBoxOut:
+        """Asynchoronously execute code in the PyBox and return the result.
+        Default implementation is to call the synchronous `run` method.
+
+        Args:
+            code (str): code to execute
+
+        Returns:
+            PyBoxOut: result of the code execution
+
+        Raises:
+            TimeoutError: if the code execution times out
+        """
+        return self.run(code=code, **kwargs)
+
+
+class BasePyBoxManager(ABC):
+    """Abstract Base class for managing kernels"""
+
+    @abstractmethod
+    def start(
+        self,
+        kernel_id: str | None = None,
+        **kwargs,
+    ) -> BasePyBox:
+        """Retrieve an existing kernel or create a new one.
+
+        Args:
+            kernel_id (str): pybox id.
+
+        Returns:
+            Kernel: An iPython kernel that executes code.
+        """
+
+    async def astart(
+        self,
+        kernel_id: str | None = None,
+        **kwargs,
+    ) -> BasePyBox:
+        """Retrieve an existing kernel or create a new one.
+
+        Args:
+            kernel_id (str): kernel id.
+
+        Returns:
+            Kernel: An iPython kernel that executes code.
+        """
+        return self.start(kernel_id=kernel_id, **kwargs)
+
+    @abstractmethod
+    def shutdown(
+        self,
+        kernel_id: str,
+        **kwargs,
+    ) -> None:
+        """Shutdown the kernel."""
+        ...
+
+    async def ashutdown(
+        self,
+        kernel_id: str,
+        **kwargs,
+    ) -> None:
+        """Shutdown the kernel."""
+        self.shutdown(kernel_id=kernel_id, **kwargs)
