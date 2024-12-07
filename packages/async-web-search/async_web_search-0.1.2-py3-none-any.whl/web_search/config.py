636d5392fa57fdead6b149e21c30412642fe997b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:15 +0000

    Add package async-web-search/0.1.2/async_web_search-0.1.2-py3-none-any.whl

---
+++ packages/async-web-search/async_web_search-0.1.2-py3-none-any.whl/web_search/config.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass, field
+from typing import Literal
+
+SearchSources = Literal["google", "wikipedia", "arxiv"]
+
+
+@dataclass
+class GoogleSearchConfig:
+    api_key: str
+    cse_id: str
+    max_results: int = 3
+    app_domain: str | None = None
+
+
+@dataclass
+class KnowledgeSearchConfig:
+    max_results: int = 3
+    max_sources: int = 10
+    max_preview_chars: int = 1024
+
+
+@dataclass
+class WebSearchConfig:
+    sources: list[SearchSources] = field(default_factory=lambda: ["google"])
+    google_config: GoogleSearchConfig | None = None
+    knowledge_config: KnowledgeSearchConfig | None = None
+
+
+@dataclass
+class SearchResult:
+    url: str
+    title: str
+    preview: str
+
+    def __str__(self):
+        return f"Title: {self.title}\nPreview: {self.preview}"
