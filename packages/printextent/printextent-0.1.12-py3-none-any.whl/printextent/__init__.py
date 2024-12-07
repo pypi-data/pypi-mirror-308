3d579f55f28043a02f5161301b99d126a2c7401a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:19 +0000

    Add package printextent/0.1.12/printextent-0.1.12-py3-none-any.whl

---
+++ packages/printextent/printextent-0.1.12-py3-none-any.whl/printextent/__init__.py
@@ -0,0 +1,4 @@
+from .colorPrint import *
+from .typewritePrint import *; from .reversePrint import *; from .asciiPrint import *
+from .informUserPrint import *; from .warnUserPrint import *; from .errorUserPrint import *
+from .colorReturn import *
