8c33a154ae6cda29a6d051498e5a3d00aeb0588c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:49 +0000

    Add package q-transformer/0.2.2/q_transformer-0.2.2.tar.gz

---
+++ packages/q-transformer/q_transformer-0.2.2.tar.gz/q_transformer-0.2.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'q-transformer',
+  packages = find_packages(exclude=[]),
+  version = '0.2.2',
+  license='MIT',
+  description = 'Q-Transformer',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/q-transformer',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'attention mechanisms',
+    'transformers',
+    'q-learning'
+  ],
+  install_requires=[
+    'accelerate',
+    'adam-atan2-pytorch>=0.0.12',
+    'beartype',
+    'classifier-free-guidance-pytorch>=0.7.1',
+    'einops>=0.8.0',
+    'ema-pytorch>=0.5.3',
+    'jaxtyping',
+    'numpy',
+    'sentencepiece',
+    'torch>=2.0'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
