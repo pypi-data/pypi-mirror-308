9cdb3a4f447ee9c7b1f201eae99e8a16279e38fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:16 +0000

    Add package FTL-Extract/0.4.6/ftl_extract-0.4.6-py3-none-any.whl

---
+++ packages/FTL-Extract/ftl_extract-0.4.6-py3-none-any.whl/ftl_extract/ftl_importer.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from fluent.syntax import ast, parse
+
+from ftl_extract.matcher import FluentKey
+
+if TYPE_CHECKING:
+    from fluent.syntax.ast import Resource
+
+
+def import_from_ftl(
+    path: Path,
+    locale: str,
+) -> tuple[dict[str, FluentKey], Resource, list[FluentKey]]:
+    """Import `FluentKey`s from FTL."""
+    ftl_keys = {}
+    leave_as_is = []
+
+    resource = parse(path.read_text(encoding="utf-8"), with_spans=True)
+
+    for position, entry in enumerate(resource.body, start=0):
+        if isinstance(entry, ast.Message):
+            ftl_keys[entry.id.name] = FluentKey(
+                code_path=Path(),
+                key=entry.id.name,
+                translation=entry,
+                path=path,
+                locale=locale,
+                position=position,
+            )
+        else:
+            leave_as_is.append(
+                FluentKey(
+                    code_path=Path(),
+                    key="",
+                    translation=entry,
+                    path=path,
+                    locale=locale,
+                    position=position,
+                ),
+            )
+
+    return ftl_keys, resource, leave_as_is
+
+
+def import_ftl_from_dir(path: Path, locale: str) -> tuple[dict[str, FluentKey], list[FluentKey]]:
+    """Import `FluentKey`s from directory of FTL files."""
+    ftl_files = (path / locale).rglob("*.ftl") if path.is_dir() else [path]
+    ftl_keys = {}
+    leave_as_is = []
+
+    for ftl_file in ftl_files:
+        keys, _, as_is_keys = import_from_ftl(ftl_file, locale)
+        ftl_keys.update(keys)
+        leave_as_is.extend(as_is_keys)
+
+    return ftl_keys, leave_as_is
---
+++ packages/FTL-Extract/ftl_extract-0.4.6-py3-none-any.whl/ftl_extract/process/kwargs_extractor.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from fluent.syntax import ast
+
+if TYPE_CHECKING:
+    from ftl_extract.matcher import FluentKey
+
+
+def _extract_kwargs_from_variable_reference(
+    variable_reference: ast.VariableReference,
+    kwargs: set[str],
+) -> None:
+    kwargs.add(variable_reference.id.name)
+
+
+def _extract_kwargs_from_selector_expression(
+    selector_expression: ast.SelectExpression,
+    kwargs: set[str],
+) -> None:
+    if isinstance(selector_expression.selector, ast.VariableReference):
+        _extract_kwargs_from_variable_reference(selector_expression.selector, kwargs)
+
+    for variant in selector_expression.variants:
+        for placeable in variant.value.elements:
+            if isinstance(placeable, ast.Placeable):
+                _extract_kwargs_from_placeable(placeable, kwargs)
+
+
+def _extract_kwargs_from_placeable(placeable: ast.Placeable, kwargs: set[str]) -> None:
+    expression = placeable.expression
+
+    if isinstance(expression, ast.VariableReference):
+        _extract_kwargs_from_variable_reference(expression, kwargs)
+
+    elif isinstance(expression, ast.SelectExpression):
+        _extract_kwargs_from_selector_expression(expression, kwargs)
+
+
+def extract_kwargs(key: FluentKey) -> set[str]:
+    kwargs: set[str] = set()
+
+    if not isinstance(key.translation, ast.Message):
+        return kwargs
+
+    if not key.translation.value:
+        return kwargs
+
+    for placeable in key.translation.value.elements:
+        if isinstance(placeable, ast.Placeable):
+            _extract_kwargs_from_placeable(placeable, kwargs)
+
+    return kwargs
