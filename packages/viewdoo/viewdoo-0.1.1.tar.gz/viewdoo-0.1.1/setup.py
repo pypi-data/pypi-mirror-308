3e441df2c19a088458b9c5e90ccac5a9a7b91b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:31 +0000

    Add package viewdoo/0.1.1/viewdoo-0.1.1.tar.gz

---
+++ packages/viewdoo/viewdoo-0.1.1.tar.gz/viewdoo-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="viewdoo",
+    version="0.1.1",
+    author="Zachery Morton Colbert",
+    author_email="zachery.colbert@health.qld.gov.au",
+    description="Medical Image Viewing and Conversion Tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/humunumuh/viewdoo",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "pydicom",
+        "opencv-python",
+        "rt-utils",
+        "scipy",
+        "customtkinter",
+        "nibabel",
+        "pynrrd",
+        "Pillow",
+        "SimpleITK"
+    ],
+)
