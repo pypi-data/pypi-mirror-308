3c5364b2f8c2b1e38e1fc27cdac1dfae5918fcaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:16 +0000

    Add package poshello/0.0.1/poshello-0.0.1.tar.gz

---
+++ packages/poshello/poshello-0.0.1.tar.gz/poshello-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='poshello',
+    version='0.0.1',
+    description='A example Python package',
+    url='',
+    author='',
+    author_email='',
+    license='MIT',
+    packages=['poshello'],
+    install_requires=[]
+)
