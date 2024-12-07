3942e6072f9391bec211dfa3c60941bc62a1c6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:11:46 +0000

    Add package pysciencemode/1.1.3/pysciencemode-1.1.3-py3-none-any.whl

---
+++ packages/pysciencemode/pysciencemode-1.1.3-py3-none-any.whl/pysciencemode/__init__.py
@@ -0,0 +1,8 @@
+from .motomed_interface import _Motomed
+from .sciencemode import RehastimGeneric
+from . import utils
+from .rehastim2_interface import Rehastim2
+from .rehastimP24_interface import RehastimP24
+from . import acks
+from .channel import Channel, Point
+from .enums import Rehastim2Commands, RehastimP24Commands, Modes, Device
