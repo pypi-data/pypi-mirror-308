3e90d5647a24b76df919faca9a2c04aa13d4343f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:14 +0000

    Add package compas-cadwork/0.8.0/compas_cadwork-0.8.0-py3-none-any.whl

---
+++ packages/compas-cadwork/compas_cadwork-0.8.0-py3-none-any.whl/compas_cadwork/datamodel/__init__.py
@@ -0,0 +1,18 @@
+from .element import Element
+from .element import ElementType
+from .element import ElementGroup
+from .element import ElementGroupingType
+from .element import ATTR_INSTRUCTION_ID
+from .dimension import Dimension
+from .dimension import AnchorPoint
+
+
+__all__ = [
+    "Element",
+    "ElementType",
+    "ElementGroup",
+    "ElementGroupingType",
+    "ATTR_INSTRUCTION_ID",
+    "AnchorPoint",
+    "Dimension",
+]
