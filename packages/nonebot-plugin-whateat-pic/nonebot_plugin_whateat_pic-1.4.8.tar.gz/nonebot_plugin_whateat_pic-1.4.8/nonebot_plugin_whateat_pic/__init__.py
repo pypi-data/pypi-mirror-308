9aad050c902e7d768fcd14668983bf1132ef128d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:19 +0000

    Add package nonebot-plugin-whateat-pic/1.4.8/nonebot_plugin_whateat_pic-1.4.8.tar.gz

---
+++ packages/nonebot-plugin-whateat-pic/nonebot_plugin_whateat_pic-1.4.8.tar.gz/nonebot_plugin_whateat_pic-1.4.8/nonebot_plugin_whateat_pic/__init__.py
@@ -0,0 +1,22 @@
+from nonebot import require
+
+require("nonebot_plugin_alconna")
+require("nonebot_plugin_apscheduler")
+
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters
+from . import matcher, config, check_on_statup
+
+
+__plugin_meta__ = PluginMetadata(
+    name="今天吃什么（图片版）",
+    description="随机发送吃的或者喝的图片",
+    usage="""
+    今天吃什么
+    今天喝什么
+    添加菜单
+    删除菜单
+    查看菜单
+    """,
+    homepage="https://github.com/Cvandia/nonebot-plugin-whateat-pic",
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
+)
