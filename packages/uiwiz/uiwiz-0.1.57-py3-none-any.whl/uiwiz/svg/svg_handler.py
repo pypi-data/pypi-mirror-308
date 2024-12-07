7f6a576a1ae8dd76035c31bedc63410193d7ecd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:13 +0000

    Add package uiwiz/0.1.57/uiwiz-0.1.57-py3-none-any.whl

---
+++ packages/uiwiz/uiwiz-0.1.57-py3-none-any.whl/uiwiz/svg/svg_handler.py
@@ -0,0 +1,18 @@
+from typing import Literal
+
+from anyio import Path
+
+_type = Literal[
+    "info",
+    "error",
+    "success",
+    "warning",
+    "menu",
+    None,
+]
+
+
+def get_svg(svg: Literal["info", "error", "success", "warning", "menu", "uiwiz"]) -> str:
+    path = Path(__file__).parent / (svg + ".svg")
+    with open(path, "r") as f:
+        return f.read()
