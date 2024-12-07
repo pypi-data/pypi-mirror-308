8c5959e788238927831f6ec6453ef4925e39166a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:06 +0000

    Add package botrun-flow-lang/4.11.151/botrun_flow_lang-4.11.151-py3-none-any.whl

---
+++ packages/botrun-flow-lang/botrun_flow_lang-4.11.151-py3-none-any.whl/botrun_flow_lang/services/hatch/hatch.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+
+
+class Hatch(BaseModel):
+    user_id: str
+    id: str
+    prompt_template: str
+    name: str = ""  # 将 name 设为可选字段，默认为空字符串
+    is_default: bool = False
