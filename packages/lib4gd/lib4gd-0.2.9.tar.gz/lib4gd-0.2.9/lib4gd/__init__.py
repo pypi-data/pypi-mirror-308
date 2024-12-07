36996e4e070ca9c3a17291c2027bbc60617df836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:52 +0000

    Add package lib4gd/0.2.9/lib4gd-0.2.9.tar.gz

---
+++ packages/lib4gd/lib4gd-0.2.9.tar.gz/lib4gd-0.2.9/lib4gd/__init__.py
@@ -0,0 +1,5 @@
+# lib4gd/__init__.py
+
+from .image_handling.handle_base64 import ImageHandler
+
+__all__ = ["ImageHandler"]
---
+++ packages/lib4gd/lib4gd-0.2.9.tar.gz/lib4gd-0.2.9/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+setup(
+    name='lib4gd',
+    version='0.2.9',  # Incremented version
+    description='A comprehensive library for image handling, MongoDB, and Google Drive file management.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Tejas R Simha',
+    author_email='tejas@4gd.ai',
+    url='https://github.com/4dg-ai/lib4gd',
+    license='MIT',
+    packages=[
+        "lib4gd",
+        "lib4gd.file_transfer",
+        "lib4gd.image_handling",
+        "lib4gd.mongo_handler"
+    ],
+    package_dir={"": "."},  # This points to the current directory as the root
+    install_requires=[
+        'aiofiles>=0.8.0',
+        'google-auth>=2.0.0',
+        'google-auth-oauthlib>=0.4.0',
+        'google-auth-httplib2>=0.1.0',
+        'google-api-python-client>=2.0.0',
+        'numpy>=1.19.0',
+        'opencv-python>=4.5.0',
+        'Pillow>=8.0.0',
+        'asyncssh>=2.0.0',  # For SFTP downloading
+        'motor>=2.0.0',  # MongoDB async driver
+        'psutil>=5.9.0',  # For memory and CPU monitoring
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
