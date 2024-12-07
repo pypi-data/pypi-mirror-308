4ac22cef64c739b27b388fcd7fa01ae5b0350c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:42 +0000

    Add package ismail-rocket-aissaoui-group-3-aisd/1.0.0/ismail_rocket_aissaoui_group_3_aisd-1.0.0.tar.gz

---
+++ packages/ismail-rocket-aissaoui-group-3-aisd/ismail_rocket_aissaoui_group_3_aisd-1.0.0.tar.gz/ismail_rocket_aissaoui_group_3_aisd-1.0.0/README.md
@@ -0,0 +1,26 @@
+# rocket
+
+`rocket` is a Python library for simulating and managing rocket launches. It provides a simple interface to control various aspects of a rocket's lifecycle, from launch to abort.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install `rocket`.
+
+```bash
+pip install rocket
+```
+
+## Usage
+
+```python
+import rocket
+
+# Example usage
+rocket.launch()
+rocket.status()
+rocket.abort()
+```
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/ismail-rocket-aissaoui-group-3-aisd/ismail_rocket_aissaoui_group_3_aisd-1.0.0.tar.gz/ismail_rocket_aissaoui_group_3_aisd-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='ismail-rocket-aissaoui-group-3-aisd',
+   version='1.0.0',
+   author='ismail',
+   author_email='i.aissaoui@esi-sba.dz',
+   packages=find_packages(),
+   url='http://pypi.python.org/pypi/irocket/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=["ismail-rocket-aissaoui-group-3-aisd"]
+)
