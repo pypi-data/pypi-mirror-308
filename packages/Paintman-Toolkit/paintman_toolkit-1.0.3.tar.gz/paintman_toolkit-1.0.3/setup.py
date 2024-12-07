22d40f16d09f3c489e7cb34eed011cd5b4f23352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:35 +0000

    Add package Paintman-Toolkit/1.0.3/paintman_toolkit-1.0.3.tar.gz

---
+++ packages/Paintman-Toolkit/paintman_toolkit-1.0.3.tar.gz/paintman_toolkit-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Paintman-Toolkit",
+    version="1.0.3",
+    author="Ma Chenxing",
+    author_email="tammcx@gmail.com",
+    description="A toolkit for reading and generating Paintman related files for color management.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChenxingM/PaintmanToolkit",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # 支持的 Python 版本
+)
