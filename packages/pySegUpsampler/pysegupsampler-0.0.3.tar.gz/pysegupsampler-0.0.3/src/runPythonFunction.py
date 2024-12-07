67a5b4dba225dbc44224e464314448b7be57d829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:18 +0000

    Add package pySegUpsampler/0.0.3/pysegupsampler-0.0.3.tar.gz

---
+++ packages/pySegUpsampler/pysegupsampler-0.0.3.tar.gz/pysegupsampler-0.0.3/src/runPythonFunction.py
@@ -0,0 +1,6 @@
+#import SegmentationUpsampler.UpsampleMultiLabels as um
+
+newMatrix = a
+#newMatrix = um.upsample(multiLabelMatrix, sigma, targetVolume, scale, 
+#                        spacing, iso, fillGaps, NB)
+#np.save('multilabelTestShape.npy', multiLabelMatrix)
