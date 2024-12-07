699232dfe2a7525fc03edc856c6cbee16316a6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:08 +0000

    Add package spmlab/1.1.21/spmlab-1.1.21-py3-none-any.whl

---
+++ packages/spmlab/spmlab-1.1.21-py3-none-any.whl/spmlab/formats/sm4/read.py
@@ -0,0 +1,21 @@
+def read(src_path: str):
+    ## External packages
+    from ...readers.spym import io
+
+    ## Internal packages
+    from .image import ImageData
+    from .sts import STSData
+    from .iz import IZData
+
+    sm4 = io.load(src_path)
+    if sm4 is None:
+        raise FileExistsError("There was a problem loading the file. Ensure that the full path to the file is correct.")
+    
+    if 'Current' in sm4.data_vars:
+        match sm4.Current.RHK_LineTypeName:
+            case 'RHK_LINE_IV_SPECTRUM':
+                return STSData(src_path, sm4)
+            case 'RHK_LINE_IZ_SPECTRUM':
+                return IZData(src_path, sm4)
+    elif 'Topography_Forward' in sm4.data_vars:
+        return ImageData(src_path, sm4)
