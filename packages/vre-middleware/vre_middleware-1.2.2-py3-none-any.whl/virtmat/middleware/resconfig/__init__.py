d0c3e4401ed4adea05a1a01b2492fdfa8c70076d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:39 +0000

    Add package vre-middleware/1.2.2/vre_middleware-1.2.2-py3-none-any.whl

---
+++ packages/vre-middleware/vre_middleware-1.2.2-py3-none-any.whl/virtmat/middleware/resconfig/__init__.py
@@ -0,0 +1,6 @@
+from .resconfig import ResourceConfig, QueueConfig, WorkerConfig, ResConfig
+from .resconfig import ModuleConfig, get_resconfig_loc, get_default_resconfig
+from .resconfig import set_defaults_from_guess, configure
+from .qadapter import get_default_qadapter, get_custom_qadapter
+from .qadapter import get_qadapter, get_worker_get_queue
+from .cli import setup_resconfig
