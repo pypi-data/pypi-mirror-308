7f52c96a576d909c5b8f276791776223fec5347a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:38 +0000

    Add package testproject1999/0.0.1/testproject1999-0.0.1.tar.gz

---
+++ packages/testproject1999/testproject1999-0.0.1.tar.gz/testproject1999-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(
+    name="testproject1999",
+    description="testproject1999",
+    version="0.0.1",
+    author="shobrook",
+    author_email="shobrookj@gmail.com"
+)
