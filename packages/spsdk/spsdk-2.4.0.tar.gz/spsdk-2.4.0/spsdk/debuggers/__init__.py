780c3c46adf67d6402fceee46a754e5910de53e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:26 +0000

    Add package spsdk/2.4.0/spsdk-2.4.0.tar.gz

---
+++ packages/spsdk/spsdk-2.4.0.tar.gz/spsdk-2.4.0/spsdk/debuggers/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2020-2024 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Wrappers for various types of debuggers."""
+
+from spsdk.debuggers.debug_probe_pyocd import DebugProbePyOCD
+
+__all__ = ["DebugProbePyOCD"]
