73cdaa6a6e12b677103a81f656848d6412c8a127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:06 +0000

    Add package liveness-detect/0.1.1/liveness_detect-0.1.1.tar.gz

---
+++ packages/liveness-detect/liveness_detect-0.1.1.tar.gz/liveness_detect-0.1.1/README.md
@@ -0,0 +1,8 @@
+# Liveness Detection SDK
+
+This is a Python SDK for performing liveness detection using a pre-trained deep learning model. The SDK uses OpenCV for face detection and TensorFlow for liveness prediction.
+
+## Installation
+
+You can install this SDK using pip:
+
---
+++ packages/liveness-detect/liveness_detect-0.1.1.tar.gz/liveness_detect-0.1.1/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="liveness_detect",  # Name of your SDK
+    version="0.1.1",  # Version number
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=[  # List of dependencies for your SDK
+        "tensorflow",
+        "opencv-python-headless",
+        "flask",
+        "flask-cors",
+        "imutils",
+        "numpy",
+    ],
+    author="rapiddata",
+    author_email="datascience.rapiddata@gmail.com",
+    description="A Python SDK for liveness detection using a pre-trained model.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gayathri-kannanv/liveness-detection-sdk",  # URL to your SDK's repo (if applicable)
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
