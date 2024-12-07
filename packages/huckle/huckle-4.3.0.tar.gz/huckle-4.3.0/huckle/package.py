4a85aabfc5c69f15cf8440388557d3970a0defdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:35 +0000

    Add package huckle/4.3.0/huckle-4.3.0.tar.gz

---
+++ packages/huckle/huckle-4.3.0.tar.gz/huckle-4.3.0/huckle/package.py
@@ -0,0 +1,5 @@
+__version__ = "4.3.0"
+dependencies = ["restnavigator==1.0.1",
+                "requests[socks]==2.28.1",
+                "configparser==5.3.0",
+                "certifi==2022.9.24"]
