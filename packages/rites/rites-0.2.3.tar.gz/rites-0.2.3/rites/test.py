ebca7ab6a7330e7ec2e1c9f1ec1b09d4ceb64e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:16 +0000

    Add package rites/0.2.3/rites-0.2.3.tar.gz

---
+++ packages/rites/rites-0.2.3.tar.gz/rites-0.2.3/rites/test.py
@@ -0,0 +1,4 @@
+from rituals import Math as m
+
+a = m.Matrix([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+print(a.inferior_triangular_matrix(offset=0))
---
+++ packages/rites/rites-0.2.3.tar.gz/rites-0.2.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rites",
+    version="0.2.3",
+    description="Reclipse's Initial Try at Enhanced Simplicity or R.I.T.E.S. A simple and lightweight QoL module.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Reclipse",
+    maintainer="Reclipse",
+    url="https://github.com/ReclipseTheOne/rites",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'colored>=2.2.4'
+    ]
+)
