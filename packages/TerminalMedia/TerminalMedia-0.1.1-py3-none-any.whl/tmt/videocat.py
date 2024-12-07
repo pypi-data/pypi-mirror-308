fbe7f8dace320caa5a263ec91644d85a287bc65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:25 +0000

    Add package TerminalMedia/0.1.1/TerminalMedia-0.1.1-py3-none-any.whl

---
+++ packages/TerminalMedia/TerminalMedia-0.1.1-py3-none-any.whl/tmt/videocat.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from .utils import videoCat
+
+
+    
+        
+def show_help():
+    print("Display a video on the terminal")
+    print("Usage:")
+    print("  vcat <videp_path>")
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        show_help()
+    else:
+        if not os.path.isfile(sys.argv[1]):
+            print("File does not exist")
+            return
+        video_path = sys.argv[1]
+        videoCat(video_path)
+        
+if __name__ == "__main__":
+    main()
