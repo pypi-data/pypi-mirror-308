03bc2ab0aaead754321d8cbb49edb4adca5f31a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:25 +0000

    Add package coordinax/0.13.4/coordinax-0.13.4.tar.gz

---
+++ packages/coordinax/coordinax-0.13.4.tar.gz/coordinax-0.13.4/src/coordinax/_src/base/compat.py
@@ -0,0 +1,186 @@
+"""Intra-ecosystem Compatibility."""
+
+__all__: list[str] = []
+
+
+from jaxtyping import Shaped
+from plum import conversion_method, convert
+
+import quaxed.numpy as xp
+from dataclassish import field_values
+from unxt.quantity import AbstractQuantity, Quantity, UncheckedQuantity
+
+from .base_pos import AbstractPos
+from coordinax._src.distance import Distance
+from coordinax._src.utils import full_shaped
+
+#####################################################################
+# Convert to Quantity
+
+
+@conversion_method(type_from=AbstractPos, type_to=AbstractQuantity)  # type: ignore[misc]
+def convert_pos_to_absquantity(obj: AbstractPos, /) -> AbstractQuantity:
+    """`coordinax.AbstractPos` -> `unxt.AbstractQuantity`.
+
+    Examples
+    --------
+    >>> import coordinax as cx
+    >>> from unxt.quantity import AbstractQuantity, Quantity
+
+    >>> pos = cx.CartesianPos1D.from_([1.0], "km")
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1.], dtype=float32), unit='km')
+
+    >>> pos = cx.RadialPos.from_([1.0], "km")
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1.], dtype=float32), unit='km')
+
+    >>> pos = cx.CartesianPos2D.from_([1.0, 2.0], "km")
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1., 2.], dtype=float32), unit='km')
+
+    >>> pos = cx.PolarPos(Quantity(1.0, "km"), Quantity(0, "deg"))
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1., 0.], dtype=float32), unit='km')
+
+    >>> pos = cx.CartesianPos3D.from_([1.0, 2.0, 3.0], "km")
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1., 2., 3.], dtype=float32), unit='km')
+
+    >>> pos = cx.SphericalPos(Quantity(1.0, "km"), Quantity(0, "deg"), Quantity(0, "deg"))
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([0., 0., 1.], dtype=float32), unit='km')
+
+    >>> pos = cx.CylindricalPos(Quantity(1.0, "km"), Quantity(0, "deg"), Quantity(0, "km"))
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1., 0., 0.], dtype=float32), unit='km')
+
+    """  # noqa: E501
+    cart = full_shaped(obj.represent_as(obj._cartesian_cls))  # noqa: SLF001
+    return xp.stack(tuple(field_values(cart)), axis=-1)
+
+
+@conversion_method(type_from=AbstractPos, type_to=Quantity)  # type: ignore[misc]
+def convert_pos_to_q(obj: AbstractPos, /) -> Quantity["length"]:
+    """`coordinax.AbstractPos` -> `unxt.Quantity`.
+
+    Examples
+    --------
+    >>> import coordinax as cx
+    >>> from unxt.quantity import AbstractQuantity, Quantity
+
+    >>> pos = cx.CartesianPos1D.from_([1.0], "km")
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1.], dtype=float32), unit='km')
+
+    >>> pos = cx.RadialPos.from_([1.0], "km")
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1.], dtype=float32), unit='km')
+
+    >>> pos = cx.CartesianPos2D.from_([1.0, 2.0], "km")
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1., 2.], dtype=float32), unit='km')
+
+    >>> pos = cx.PolarPos(Quantity(1.0, "km"), Quantity(0, "deg"))
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1., 0.], dtype=float32), unit='km')
+
+    >>> pos = cx.CartesianPos3D.from_([1.0, 2.0, 3.0], "km")
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1., 2., 3.], dtype=float32), unit='km')
+
+    >>> pos = cx.SphericalPos(Quantity(1.0, "km"), Quantity(0, "deg"), Quantity(0, "deg"))
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([0., 0., 1.], dtype=float32), unit='km')
+
+    >>> pos = cx.CylindricalPos(Quantity(1.0, "km"), Quantity(0, "deg"), Quantity(0, "km"))
+    >>> convert(pos, AbstractQuantity)
+    Quantity['length'](Array([1., 0., 0.], dtype=float32), unit='km')
+
+    """  # noqa: E501
+    return convert(convert(obj, AbstractQuantity), Quantity)
+
+
+@conversion_method(type_from=AbstractPos, type_to=UncheckedQuantity)  # type: ignore[misc]
+def convert_pos_to_uncheckedq(
+    obj: AbstractPos, /
+) -> Shaped[UncheckedQuantity, "*batch dims"]:
+    """`coordinax.AbstractPos` -> `unxt.UncheckedQuantity`.
+
+    Examples
+    --------
+    >>> import coordinax as cx
+    >>> from unxt.quantity import AbstractQuantity, Quantity, UncheckedQuantity
+
+    >>> pos = cx.CartesianPos1D.from_([1.0], "km")
+    >>> convert(pos, UncheckedQuantity)
+    UncheckedQuantity(Array([1.], dtype=float32), unit='km')
+
+    >>> pos = cx.RadialPos.from_([1.0], "km")
+    >>> convert(pos, UncheckedQuantity)
+    UncheckedQuantity(Array([1.], dtype=float32), unit='km')
+
+    >>> pos = cx.CartesianPos2D.from_([1.0, 2.0], "km")
+    >>> convert(pos, UncheckedQuantity)
+    UncheckedQuantity(Array([1., 2.], dtype=float32), unit='km')
+
+    >>> pos = cx.PolarPos(Quantity(1.0, "km"), Quantity(0, "deg"))
+    >>> convert(pos, UncheckedQuantity)
+    UncheckedQuantity(Array([1., 0.], dtype=float32), unit='km')
+
+    >>> pos = cx.CartesianPos3D.from_([1.0, 2.0, 3.0], "km")
+    >>> convert(pos, UncheckedQuantity)
+    UncheckedQuantity(Array([1., 2., 3.], dtype=float32), unit='km')
+
+    >>> pos = cx.SphericalPos(Quantity(1.0, "km"), Quantity(0, "deg"), Quantity(0, "deg"))
+    >>> convert(pos, UncheckedQuantity)
+    UncheckedQuantity(Array([0., 0., 1.], dtype=float32), unit='km')
+
+    >>> pos = cx.CylindricalPos(Quantity(1.0, "km"), Quantity(0, "deg"), Quantity(0, "km"))
+    >>> convert(pos, UncheckedQuantity)
+    UncheckedQuantity(Array([1., 0., 0.], dtype=float32), unit='km')
+
+    """  # noqa: E501
+    return convert(convert(obj, AbstractQuantity), UncheckedQuantity)
+
+
+@conversion_method(type_from=AbstractPos, type_to=Distance)  # type: ignore[misc]
+def convert_pos_to_distance(obj: AbstractPos, /) -> Shaped[Distance, "*batch dims"]:
+    """`coordinax.AbstractPos` -> `coordinax.Distance`.
+
+    Examples
+    --------
+    >>> import coordinax as cx
+    >>> from unxt.quantity import AbstractQuantity, Quantity
+    >>> from coordinax.distance import Distance
+
+    >>> pos = cx.CartesianPos1D.from_([1.0], "km")
+    >>> convert(pos, Distance)
+    Distance(Array([1.], dtype=float32), unit='km')
+
+    >>> pos = cx.RadialPos.from_([1.0], "km")
+    >>> convert(pos, Distance)
+    Distance(Array([1.], dtype=float32), unit='km')
+
+    >>> pos = cx.CartesianPos2D.from_([1.0, 2.0], "km")
+    >>> convert(pos, Distance)
+    Distance(Array([1., 2.], dtype=float32), unit='km')
+
+    >>> pos = cx.PolarPos(Quantity(1.0, "km"), Quantity(0, "deg"))
+    >>> convert(pos, Distance)
+    Distance(Array([1., 0.], dtype=float32), unit='km')
+
+    >>> pos = cx.CartesianPos3D.from_([1.0, 2.0, 3.0], "km")
+    >>> convert(pos, Distance)
+    Distance(Array([1., 2., 3.], dtype=float32), unit='km')
+
+    >>> pos = cx.SphericalPos(Quantity(1.0, "km"), Quantity(0, "deg"), Quantity(0, "deg"))
+    >>> convert(pos, Distance)
+    Distance(Array([0., 0., 1.], dtype=float32), unit='km')
+
+    >>> pos = cx.CylindricalPos(Quantity(1.0, "km"), Quantity(0, "deg"), Quantity(0, "km"))
+    >>> convert(pos, Distance)
+    Distance(Array([1., 0., 0.], dtype=float32), unit='km')
+
+    """  # noqa: E501
+    return convert(convert(obj, AbstractQuantity), Distance)
---
+++ packages/coordinax/coordinax-0.13.4.tar.gz/coordinax-0.13.4/src/coordinax/_src/compat.py
@@ -0,0 +1,230 @@
+"""Intra-ecosystem Compatibility."""
+
+__all__: list[str] = []
+
+
+from typing import TypeAlias
+
+from jaxtyping import Shaped
+from plum import conversion_method, convert
+
+import quaxed.numpy as xp
+from dataclassish import field_values
+from unxt.quantity import AbstractQuantity, Quantity, UncheckedQuantity
+
+from coordinax._src.base.base import AbstractVector
+from coordinax._src.d1.cartesian import CartesianAcc1D, CartesianPos1D, CartesianVel1D
+from coordinax._src.d1.radial import RadialAcc, RadialVel
+from coordinax._src.d2.cartesian import CartesianAcc2D, CartesianVel2D
+from coordinax._src.d3.cartesian import CartesianAcc3D, CartesianVel3D
+from coordinax._src.operators.base import AbstractOperator, op_call_dispatch
+from coordinax._src.typing import TimeBatchOrScalar
+from coordinax._src.utils import full_shaped
+
+#####################################################################
+# Convert to Quantity
+
+
+def _vec_diff_to_q(obj: AbstractVector, /) -> AbstractQuantity:
+    """`coordinax.AbstractVector` -> `unxt.AbstractQuantity`."""
+    return xp.stack(tuple(field_values(full_shaped(obj))), axis=-1)
+
+
+# -------------------------------------------------------------------
+# 1D
+
+QConvertible1D: TypeAlias = CartesianVel1D | CartesianAcc1D | RadialVel | RadialAcc
+
+
+@conversion_method(type_from=RadialAcc, type_to=UncheckedQuantity)
+@conversion_method(type_from=RadialVel, type_to=UncheckedQuantity)
+@conversion_method(type_from=CartesianAcc1D, type_to=UncheckedQuantity)
+@conversion_method(type_from=CartesianVel1D, type_to=UncheckedQuantity)
+def vec_diff1d_to_uncheckedq(
+    obj: QConvertible1D, /
+) -> Shaped[UncheckedQuantity, "*batch 1"]:
+    """1D Differentials -> `unxt.UncheckedQuantity`.
+
+    Examples
+    --------
+    >>> from plum import convert
+    >>> from unxt.quantity import UncheckedQuantity
+    >>> import coordinax as cx
+
+    >>> cart_vel = cx.CartesianVel1D.from_([1], "km/s")
+    >>> convert(cart_vel, UncheckedQuantity)
+    UncheckedQuantity(Array([1], dtype=int32), unit='km / s')
+
+    >>> cart_acc = cx.CartesianAcc1D.from_([1], "km/s2")
+    >>> convert(cart_acc, UncheckedQuantity)
+    UncheckedQuantity(Array([1], dtype=int32), unit='km / s2')
+
+    >>> rad_vel = cx.RadialVel.from_([1], "km/s")
+    >>> convert(rad_vel, UncheckedQuantity)
+    UncheckedQuantity(Array([1], dtype=int32), unit='km / s')
+
+    >>> rad_acc = cx.RadialAcc.from_([1], "km/s2")
+    >>> convert(rad_acc, UncheckedQuantity)
+    UncheckedQuantity(Array([1], dtype=int32), unit='km / s2')
+
+    """
+    return convert(_vec_diff_to_q(obj), UncheckedQuantity)
+
+
+@conversion_method(type_from=RadialAcc, type_to=Quantity)
+@conversion_method(type_from=RadialVel, type_to=Quantity)
+@conversion_method(type_from=CartesianAcc1D, type_to=Quantity)
+@conversion_method(type_from=CartesianVel1D, type_to=Quantity)
+def vec_diff1d_to_uncheckedq(obj: QConvertible1D, /) -> Shaped[Quantity, "*batch 1"]:
+    """1D Differentials -> `unxt.Quantity`.
+
+    Examples
+    --------
+    >>> from plum import convert
+    >>> from unxt import Quantity
+    >>> import coordinax as cx
+
+    >>> cart_vel = cx.CartesianVel1D.from_([1], "km/s")
+    >>> convert(cart_vel, Quantity)
+    Quantity['speed'](Array([1], dtype=int32), unit='km / s')
+
+    >>> cart_acc = cx.CartesianAcc1D.from_([1], "km/s2")
+    >>> convert(cart_acc, Quantity)
+    Quantity['acceleration'](Array([1], dtype=int32), unit='km / s2')
+
+    >>> rad_vel = cx.RadialVel.from_([1], "km/s")
+    >>> convert(rad_vel, Quantity)
+    Quantity['speed'](Array([1], dtype=int32), unit='km / s')
+
+    >>> rad_acc = cx.RadialAcc.from_([1], "km/s2")
+    >>> convert(rad_acc, Quantity)
+    Quantity['acceleration'](Array([1], dtype=int32), unit='km / s2')
+
+    """
+    return convert(_vec_diff_to_q(obj), Quantity)
+
+
+# -------------------------------------------------------------------
+# 2D
+
+QConvertible2D: TypeAlias = CartesianVel2D | CartesianAcc2D
+
+
+@conversion_method(type_from=CartesianAcc2D, type_to=UncheckedQuantity)
+@conversion_method(type_from=CartesianVel2D, type_to=UncheckedQuantity)
+def vec_diff_to_q(obj: QConvertible2D, /) -> Shaped[UncheckedQuantity, "*batch 2"]:
+    """2D Differentials -> `unxt.UncheckedQuantity`.
+
+    Examples
+    --------
+    >>> from plum import convert
+    >>> from unxt.quantity import UncheckedQuantity
+    >>> import coordinax as cx
+
+    >>> vel = cx.CartesianVel2D.from_([1, 2], "km/s")
+    >>> convert(vel, UncheckedQuantity)
+    UncheckedQuantity(Array([1., 2.], dtype=float32), unit='km / s')
+
+    >>> acc = cx.CartesianAcc2D.from_([1, 2], "km/s2")
+    >>> convert(acc, UncheckedQuantity)
+    UncheckedQuantity(Array([1., 2.], dtype=float32), unit='km / s2')
+
+    """
+    return convert(_vec_diff_to_q(obj), UncheckedQuantity)
+
+
+@conversion_method(type_from=CartesianAcc2D, type_to=Quantity)
+@conversion_method(type_from=CartesianVel2D, type_to=Quantity)
+def vec_diff_to_q(obj: QConvertible2D, /) -> Shaped[Quantity, "*batch 2"]:
+    """2D Differentials -> `unxt.Quantity`.
+
+    Examples
+    --------
+    >>> from plum import convert
+    >>> from unxt import Quantity
+    >>> import coordinax as cx
+
+    >>> vel = cx.CartesianVel2D.from_([1, 2], "km/s")
+    >>> convert(vel, Quantity)
+    Quantity['speed'](Array([1., 2.], dtype=float32), unit='km / s')
+
+    >>> acc = cx.CartesianAcc2D.from_([1, 2], "km/s2")
+    >>> convert(acc, Quantity)
+    Quantity['acceleration'](Array([1., 2.], dtype=float32), unit='km / s2')
+
+    """
+    return convert(_vec_diff_to_q(obj), Quantity)
+
+
+# -------------------------------------------------------------------
+# 3D
+
+QConvertible3D: TypeAlias = CartesianVel3D | CartesianAcc3D
+
+
+@conversion_method(CartesianAcc3D, UncheckedQuantity)
+@conversion_method(CartesianVel3D, UncheckedQuantity)
+def vec_diff_to_q(obj: QConvertible3D, /) -> Shaped[UncheckedQuantity, "*batch 3"]:
+    """3D Differentials -> `unxt.UncheckedQuantity`.
+
+    Examples
+    --------
+    >>> import coordinax as cx
+    >>> from plum import convert
+    >>> from unxt.quantity import UncheckedQuantity
+
+    >>> vel = cx.CartesianVel3D.from_([1, 2, 3], "km/s")
+    >>> convert(vel, UncheckedQuantity)
+    UncheckedQuantity(Array([1., 2., 3.], dtype=float32), unit='km / s')
+
+    >>> acc = cx.CartesianAcc3D.from_([1, 2, 3], "km/s2")
+    >>> convert(acc, UncheckedQuantity)
+    UncheckedQuantity(Array([1., 2., 3.], dtype=float32), unit='km / s2')
+
+    """
+    return convert(_vec_diff_to_q(obj), UncheckedQuantity)
+
+
+@conversion_method(CartesianAcc3D, Quantity)
+@conversion_method(CartesianVel3D, Quantity)
+def vec_diff_to_q(obj: QConvertible3D, /) -> Shaped[Quantity, "*batch 3"]:
+    """3D Differentials -> `unxt.Quantity`.
+
+    Examples
+    --------
+    >>> import coordinax as cx
+    >>> from plum import convert
+    >>> from unxt import Quantity
+
+    >>> vel = cx.CartesianVel3D.from_([1, 2, 3], "km/s")
+    >>> convert(vel, Quantity)
+    Quantity['speed'](Array([1., 2., 3.], dtype=float32), unit='km / s')
+
+    >>> acc = cx.CartesianAcc3D.from_([1, 2, 3], "km/s2")
+    >>> convert(acc, Quantity)
+    Quantity['acceleration'](Array([1., 2., 3.], dtype=float32), unit='km / s2')
+
+    """
+    return convert(_vec_diff_to_q(obj), Quantity)
+
+
+#####################################################################
+# Operators
+
+
+Q1: TypeAlias = Shaped[Quantity["length"], "*#batch 1"]
+
+
+@op_call_dispatch
+def call(self: AbstractOperator, x: Q1, /) -> Q1:
+    """Dispatch to the operator's `__call__` method."""
+    return convert(self(CartesianPos1D.from_(x)), Quantity)
+
+
+@op_call_dispatch
+def call(
+    self: AbstractOperator, x: Q1, t: TimeBatchOrScalar, /
+) -> tuple[Q1, TimeBatchOrScalar]:
+    """Dispatch to the operator's `__call__` method."""
+    vec, t = self(CartesianPos1D.from_(x), t)
+    return convert(vec, Quantity), t
