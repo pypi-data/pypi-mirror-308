bee57c6dbe3d0c441e6630eda0e1f1b5d3435148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:42 +0000

    Add package freeneiroapi/0.3/freeneiroapi-0.3-py3-none-any.whl

---
+++ packages/freeneiroapi/freeneiroapi-0.3-py3-none-any.whl/freeneiroapi/api.py
@@ -0,0 +1,24 @@
+import requests
+
+class FreeNeiroAPI:
+    def __init__(self, base_url="https://duck.gpt-api.workers.dev/chat/"):
+        self.base_url = base_url
+
+    def get_response(self, prompt, preprompt=""):
+        try:
+            fullprompt = preprompt + prompt
+            params = {'prompt': fullprompt}
+            response = requests.get(self.base_url, params=params)
+            response.raise_for_status()
+
+            json_data = response.json()
+            if json_data.get('action') == 'success':
+                response_text = json_data.get('response', 'Нет ответа от DuckGPT')
+                return response_text
+            else:
+                return f"API ERROR: {json_data.get('action')}"
+
+        except requests.exceptions.RequestException as e:
+            return f"Request error: {e}"
+        except ValueError as e:
+            return f"JSON decode error: {e}"
