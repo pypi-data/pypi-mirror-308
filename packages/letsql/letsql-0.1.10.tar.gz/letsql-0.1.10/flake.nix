46733dd59f4299f64ba811ca92c7169a564c1357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:23 +0000

    Add package letsql/0.1.10/letsql-0.1.10.tar.gz

---
+++ packages/letsql/letsql-0.1.10.tar.gz/letsql-0.1.10/flake.nix
@@ -0,0 +1,132 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, rust-overlay, crane }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+            poetry2nix.overlays.default
+          ];
+        };
+        mkLETSQL = (import ./nix/letsql.nix { inherit system pkgs poetry2nix crane; }) ./.;
+        mkCommands = python: import ./nix/commands.nix {
+          inherit pkgs python;
+        };
+        mkShellHook = python: let
+          commands = mkCommands python;
+        in ''
+          export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+          ${commands.letsql-commands.letsql-ensure-download-data}/bin/letsql-ensure-download-data
+        '';
+        mkToolsPackages = python: let
+          commands = mkCommands python;
+          letsql = mkLETSQL python;
+        in pkgs.buildEnv {
+          name = "tools-${python.pythonVersion}";
+          paths = [
+            letsql.toolchain
+            pkgs.poetry
+            python
+            commands.letsql-commands-star
+          ];
+        };
+        mkToolsShell = python: let
+          toolsPackages = mkToolsPackages python;
+          shellHook = mkShellHook python;
+        in pkgs.mkShell {
+          packages = [
+            toolsPackages
+            pkgs.maturin
+          ];
+          inherit shellHook;
+        };
+        mkDevShell = python: let
+          toolsPackages = mkToolsPackages python;
+          letsql = mkLETSQL python;
+          shellHook = mkShellHook python;
+        in pkgs.mkShell {
+          packages = [
+            letsql.appFromWheel
+            toolsPackages
+          ];
+          inherit shellHook;
+        };
+        letsql310 = mkLETSQL pkgs.python310;
+        letsql311 = mkLETSQL pkgs.python311;
+        letsql312 = mkLETSQL pkgs.python312;
+        toolsPackages310 = mkToolsPackages pkgs.python310;
+        toolsPackages311 = mkToolsPackages pkgs.python311;
+        toolsPackages312 = mkToolsPackages pkgs.python312;
+        tools310 = mkToolsShell pkgs.python310;
+        tools311 = mkToolsShell pkgs.python311;
+        tools312 = mkToolsShell pkgs.python312;
+        dev310 = mkDevShell pkgs.python310;
+        dev311 = mkDevShell pkgs.python311;
+        dev312 = mkDevShell pkgs.python312;
+        #
+        mkShellApp = drv: name: bin-name: pkgs.writeShellApplication {
+          inherit name;
+          runtimeInputs = [ ];
+          text = pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
+            export PINS_DATA_DIR="$HOME/Library/Application Support/pins-py"
+            export PINS_CACHE_DIR="$HOME/Library/Caches/pins-py"
+          '' + ''
+            export IPYTHONDIR="$HOME/.ipython"
+            bash <(
+              cat <(grep ^declare ${drv} | grep -v "SSL_CERT_FILE=") <(echo ${bin-name} "''${@}")
+            )
+          '';
+        };
+        letsql-ipython-310 = mkShellApp dev310 "letsql-ipython-310" "ipython";
+        letsql-ipython-311 = mkShellApp dev311 "letsql-ipython-311" "ipython";
+        letsql-ipython-312 = mkShellApp dev312 "letsql-ipython-312" "ipython";
+      in
+      {
+        packages = {
+          app310 = letsql310.app;
+          appFromWheel310 = letsql310.appFromWheel;
+          app311 = letsql311.app;
+          appFromWheel311 = letsql311.appFromWheel;
+          app312 = letsql312.app;
+          appFromWheel312 = letsql312.appFromWheel;
+          inherit toolsPackages310 toolsPackages311 toolsPackages312;
+          inherit letsql-ipython-310 letsql-ipython-311 letsql-ipython-312;
+          #
+          app = self.packages.${system}.app310;
+          appFromWheel = self.packages.${system}.appFromWheel310;
+          toolsPackages = self.packages.${system}.toolsPackages310;
+          letsql = self.packages.${system}.letsql-ipython-310;
+          #
+          default = self.packages.${system}.letsql;
+        };
+        lib = {
+          inherit (letsql310) poetryOverrides maturinOverride toolchain;
+          inherit mkLETSQL mkCommands mkShellHook mkToolsPackages mkDevShell;
+          inherit pkgs;
+        };
+        devShells = {
+          inherit tools310 tools311 tools312;
+          tools = self.devShells.${system}.tools310;
+          inherit dev310 dev311 dev312;
+          dev = self.devShells.${system}.dev310;
+          default = self.devShells.${system}.dev;
+        };
+      });
+}
---
+++ packages/letsql/letsql-0.1.10.tar.gz/letsql-0.1.10/nix/letsql.nix
@@ -0,0 +1,144 @@
+{ system, pkgs, poetry2nix, crane }:
+let
+  mkLETSQL = src: python: with pkgs.lib.path; let
+    # how do we ensure pkgs comes with rust-overlay overlay?
+    cargoToml = append src "Cargo.toml";
+    rustSrcSet = with pkgs.lib.fileset; unions [
+      cargoToml
+      (append src "Cargo.lock")
+      (append src "pyproject.toml")
+      (append src "README.md")
+      (append src "LICENSE")
+      (fileFilter (file: file.hasExt "rs") (append src "src"))
+    ];
+    pySrcSet = with pkgs.lib.fileset; unions [
+      rustSrcSet
+      (append src "poetry.lock")
+      (fileFilter (file: file.hasExt "py") (append src "python"))
+      (fileFilter (file: file.hasExt "sql") (append src "python"))
+    ];
+    rustSrc = with pkgs.lib.fileset; toSource {
+      root = src;
+      fileset = rustSrcSet;
+    };
+    pySrc = with pkgs.lib.fileset; toSource {
+      root = src;
+      fileset = unions [ rustSrcSet pySrcSet ];
+    };
+    toolchain = pkgs.rust-bin.fromRustupToolchainFile (append src "rust-toolchain.toml");
+    craneLib = (crane.mkLib pkgs).overrideToolchain (p: toolchain);
+    wheelName = let
+      inherit (craneLib.crateNameFromCargoToml { inherit cargoToml; }) pname version;
+      wheelTail = {
+        x86_64-linux = "linux_x86_64";
+        aarch64-darwin = "macosx_11_0_arm64";
+        # aarch64-linux = "";
+        # x86_64-darwin = "";
+      }.${system};
+    in "${pname}-${version}-cp38-abi3-${wheelTail}.whl";
+    buildPhaseCargoCommand = ''
+      ${pkgs.maturin}/bin/maturin build \
+        --offline \
+        --target-dir target \
+        --manylinux off \
+        --strip \
+        --release
+    '';
+    commonCraneArgs = {
+      src = rustSrc;
+      strictDeps = true;
+      nativeBuildInputs = [
+        python
+        pkgs.pkg-config
+      ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        pkgs.darwin.apple_sdk.frameworks.Security
+      ];
+      buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        pkgs.libiconv
+        python
+      ];
+      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    };
+    crateWheelDeps = craneLib.buildPackage (commonCraneArgs // {
+      pname = "crateWheel-deps";
+      src = rustSrc;
+      doInstallCargoArtifacts = true;
+      inherit buildPhaseCargoCommand;
+      installPhaseCommand = "mkdir -p $out";
+    });
+    crateWheel = craneLib.buildPackage (commonCraneArgs // {
+      cargoArtifacts = crateWheelDeps;
+      src = pySrc;
+      inherit buildPhaseCargoCommand;
+      installPhaseCommand = ''
+        ls target/wheels/*
+        mkdir -p $out
+        cp target/wheels/*.whl $out/
+      '';
+    });
+    crateWheelSrc = "${crateWheel}/${wheelName}";
+
+    inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication overrides;
+    poetryOverrides = final: prev: {
+      ibis-framework = prev.ibis-framework.overridePythonAttrs (old: {
+        buildInputs = (old.buildInputs or [ ]) ++ [ prev.poetry-dynamic-versioning ];
+      });
+      xgboost = prev.xgboost.overridePythonAttrs (old: {
+      } // pkgs.lib.attrsets.optionalAttrs pkgs.stdenv.isDarwin {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.cmake ];
+        cmakeDir = "../cpp_src";
+        preBuild = ''
+          cd ..
+        '';
+      });
+      scipy = prev.scipy.overridePythonAttrs (old: {
+      } // pkgs.lib.attrsets.optionalAttrs pkgs.stdenv.isDarwin {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.xcbuild.xcrun ];
+      });
+      scikit-learn = prev.scikit-learn.overridePythonAttrs (old: {
+      } // pkgs.lib.attrsets.optionalAttrs pkgs.stdenv.isDarwin {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.meson-python ];
+      });
+    };
+    maturinOverride = old: with pkgs.rustPlatform; {
+      cargoDeps = importCargoLock {
+        lockFile = "${old.src}/Cargo.lock";
+        outputHashes = {
+          "gbdt-0.1.3" = "sha256-f2uqulFSNGwrDM7RPdGIW11VpJRYexektXjHxTJHHmA=";
+        };
+      };
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
+        cargoSetupHook
+        maturinBuildHook
+        toolchain
+      ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        pkgs.libiconv
+      ];
+      buildInputs = (old.buildInputs or []) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        pkgs.libiconv
+      ];
+    };
+    commonPoetryArgs = {
+      projectDir = src;
+      preferWheels = true;
+      inherit python;
+      groups = [ "dev" "test" "docs" ];
+    };
+    app = (mkPoetryApplication (commonPoetryArgs // {
+      buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        pkgs.libiconv
+      ];
+    })).overridePythonAttrs maturinOverride;
+    appFromWheel = (mkPoetryApplication (commonPoetryArgs // {
+      src = crateWheelSrc;
+      overrides = overrides.withDefaults poetryOverrides;
+    })).override {
+      format = "wheel";
+    };
+  in {
+    inherit toolchain;
+    inherit rustSrcSet pySrcSet rustSrc pySrc crateWheelSrc;
+    inherit poetryOverrides maturinOverride;
+    inherit app appFromWheel;
+  };
+in mkLETSQL
